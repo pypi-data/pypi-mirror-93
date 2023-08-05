ade195a4118981fb23e38c8d4efbcd80bccd58b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package saucenao-api/2.3.1/saucenao_api-2.3.1.tar.gz

---
+++ packages/saucenao-api/saucenao_api-2.3.1.tar.gz/saucenao_api-2.3.1/saucenao_api/__init__.py
@@ -0,0 +1,11 @@
+from .containers import BasicSauce, BookSauce, VideoSauce
+from .saucenao_api import SauceNao
+
+__author__ = 'nomnoms12'
+__copyright__ = 'Copyright 2020, Taiga.sh Development'
+__credits__ = ['nomnoms12', 'FujiMakoto', 'rubinduck', 'spoopcity']
+__license__ = 'GNU General Public License v3.0'
+__version__ = '2.3.1'
+__maintainer__ = 'nomnoms12'
+__email__ = 'alexander.ign0918@gmail.com'
+__status__ = 'Production'
---
+++ packages/saucenao-api/saucenao_api-2.3.1.tar.gz/saucenao_api-2.3.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+
+with open('README.md', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='saucenao_api',
+    version='2.3.1',
+    author='nomnoms12',
+    author_email='alexander.ign0918@gmail.com',
+    description='Wrapper for SauceNAO JSON API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nomnoms12/saucenao_api/',
+    packages=['saucenao_api'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>= 3.6',
+    install_requires=[
+        'requests ~= 2.23.0',
+    ],
+    extras_require={
+        'test': [
+            'responses ~= 0.10.14',
+            'pytest ~= 5.4.2',
+            'pytest-cov ~= 2.8.1',
+        ],
+    },
+    project_urls={
+       'Bug Reports': 'https://github.com/nomnoms12/saucenao_api/issues/',
+       'Source': 'https://github.com/nomnoms12/saucenao_api/',
+    },
+)
