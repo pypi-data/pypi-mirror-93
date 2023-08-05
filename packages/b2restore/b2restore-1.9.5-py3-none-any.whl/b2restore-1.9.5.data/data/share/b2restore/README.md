c7b60adfe67e181790b2813a214abc8483d39c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package b2restore/1.9.5/b2restore-1.9.5-py3-none-any.whl

---
+++ packages/b2restore/b2restore-1.9.5-py3-none-any.whl/b2restore-1.9.5.data/data/share/b2restore/README.md
@@ -0,0 +1,181 @@
+## B2RESTORE
+
+[b2restore](http://github.com/bulletmark/b2restore) is a command line
+utility which can be used with [rclone](https://rclone.org/) to
+manually restore a [Backblaze B2](https://www.backblaze.com/b2/) archive
+for any given date and time. Alternatively, you can create a git
+repository of all date and time snapshots.
+
+### INSTALLATION
+
+Arch users can install [b2restore from the
+AUR](https://aur.archlinux.org/packages/b2restore/).
+
+Requires python 3.5 or later. Note [b2restore is on
+PyPI](https://pypi.org/project/b2restore/) so you can `sudo pip3 install
+b2restore` or:
+
+```
+$ git clone http://github.com/bulletmark/b2restore
+$ cd b2restore
+$ sudo pip3 install .
+```
+
+### CREATION OF INITIAL RCLONE COPY
+
+This utility is typically used with [rclone](https://rclone.org/).
+Simply `rclone sync` or `rclone copy` the B2 bucket or sub-paths from
+the bucket which you want to restore. You **MUST** specify
+`--b2-versions` to include all file versions, e.g:
+
+```
+rclone sync --b2-versions --fast-list --transfers=4 $* B2:mybucket b2files
+```
+
+The above command will copy all files and available versions to the
+created `b2files` directory. You only need to do this once.
+
+### CREATION OF SNAPSHOT AT GIVEN TIME
+
+Given the above `rclone` initial copy, you run this utility to
+create a snapshot of the directory tree for the time you are interested
+in.
+
+E.g. to recreate the tree of latest files, in `outdir`:
+
+```
+b2restore b2files outdir
+```
+
+E.g. to recreate the tree of files at a specified time:
+
+```
+b2restore -t 2018-01-01T09:10:00 b2files outdir
+```
+
+Just keep selecting different times to incrementally recreate `outdir`
+as it existed at that time. The utility prints a line for each file
+updated, created, or deleted in `outdir` compared to the previous
+contents. The date and time of each updated/created/deleted file is also
+listed. The target files are all hard-linked from the files in the
+source directory so the `outdir` tree is created very quickly since
+files do not need to be actually copied. Thus you can conveniently
+experiment with the time string to quickly see file differences.
+
+Rather than specifying an explicit time string using `-t/--time`, you
+can instead choose to use `-f/--filetime` to specify any one specific
+file's modification time at which to recreate the target tree of files.
+
+You may wish to manually `git commit` each snapshot you create in the
+outdir tree between your manually time-selected runs. If so, you will
+need to add the `-g` switch to prevent `b2restore` from deleting your
+top level `.git/` repo on each run.
+
+Note that this utility does not recreate empty directory hierarchies.
+All empty directories in the target tree are deleted.
+
+#### B2RESTORE COMMAND LINE OPTIONS
+
+```
+usage: b2restore [-h] [-t TIME | -f FILETIME] [-s] [-g] [-p PATH]
+                 indir [outdir]
+
+Program to recreate Backblaze B2 file archive at specified date and time.
+
+positional arguments:
+  indir                 input B2 archive containing all file versions (from
+                        --b2-versions)
+  outdir                output directory to recreate for given time
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TIME, --time TIME  set time YYYY-MM-DDTHH:MM.SS, default=latest
+  -f FILETIME, --filetime FILETIME
+                        set time based on specified file
+  -s, --summary         just print a summary of files and versions
+  -g, --gitkeep         preserve any top level git dir in outdir
+  -p PATH, --path PATH  only process files under given path
+```
+
+### CREATION OF GIT REPOSITORY OF ALL SNAPSHOTS
+
+Rather than run `b2restore` for the given date + times you are
+interested in, you can instead choose to run the provided
+`b2restore-create-git` utility to automatically create a git repository
+of snapshots of files for all the dates + times inherent in the `rclone`
+initial copy.
+
+So after performing the `rclone` initial copy above, run the following
+command to create a complete git repository:
+
+```
+b2restore-create-git b2files outdir
+```
+
+Then `cd outdir` and run `git log` etc to view the history.
+
+#### B2RESTORE-CREATE-GIT COMMAND LINE OPTIONS
+
+```
+Usage: b2restore-create-git [-options] indir outdir
+Create git repository from given B2 rclone copy.
+Options:
+-t YYYY-MM-DDTHH:MM.SS (start git repo from given time)
+-e YYYY-MM-DDTHH:MM.SS (end git repo before given time)
+-p (only process files under given path)
+```
+
+### TEST RUN UTILITY
+
+A command line utility `b2restore-create-dummy-files` is included to
+facilitate testing `b2restore` on your restored file tree without
+actually downloading any files from your B2 archive(!). This utility
+parses `rclone lsl` output to recreate your B2 bucket directory and
+hierarchy of file versions. Only the file names are recreated of course,
+the file contents are set to their actual byte size but with random byte
+contents (or zero filled if you specify `-z`, or to zero length if you
+specify `-s`).
+
+This utility requires almost nothing to download from your B2 archive
+and runs extremely quickly. You can then run `b2restore` against this
+dummy archive to simulate what files are changed between versions, etc.
+It is also good to get a feel for how `b2restore` works, what it does,
+and whether it suits your needs without requiring you to first perform
+an onerous huge download of your entire B2 archive.
+
+Here is an example usage:
+
+```
+rclone lsl --b2-versions B2:mybucket | b2restore-create-dummy-files b2files
+b2restore b2files outdir
+du -shl outdir # (see how much storage tree of latest versions uses)
+b2restore -t 2018-05-10T12:00.00 b2files outdir
+du -shl outdir # (see how much storage tree of yesterdays versions uses)
+```
+
+#### B2RESTORE-CREATE-DUMMY-FILES COMMAND LINE OPTIONS
+
+```
+Usage: b2restore-create-dummy-files [-options] outdir
+Reads B2 file list (from lsl output) from standard input to create
+dummy tree of files.
+Options:
+-z (zero fill files, not with random content which is default)
+-s (set files to zero length, not their actual size)
+-p (only process files under given path)
+```
+
+### LICENSE
+
+Copyright (C) 2018 Mark Blakeney. This program is distributed under the
+terms of the GNU General Public License.
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later
+version.
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License at <http://www.gnu.org/licenses/> for more details.
+
+<!-- vim: se ai syn=markdown: -->
---
+++ packages/b2restore/b2restore-1.9.5-py3-none-any.whl/b2restore.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python3
+'Program to recreate Backblaze B2 file archive at specified date and time.'
+# Author: Mark Blakeney, May 2018.
+
+import sys, os, argparse, re, filecmp, time
+from pathlib import Path
+from bisect import bisect
+
+# Format of input and output datetimes
+TIMEFMT = '%Y-%m-%dT%H:%M.%S'
+
+indir = None
+outdir = None
+exgit = []
+args = None
+
+class FileName():
+    'Class to manage canonical file paths'
+    namemap = {}
+
+    def __init__(self, name):
+        self.namemap[name] = self
+        self.name = name
+        self.times = []
+        self.paths = []
+
+    def add(self, path):
+        'Insert the versioned file into list of sorted times'
+        ix = bisect(self.times, path.time)
+        if ix > 0:
+            opath = self.paths[ix - 1]
+            # If this file looks to be the same as another then prefer
+            # the non-versioned file
+            if opath.time == path.time:
+                if opath.version and not path.version:
+                    self.paths[ix - 1] = path
+                return
+
+        self.times.insert(ix, path.time)
+        self.paths.insert(ix, path)
+
+class FileVersion():
+    'Class to manage all versioned file paths'
+    def __init__(self, path, subpath):
+        stat = path.stat()
+        self.path = subpath
+        self.time = stat.st_mtime
+        self.size = stat.st_size
+        self.name = str(subpath)
+        self.version = None
+
+        # Find B2 version string in file name. I will admit I don't
+        # understand the logic of where they embed the version string so
+        # this is crude.
+        match = re.search(r'^(.+)-v(20\d{2}-\d{2}-\d{2}-\d{6})-000(.*)$',
+                self.name)
+        if not match:
+            return
+
+        # Ensure we did actually find a valid datetime string
+        try:
+            fver = time.strptime(match.group(2), '%Y-%m-%d-%H%M%S')
+        except ValueError:
+            return
+
+        self.name = match.group(1) + match.group(3)
+        self.version = time.mktime(fver)
+
+def parsefile(path):
+    'Parse given file'
+    subpath = path.relative_to(indir)
+    if args.path and not str(subpath).startswith(args.path):
+        return
+
+    fver = FileVersion(path, subpath)
+    fname = FileName.namemap.get(fver.name)
+    if not fname:
+        fname = FileName(fver.name)
+
+    # Add this file instance into the list of versions
+    fname.add(fver)
+
+def parsedir(dirpath, func):
+    'Parse given dir and apply func() to files found'
+    for f in dirpath.iterdir():
+        if f.is_dir():
+            parsedir(f, func)
+        else:
+            func(f)
+
+# Keep valid file list
+validfiles = set()
+
+def fmttime(mtime):
+    'Return string rep of given mtime'
+    return time.strftime(TIMEFMT, time.localtime(mtime))
+
+def addfile(fp, infile, outfile):
+    'Copy infile to outfile if changed'
+    validfiles.add(fp.name)
+    if outfile.exists():
+        if filecmp.cmp(infile, outfile):
+            return
+        outfile.unlink()
+        action = 'updating'
+    else:
+        action = 'creating'
+        outfile.parent.mkdir(parents=True, exist_ok=True)
+
+    print('{} {}: {}'.format(action, fmttime(fp.time), fp.name))
+    os.link(infile, outfile)
+
+def delfile(path):
+    'Delete given file if not needed anymore'
+    ipath = path.relative_to(outdir)
+    if ipath.parts[0] in exgit:
+        return
+    if str(ipath) not in validfiles:
+        print('deleting {}: {}'.format(fmttime(path.stat().st_mtime), ipath))
+        path.unlink()
+
+def main():
+    'Main code'
+    global indir, outdir, args
+
+    # Process command line options
+    opt = argparse.ArgumentParser(description=__doc__.strip())
+    grp = opt.add_mutually_exclusive_group()
+    grp.add_argument('-t', '--time',
+            help='set time YYYY-MM-DDTHH:MM.SS, default=latest')
+    grp.add_argument('-f', '--filetime',
+            help='set time based on specified file')
+    opt.add_argument('-s', '--summary', action='store_true',
+            help='just print a summary of files and versions')
+    opt.add_argument('-g', '--gitkeep', action='store_true',
+            help='preserve any top level git dir in outdir')
+    opt.add_argument('-p', '--path',
+            help='only process files under given path')
+    opt.add_argument('indir',
+            help='input B2 archive containing all file versions '
+            ' (from --b2-versions)')
+    opt.add_argument('outdir', nargs='?',
+            help='output directory to recreate for given time')
+    args = opt.parse_args()
+
+    indir = Path(args.indir).expanduser()
+
+    if not indir.is_dir():
+        opt.error('indir must be a directory')
+
+    if not args.summary:
+        if not args.outdir:
+            opt.error('outdir must be specified')
+
+        outdir = Path(args.outdir).expanduser()
+
+        if outdir.exists():
+            if not outdir.is_dir():
+                opt.error('outdir must be a directory')
+
+            if args.gitkeep:
+                exgit.extend([str(d) for d in outdir.glob('.git*')])
+
+        outdir.mkdir(parents=True, exist_ok=True)
+
+        if indir.stat().st_dev != outdir.stat().st_dev:
+            opt.error('indir and outdir must on same file system')
+
+    if args.filetime:
+        afile = Path(args.filetime)
+        if not afile.exists():
+            opt.error('{} does not exist'.format(args.filetime))
+
+        argstime = afile.stat().st_mtime
+    elif args.time:
+        argstime = time.mktime(time.strptime(args.time, TIMEFMT))
+
+        # Add a large fraction to ensure we match again file times which
+        # include msecs.
+        argstime += 1 - time.clock_getres(time.CLOCK_MONOTONIC)
+    else:
+        argstime = None
+
+    # Parse all files in the versioned indir
+    parsedir(indir, parsefile)
+
+    if args.summary:
+        fnames = sorted(FileName.namemap)
+        for fname in fnames:
+            print('{}:'.format(fname))
+            for fpath in FileName.namemap[fname].paths:
+                print('  {} {:8} B'.format(fmttime(fpath.time), fpath.size))
+        return
+
+    # Iterate through all files and restore version for given time
+    for fname in FileName.namemap.values():
+        ix = 0
+
+        for i, tm in enumerate(fname.times):
+            if argstime and tm > argstime:
+                break
+            ix += 1
+
+        # Candidate files may all be newer than specified
+        if ix == 0:
+            continue
+
+        fp = fname.paths[ix - 1]
+
+        # If the latest version had a version string then this file must
+        # have been deleted at this time
+        if i == ix or not fp.version:
+            addfile(fp, indir / fp.path, outdir / fname.name)
+
+    # Delete any leftover files
+    parsedir(outdir, delfile)
+
+    # Delete all leftover empty dirs
+    for root, dirs, files in os.walk(outdir, topdown=False):
+        for name in dirs:
+            dird = Path(root, name)
+            if dird.parts[0] not in exgit:
+                if not any(dird.iterdir()) and dird != outdir:
+                    print('deleting empty {}'.format(dird.relative_to(outdir)))
+                    dird.rmdir()
+
+if __name__ == '__main__':
+    sys.exit(main())
