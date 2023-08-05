6686bc456a4458028bfb5787a7d3ce44e053fc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:55 +0000

    Add package ddSMT/0.1.dev738/ddSMT-0.1.dev738.tar.gz

---
+++ packages/ddSMT/ddSMT-0.1.dev738.tar.gz/ddSMT-0.1.dev738/ddsmt/tests/test_mutators_core.py
@@ -0,0 +1,220 @@
+from ..nodes import Node
+from .. import mutators_core
+from .. import options
+from .. import smtlib
+
+
+def test_binary_reduction():
+    m = mutators_core.BinaryReduction()
+    assert isinstance(str(m), str)
+    assert not hasattr(m, 'filter')
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    f = Node('f')
+    g = Node('g')
+    h = Node('h')
+    i = Node('i')
+    exprs = [a, b, c, d, e, f, g, h, i]
+    mut = [
+        {
+            e.id: None,
+            f.id: None,
+            g.id: None,
+            h.id: None,
+            i.id: None
+        },
+        {
+            a.id: None,
+            b.id: None,
+            c.id: None,
+            d.id: None
+        },
+        {
+            g.id: None,
+            h.id: None,
+            i.id: None
+        },
+        {
+            e.id: None,
+            f.id: None
+        },
+        {
+            c.id: None,
+            d.id: None
+        },
+        {
+            a.id: None,
+            b.id: None
+        },
+    ]
+    assert list(m.global_mutations(b, exprs)) == []
+    assert list(m.global_mutations(a, exprs)) == mut
+
+    assert list(m.mutations(Node('and', 'a', 'b', 'c'))) == []
+    n = Node('and', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j')
+    assert list(m.mutations(n)) == [
+        Node('and', 'a', 'b', 'c', 'd'),
+        Node('e', 'f', 'g', 'h', 'i', 'j'),
+        Node('and', 'a', 'b', 'c', 'd', 'e', 'f', 'g'),
+        Node('and', 'a', 'b', 'c', 'd', 'h', 'i', 'j'),
+        Node('and', 'a', 'e', 'f', 'g', 'h', 'i', 'j'),
+        Node('b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'),
+    ]
+
+
+def test_binary_reduction_named():
+    m = mutators_core.BinaryReduction()
+    m.ident = 'assert'
+    assert isinstance(str(m), str)
+    assert not hasattr(m, 'filter')
+    a = Node('a')
+    b = Node('assert', 'b')
+    c = Node('c')
+    d = Node('assert', 'd')
+    e = Node('e')
+    f = Node('assert', 'f')
+    g = Node('g')
+    h = Node('assert', 'h')
+    i = Node('assert', 'i')
+    exprs = [a, b, c, d, e, f, g, h, i]
+    mut = [
+        {
+            f.id: None,
+            h.id: None,
+            i.id: None
+        },
+        {
+            b.id: None,
+            d.id: None
+        },
+    ]
+    assert list(m.global_mutations(a, exprs)) == mut
+
+
+def test_constants():
+    smtlib.reset_information()
+    m = mutators_core.Constants()
+    assert isinstance(str(m), str)
+    exprs = [Node('declare-const', 'x', 'Real'), Node('#b1011')]
+    assert not m.filter(Node('x'))
+    assert m.mutations(Node('x')) == []
+    smtlib.collect_information(exprs)
+    assert m.filter(Node('x'))
+    assert m.filter(Node('#b1011'))
+    assert m.filter(Node('=', 'x', '1'))
+    assert not m.filter(Node('y'))
+
+    # can't handle this case without BV const normalization
+    # else this should be an empty set
+    assert m.mutations(
+        Node('#b0')) == [Node('_', 'bv0', 1),
+                         Node('_', 'bv1', 1)]
+    assert m.mutations(Node('_', 'bv0', 1)) == []
+
+    assert isinstance(m.mutations(Node('#b1011'))[0], Node)
+    assert isinstance(m.mutations(Node('#b1011'))[0].data, tuple)
+    assert m.mutations(Node('#b1011'))[0] == Node('_', 'bv0', 4)
+    assert m.mutations(
+        Node('#b1011')) == [Node('_', 'bv0', 4),
+                            Node('_', 'bv1', 4)]
+    assert m.mutations(Node('x')) == [Node('0.0'), Node('1.0')]
+    assert m.mutations(Node('=', 'x', '1')) == [Node('false'), Node('true')]
+
+
+def test_erase_node():
+    m = mutators_core.EraseNode()
+    assert isinstance(str(m), str)
+    assert m.filter(Node('x'))
+    assert m.mutations(Node('x')) == [None]
+
+
+def test_erase_named_node():
+    m = mutators_core.EraseNode()
+    m.ident = 'assert'
+    assert isinstance(str(m), str)
+    assert m.filter(Node('assert', 'x'))
+    assert not m.filter(Node('x'))
+    assert m.mutations(Node(Node('assert'), Node('true'))) == [None]
+
+
+def test_merge_with_children():
+    node = Node(Node('+'), Node('x'), Node(Node('+'), Node('x'), Node('y')))
+    m = mutators_core.MergeWithChildren()
+    assert isinstance(str(m), str)
+    assert m.filter(node)
+    assert list(m.mutations(node)) == [
+        Node(Node('+'), Node('x'), Node('x'), Node('y'))
+    ]
+
+
+def test_replace_by_child():
+    smtlib.collect_information([
+        Node('declare-const', 'x', 'Int'),
+        Node('declare-const', 'y', 'Int'),
+    ])
+    node = Node('+', 'x', 'y')
+    m = mutators_core.ReplaceByChild()
+    assert isinstance(str(m), str)
+    assert m.filter(node)
+    assert not m.filter(Node('x'))
+    assert list(m.mutations(node)) == ['x', 'y']
+    smtlib.reset_information()
+
+
+def test_replace_by_variable():
+    smtlib.reset_information()
+    options.args(['in', 'out', 'bin'])
+
+    x = Node('x')
+    c = Node('42')
+    node = Node(Node('+'), x, c)
+    m = mutators_core.ReplaceByVariable()
+    assert isinstance(str(m), str)
+    assert m.mutations(Node('x')) == []
+
+    m.repl_mode = 'inc'
+    assert isinstance(str(m), str)
+    assert m.mutations(node) == []
+
+    v1 = Node('v1')
+    v3 = Node('v3')
+    exprs = [
+        Node(Node('declare-const'), v1, Node('Int')),
+        Node(Node('declare-const'), x, Node('Int')),
+        Node(Node('declare-const'), Node('v2'), Node('Bool')),
+        Node(Node('declare-const'), v3, Node('Int'))
+    ]
+
+    smtlib.collect_information(exprs)
+    assert m.filter(node)
+    assert m.filter(x)
+    assert not m.filter(c)
+    assert list(m.mutations(node)) == [v1, x, v3]
+    assert list(m.mutations(x)) == []
+    assert list(m.mutations(v1)) == [x, v3]
+    assert list(m.mutations(v3)) == [x]
+
+    m.repl_mode = 'dec'
+    assert list(m.mutations(node)) == [v1, x, v3]
+    assert list(m.mutations(x)) == [v1, v3]
+    assert list(m.mutations(v1)) == []
+    assert list(m.mutations(v3)) == [v1]
+
+
+def test_sort_children():
+    node = Node(Node('+'), Node('123'), Node(Node('*'), Node('y'), Node('2')),
+                Node('x'))
+    expected = Node(Node('+'), Node('123'), Node('x'),
+                    Node(Node('*'), Node('y'), Node('2')))
+
+    m = mutators_core.SortChildren()
+    assert isinstance(str(m), str)
+    assert not m.filter(Node('x'))
+    assert not m.filter(Node('123'))
+    assert m.filter(node)
+    assert m.filter(expected)
+    assert list(m.mutations(node)) == [expected]
+    assert list(m.mutations(expected)) == []
---
+++ packages/ddSMT/ddSMT-0.1.dev738.tar.gz/ddSMT-0.1.dev738/ddsmt/tests/test_mutators_smtlib.py
@@ -0,0 +1,224 @@
+from ..nodes import Node
+from .. import mutators_smtlib
+from .. import smtlib
+
+
+def test_smtlib_get_mutators():
+    d = mutators_smtlib.get_mutators()
+    assert isinstance(d, dict)
+    assert len(d) == 9
+
+
+def test_smtlib_checksat_assuming():
+    m = mutators_smtlib.CheckSatAssuming()
+    assert isinstance(str(m), str)
+    assert not m.filter(Node(Node('check-sat')))
+    assert not m.filter(Node('assert', 'true'))
+    assert m.filter(Node('check-sat-assuming', 'true'))
+    assert m.filter(Node('check-sat-assuming', ('xor', 'x', 'y')))
+
+    assert m.mutations(Node('check-sat-assuming',
+                            'true')) == [Node(Node('check-sat'))]
+    assert m.mutations(Node('check-sat-assuming',
+                            ('xor', 'x', 'y'))) == [Node(Node('check-sat'))]
+
+
+def test_smtlib_eliminate_variable():
+    m = mutators_smtlib.EliminateVariable()
+    assert isinstance(str(m), str)
+    # special case where 'x' occurs in the replacement
+    eq = Node('=', 'x', ('*', 'x', 'y'))
+    assert m.filter(eq)
+    assert list(m.global_mutations(eq, eq)) == []
+    eq = Node('=', 'x', ('*', 'y', 'y'))
+    exprs = [
+        Node('declare-const', 'x', 'Real'),
+        Node('define-fun', 'y', (), 'Real', ('*', 'x', 'x')),
+        Node('define-fun', 'z', (), 'Real', ('*', 'x', 'y')),
+        Node('assert', eq),
+        Node('assert', ('>', 'x', 'y')),
+    ]
+    assert m.filter(eq)
+    assert list(m.global_mutations(eq, exprs)) == [[
+        Node('declare-const', 'x', 'Real'),
+        Node('define-fun', 'y', (), 'Real',
+             ('*', ('*', 'y', 'y'), ('*', 'y', 'y'))),
+        Node('define-fun', 'z', (), 'Real', ('*', ('*', 'y', 'y'), 'y')),
+        Node('assert', ('=', ('*', 'y', 'y'), ('*', 'y', 'y'))),
+        Node('assert', ('>', ('*', 'y', 'y'), 'y')),
+    ]]
+
+    eq = Node('=', 'x', 'y', 'a', ('*', 'y', 'y'), ('+', 'a', 'b'))
+    exprs = [Node('assert', ('>', 'x', 'y'))]
+    assert m.filter(eq)
+    assert list(m.global_mutations(eq, exprs)) == [
+        [Node('assert', ('>', 'y', 'y'))],
+        [Node('assert', ('>', 'a', 'y'))],
+        [Node('assert', ('>', ('*', 'y', 'y'), 'y'))],
+        [Node('assert', ('>', ('+', 'a', 'b'), 'y'))],
+        [Node('assert', ('>', 'x', 'x'))],
+        [Node('assert', ('>', 'x', 'a'))],
+        [Node('assert', ('>', 'x', ('+', 'a', 'b')))],
+    ]
+
+
+def test_smtlib_inline_define_fun():
+    smtlib.reset_information()
+    m = mutators_smtlib.InlineDefinedFuns()
+    assert isinstance(str(m), str)
+    # special case where we should not attempt to inline into itself
+    inner = Node('x')
+    n = Node('define-fun', 'x', (), 'Bool', ('not', inner))
+    smtlib.collect_information([n])
+    assert m.filter(inner)
+    assert m.mutations(inner) == []
+
+    n = Node('define-fun', 'x', (), 'Bool', inner)
+    smtlib.collect_information([n])
+    assert m.mutations(inner) == []
+
+    inner = Node('x')
+    expr = Node(inner, 'a', 'b')
+    smtlib.collect_information([
+        Node('define-fun', 'x', (('y', 'Bool'), ('z', 'Bool')), 'Bool',
+             ('and', 'y', 'z'))
+    ])
+    assert m.filter(expr)
+    assert m.mutations(expr) == [Node('and', 'a', 'b')]
+    smtlib.reset_information()
+
+
+def test_smtlib_introduce_fresh_variable():
+    smtlib.reset_information()
+    m = mutators_smtlib.IntroduceFreshVariable()
+    assert isinstance(str(m), str)
+
+    decl = Node('declare-const', 'x', 'Bool')
+    smtlib.collect_information([decl])
+    term = Node('not', 'x')
+    exprs = [decl, Node('assert', term)]
+    assert not m.filter(Node('false'))
+    assert m.filter(Node('x'))
+    assert not m.filter(Node('__fresh_'))
+    assert m.filter(term)
+    assert m.global_mutations(term, exprs) == [[
+        decl, ('declare-fun', f'__fresh_{term.id}', (), 'Bool'),
+        Node('assert', f'__fresh_{term.id}')
+    ]]
+
+
+def test_smtlib_let_elimination():
+    m = mutators_smtlib.LetElimination()
+    assert isinstance(str(m), str)
+    assert not m.filter(Node('and', 'x', 'y'))
+
+    n = Node('let', (('a', 'x'), ('b', 'y')), ('and', 'a', 'b'))
+    assert m.filter(n)
+    assert m.mutations(n) == [('and', 'a', 'b')]
+
+    n = Node(
+        'let',
+        (('a', 'x'), ('b', 'y')),
+    )
+    assert m.filter(n)
+    assert m.mutations(n) == []
+
+
+def test_smtlib_let_substitution():
+    m = mutators_smtlib.LetSubstitution()
+    assert isinstance(str(m), str)
+    assert not m.filter(Node('and', 'x', 'y'))
+    n = Node('let', (('a', 'x'), ('b', 'y')), ('and', 'a', 'b'))
+    assert m.filter(n)
+    assert m.mutations(n) == [
+        ('let', (('a', 'x'), ('b', 'y')), ('and', 'x', 'b')),
+        ('let', (('a', 'x'), ('b', 'y')), ('and', 'a', 'y'))
+    ]
+    n = Node('let', (('a', 'x'), ('b', 'y')), ('and', 'abc', 'def'))
+    assert m.filter(n)
+    assert m.mutations(n) == []
+
+    n = Node('let', (('a', 'x'), ('b', 'y')))
+    assert m.filter(n)
+    assert m.mutations(n) == []
+
+
+def test_smtlib_simplify_logic():
+    m = mutators_smtlib.SimplifyLogic()
+    assert isinstance(str(m), str)
+
+    assert not m.filter(Node('assert', 'x'))
+    assert m.filter(Node('set-logic', 'QF_NRA'))
+    assert m.mutations(Node('set-logic',
+                            'QF_NRA')) == [('set-logic', 'QF_LRA')]
+    assert m.mutations(Node('set-logic', 'QF_BVFPLRAS')) == [
+        ('set-logic', 'QF_FPLRAS'),
+        ('set-logic', 'QF_BVLRAS'),
+        ('set-logic', 'QF_BVFPLRA'),
+        ('set-logic', 'QF_BVFPS'),
+    ]
+
+
+def test_smtlib_quoted_symbols():
+    m = mutators_smtlib.SimplifyQuotedSymbols()
+    assert isinstance(str(m), str)
+    assert not m.filter(Node('x'))
+    assert m.filter(Node('|x|'))
+    assert not m.filter(Node('|"x|'))
+    assert not m.filter(Node('and', 'x', 'y'))
+    assert not m.filter(Node('and', '|x|', 'y'))
+    assert m.mutations(Node('|x|')) == ['x']
+    assert m.global_mutations(Node('|x|'), Node('assert', '|x|')) == [{
+        Node('|x|'):
+        Node('x')
+    }]
+
+
+def test_smtlib_simplify_symbol_names():
+    m = mutators_smtlib.SimplifySymbolNames()
+    assert isinstance(str(m), str)
+    assert m.filter(Node('declare-const', 'x', 'Real'))
+    assert not m.filter(Node('declare-const', 'false', 'Real'))
+    assert m.filter(Node('declare-const', 'x', 'Real'))
+    exprs = [
+        Node('declare-const', 'abcdef', 'Bool'),
+        Node('declare-datatype', 'Color', (('red', ), ('|green|', ))),
+        Node('assert', 'abcdef'),
+        Node('assert',
+             ('exists', (('xy', 'Bool'), ('z', 'Bool')), ('and', 'xy', 'z'))),
+    ]
+    assert list(m.global_mutations(exprs[0], exprs)) == [{
+        Node('abcdef'):
+        Node('abc')
+    }, {
+        Node('abcdef'):
+        Node('abcde')
+    }, {
+        Node('abcdef'):
+        Node('bcdef')
+    }]
+    assert list(m.global_mutations(exprs[1], exprs)) == [{
+        Node('Color'): 'Co'
+    }, {
+        Node('Color'): 'Colo'
+    }, {
+        Node('Color'): 'olor'
+    }, {
+        Node('red'): 're'
+    }, {
+        Node('red'): 'ed'
+    }, {
+        Node('|green|'):
+        '|gr|'
+    }, {
+        Node('|green|'):
+        '|gree|'
+    }, {
+        Node('|green|'):
+        '|reen|'
+    }]
+    assert list(m.global_mutations(exprs[3][1], exprs)) == [{
+        Node('xy'): 'x'
+    }, {
+        Node('xy'): 'y'
+    }]
