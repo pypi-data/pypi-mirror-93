954ccf44e83439728755547a184c295f1a02b499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package doclever2md/0.0.3/doclever2md-0.0.3.tar.gz

---
+++ packages/doclever2md/doclever2md-0.0.3.tar.gz/doclever2md-0.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: skyleft
+# Description: doclever导出的json转藏经阁风格markdown
+
+from setuptools import setup, find_packages
+
+setup(
+    name='doclever2md',
+    version='0.0.3',
+    keywords='doclever,markdown',
+    description='a tool which can convert doclever json to cangjingge style markdown',
+    license='MIT License',
+    url='https://andy-cheung.me',
+    author='skyleft',
+    author_email='skyleft123@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Jinja2>=2.11.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'doclever2md = doclever2md.main:main',
+        ],
+    },
+)
