a369df66f281fc546c23276768621728e4186a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:28 +0000

    Add package dsnd-probability-jvil/0.1/dsnd_probability_jvil-0.1.tar.gz

---
+++ packages/dsnd-probability-jvil/dsnd_probability_jvil-0.1.tar.gz/dsnd_probability_jvil-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (name='dsnd_probability_jvil',
+       version='0.1',
+       description = 'Gaussian and Binomial distributions',
+       packages=['dsnd_probability_jvil'],
+       author = 'Jed Villareal',
+       author_email = 'jedmarievillareal@gmail.com',
+       zip_safe=False)
