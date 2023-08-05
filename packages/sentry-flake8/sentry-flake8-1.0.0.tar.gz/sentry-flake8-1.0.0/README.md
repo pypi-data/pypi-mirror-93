c68dbe787a9bb85bc13e2262d86c94dd84de447c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package sentry-flake8/1.0.0/sentry-flake8-1.0.0.tar.gz

---
+++ packages/sentry-flake8/sentry-flake8-1.0.0.tar.gz/sentry-flake8-1.0.0/README.md
@@ -0,0 +1,21 @@
+# sentry-flake8
+
+[![PyPI](https://img.shields.io/pypi/v/sentry-flake8.svg)](https://pypi.org/project/sentry-flake8)
+[![Travis](https://img.shields.io/travis/com/getsentry/sentry-flake8.svg)](https://travis-ci.com/getsentry/sentry-flake8)
+
+Sentry's custom flake8 checker plugin.
+
+## Install
+
+`pip install sentry-flake8`
+
+No further configuration is necessary for `flake8` to load the plugin. An example successful installation:
+
+```
+$ flake8 --version
+3.8.4 (mccabe: 0.6.1, pycodestyle: 2.6.0, pyflakes: 2.2.0, sentry-flake8: 1.0.0) CPython 3.6.10 on Darwin
+```
+
+## Credits
+
+This extension is inspired by, and based upon work done by Łukasz Langa in [flake8-bugbear](https://github.com/PyCQA/flake8-bugbear).
---
+++ packages/sentry-flake8/sentry-flake8-1.0.0.tar.gz/sentry-flake8-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import sys
+from setuptools import setup
+
+assert (3, 6, 0) <= sys.version_info < (3, 10, 0), "sentry-flake8 requires Python 3.6 - 3.9."
+
+setup(
+    name="sentry-flake8",
+    version="1.0.0",
+    description="Sentry's custom flake8 checker plugin.",
+    license="Apache 2.0",
+    url="https://github.com/getsentry/sentry-flake8",
+    author="Sentry",
+    author_email="hello@sentry.io",
+    install_requires=["flake8>=3.8.0,<3.9.0"],
+    package_dir={"": "src"},
+    extras_require={
+        "tests": [
+            "pytest==6.2.2",
+        ]
+    },
+    python_requires=">=3.6, <3.10",
+    py_modules=["sentry_check"],
+    entry_points={"flake8.extension": ["B = sentry_check:SentryCheck"]},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development",
+    ],
+)
