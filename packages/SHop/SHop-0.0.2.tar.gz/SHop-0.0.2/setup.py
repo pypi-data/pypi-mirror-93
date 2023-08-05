fb28e40689395ddfc70c4b9a2cdf8e68859f11ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:01 +0000

    Add package SHop/0.0.2/SHop-0.0.2.tar.gz

---
+++ packages/SHop/SHop-0.0.2.tar.gz/SHop-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SHop",
+    version="0.0.2",
+    author="Soraya Castillo",
+    author_email="socastillogi@unal.edu.co",
+    description="Paquete para simulacion hidrologica operacional",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
