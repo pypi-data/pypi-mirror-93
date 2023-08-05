6b82fef00a38fc84e6c487b13652c7d110625eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package tspwplib/0.5.6/tspwplib-0.5.6.tar.gz

---
+++ packages/tspwplib/tspwplib-0.5.6.tar.gz/tspwplib-0.5.6/tests/test_utils.py
@@ -0,0 +1,15 @@
+"""Tests for the utils module"""
+
+from tspwplib import build_path_to_oplib_instance, build_path_to_tsplib_instance
+
+
+def test_build_path_to_oplib_instance(oplib_root, generation, graph_name, alpha):
+    """Test path building"""
+    assert build_path_to_oplib_instance(
+        oplib_root, generation, graph_name, alpha=alpha
+    ).exists()
+
+
+def test_build_path_to_tsplib_instance(tsplib_root, graph_name):
+    """Test path building for tsplib"""
+    assert build_path_to_tsplib_instance(tsplib_root, graph_name).exists()
---
+++ packages/tspwplib/tspwplib-0.5.6.tar.gz/tspwplib-0.5.6/tspwplib/utils.py
@@ -0,0 +1,21 @@
+"""Useful functions for parsing"""
+
+from pathlib import Path
+from .types import Alpha, Generation, GraphName
+
+
+def build_path_to_oplib_instance(
+    oplib_root: Path,
+    generation: Generation,
+    name: GraphName,
+    alpha: int = Alpha.fifty.value,
+) -> Path:
+    """Build a filepath to a oplib instance"""
+    filename: str = name + "-" + generation.value + "-" + str(alpha) + ".oplib"
+    return oplib_root / "instances" / generation.value / filename
+
+
+def build_path_to_tsplib_instance(tsplib_root: Path, name: GraphName) -> Path:
+    """Build a filepath to a tsplib instance"""
+    filename = name.value + ".tsp"
+    return tsplib_root / filename
