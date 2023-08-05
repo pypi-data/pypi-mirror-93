42f20d93830db89c09935d0f4e14497060b31b28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package beta-beam/0.0.4/beta-beam-0.0.4.tar.gz

---
+++ packages/beta-beam/beta-beam-0.0.4.tar.gz/beta-beam-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("readme", "r", encoding="utf-8") as des:
+    long_description = des.read()
+
+# scr ent fy
+
+setup(
+    version="0.0.4",
+    description="My last hope to get a degree...",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/patod01/beta-beam",
+    #project_urls={},
+    packages=find_packages(),
+    #py_modules=[], # alone
+    install_requires=[],
+    python_requires='>=3.9',
+    #package_data={'pnomo':['../readme']},
+    #data_files=[('',['readme'])], # zona mapa
+    scripts=[],
+    entry_points={
+        'console_scripts': [
+            'peta=pnomo.lib:run',
+        ],
+    },
+)
