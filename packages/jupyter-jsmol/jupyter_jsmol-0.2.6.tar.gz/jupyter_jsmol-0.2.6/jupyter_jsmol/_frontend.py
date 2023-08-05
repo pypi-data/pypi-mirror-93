725a9df5b230d00b49a098374bde3235c4e66482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:07 +0000

    Add package jupyter-jsmol/0.2.6/jupyter_jsmol-0.2.6.tar.gz

---
+++ packages/jupyter-jsmol/jupyter_jsmol-0.2.6.tar.gz/jupyter_jsmol-0.2.6/jupyter_jsmol/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Adam Fekete.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "jupyter-jsmol"
+module_version = "^0.2.6"
