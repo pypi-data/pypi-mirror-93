d4dc73cd0825f42eacb08dd695a7482f8eb16406
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:38 +0000

    Add package hived-rpc-scanner/0.0.5/hived_rpc_scanner-0.0.5.tar.gz

---
+++ packages/hived-rpc-scanner/hived_rpc_scanner-0.0.5.tar.gz/hived_rpc_scanner-0.0.5/hived_rpc_scanner/runner.py
@@ -0,0 +1,38 @@
+from prettytable import PrettyTable
+
+from .core import main
+
+import argparse
+import emoji
+
+default_nodes = [
+    "https://hived.emre.sh",
+    "https://api.hive.blog",
+]
+
+
+def runner():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nodes', nargs='+', )
+    arguments = parser.parse_args()
+    resp = main(nodes=arguments.nodes or default_nodes)
+
+    for api_type, call in resp.items():
+        table = PrettyTable()
+        table.field_names = ["#", "Node", "Call", "Status", "Time [ms]",
+                             "Error"]
+        table.align = "l"
+        for sub_call, scan_statuses in call.items():
+            for scan_status, scan_results in scan_statuses.items():
+                i = 1
+                for scan_result in scan_results:
+                    table.add_row(
+                        [f"[{i}]",
+                         scan_result["node"],
+                         sub_call,
+                         emoji.emojize(":check_mark_button:") if scan_status else emoji.emojize("cross mark"),
+                         int(scan_result["time_spent"] * 1e4),
+                         scan_result.get("error", "-")]
+                    )
+                    i += 1
+        print(table.get_string(title=f"{api_type}"))
