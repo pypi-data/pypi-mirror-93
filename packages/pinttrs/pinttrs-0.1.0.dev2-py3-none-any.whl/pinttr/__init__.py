7651c388ffea51f1fc5d280911ec1b0ed4b11351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package pinttrs/0.1.0.dev2/pinttrs-0.1.0.dev2-py3-none-any.whl

---
+++ packages/pinttrs/pinttrs-0.1.0.dev2-py3-none-any.whl/pinttr/__init__.py
@@ -0,0 +1,22 @@
+from . import converters, exceptions, util, validators
+from ._defaults import get_unit_registry, set_unit_registry
+from ._interpret import interpret_units
+from ._make import attrib
+
+# Package metadata
+__version__ = "0.1.0-dev2"
+__copyright__ = "Copyright (c) 2021 Vincent Leroy"
+
+# Other definitions
+ib = attrib
+__all__ = [
+    "attrib",
+    "converters",
+    "exceptions",
+    "get_unit_registry",
+    "ib",
+    "interpret_units",
+    "set_unit_registry",
+    "util",
+    "validators",
+]
