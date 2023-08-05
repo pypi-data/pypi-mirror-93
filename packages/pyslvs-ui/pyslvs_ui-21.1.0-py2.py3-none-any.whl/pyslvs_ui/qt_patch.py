a0a8e6880986955b941cb9a79a61b029b0ad089d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:22 +0000

    Add package pyslvs-ui/21.1.0/pyslvs_ui-21.1.0-py2.py3-none-any.whl

---
+++ packages/pyslvs-ui/pyslvs_ui-21.1.0-py2.py3-none-any.whl/pyslvs_ui/qt_patch.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains all the Qt objects we needed.
+
+Customized class will define below.
+"""
+
+__all__ = ['API', 'QT_VERSION', 'qt_image_suffix', 'qt_image_format',
+           'QABCMeta']
+__author__ = "Yuan Chang"
+__copyright__ = "Copyright (C) 2016-2021"
+__license__ = "AGPL"
+__email__ = "pyslvs@gmail.com"
+
+from abc import ABCMeta
+from qtpy import API_NAME
+from qtpy.QtCore import __version__, QObject
+from importlib import import_module
+
+if API_NAME == 'PyQt5':
+    from qtpy.QtCore import PYQT_VERSION_STR
+    API = f"{API_NAME} {PYQT_VERSION_STR}"
+elif API_NAME == 'PySide2':
+    API = f"{API_NAME} {getattr(import_module('PySide2'), '__version__')}"
+else:
+    raise ModuleNotFoundError("module not found: PyQt5 or PySide2")
+
+QT_VERSION = __version__
+qt_image_suffix = []
+qt_image_format = []
+for suffix, name in (
+    ('png', "Portable Network Graphics"),
+    ('jpg', "Joint Photographic Experts Group"),
+    ('bmp', "Bitmap Image file"),
+    ('bpm', "Business Process Model"),
+    ('tiff', "Tagged Image File Format"),
+    ('ico', "Windows Icon"),
+    ('wbmp', "Wireless Application Protocol Bitmap"),
+    ('xbm', "X Bitmap"),
+    ('xpm', "X Pixmap"),
+):
+    qt_image_suffix.append(suffix)
+    qt_image_format.append(f"{name} (*.{suffix})")
+qt_image_suffix = tuple(qt_image_suffix)
+qt_image_format = tuple(qt_image_format)
+del suffix, name
+
+
+class QABCMeta(type(QObject), ABCMeta):  # type: ignore
+    """Qt ABCMeta class.
+
+    Usage:
+    >>> from abc import abstractmethod
+    >>> class MyQObject(QObject, metaclass=QABCMeta):
+    >>>    @abstractmethod
+    >>>    def my_abstract_method(self) -> None:
+    >>>        raise NotImplementedError
+    """
+    pass
---
+++ packages/pyslvs-ui/pyslvs_ui-21.1.0-py2.py3-none-any.whl/pyslvs_ui/synthesis/dimensional_synthesis/dialogs/chart.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+"""The chart dialog of dimensional synthesis result."""
+
+__author__ = "Yuan Chang"
+__copyright__ = "Copyright (C) 2016-2021"
+__license__ = "AGPL"
+__email__ = "pyslvs@gmail.com"
+
+from typing import Sequence, Mapping, Any
+from numpy import array
+from qtpy.QtCore import Qt, QSize
+from qtpy.QtWidgets import QDialog, QVBoxLayout, QTabWidget, QWidget
+from pyslvs_ui.graphics import DataChart
+
+
+class ChartDialog(QDialog):
+    """There are three charts are in the dialog.
+
+    + Fitness / Generation Chart.
+    + Generation / Time Chart.
+    + Fitness / Time Chart.
+    """
+
+    def __init__(
+        self,
+        title: str,
+        algorithm_data: Sequence[Mapping[str, Any]],
+        parent: QWidget
+    ):
+        """Add three tabs of chart."""
+        super(ChartDialog, self).__init__(parent)
+        self.setWindowTitle("Chart")
+        self.setWindowFlags(self.windowFlags() | Qt.WindowMaximizeButtonHint)
+        self.setSizeGripEnabled(True)
+        self.setModal(True)
+        self.setMinimumSize(QSize(800, 600))
+        self.title = title
+        self.algorithm_data = algorithm_data
+        # Widgets
+        main_layout = QVBoxLayout(self)
+        main_layout.setContentsMargins(6, 6, 6, 6)
+        self.tab_widget = QTabWidget(self)
+        self.__set_chart("F-G Plot", 0, 1, 'Generation', 'Fitness')
+        self.__set_chart("G-T Plot", 2, 0, 'Time', 'Generation')
+        self.__set_chart("F-T Plot", 2, 1, 'Time', 'Fitness')
+        main_layout.addWidget(self.tab_widget)
+
+    def __set_chart(
+        self,
+        name: str,
+        pos_x: int,
+        pos_y: int,
+        label_x: str,
+        label_y: str
+    ) -> None:
+        """Setting charts by data index.
+
+        pos_x / pos_y: [0], [1], [2]
+        time_fitness: List[List[Tuple[gen, fitness, time]]]
+        """
+        chart = DataChart(self)
+        ax = chart.ax()
+        for i, data in enumerate(self.algorithm_data):
+            a = array(data['time_fitness'], dtype=float)
+            ax[0].plot(a[:, pos_x], a[:, pos_y], label=f"Task {i + 1}")
+            ax[0].set_xlabel(label_x)
+            ax[0].set_ylabel(label_y)
+            ax[0].legend()
+        self.tab_widget.addTab(chart, name)
