7233439233c4763f212f1fa732ccbc6791536ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:11 +0000

    Add package nubium-utils/0.16.0/nubium-utils-0.16.0.tar.gz

---
+++ packages/nubium-utils/nubium-utils-0.16.0.tar.gz/nubium-utils-0.16.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as file_obj:
+    long_description = file_obj.read()
+
+packages = setuptools.find_packages()
+
+setuptools.setup(
+    name='nubium-utils',
+    version='0.16.0',
+    author="Edward Brennan",
+    author_email="ebrennan@redhat.com",
+    description="Some Kafka utility functions and patterns for the nubium project",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.corp.redhat.com/mkt-ops-de/nubium-utils.git",
+    packages=packages,
+    install_requires=["requests", "prometheus_client"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
