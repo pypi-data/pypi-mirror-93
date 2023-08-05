bc753a0cd3982541a3a77f4fdfb14548cb7041fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:50 +0000

    Add package aleat3/0.1.1/aleat3-0.1.1.tar.gz

---
+++ packages/aleat3/aleat3-0.1.1.tar.gz/aleat3-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aleat3",
+    version="0.1.1",
+    author="Diego Ramirez",
+    author_email="dr01191115@gmail.com",
+    description="An aleatory syntaxes package. Third generation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
