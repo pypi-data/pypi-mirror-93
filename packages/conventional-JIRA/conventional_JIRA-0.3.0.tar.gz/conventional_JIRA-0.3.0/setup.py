de4ab5490144a79ab26a77ce0e25563392f29e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package conventional-JIRA/0.3.0/conventional_JIRA-0.3.0.tar.gz

---
+++ packages/conventional-JIRA/conventional_JIRA-0.3.0.tar.gz/conventional_JIRA-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="conventional_JIRA",
+    version="0.3.0",
+    py_modules=["cz_conventional_JIRA"],
+    author = 'Michael Kuc',
+    author_email = 'michaelkuc6@gmail.com',
+    url = 'https://github.com/Crystalix007/conventional_jira',
+    license="MIT",
+    long_description="A variation of conventional commits enforcing a JIRA issue as the scope.",
+    install_requires=["commitizen"],
+)
