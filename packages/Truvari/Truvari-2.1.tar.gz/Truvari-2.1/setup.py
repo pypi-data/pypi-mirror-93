39e4bd38d6b174939f9a69b42f4e309d3543bb10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:54 +0000

    Add package Truvari/2.1/Truvari-2.1.tar.gz

---
+++ packages/Truvari/Truvari-2.1.tar.gz/Truvari-2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='Truvari',
+    version='2.1',
+    author="ACEnglish",
+    author_email="acenglish@gmail.com",
+    url="https://github.com/spiralgenetics/truvari",
+    packages=['truvari', 'truvari/annos'],
+    license='MIT',
+    scripts=["bin/truvari"],
+    description="Structural variant comparison tool for VCFs",
+    long_description=open('README.md', encoding='UTF-8').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        "ACEBinf>=1.0.2",
+        "python-Levenshtein==0.12.1",
+        "progressbar2>=3.41.0",
+        "pysam>=0.15.2",
+        "pyfaidx>=0.5.5.2",
+        "intervaltree>=3.0.2",
+        "joblib>=0.14.1",
+        "numpy>=1.18.1",
+        "pyfaidx>=0.5.8",
+        "pytabix>=0.1",
+        "bwapy>=0.1.4",
+        "pandas>=1.0.5",
+    ],
+)
