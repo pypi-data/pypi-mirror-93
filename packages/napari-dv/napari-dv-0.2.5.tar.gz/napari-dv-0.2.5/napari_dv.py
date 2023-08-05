9459867fbef6c09d6783f09049104e8e587ed63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:36 +0000

    Add package napari-dv/0.2.5/napari-dv-0.2.5.tar.gz

---
+++ packages/napari-dv/napari-dv-0.2.5.tar.gz/napari-dv-0.2.5/napari_dv.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.2.5"
+
+from napari_plugin_engine import napari_hook_implementation
+
+
+def dv_reader(path: str):
+    from numpy import memmap
+    import mrc
+
+    mfile = mrc.Mrc(path)
+    nWaves = mfile.header.NumWaves
+    channel_axis = mfile.axisOrderStr().find("w") if nWaves > 1 else None
+    dx = mfile.header.d[0]
+    dz = mfile.header.d[2]
+    metadata = {
+        name: getattr(mfile.hdr, name)
+        for name in mrc.mrc.mrcHdrNames
+        if not name.startswith("_")
+    }
+    metadata = {
+        k: v.tolist() if isinstance(metadata["mst"], memmap) else v
+        for k, v in metadata.items()
+    }
+    if nWaves == 1:
+        contrast_limits = list(mfile.header.mmm1[:2])
+        name = f"{mfile.hdr.wave[0]} nm"
+    else:
+        contrast_limits = []
+        name = []
+        for c in range(nWaves):
+            key = f"mmm{c+1}" if c == 0 else f"mm{c+1}"
+            contrast_limits.append(list(getattr(mfile.header, key)[:2]))
+            name.append(f"{mfile.hdr.wave[c]} nm")
+    params = {
+        "channel_axis": channel_axis,
+        "scale": [dz / dx, 1, 1],
+        "contrast_limits": contrast_limits,
+        "name": name,
+        "metadata": metadata,
+    }
+    return [(mfile.data, params)]
+
+
+@napari_hook_implementation
+def napari_get_reader(path):
+    if isinstance(path, str) and path.endswith((".dv", ".mrc")):
+        return dv_reader
