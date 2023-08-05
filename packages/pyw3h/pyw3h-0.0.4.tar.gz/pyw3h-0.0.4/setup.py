534fc366a44a550d8cd4d48ec4a415227ed58b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:41 +0000

    Add package pyw3h/0.0.4/pyw3h-0.0.4.tar.gz

---
+++ packages/pyw3h/pyw3h-0.0.4.tar.gz/pyw3h-0.0.4/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pyw3h",  # Replace with your own username
+    version="0.0.4",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.9',
+)
