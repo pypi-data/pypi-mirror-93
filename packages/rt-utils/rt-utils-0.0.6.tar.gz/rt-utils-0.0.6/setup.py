dbfc3a67106abebd4d4f195ffbf16a8ed34a67ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:12 +0000

    Add package rt-utils/0.0.6/rt-utils-0.0.6.tar.gz

---
+++ packages/rt-utils/rt-utils-0.0.6.tar.gz/rt-utils-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+VERSION = '0.0.6'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="rt-utils", 
+    version=VERSION,
+    author="Asim Shrestha",
+    author_email="asim.shrestha@hotmail.com",
+    description="A small library for handling masks and RT-Structs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qurit/rtutils",
+    packages=setuptools.find_packages(),
+    keywords=["RTStruct", "Dicom", "Pydicom"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+)
