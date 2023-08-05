165da3b66a9bdf6015950ecfd4ac0c067c0f6953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:26 +0000

    Add package vasil/0.0.1/vasil-0.0.1.tar.gz

---
+++ packages/vasil/vasil-0.0.1.tar.gz/vasil-0.0.1/README.txt
@@ -0,0 +1,3 @@
+This is the best something you have ever seen
+Call me plz if you would rather prefer to give me a blowjob instead of programming this shit
+My number: +380 939 027 215
---
+++ packages/vasil/vasil-0.0.1.tar.gz/vasil-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='vasil',
+  version='0.0.1',
+  description='A very basic pussy',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Grigoriy Skovoroda',
+  author_email='SuckME@g.org',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='vasil', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
