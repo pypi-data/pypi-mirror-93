2be473a11ec3350c4a5a063bb758558db6ba9ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:32 +0000

    Add package eric-tools/1.1.1.2/Eric_Tools-1.1.1.2-py2-none-any.whl

---
+++ packages/eric-tools/Eric_Tools-1.1.1.2-py2-none-any.whl/commons/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+'''
+@Author  : Eric
+@Time    : 2020-11-30 17:31
+@IDE     : PyCharm
+'''
+name = 'Eric-Tools'
+__version__ = "1.1.1.2"
+__author__ = 'Eric'
+__doc__ = ["Python Daily Development Tools"]
+
+from . import exception_class
+from . import resize_image
+from . import test
+from . import logger,ip
+from . import encryption_classmethod
+from . import pgsql
+from . import sftp
