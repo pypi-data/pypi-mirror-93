cc740fb14b0ac32d4c4532602922fb4375ed094b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:08 +0000

    Add package jsonut/0.1.0/jsonut-0.1.0.tar.gz

---
+++ packages/jsonut/jsonut-0.1.0.tar.gz/jsonut-0.1.0/tests/__init__.py
@@ -0,0 +1,8 @@
+# noinspection SpellCheckingInspection
+__author__ = 'wookjae.jo'
+
+import os
+import sys
+
+basedir = os.path.abspath(os.path.dirname(__file__), )
+sys.path.append(os.path.join(basedir, os.pardir, 'jsonut'))
