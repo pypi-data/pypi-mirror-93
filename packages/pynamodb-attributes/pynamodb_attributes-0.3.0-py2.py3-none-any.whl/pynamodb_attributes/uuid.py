2638e6028b58950cb62847685ef2f8fd0cfd3495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:21 +0000

    Add package pynamodb-attributes/0.3.0/pynamodb_attributes-0.3.0-py2.py3-none-any.whl

---
+++ packages/pynamodb-attributes/pynamodb_attributes-0.3.0-py2.py3-none-any.whl/pynamodb_attributes/uuid.py
@@ -0,0 +1,34 @@
+from typing import Any
+from uuid import UUID
+
+import pynamodb.constants
+from pynamodb.attributes import Attribute
+
+
+class UUIDAttribute(Attribute[UUID]):
+    """
+    PynamoDB attribute to for UUIDs. These are backed by DynamoDB unicode (`S`) types.
+    """
+
+    attr_type = pynamodb.constants.STRING
+
+    def __init__(self, remove_dashes: bool = False, **kwargs: Any) -> None:
+        """
+        Initializes a UUIDAttribute object.
+
+        :param remove_dashes: if set, the string serialization will be without dashes.
+                              Defaults to False.
+        """
+        super().__init__(**kwargs)
+        self._remove_dashes = remove_dashes
+
+    def serialize(self, value: UUID) -> str:
+        result = str(value)
+
+        if self._remove_dashes:
+            result = result.replace('-', '')
+
+        return result
+
+    def deserialize(self, value: str) -> UUID:
+        return UUID(value)
