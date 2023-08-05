90bcc1e749d70ed001b4f57a5f3d362b2ec578e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package soco-cli/0.3.15/soco-cli-0.3.15.tar.gz

---
+++ packages/soco-cli/soco-cli-0.3.15.tar.gz/soco-cli-0.3.15/soco_cli/keystroke_capture.py
@@ -0,0 +1,26 @@
+"""Captures single keystrokes"""
+
+import sys
+
+# termios is not available on Windows
+try:
+    import termios
+
+except ImportError:
+    pass
+
+
+def get_keystroke():
+    result = None
+    fd = sys.stdin.fileno()
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    try:
+        result = sys.stdin.read(1)
+    except IOError:
+        pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    return result
