89623e72c1cdbe995e3ab9c515e6ab0fd7073c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:08 +0000

    Add package vantage6-server/1.2.3.post1/vantage6_server-1.2.3.post1-py3-none-any.whl

---
+++ packages/vantage6-server/vantage6_server-1.2.3.post1-py3-none-any.whl/vantage6/server/model/user.py
@@ -0,0 +1,73 @@
+import bcrypt
+
+from sqlalchemy import Column, String, Integer, ForeignKey, exists
+from sqlalchemy.orm import relationship
+
+from .base import Database
+from .authenticable import Authenticatable
+
+
+class User(Authenticatable):
+    """User (person) that can access the system.
+
+    Users always belong to an organization and can have certain
+    rights within an organization.
+    """
+    _hidden_attributes = ['password']
+
+    # overwrite id with linked id to the authenticatable
+    id = Column(Integer, ForeignKey('authenticatable.id'), primary_key=True)
+    __mapper_args__ = {
+        'polymorphic_identity':'user',
+    }
+
+    # fields
+    username = Column(String, unique=True)
+    password = Column(String)
+    firstname = Column(String)
+    lastname = Column(String)
+    email = Column(String, unique=True)
+    roles = Column(String)
+    organization_id = Column(Integer, ForeignKey("organization.id"))
+
+    # relationships
+    organization = relationship("Organization", back_populates="users")
+
+    def __repr__(self):
+        organization = self.organization.name if self.organization else "None"
+        return (
+            f"<User "
+            f"id={self.id}, username='{self.username}', roles='{self.roles}', "
+            f"organization='{organization}'"
+            f">"
+        )
+
+    def set_password(self, pw):
+        pwhash = bcrypt.hashpw(pw.encode('utf8'), bcrypt.gensalt())
+        self.password = pwhash.decode('utf8')
+
+    def check_password(self, pw):
+        if self.password is not None:
+            expected_hash = self.password.encode('utf8')
+            return bcrypt.checkpw(pw.encode('utf8'), expected_hash)
+        return False
+
+    @classmethod
+    def get_by_username(cls, username):
+        session = Database().Session
+        return session.query(cls).filter_by(username=username).one()
+
+    @classmethod
+    def get_by_email(cls, email):
+        session = Database().Session
+        return session.query(cls).filter_by(email=email).one()
+
+    @classmethod
+    def get_user_list(cls, filters=None):
+        session = Database().Session
+        return session.query(cls).all()
+
+    @classmethod
+    def username_exists(cls, username):
+        session = Database().Session
+        return session.query(exists().where(cls.username == username)).scalar()
---
+++ packages/vantage6-server/vantage6_server-1.2.3.post1-py3-none-any.whl/vantage6/server/resource/user.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+Resources below '/<api_base>/user'
+"""
+from __future__ import print_function, unicode_literals
+
+import logging
+
+from flask_restful import Resource
+from flask import g
+from flask_restful import reqparse
+from http import HTTPStatus
+from flasgger import swag_from
+from pathlib import Path
+
+import sqlalchemy.exc
+
+from vantage6.server import db
+from vantage6.server.resource import with_user, only_for
+from vantage6.server.resource._schema import UserSchema
+
+module_name = __name__.split('.')[-1]
+log = logging.getLogger(module_name)
+
+
+def setup(api, API_BASE):
+    module_name = __name__.split('.')[-1]
+    path = "/".join([API_BASE, module_name])
+    log.info('Setting up "{}" and subdirectories'.format(path))
+
+    api.add_resource(
+        User,
+        path,
+        endpoint='user_without_id',
+        methods=('GET', 'POST')
+    )
+    api.add_resource(
+        User,
+        path + '/<int:id>',
+        endpoint='user_with_id',
+        methods=('GET', 'PATCH', 'DELETE')
+    )
+
+
+# ------------------------------------------------------------------------------
+# Resources / API's
+# ------------------------------------------------------------------------------
+class User(Resource):
+
+    user_schema = UserSchema()
+
+    @only_for(['user'])
+    @swag_from(str(Path(r"swagger/get_user_with_id.yaml")), endpoint='user_with_id')
+    @swag_from(str(Path(r"swagger/get_user_without_id.yaml")), endpoint='user_without_id')
+    def get(self, id=None):
+        """Return user details."""
+        retval = db.User.get(id)
+        is_root = g.user.username == "root"
+
+        if id is None:
+            # Only root can retrieve all users at once
+            if is_root:
+                return self.user_schema.dump(retval, many=True)
+
+            # Everyone else can only list the users from their own organization
+            else:
+                org_id = g.user.organization_id
+                filtered = [u for u in retval if u.organization_id == org_id]
+                return self.user_schema.dump(filtered, many=True)
+
+        if retval:
+            # You either have to be root or someone from the same organization
+            if is_root or (retval.organization_id == g.user.organization_id):
+                return self.user_schema.dump(retval, many=False)
+
+        return {"msg": f"user id {id} is not found"}, HTTPStatus.NOT_FOUND
+
+    @with_user
+    @swag_from(str(Path(r"swagger/post_user_without_id.yaml")), endpoint='user_without_id')
+    def post(self):
+        """Create a new User."""
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("username", type=str, required=True, help="This field is required")
+        parser.add_argument("firstname", type=str, required=True, help="This field is required")
+        parser.add_argument("lastname", type=str, required=True, help="This field is required")
+        parser.add_argument("roles", type=str, required=True, help="This field is required")
+        parser.add_argument("password", type=str, required=True, help="This field is required")
+        parser.add_argument("organization_id", type=int, required=False, help="This is only used if you're root")
+        parser.add_argument("email", type=str, required=True, help="This field is required")
+        data = parser.parse_args()
+
+        if db.User.username_exists(data["username"]):
+            return {"msg": "username already exists"}, HTTPStatus.BAD_REQUEST
+
+        if data["username"] == 'root':
+            msg = {"msg": "You're funny! You can't create root!?"}
+            return msg, HTTPStatus.BAD_REQUEST
+
+        roles = data['roles'].split(',')
+        if 'root' in roles:
+            msg = {"msg": "You're funny! You can't assign the role 'root'!?"}
+            return msg, HTTPStatus.BAD_REQUEST
+
+        if g.user.username == 'root':
+            organization_id = data['organization_id']
+            log.warn(f'Running as root and creating user for organization_id={organization_id}')
+        else:
+            organization_id = g.user.organization_id
+            log.warn(f'Creating user for organization_id={organization_id}')
+
+        # Ok, looks like we got most of the security hazards out of the way
+        user = db.User(
+            username=data["username"],
+            firstname=data["firstname"],
+            lastname=data["lastname"],
+            roles=data["roles"],
+            organization_id=organization_id,
+            email=data["email"]
+        )
+
+        user.set_password(data["password"])
+        user.save()
+
+        return self.user_schema.dump(user), HTTPStatus.CREATED
+
+    @with_user
+    @swag_from(str(Path(r"swagger/patch_user_with_id.yaml")), endpoint='user_with_id')
+    def patch(self, id):
+
+        user = db.User.get(id)
+
+        if not user:
+            return {"msg": "user id={} not found".format(id)}, HTTPStatus.NOT_FOUND
+
+        is_root = g.user.username == 'root'
+        if (user.organization_id != g.user.organization_id) and not is_root:
+            return {"msg": f"No permission to modify user_id={id}"}, \
+                HTTPStatus.FORBIDDEN
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("username", type=str, required=False, help="This field is required")
+        parser.add_argument("firstname", type=str, required=False, help="This field is required")
+        parser.add_argument("lastname", type=str, required=False, help="This field is required")
+        parser.add_argument("roles", type=str, required=False, help="This field is required")
+        parser.add_argument("password", type=str, required=False, help="This field is required")
+        parser.add_argument("organization_id", type=int, required=False, help="This is only used if you're root")
+        data = parser.parse_args()
+
+        # Username cannot be changed once set?
+        # if data["username"]:
+        #     user.username = data["username"]
+        if data["firstname"]:
+            user.firstname = data["firstname"]
+        if data["lastname"]:
+            user.lastname = data["lastname"]
+        if data["password"]:
+            user.set_password(data["password"])
+        if data["roles"]:
+            user.roles = data["roles"]
+        if data["organization_id"]:
+            if is_root:
+                user.organization_id = data["organization_id"]
+                log.warn(f'Running as root and assigning (new) organization_id={data["organization_id"]}')
+            else:
+                log.error('Current user cannot assign new organizations!')
+
+        try:
+            user.save()
+        except sqlalchemy.exc.IntegrityError as e:
+            log.error(e)
+            user.session.rollback()
+
+        return user, HTTPStatus.OK
+
+    @with_user
+    @swag_from(str(Path(r"swagger/delete_user_with_id.yaml")), endpoint='user_with_id')
+    def delete(self, id):
+
+        user = db.User.get(id)
+        if not user:
+            return {"msg": "user id={} not found".format(id)}, HTTPStatus.NOT_FOUND
+        is_root = g.user.username == 'root'
+        if user.organization_id != g.user.organization_id and not is_root:
+            log.warning(
+                "user {} has tried to delete user {} but does not have the required permissions".format(
+                    g.user.id,
+                    user.id
+                )
+            )
+            return {"msg": "you do not have permission to modify user id={}".format(id)}, HTTPStatus.FORBIDDEN
+
+        user.delete()
+        log.info("user id={} is removed from the database".format(id))
+        return {"msg": "user id={} is removed from the database".format(id)}, HTTPStatus.OK
