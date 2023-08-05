f38fcf39fd1d7a56264a3a20507759db22e76da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:27 +0000

    Add package pyshine/0.0.7/pyshine-0.0.7-py3-none-any.whl

---
+++ packages/pyshine/pyshine-0.0.7-py3-none-any.whl/pyshine/__init__.py
@@ -0,0 +1,11 @@
+# author:	PyShine
+# website:	http://www.pyshine.com
+
+# set the version number
+__version__= "0.0.7"
+
+# import the necessary packages
+from .convenience import putBText
+from .convenience import audioCapture
+from .convenience import showPlot
+from .convenience import RPSNET
