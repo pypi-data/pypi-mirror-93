1a0246c232402658f94897e0ed91c80faeb8a636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:01 +0000

    Add package allennlp-dataframe-mapper/0.1.0/allennlp_dataframe_mapper-0.1.0-py3-none-any.whl

---
+++ packages/allennlp-dataframe-mapper/allennlp_dataframe_mapper-0.1.0-py3-none-any.whl/allennlp_dataframe_mapper/transforms/__init__.py
@@ -0,0 +1,8 @@
+from allennlp_dataframe_mapper.transforms.base import RegistrableTransform  # NOQA
+from allennlp_dataframe_mapper.transforms.preprocessing import (  # NOQA
+    FlattenTransformer,
+    LabelEncoder,
+    Logarithmer,
+    MinMaxScaler,
+    StandardScaler,
+)
