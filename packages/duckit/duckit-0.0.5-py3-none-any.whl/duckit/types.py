4677850be3121411d671130ca6887b7513e0ee60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:37 +0000

    Add package duckit/0.0.5/duckit-0.0.5-py3-none-any.whl

---
+++ packages/duckit/duckit-0.0.5-py3-none-any.whl/duckit/types.py
@@ -0,0 +1,23 @@
+
+# https://duckdb.org/docs/sql/data_types/overview
+SUPPORTED_TYPES = {
+    'BIGINT': {'INT8', 'LONG'},
+    'BOOLEAN': {'BOOL', 'LOGICAL'},
+    'BLOB': {'BYTEA', 'BINARY', 'VARBINARY'},
+    'DATE': {},
+    'DOUBLE': {'FLOAT8', 'NUMERIC', 'DECIMAL'},
+    'HUGEINT': {},
+    'INTEGER': {'INT4', 'INT', 'SIGNED'},
+    'REAL': {'FLOAT4', 'FLOAT'},
+    'SMALLINT': {'INT2', 'SHORT'},
+    'TIMESTAMP': {'DATETIME'},
+    'TINYINT': {'INT1'},
+    'VARCHAR': {'CHAR', 'BPCHAR', 'TEXT', 'STRING'},
+}
+
+ALL_VALID_TYPES = set()
+for k, e in SUPPORTED_TYPES.items():
+    ALL_VALID_TYPES.add(k)
+    for e2 in e:
+        ALL_VALID_TYPES.add(e2)
+
