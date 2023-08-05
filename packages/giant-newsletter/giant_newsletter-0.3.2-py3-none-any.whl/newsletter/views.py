ec5e3d13bb85ed4940d12ed086c8899d78c3222f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:00 +0000

    Add package giant-newsletter/0.3.2/giant_newsletter-0.3.2-py3-none-any.whl

---
+++ packages/giant-newsletter/giant_newsletter-0.3.2-py3-none-any.whl/newsletter/views.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+from django.conf import settings
+from django.shortcuts import redirect, render
+
+from . import forms
+
+
+def index(request):
+    """
+    Index view for newsletter app
+    """
+    form = forms.SubscriptionForm(data=request.POST or None)
+
+    success_url = getattr(settings, "NEWSLETTER_SUCCESS_URL", "newsletter:index")
+    template_name = "newsletter/index.html"
+
+    if form.is_valid():
+        form.save()
+        return redirect(reverse(success_url) + "?thanks")
+
+    return render(
+        request, template_name, {"form": form, "thanks": "thanks" in request.GET},
+    )
