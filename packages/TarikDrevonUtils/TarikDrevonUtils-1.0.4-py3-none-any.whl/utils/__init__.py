c50ca4eb671a4f65e75e9e1e4c064d7aae3ae837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:57 +0000

    Add package TarikDrevonUtils/1.0.4/TarikDrevonUtils-1.0.4-py3-none-any.whl

---
+++ packages/TarikDrevonUtils/TarikDrevonUtils-1.0.4-py3-none-any.whl/utils/__init__.py
@@ -0,0 +1,20 @@
+#print('importing...')
+
+# py standard library
+#from math import*
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.special as spe
+import pandas as pd
+
+
+# specific libraries
+from . import displayStandards as dsp
+from . import regionManager as regMg
+from . import materialManager as matMg
+from . import newtonRaphson as nwt
+from . import bissection as bis
+from . import commonInputOutput as iostd
+from . import physicsConstants as cst
+from . import glob_colors  as colors
+from .glob_colors import *
