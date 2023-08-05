8bc0223f51eadea74f2398c7676348126d62175d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:54 +0000

    Add package apkid/2.1.2/apkid-2.1.2.tar.gz

---
+++ packages/apkid/apkid-2.1.2.tar.gz/apkid-2.1.2/apkid/rules.py
@@ -0,0 +1,77 @@
+"""
+ Copyright (C) 2021  RedNaga. https://rednaga.io
+ All rights reserved. Contact: rednaga@protonmail.com
+
+
+ This file is part of APKiD
+
+
+ Commercial License Usage
+ ------------------------
+ Licensees holding valid commercial APKiD licenses may use this file
+ in accordance with the commercial license agreement provided with the
+ Software or, alternatively, in accordance with the terms contained in
+ a written agreement between you and RedNaga.
+
+
+ GNU General Public License Usage
+ --------------------------------
+ Alternatively, this file may be used under the terms of the GNU General
+ Public License version 3.0 as published by the Free Software Foundation
+ and appearing in the file LICENSE.GPL included in the packaging of this
+ file. Please visit http://www.gnu.org/copyleft/gpl.html and review the
+ information to ensure the GNU General Public License version 3.0
+ requirements will be met.
+"""
+
+import hashlib
+import os
+from typing import Dict
+from typing import Optional
+
+import yara
+
+
+class RulesManager(object):
+    def __init__(self, rules_dir=None, rules_ext='.yara'):
+        if not rules_dir:
+            rules_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rules')
+        self.rules_dir: str = rules_dir
+        self.rules_path: str = os.path.join(self.rules_dir, 'rules.yarc')
+        self.rules_ext: str = rules_ext
+        self.rules: Optional[yara.Rules] = None
+        self.rules_hash: Optional[str] = None
+
+    def load(self) -> yara.Rules:
+        self.rules = yara.load(self.rules_path)
+        return self.rules
+
+    def _collect_yara_files(self) -> Dict[str, str]:
+        files = {}
+        for root, dirnames, filenames in os.walk(self.rules_dir):
+            for filename in filenames:
+                if not filename.lower().endswith(self.rules_ext):
+                    continue
+                path = os.path.join(root, filename)
+                files[path] = path
+        return files
+
+    def compile(self) -> yara.Rules:
+        yara_files = self._collect_yara_files()
+        self.rules = yara.compile(filepaths=yara_files)
+        return self.rules
+
+    def save(self) -> int:
+        self.rules.save(self.rules_path)
+        rules_count = len(set([r.identifier for r in self.rules]))
+        return rules_count
+
+    @property
+    def hash(self) -> str:
+        if not self.rules_hash:
+            h = hashlib.sha256()
+            for file_path in self._collect_yara_files():
+                with open(file_path, 'rb') as f:
+                    h.update(f.read())
+            self.rules_hash = h.hexdigest()
+        return self.rules_hash
---
+++ packages/apkid/apkid-2.1.2.tar.gz/apkid-2.1.2/setup.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+"""
+ Copyright (C) 2021  RedNaga. https://rednaga.io
+ All rights reserved. Contact: rednaga@protonmail.com
+
+
+ This file is part of APKiD
+
+
+ Commercial License Usage
+ ------------------------
+ Licensees holding valid commercial APKiD licenses may use this file
+ in accordance with the commercial license agreement provided with the
+ Software or, alternatively, in accordance with the terms contained in
+ a written agreement between you and RedNaga.
+
+
+ GNU General Public License Usage
+ --------------------------------
+ Alternatively, this file may be used under the terms of the GNU General
+ Public License version 3.0 as published by the Free Software Foundation
+ and appearing in the file LICENSE.GPL included in the packaging of this
+ file. Please visit http://www.gnu.org/copyleft/gpl.html and review the
+ information to ensure the GNU General Public License version 3.0
+ requirements will be met.
+"""
+
+from codecs import open
+from os import path, walk
+
+from setuptools import setup, find_packages
+
+import apkid
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def package_files(directory):
+    paths = []
+    for (filepath, directories, filenames) in walk(directory):
+        for filename in filenames:
+            paths.append(path.join(filepath, filename))
+    return paths
+
+
+install_requires = [
+    'yara-python-dex>=1.0.0',
+]
+
+dev_requires = [
+    'mypy',
+    'pypandoc',
+    'twine',
+]
+
+test_requires = [
+    'delayed-assert',
+    'factory_boy',
+    'mock',
+    'pytest',
+    'pytest-cov',
+    'pytest-factoryboy',
+    'pytest-flask',
+    'pytest-runner',
+    'tox',
+]
+
+setup(
+    name=apkid.__title__,
+    version=apkid.__version__,
+    description="Android Package Identifier",
+    long_description=long_description,
+    url='https://github.com/rednaga/APKiD',
+    author=apkid.__author__,
+    author_email='rednaga@protonmail.com',
+    license=apkid.__license__,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Security',
+        'Topic :: Utilities',
+    ],
+    keywords='android analysis reversing malware apk dex dalvik',
+    packages=find_packages(exclude=['docs', 'tests']),
+    package_data={
+        'rules': package_files('apkid/rules/'),
+    },
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        'dev': dev_requires,
+        'test': test_requires,
+    },
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'apkid=apkid.main:main',
+        ],
+    },
+)
