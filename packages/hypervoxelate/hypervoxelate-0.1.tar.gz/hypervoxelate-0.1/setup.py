67d3368cb13a3bc7eee21194c98ad11c9530247f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:29 +0000

    Add package hypervoxelate/0.1/hypervoxelate-0.1.tar.gz

---
+++ packages/hypervoxelate/hypervoxelate-0.1.tar.gz/hypervoxelate-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hypervoxelate',
+      version='0.1',
+      description='Simple hypervoxelation procedure w/ support for jagged cut-points',
+      url='http://github.com/califynic/hypervoxelate',
+      author='Ali Cy',
+      author_email='califynic@gmail.com',
+      license='MIT',
+      packages=['hypervoxelate'],
+      zip_safe=False)
