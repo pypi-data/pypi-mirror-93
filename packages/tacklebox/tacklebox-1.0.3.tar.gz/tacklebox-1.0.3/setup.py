eb94d2ceda157d05c2ba6728aa8d4926d814ef98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:51 +0000

    Add package tacklebox/1.0.3/tacklebox-1.0.3.tar.gz

---
+++ packages/tacklebox/tacklebox-1.0.3.tar.gz/tacklebox-1.0.3/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='tacklebox',
+    version='1.0.3',
+    description='Improved handling of PyTorch module hooks',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/IsaacRe/tacklebox',
+    author='Isaac Rehg',
+    author_email='isaacrehg@gmail.com',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=False,
+    install_requires=[
+        'torch',
+        'tqdm',
+    ],
+)
