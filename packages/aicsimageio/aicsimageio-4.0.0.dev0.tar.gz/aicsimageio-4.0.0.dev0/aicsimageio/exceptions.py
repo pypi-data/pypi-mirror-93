8e753dc631a12f0ee4b503b7e2eaa065b2225830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package aicsimageio/4.0.0.dev0/aicsimageio-4.0.0.dev0.tar.gz

---
+++ packages/aicsimageio/aicsimageio-4.0.0.dev0.tar.gz/aicsimageio-4.0.0.dev0/aicsimageio/exceptions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class ConflictingArgumentsError(Exception):
+    """
+    This exception is returned when 2 arguments to the same function are in conflict.
+    """
+
+    pass
+
+
+class InvalidDimensionOrderingError(Exception):
+    """
+    A general exception that can be thrown when handling dimension ordering or
+    validation. Should be provied a message for the user to be given more context.
+    """
+
+    pass
+
+
+class UnsupportedFileFormatError(Exception):
+    """
+    This exception is intended to communicate that the file extension is not one of
+    the supported file types and cannot be parsed with AICSImage.
+    """
+
+    def __init__(self, reader_name: str, path: str, **kwargs):
+        super().__init__(**kwargs)
+        self.reader_name = reader_name
+        self.path = path
+
+    def __str__(self):
+        return f"{self.reader_name} does not support the image: '{self.path}'."
