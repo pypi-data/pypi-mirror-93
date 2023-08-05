8a5a2c92ff1deb9e62836e519e74a91432e8986f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:19 +0000

    Add package brt-classifier/0.1.3/brt_classifier-0.1.3-py3-none-any.whl

---
+++ packages/brt-classifier/brt_classifier-0.1.3-py3-none-any.whl/brt_classifier/postprocess/findLogLowerBounds.py
@@ -0,0 +1,13 @@
+def findLogLowerBounds(problem, idx, data):
+  constrs = problem["constraint"]
+  lowerBound = problem["lower-bound"]
+
+  if lowerBound == "":
+    children = [x[0] + x[2] for x in constrs]
+    if len(children) == len(set(children)): # children uniquely determnine parents
+      print(constrs)
+      data[idx]["lower-bound"] = "(log n)"
+      return 1
+
+  return 0
+
