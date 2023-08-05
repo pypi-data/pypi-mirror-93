d323bc24a7bee99cbcb3956481dbc0a9f9785df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:39 +0000

    Add package AidlabSDK/1.3.16/AidlabSDK-1.3.16-py3-none-any.whl

---
+++ packages/AidlabSDK/AidlabSDK-1.3.16-py3-none-any.whl/Aidlab/AidlabCharacteristicsUUID.py
@@ -0,0 +1,22 @@
+#
+# AidlabCharacteristicsUUID.py
+# Aidlab-SDK
+# Created by Szymon Gesicki on 10.05.2020.
+#
+
+import sys
+
+class AidlabCharacteristicsUUID:
+
+    temperatureUUID =        {"uuid": "45366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 25}
+    ecgUUID =                {"uuid": "46366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 16}
+    batteryUUID =            {"uuid": "47366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 22}
+    respirationUUID =        {"uuid": "48366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 19}
+    motionUUID =             {"uuid": "49366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 58}
+    activityUUID =           {"uuid": "61366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 49}
+    stepsUUID =              {"uuid": "62366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 52}
+    orientationUUID =        {"uuid": "63366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 55}
+    soundVolumeUUID =        {"uuid": "52366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 31}
+    cmdUUID =                {"uuid": "51366e80-cf3a-11e1-9ab4-0002a5d5c51b", "handle": 13}
+    heartRateUUID =          {"uuid": "00002a37-0000-1000-8000-00805f9b34fb", "handle": 45}
+    healthThermometerUUID =  {"uuid": "00002a1c-0000-1000-8000-00805f9b34fb", "handle": 63}
