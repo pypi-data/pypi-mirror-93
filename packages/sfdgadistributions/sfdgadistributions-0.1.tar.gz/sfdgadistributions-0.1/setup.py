089114bf9f6102ba869f6037851b3b2c50ed31c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:39 +0000

    Add package sfdgadistributions/0.1/sfdgadistributions-0.1.tar.gz

---
+++ packages/sfdgadistributions/sfdgadistributions-0.1.tar.gz/sfdgadistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sfdgadistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sfdgadistributions'],
+      zip_safe=False)
