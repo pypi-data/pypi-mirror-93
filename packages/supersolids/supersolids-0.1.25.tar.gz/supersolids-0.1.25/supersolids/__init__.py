cfa23f6d90ee6bd628bf091bb07b51788044868e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:14 +0000

    Add package supersolids/0.1.25/supersolids-0.1.25.tar.gz

---
+++ packages/supersolids/supersolids-0.1.25.tar.gz/supersolids-0.1.25/supersolids/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+__all__ = ["Animation",
+           "constants",
+           "density_in_trap",
+           "functions",
+           "run_time",
+           "Schroedinger",
+           ]
