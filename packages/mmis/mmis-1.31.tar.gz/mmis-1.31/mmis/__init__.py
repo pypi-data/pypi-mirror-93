c64bd3c4e8bdfb80733347ebdc510444720f7e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:16 +0000

    Add package mmis/1.31/mmis-1.31.tar.gz

---
+++ packages/mmis/mmis-1.31.tar.gz/mmis-1.31/mmis/__init__.py
@@ -0,0 +1,14 @@
+'''
+mmis is a package to control the Modular Microscope Instrument of TU Delft
+'''
+
+import mmis.Functions
+import mmis.DebugFunctions
+import mmis.RealTimeClock
+import mmis.Clock
+import mmis.Softwareupdate
+
+import mmis.SampleHeater
+import mmis.DualHeater
+import mmis.LiqN2Control
+import mmis.GUI
