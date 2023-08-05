f5dfc15aa6cdc7480b0386320545a7de2c82899a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:45 +0000

    Add package genhr/0.1.1/genhr-0.1.1.tar.gz

---
+++ packages/genhr/genhr-0.1.1.tar.gz/genhr-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['genhr']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'genhr',
+    'version': '0.1.1',
+    'description': 'Create zipped test files to upload to HackerRank',
+    'long_description': None,
+    'author': 'Thomas Breydo',
+    'author_email': 'tbreydo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
