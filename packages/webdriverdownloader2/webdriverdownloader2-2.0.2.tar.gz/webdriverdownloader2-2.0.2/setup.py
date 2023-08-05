10b9b2bd3b338443d787d7f8df3a3633afbf76e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:23 +0000

    Add package webdriverdownloader2/2.0.2/webdriverdownloader2-2.0.2.tar.gz

---
+++ packages/webdriverdownloader2/webdriverdownloader2-2.0.2.tar.gz/webdriverdownloader2-2.0.2/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="webdriverdownloader2",
+    version="2.0.2",
+    description="Python module and CLI tool for facilitating download and deploy of WebDriver binaries.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Quality Assurance",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+    ],
+    url="https://github.com/bodharma/webdriverdownloader2",
+    authors="Leonides T. Saguisag Jr.",
+    author_email="leonidessaguisagjr@gmail.com",
+    maintainer="Bohdan Lesiv",
+    maintainer_email="boghdanlesiv@gmail.com",
+    license="MIT",
+    packages=["webdriverdownloader2"],
+    install_requires=["beautifulsoup4", "requests", "tqdm", "loguru", "py-cpuinfo"],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "webdriverdownloader2 = webdriverdownloader2.cli:main",
+        ],
+    },
+)
---
+++ packages/webdriverdownloader2/webdriverdownloader2-2.0.2.tar.gz/webdriverdownloader2-2.0.2/webdriverdownloader2/util.py
@@ -0,0 +1,14 @@
+"""
+Module for utility functions.
+
+This code is released under the MIT license.
+"""
+from cpuinfo import get_cpu_info
+
+
+def get_architecture_bitness() -> str:
+    """
+    Function for getting the "bitness" of the underlying architecture i.e. if it is 32-bit or 64-bit.
+    :return: The bitness of the underlying architecture, as a str i.e. "32" or "64"
+    """
+    return str(get_cpu_info()["bits"])
