24f68b336369f7230ae26b0e31f0e7ab2b48b5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:13 +0000

    Add package git-annex-remote-googledrive/1.3.0/git-annex-remote-googledrive-1.3.0.tar.gz

---
+++ packages/git-annex-remote-googledrive/git-annex-remote-googledrive-1.3.0.tar.gz/git-annex-remote-googledrive-1.3.0/git_annex_remote_googledrive/__init__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2017-2020  Silvio Ankermann
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of version 3 of the GNU
+# General Public License as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+_default_client_id = '275666578511-ndjt6mkns3vgb60cbo7csrjn6mbh8gbf.apps.googleusercontent.com'
+_default_client_secret = 'Den2tu08pRU4s5KeCp5whas_'
---
+++ packages/git-annex-remote-googledrive/git-annex-remote-googledrive-1.3.0.tar.gz/git-annex-remote-googledrive-1.3.0/setup.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# git-annex-remote-googledrive Setup
+# Copyright (C) 2017 Silvio Ankermann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as published by
+# the Free Software Foundation
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+from setuptools import setup, find_packages
+from codecs import open
+import os, tempfile
+
+import versioneer
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='git-annex-remote-googledrive',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['git_annex_remote_googledrive'],
+    description='git annex special remote for Google Drive',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Lykos153/git-annex-remote-googledrive',
+    author='Silvio Ankermann',
+    author_email='silvio@booq.org',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='git-annex remote googledrive',
+    entry_points = {
+        'console_scripts': ['git-annex-remote-googledrive=git_annex_remote_googledrive.run:main'],
+    },
+    install_requires=[
+          'annexremote',
+          'drivelib>=0.1.4',
+          'gitpython',
+          'tenacity',
+          'humanfriendly',
+      ],
+)
