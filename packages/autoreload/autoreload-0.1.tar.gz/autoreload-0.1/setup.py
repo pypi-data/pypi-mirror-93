701c65e31096ae0727e647baea6060d8523450ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:36 +0000

    Add package autoreload/0.1/autoreload-0.1.tar.gz

---
+++ packages/autoreload/autoreload-0.1.tar.gz/autoreload-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='autoreload',
+    packages=['autoreload'],
+    version='0.1',
+    license='MIT',
+    description='Watch your code live reload in the browser',
+    author='CF8',
+    author_email='hburt2003@gmail.com',
+    url='https://github.com/ChillFish8/autoreload',
+    download_url='https://github.com/ChillFish8/autoreload/archive/0.1.0.tar.gz',
+    keywords=['live', 'auto', 'reloading', 'web development', 'web'],
+    install_requires=[
+        'selenium',
+        'click',
+        'watchdog'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
