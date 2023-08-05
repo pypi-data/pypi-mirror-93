71a01cb43a5b6407c1f8a57c429a974d47b2dd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:56 +0000

    Add package kodland-sso-server/0.2.0/kodland_sso_server-0.2.0-py3-none-any.whl

---
+++ packages/kodland-sso-server/kodland_sso_server-0.2.0-py3-none-any.whl/sso_server/services/get_student_info.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from basics.models import User, Student
+
+
+class GetStudentInfo(object):
+	"""
+	получение данных о пользователе (студенте/преподавателе)
+	"""
+	def __init__(self, user_id: int):
+		try:
+			self.user = User.objects.get(pk=user_id)
+		except ObjectDoesNotExist:
+			self.user = None
+			
+	def get_student_info_by_method(self, method_name: str) -> dict:
+		result = {
+			'value': None
+		}
+		
+		if self.user:
+			try:
+				student = Student.objects.get(user=self.user)
+			except ObjectDoesNotExist:
+				pass
+			else:
+				try:
+					value = getattr(student, method_name)()
+					result.update({'value': value})
+				except AttributeError:
+					pass
+		
+		return result
