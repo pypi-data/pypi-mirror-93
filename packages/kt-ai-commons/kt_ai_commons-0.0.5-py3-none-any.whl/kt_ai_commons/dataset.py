9f232f315df318db9c6ed251075414609a43677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:41:27 +0000

    Add package kt-ai-commons/0.0.5/kt_ai_commons-0.0.5-py3-none-any.whl

---
+++ packages/kt-ai-commons/kt_ai_commons-0.0.5-py3-none-any.whl/kt_ai_commons/dataset.py
@@ -0,0 +1,16 @@
+import psycopg2
+import pandas as pd
+import pandas.io.sql as sqlio
+
+def onenavi_train(dataframe_name):
+    def data_loading():
+        conn = psycopg2.connect(host='211.253.10.76', dbname='postgres',user='postgres',password='commons1234!',port=5432)
+        cur=conn.cursor()
+        sql="""
+            SELECT *
+            FROM onenavi_train
+            ;
+            """
+        sqlio.read_sql_query(sql, conn)
+    dataframe_name=data_loading()
+    print("{}으로 onenavi_train 데이터 불러오기가 완료되었습니다.".format(dataframe_name))
