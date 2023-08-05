6d35e02220ab78d3ec4f53d79fcb646a3f7ee52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:08 +0000

    Add package cisco-anyconnect-cli/0.4/cisco-anyconnect-cli-0.4.tar.gz

---
+++ packages/cisco-anyconnect-cli/cisco-anyconnect-cli-0.4.tar.gz/cisco-anyconnect-cli-0.4/README.md
@@ -0,0 +1,72 @@
+[![PyPi Version Alt](https://badge.fury.io/py/cisco-anyconnect-cli.svg)](https://pypi.org/project/cisco-anyconnect-cli/)  
+
+# cisco-anyconnect-cli
+Cisco AnyConnect client command line with KeePass support
+
+# Installation
+KeePassHTTP Plugin is required
+vpncli.exe from Cisco AnyConnect Secure Mobility Client is required  
+`pip install cisco_anyconnect_cli`
+
+# Usage
+```
+Usage: anyconnect [OPTIONS] COMMAND [ARGS]...
+
+  Connect to Cisco AnyConnect VPN Gateway
+
+  I need vpncli.exe and will search in:
+
+  - Current working directory
+  - C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client
+  - C:\Program Files\Cisco\Cisco AnyConnect Secure Mobility Client"
+  - In -p/--path given to the command (as file or directory)
+  - In CISCO_ANYCONNECT_HOME environment variable
+  - Availability in PATH variable
+
+Options:
+  -p, --path PATH  Directory or path to vpncli.exe
+  -h, --help       Show this message and exit.
+
+Commands:
+  connect     Connect to a cisco vpn server
+  disconnect  Disconnect
+  state       Get connection status
+```
+
+## Connect
+User and password will be fetched from Keepass if no user is given
+```
+Usage: anyconnect connect [OPTIONS] URL
+
+  Endpoint address
+
+Options:
+  -u, --user TEXT                 Give username instead of KeePass lookup
+  -p, --password TEXT             Give password instead of KeePass lookup
+  --autorespond / --noautorespond
+                                  Defines whether connect will automatically
+                                  respond to login banners
+
+  --help                          Show this message and exit.
+```
+
+# Examples
+```
+Entry in KeePass must be named or have a configured URL equal vpn-server-url.
+$ anyconnect connect vpn.example.com
+
+Without KeePass 
+$ anyconnect connect vpn.example.com -u user -p pass
+
+$ anyconnect disconnect
+```
+
+# Changelog
+## v0.4
+- Added autorespond to login banners (thanks @ott-egs-plan)
+
+## v0.2
+- Added state command (thanks @GOST-UA)
+
+## v0.1
+- Initial version with basic features
---
+++ packages/cisco-anyconnect-cli/cisco-anyconnect-cli-0.4.tar.gz/cisco-anyconnect-cli-0.4/cisco_anyconnect_cli/cisco_anyconnect.py
@@ -0,0 +1,61 @@
+import logging
+import shutil
+from subprocess import Popen, PIPE, STDOUT
+import os
+import re
+
+
+class CiscoAnyConnect:
+
+    def __init__(self, path=None):
+        self.path = path
+        self.bin = self.detect_binary()
+        logging.info(f"Using {self.bin}")
+
+    def connect(self, url: str, user: str, password: str, autorespond: bool = False):
+        logging.info(f"Connecting to '{url}' as '{user}'")
+        proc = Popen([self.bin, "connect", url, "-s"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+        answer = "y\n" if autorespond else ""
+        stdout = proc.communicate(input=f"{user}\n{password}\n{answer}".encode())[0]
+        logging.debug(stdout.decode())
+        proc.wait()
+
+    def disconnect(self):
+        proc = Popen([self.bin, "disconnect"])
+        proc.communicate()
+
+    def state(self) -> str:
+        """
+        Get connection state. Return actual state or Unknown if result was unexpected
+        """
+        proc = Popen([self.bin, "state"], stdout=PIPE, stderr=STDOUT)
+        stdout = proc.communicate()[0].decode()
+        patt = re.compile(r".*state: (\w+)", re.MULTILINE)
+        res = re.findall(patt, stdout)
+
+        state = res[-1] if res else "Unknown"
+
+        logging.debug(state)
+        return state
+
+    def detect_binary(self):
+        executable = "vpncli.exe"
+        env_home = os.environ.get("CISCO_ANYCONNECT_HOME")
+        env_path = shutil.which(executable)
+
+        possible_paths = [
+            os.path.join(os.getcwd(), executable),
+            os.path.join("C:\\Program Files (x86)\\Cisco\\Cisco AnyConnect Secure Mobility Client", executable),
+            os.path.join("C:\\Program Files\\Cisco\\Cisco AnyConnect Secure Mobility Client", executable),
+            os.path.join(env_home, executable) if env_home is not None else None,
+            self.path if self.path is not None else None,
+            os.path.join(self.path, executable) if self.path is not None else None,
+            os.path.join(env_path) if env_path is not None else None
+        ]
+
+        for path in possible_paths:
+            if path is not None and os.path.isfile(path):
+                return path
+
+        raise Exception(f"Could not find  {executable}")
