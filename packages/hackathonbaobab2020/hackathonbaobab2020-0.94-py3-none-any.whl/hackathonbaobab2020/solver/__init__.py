9c9899000d955160cebc9184c9a3e58c7de69e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:33 +0000

    Add package hackathonbaobab2020/0.94/hackathonbaobab2020-0.94-py3-none-any.whl

---
+++ packages/hackathonbaobab2020/hackathonbaobab2020-0.94-py3-none-any.whl/hackathonbaobab2020/solver/__init__.py
@@ -0,0 +1,19 @@
+from .algorithm1 import Algorithm
+from .cp_ortools import CPModel1
+from .Milp1 import Milp1
+from .Iterator1 import Iterator1
+from .loop_solver import Loop_solver
+from .brute_solver import Brute_solver
+
+solvers = \
+    dict(default=Algorithm,
+		 Milp_LP_HL = Milp1,
+         ortools=CPModel1,
+         Iterator_HL = Iterator1,
+         loop_EJ=Loop_solver,
+         brute_EJ = Brute_solver,
+         )
+
+# factory of solvers
+def get_solver(name='default'):
+    return solvers.get(name)
