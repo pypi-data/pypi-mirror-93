6f1c9f49d083a46c0597f0f221c422fc10f5e39e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:17 +0000

    Add package hotviz/0.0.9/hotviz-0.0.9.tar.gz

---
+++ packages/hotviz/hotviz-0.0.9.tar.gz/hotviz-0.0.9/hotviz/__init__.py
@@ -0,0 +1,22 @@
+
+#metadata
+__version__ = "0.0.9"
+__author__ =  "Axel Almquist"
+__author_email__ =  "axel@almquist.io"
+__license__ = "MIT"
+
+try:
+    __HOTVIZ_SETUP__
+except NameError:
+    __HOTVIZ_SETUP__ = False
+
+
+if not __HOTVIZ_SETUP__:
+
+    from hotviz.hot_text import hot_text
+    from hotviz.hot_tree import hot_tree
+
+    __all__ = [
+                "hot_text",
+                "hot_tree"
+                ]
