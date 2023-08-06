ab9cb9e51ebcc62cf2fafdd225418a34c6d3d911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:11 +0000

    Add package naim/0.2.12/naim-0.2.12-py3-none-any.whl

---
+++ packages/naim/naim-0.2.12-py3-none-any.whl/daim/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 16:20:32 2021
+
+@author: tardis
+"""
+from . import annotate_identified_variants
+from annotate_identified_variants import annotate_mutations
+
+__all__ = ['annotate_identified_variants', 'annotate_identified_variants.annotate_mutations']
