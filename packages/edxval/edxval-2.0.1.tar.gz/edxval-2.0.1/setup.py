3e042d9ea845626e31afae200a3d2fded59381e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package edxval/2.0.1/edxval-2.0.1.tar.gz

---
+++ packages/edxval/edxval-2.0.1.tar.gz/edxval-2.0.1/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+
+from setuptools import setup
+
+PACKAGES = [
+    'edxval',
+    'edxval.config',
+    'edxval.migrations',
+    'edxval.tests',
+]
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+') or
+        line.startswith('-c')
+    )
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.split('#')[0].strip() for line in open(path).readlines()
+            if is_requirement(line)
+        )
+    return list(requirements)
+
+
+VERSION = '2.0.1'
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a v%s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+setup(
+    name='edxval',
+    version=VERSION,
+    author='edX',
+    url='http://github.com/edx/edx-val',
+    description='edx-val',
+    long_description='edx-val',
+    long_description_content_type='text/x-rst',
+    license='AGPL',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+    ],
+    packages=PACKAGES,
+    install_requires=load_requirements('requirements/base.in'),
+    tests_require=load_requirements('requirements/test.in'),
+)
