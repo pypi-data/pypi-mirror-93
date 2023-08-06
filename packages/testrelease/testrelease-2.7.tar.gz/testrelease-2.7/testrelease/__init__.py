574874f99fec39ef7be0b66c7c290722a194b5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:27 +0000

    Add package testrelease/2.7/testrelease-2.7.tar.gz

---
+++ packages/testrelease/testrelease-2.7.tar.gz/testrelease-2.7/testrelease/__init__.py
@@ -0,0 +1,24 @@
+
+from ._version import __version__
+
+
+__author__ = 'testrelease'
+__copyright__ = 'Copyright 2020'
+
+__license__ = "BSD 3-Clause License"
+
+
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__)) 
+sys.path.append(current_dir) 
+
+from .utils import (
+    say,
+    yell
+    )
+
+__all__ = ['testrelease']
+
+
+
