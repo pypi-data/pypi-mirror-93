321dbced8c6fe8ee4a025d8d05247fd9c3ae5a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:29 +0000

    Add package orthopoly/0.9.2/orthopoly-0.9.2.tar.gz

---
+++ packages/orthopoly/orthopoly-0.9.2.tar.gz/orthopoly-0.9.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="orthopoly",
+    version="0.9.2",
+    author="Mark M. Baum",
+    author_email="markbaum@g.harvard.edu",
+    description="Python functions for orthogonal polynomials and (real, 2D, orthonormal) spherical harmonics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wordsworthgroup/orthopoly",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'numpy',
+        'scipy>=1.5.4'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
