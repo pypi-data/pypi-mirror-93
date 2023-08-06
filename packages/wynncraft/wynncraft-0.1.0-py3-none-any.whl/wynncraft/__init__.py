5df76360c05388970fed80e9ef2e3137c9245fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:35 +0000

    Add package wynncraft/0.1.0/wynncraft-0.1.0-py3-none-any.whl

---
+++ packages/wynncraft/wynncraft-0.1.0-py3-none-any.whl/wynncraft/__init__.py
@@ -0,0 +1,11 @@
+import wynncraft.cache
+
+from wynncraft.wynncraft import Guild
+from wynncraft.wynncraft import Item
+from wynncraft.wynncraft import Ingredient
+from wynncraft.wynncraft import Leaderboard
+from wynncraft.wynncraft import Network
+from wynncraft.wynncraft import Player
+from wynncraft.wynncraft import Recipe
+from wynncraft.wynncraft import Search
+from wynncraft.wynncraft import Territory
