d07e4cfc5868326287bbfe62998f86d8546d539d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package datadog-log/1.2.0/datadog_log-1.2.0-py3-none-any.whl

---
+++ packages/datadog-log/datadog_log-1.2.0-py3-none-any.whl/datadoglog/init.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+from .formatter import DatadogFormatter
+
+
+def init_logging(level=logging.INFO, replace_existing_handlers=False):
+    log_handler = logging.StreamHandler(sys.stdout)
+    log_handler.setFormatter(DatadogFormatter())
+    root_logger = logging.getLogger()
+    if replace_existing_handlers:
+        for handler in root_logger.handlers:
+            root_logger.removeHandler(handler)
+    root_logger.addHandler(log_handler)
+    root_logger.setLevel(level)
