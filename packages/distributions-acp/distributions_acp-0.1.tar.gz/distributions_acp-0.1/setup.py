208d2fbf64b38566e1b1e729237590ef82ed417d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:50 +0000

    Add package distributions-acp/0.1/distributions_acp-0.1.tar.gz

---
+++ packages/distributions-acp/distributions_acp-0.1.tar.gz/distributions_acp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_acp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_acp'],
+      zip_safe=False)
