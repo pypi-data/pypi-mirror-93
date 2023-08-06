847ea560566610f727dc85ae3f99de0fdbfad6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:05 +0000

    Add package nd2-dask/0.0.5/nd2_dask-0.0.5.tar.gz

---
+++ packages/nd2-dask/nd2_dask-0.0.5.tar.gz/nd2_dask-0.0.5/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+# Add your dependencies in requirements.txt
+# Note: you can add test-specific requirements in tox.ini
+requirements = []
+with open('requirements.txt') as f:
+    for line in f:
+        stripped = line.split("#")[0].strip()
+        if len(stripped) > 0:
+            requirements.append(stripped)
+
+
+# https://github.com/pypa/setuptools_scm
+use_scm = {"write_to": "nd2_dask/_version.py"}
+
+setup(
+    name='nd2_dask',
+    version='0.0.5',
+    author='Draga Doncila Pop',
+    author_email='ddon0001@student.monash.edu',
+    license='BSD-3',
+    url='https://github.com/DragaDoncila/nd2-dask',
+    description='Plugin to load nd2 data into napari',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    install_requires=requirements,
+    # use_scm_version=use_scm,
+    # setup_requires=['setuptools_scm'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: napari',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+    ],
+    entry_points={
+        'napari.plugin': [
+            'nd2-reader = nd2_dask.nd2_reader',
+        ],
+    },
+)
