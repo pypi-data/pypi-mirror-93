b5597bc7b6d2c48adb82a0a34e445b20fa5d8c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package gaction-lib/0.1a1/gaction-lib-0.1a1.tar.gz

---
+++ packages/gaction-lib/gaction-lib-0.1a1.tar.gz/gaction-lib-0.1a1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="gaction-lib",  # package name
+    version="0.1a1",  # version
+    description="[TODO]: Fill your description here",
+    author="gawainx",
+    author_email="liangyixp@live.cn",
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+    ],
+    scripts=[],
+    packages=setuptools.find_packages(),
+)
