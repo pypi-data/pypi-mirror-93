745d142a6ea9b3d7ced90571645804942cc8f248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:02 +0000

    Add package sparkles/0.1.25/sparkles-0.1.25.tar.gz

---
+++ packages/sparkles/sparkles-0.1.25.tar.gz/sparkles-0.1.25/sparkles/__init__.py
@@ -0,0 +1,179 @@
+"""Just utilities made by Jim Vogel"""
+import itertools
+from collections import namedtuple
+from datetime import datetime
+from functools import wraps
+from typing import Iterable, List
+from subprocess import check_output
+
+from loguru import logger
+import pytz
+import requests
+
+from .mongo_tools import clean_json
+from .data_utils import to_dicts
+from .parsing import parse_float, parse_int, lower_string
+
+__version__ = "0.1.25"
+
+
+def logged_user(logger=logger):
+    def logging_user_wrapper(func):
+        function_name = func.__name__
+
+        @wraps(func)
+        def log(*args, **kwargs):
+            user = kwargs.get("current_user")
+            username = user.username if user else "Unknown User"
+
+            logger.info(f"{username} ran {function_name}")
+            return func(*args, **kwargs)
+
+        return log
+
+    return logging_user_wrapper
+
+
+def now_utc():
+    return datetime.now(pytz.utc).replace(second=0, microsecond=0, tzinfo=None)
+
+
+def trunc_date(dt):
+    return dt.replace(hour=0, minute=0, second=0, microsecond=0)
+
+
+def chunked(iterable, size):
+    it = iter(iterable)
+    while True:
+        chunk = tuple(itertools.islice(it, size))
+        if not chunk:
+            break
+        yield chunk
+
+
+def add_key_from_id(data: Iterable[dict]):
+    for row in data:
+        row["key"] = row["id"]
+
+
+def quick_search(query, model, fields=None, base_raw_query=None):
+    fields = fields or model.quick_search_fields
+    query = query.lower()
+    ret = []
+    for title, field_name in fields:
+        values_with_query_in_it = [
+            f
+            for f in model.objects(__raw__=base_raw_query or {}).distinct(
+                field=field_name
+            )
+            if query in f.lower()
+        ]
+
+        options = []
+        for value in values_with_query_in_it:
+            row_count = len(
+                model.objects(__raw__={**(base_raw_query or {}), field_name: value})
+            )
+            options.append(
+                {"label": value, "count": row_count, "filter": {field_name: value}}
+            )
+        if options:
+            ret.append({"title": title, "options": options})
+
+    return ret
+
+
+GpsCoords = namedtuple("GpsCoords", "lat,lon")
+
+
+def build_send_slack_message(prefix, slack_webhook):
+    def send_slack_message(message):
+        from notifiers import get_notifier
+
+        message = f"{prefix} | {message}"
+
+        lines = message.splitlines(keepends=True)
+        line_count = len(lines)
+        if line_count > 10:
+            url, worked = send_to_haste_bin(message)
+            if worked:
+                slack_message = "".join(
+                    [lines[0], f"{line_count} lines long, pasted here: {url}"]
+                )
+            else:
+                slack_message = message + "\n" + url
+        else:
+            slack_message = message
+
+        notifier = get_notifier("slack")
+        try:
+            notifier.notify(webhook_url=slack_webhook, message=slack_message)
+        except Exception:
+            pass
+
+    return send_slack_message
+
+
+HASTEBIN = "https://hastebin.com/"
+
+
+def send_to_haste_bin(message):
+    try:
+        response = requests.post(f"{HASTEBIN}documents", message.encode("utf-8"))
+        return f'{HASTEBIN}{response.json()["key"]}', True
+    except Exception:
+        import traceback
+
+        return f"Failed to send to hastebin, {traceback.format_exc()}", False
+
+
+def parse_csl(value, lower=True):
+    """
+    reads a string that is a comma separated list and converts it into a list of strings
+    stripping out white space along the way
+    :param value: the string to parse
+    :param lower: should we also lowercase everything (default true)
+    :return: a list of strings
+    """
+    value = value.strip()
+    if not value:
+        return []
+    if lower:
+        return [t.strip() for t in value.lower().strip().split(",")]
+    return [t.strip() for t in value.strip().split(",")]
+
+
+def sorted_groupby(iterable, key, value=lambda x: x):
+    """
+    just like regular pythons regular group by except it sorts for you
+    :param iterable: the thing to iterate through
+    :param key: the key to both sort and group by
+    :param value: a function to apply to the group before returning (defaults to groupby iterator)
+    :return: iterable of tuples if (key_results, grouped_values)
+    """
+    return (
+        (k, value(v))
+        for k, v in itertools.groupby(sorted(iter(iterable), key=key), key=key)
+    )
+
+
+def counter(start=0, step=1):
+    count = start
+    while True:
+        yield count
+        count += step
+
+
+def pairwise(iterable: Iterable):
+    """s -> (s0,s1), (s1,s2), (s2, s3), ..."""
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+def get_stdout_lines(command: List[str]) -> List[str]:
+    return check_output(command).decode().splitlines()
+
+
+def lookup(iterable: Iterable, key: callable):
+    return {key(i): i for i in iterable}
---
+++ packages/sparkles/sparkles-0.1.25.tar.gz/sparkles-0.1.25/sparkles/google_sheet.py
@@ -0,0 +1,121 @@
+import logging
+import os
+from pathlib import Path
+
+from gspread import WorksheetNotFound, authorize
+from loguru import logger
+from oauth2client.service_account import ServiceAccountCredentials
+
+from .file_utils import find_file_above
+
+
+class Config:
+    google_credentials_file = "service_account.json"
+    default_google_sheet_key = ""
+    ignored_sheets = ["Results", "Status"]
+
+
+config = Config()
+
+
+def ignored_sheets():
+    return [tab_name.casefold() for tab_name in config.ignored_sheets]
+
+
+def auth(credential_file=None):
+    credential_file = credential_file or config.google_credentials_file
+    path = absolute_path_for(credential_file)
+    logger.info(f"Authorizing google with credential file {path}")
+    credentials = load_gspread_credentials(path)
+    return authorize(credentials)
+
+
+def load_gspread_credentials(absolute_path):
+    scope = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    return ServiceAccountCredentials.from_json_keyfile_name(absolute_path, scope)
+
+
+def absolute_path_for(original_credential_file):
+    if os.sep in original_credential_file:
+        credential_file = Path(__file__).parent.parent / original_credential_file
+    else:
+        credential_file = find_file_above(original_credential_file, os.getcwd())
+
+    if not credential_file:
+        raise FileNotFoundError(
+            f"Could not find a credential file {original_credential_file} above {os.getcwd()}"
+        )
+    return credential_file
+
+
+def get_book(sheet_key, gc=None):
+    if gc is None:
+        gc = auth()
+    elif type(gc) == str:
+        gc = auth(gc)
+    logging.info(f"Opening Google Sheet {sheet_key}")
+    book = gc.open_by_key(sheet_key)
+    logging.info(f"Google Sheet Title is {book.title}")
+    return book
+
+
+def get_fast_book(sheet_key, gc=None):
+    if gc is None:
+        gc = auth()
+    elif type(gc) == str:
+        gc = auth(gc)
+    logging.info(f"Opening Google Sheet {sheet_key}")
+    from .fast_gsheet import FastGSheet
+
+    book = FastGSheet(sheet_key, gc)
+    return book
+
+
+def data_sheets(book):
+    """Lists all worksheets that aren't in ignored sheets"""
+    for worksheet in book:
+        title = worksheet.title
+        if title.casefold() in ignored_sheets():
+            continue
+        yield worksheet
+
+
+def clean_worksheet_data(worksheet, log):
+    log(f"Loading sheet {worksheet.title}")
+    data = worksheet.get_all_values()
+    data[0] = [str(col).strip() for col in data[0]]  # strip whitespace on column names
+    return data
+
+
+def to_dataset(book, log=lambda x: None):
+    log("Loading sheets")
+    return {
+        worksheet.title: clean_worksheet_data(worksheet, log)
+        for worksheet in data_sheets(book)
+    }
+
+
+def data_set(sheet_key, log=lambda x: None):
+    book = get_book(sheet_key)
+    return book.title, to_dataset(book, log)
+
+
+def get_sheet(book, name):
+
+    try:
+        return book.worksheet(name)
+    except WorksheetNotFound:
+        logger.exception(
+            f"Tried to load a sheet named: {name} but there is not sheet named that here: {book.url}"
+        )
+        return
+
+
+def get_sheet_data(name, book=None):
+    book = book or get_book(config.default_google_sheet_key)
+    if sheet := get_sheet(book, name):
+        return sheet.get_all_records()
+    return []
