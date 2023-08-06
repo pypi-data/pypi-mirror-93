e54150d4b6dbc243d4ae29e66b9f509194f33185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:45 +0000

    Add package oligo-melting/2.0.1.post5/oligo_melting-2.0.1.post5.tar.gz

---
+++ packages/oligo-melting/oligo_melting-2.0.1.post5.tar.gz/oligo_melting-2.0.1.post5/oligo_melting/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+'''
+@author: Gabriele Girelli
+@email: gigi.ga90@gmail.com
+@description: oligo_melting package.
+'''
+
+# DEPENDENCIES =================================================================
+
+__all__ = []
+
+import math
+
+from oligo_melting.seq import *
+from oligo_melting import duplex as Duplex
+from oligo_melting import secstr as SecStr
+
+# END ==========================================================================
+
+################################################################################
