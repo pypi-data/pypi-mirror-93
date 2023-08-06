9f2bd6817e67de9a3dc0f783209d15b28750ef11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:01 +0000

    Add package MinecraftWikiSearch/1.1/MinecraftWikiSearch-1.1-py3-none-any.whl

---
+++ packages/MinecraftWikiSearch/MinecraftWikiSearch-1.1-py3-none-any.whl/MinecraftWikiSearch.py
@@ -0,0 +1,27 @@
+import sys
+import function
+
+from Errors import ArgsError
+from JSONRead import EasyInput
+from function.DataReturn import DataReturn
+
+
+# 主函数
+def main(arg=None):
+    if EasyInput:
+        File = open("wiki.txt", "r", encoding="utf-8").read()
+        Return = DataReturn(File)
+        return Return
+    else:
+        if len(sys.argv) < 2 and arg is None:
+            raise ArgsError("没有传入参数")
+        else:
+            try:
+                return DataReturn(sys.argv[1])
+            except IndexError:
+                return DataReturn(arg)
+
+
+if __name__ == '__main__':
+    args = input(">>> ")
+    print(main(arg=args))
---
+++ packages/MinecraftWikiSearch/MinecraftWikiSearch-1.1-py3-none-any.whl/function/Special_Entry.py
@@ -0,0 +1,11 @@
+# 你可以通过提交bug来使条目完善
+# 或者自行修改它，只需要修改如下的版权说明即可
+"""
+Copyright 2021 MingxuanGame
+其修改是基于MinecraftWikiSearch的，由[你的名称]修改完善
+修改了：[修改内容]
+"""
+
+
+class Special_Entry:
+    Special_Entry = ["创，现由Mojang"]
