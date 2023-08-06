9f3e6731d9a8e0d3398ce8d131b790b1ca5dbd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:16 +0000

    Add package jittor/1.2.2.24/jittor-1.2.2.24.tar.gz

---
+++ packages/jittor/jittor-1.2.2.24.tar.gz/jittor-1.2.2.24/python/jittor/src/mem/allocator/cuda_managed_allocator.cc
@@ -0,0 +1,31 @@
+// ***************************************************************
+// Copyright (c) 2021 Jittor. All Rights Reserved. 
+// Maintainers: Dun Liang <randonlang@gmail.com>. 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ***************************************************************
+#ifdef HAS_CUDA
+#include <cuda_runtime.h>
+#include "helper_cuda.h"
+#include "mem/allocator/cuda_managed_allocator.h"
+
+namespace jittor {
+
+CudaManagedAllocator cuda_managed_allocator;
+DEFINE_FLAG(int, use_cuda_managed_allocator, 1, "Enable cuda_managed_allocator");
+
+const char* CudaManagedAllocator::name() const {return "cuda_managed";}
+
+void* CudaManagedAllocator::alloc(size_t size, size_t& allocation) {
+    void* ptr;
+    checkCudaErrors(cudaMallocManaged(&ptr, size));
+    return ptr;
+}
+
+void CudaManagedAllocator::free(void* mem_ptr, size_t size, const size_t& allocation) {
+    checkCudaErrors(cudaFree(mem_ptr));
+}
+
+} // jittor
+
+#endif
