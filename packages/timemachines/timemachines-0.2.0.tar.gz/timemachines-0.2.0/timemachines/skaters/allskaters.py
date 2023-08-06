9b7fbf327dd9dc4a166993dabe70802541c33a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:25 +0000

    Add package timemachines/0.2.0/timemachines-0.2.0.tar.gz

---
+++ packages/timemachines/timemachines-0.2.0.tar.gz/timemachines-0.2.0/timemachines/skaters/allskaters.py
@@ -0,0 +1,8 @@
+from timemachines.skaters.simple.basic import BASIC_SKATERS
+from timemachines.skaters.simple.linear import LINEAR_SKATERS
+from timemachines.skaters.divine.divineskaters import DIVINE_SKATERS
+from timemachines.skaters.proph.prophskaters import PROPHET_SKATERS
+from timemachines.skaters.dlm.alldlmskaters import DLM_SKATERS
+
+SKATERS = BASIC_SKATERS + LINEAR_SKATERS + DIVINE_SKATERS + PROPHET_SKATERS + DLM_SKATERS
+
