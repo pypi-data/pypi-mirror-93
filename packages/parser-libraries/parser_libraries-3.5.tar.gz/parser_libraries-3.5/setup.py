dffb8f8534c66d25db4e1e77b80e1bb34b3a8ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:12 +0000

    Add package parser-libraries/3.5/parser_libraries-3.5.tar.gz

---
+++ packages/parser-libraries/parser_libraries-3.5.tar.gz/parser_libraries-3.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='parser_libraries',
+    version='3.5',
+    packages=['parser_libraries'],
+    author_email='ivan.frinom@gmail.com',
+    install_requires=[
+        'pymysql',
+        'openpy',
+        'requests',
+	'bs4',
+	'selenium',
+	'pytelegrambotapi',
+	'cryptography'
+    ]
+)
