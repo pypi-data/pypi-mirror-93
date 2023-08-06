d9e74cf13632506cee589bde89bb9ad6ccefa989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:17 +0000

    Add package blackfynn/5.0.1/blackfynn-5.0.1.tar.gz

---
+++ packages/blackfynn/blackfynn-5.0.1.tar.gz/blackfynn-5.0.1/blackfynn/log.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, division, print_function
+
+import logging
+import os
+
+logging.basicConfig()
+
+
+def get_logger(name="blackfynn-python"):
+    """
+    Returns a logger configured to be used throughout the
+    blackfynn-python library
+
+    Args:
+      name (string): Name of the logger, default "blackfynn-python"
+
+    Returns:
+      Logger
+    """
+
+    logger = logging.getLogger(name)
+    logger.setLevel(os.environ.get("BLACKFYNN_LOG_LEVEL", "INFO"))
+
+    return logger
---
+++ packages/blackfynn/blackfynn-5.0.1.tar.gz/blackfynn-5.0.1/tests/utils.py
@@ -0,0 +1,38 @@
+""" Utility functions for generating test fixtures """
+
+import time
+from uuid import uuid4
+
+from blackfynn import Blackfynn
+
+
+def current_ts():
+    """ Gets current timestamp """
+    return int(round(time.time() * 1000))
+
+
+def get_test_client(profile=None, api_token=None, api_secret=None, **overrides):
+    """ Utility function to get a Blackfynn client object """
+    bf = Blackfynn(
+        profile=profile, api_token=api_token, api_secret=api_secret, **overrides
+    )
+    assert bf.context is not None
+    orgs = bf.organizations()
+    assert len(orgs) > 0
+
+    # explicitly set context to Blackfyn org
+    assert bf.context is not None
+
+    return bf
+
+
+def create_test_dataset(bf_client):
+    """Utility function to generate a dataset for testing. It is up to the
+    caller to ensure the dataset is cleaned up
+    """
+    ds = bf_client.create_dataset("test_dataset_{}".format(uuid4()))
+    ds_id = ds.id
+    # Removing this check to limit the number of API calls:
+    # all_dataset_ids = [x.id for x in bf_client.datasets()]
+    # assert ds_id in all_dataset_ids
+    return ds
