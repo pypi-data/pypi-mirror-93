34df0c5f4ae3e28c52f302e20565e0f07f613ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:45 +0000

    Add package golog/0.4.9/golog-0.4.9.tar.gz

---
+++ packages/golog/golog-0.4.9.tar.gz/golog-0.4.9/golog/__init__.py
@@ -0,0 +1,25 @@
+# import submodules you want to install
+from .decorators import *
+from .tools import *
+from .log import *
+from .alerts import *
+
+
+__docformat__ = "restructuredtext"
+
+
+# module level doc-string
+__doc__ = """
+golog - a powerful general logging and monitoring library for Python
+=====================================================================
+
+*golog* is a Python package providing many different useful functions
+
+Main Features
+-------------
+Here are just a few of the things that iago does well:
+  - Nice Logging
+  - Integrated Alerts (Slack, Email , SMS)
+  - Useful Decorators
+  - Tools to make your code nicer and more reliable
+"""
