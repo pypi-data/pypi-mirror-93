8004911126d607e688d58867953f6923f066d193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:06 +0000

    Add package autogluon-contrib-nlp/0.0.1b20210201/autogluon_contrib_nlp-0.0.1b20210201-py3-none-any.whl

---
+++ packages/autogluon-contrib-nlp/autogluon_contrib_nlp-0.0.1b20210201-py3-none-any.whl/autogluon_contrib_nlp/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.0.1'
+from . import base
+from . import data
+from . import models
+from . import utils
+from . import attention_cell
+from . import initializer as init
+from . import layers
+from . import loss
+from . import lr_scheduler
+from . import op
+from . import optimizers
+from . import registry
+from . import sequence_sampler
