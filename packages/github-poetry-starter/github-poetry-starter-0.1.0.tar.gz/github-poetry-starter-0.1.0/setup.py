fbbc1df5364d4f2cc1c74d7157e496b6ff528351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:05 +0000

    Add package github-poetry-starter/0.1.0/github-poetry-starter-0.1.0.tar.gz

---
+++ packages/github-poetry-starter/github-poetry-starter-0.1.0.tar.gz/github-poetry-starter-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['github_poetry_starter']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'github-poetry-starter',
+    'version': '0.1.0',
+    'description': 'GitHub Actions starter for python with python-poetry',
+    'long_description': None,
+    'author': 'hexatester',
+    'author_email': 'hexatester@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
