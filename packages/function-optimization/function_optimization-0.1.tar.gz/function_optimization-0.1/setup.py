1c8d86f38123f84f158107d227f9f23e02fcace0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package function-optimization/0.1/function_optimization-0.1.tar.gz

---
+++ packages/function-optimization/function_optimization-0.1.tar.gz/function_optimization-0.1/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name='function_optimization' , version = '0.1'
+        ,description = 'Optimize functions.Newton and Gradient descent methods', packages = ['optimization'], zip_safe = False,
+        author = 'Mark Eltsefon', author_email = 'meltsefon1@gmail.com')
