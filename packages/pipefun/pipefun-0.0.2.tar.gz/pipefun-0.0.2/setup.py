c1cd464a29821e130c9a0cdb4851122c91478355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:55 +0000

    Add package pipefun/0.0.2/pipefun-0.0.2.tar.gz

---
+++ packages/pipefun/pipefun-0.0.2.tar.gz/pipefun-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pipefun", # Replace with your own username
+    version="0.0.2",
+    author="starskyC",
+    author_email="cl2004_0123@aliyun.com",
+    description="A lightweighted package that contains pipe operators and functional tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
