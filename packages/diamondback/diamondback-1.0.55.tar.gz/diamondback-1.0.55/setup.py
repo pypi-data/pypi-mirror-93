b8a0416085f436cc1c527b0368c215ed922a79e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:28 +0000

    Add package diamondback/1.0.55/diamondback-1.0.55.tar.gz

---
+++ packages/diamondback/diamondback-1.0.55.tar.gz/diamondback-1.0.55/setup.py
@@ -0,0 +1,82 @@
+""" **Description**
+
+        Diamondback setup.
+
+    **License**
+
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        Copyright (c) 2018, Larry Turner, Schneider Electric.  All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-01-23.
+
+    **Definition**
+
+"""
+
+from setuptools import find_packages, setup
+import diamondback
+
+
+try :
+
+    with open( 'readme.rst', 'r' ) as fin :
+
+        _readme = fin.read( )
+
+except :
+
+    _readme = ''
+
+
+setup( name = 'diamondback',
+       version = diamondback.__version__,
+       author = 'Larry Turner',
+       author_email = 'larry.turner@se.com',
+       url = 'https://github.com/larryturner/diamondback',
+       description = 'Diamondback digital signal processing package.',
+       long_description = _readme,
+       classifiers = [ 'Operating System :: OS Independent',
+                       'Programming Language :: Python :: 3',
+                       'License :: OSI Approved :: BSD License' ],
+       install_requires = [ 'jsonpickle >= 1.4.2',
+                            'numpy >= 1.19.4',
+                            'pandas >= 1.2.0',
+                            'requests >= 2.25.1',
+                            'scipy >= 1.5.4' ],
+       keywords = [ 'DSP',
+                    'FFT',
+                    'FIR',
+                    'GZIP',
+                    'IIR',
+                    'JSON',
+                    'PCA',
+                    'PID',
+                    'PSD',
+                    'REST',
+                    'adaptive',
+                    'complex',
+                    'derivative',
+                    'diversity',
+                    'exponential',
+                    'filter',
+                    'fourier',
+                    'frequency',
+                    'goertzel',
+                    'integral',
+                    'log',
+                    'model',
+                    'polynomial',
+                    'polyphase',
+                    'power',
+                    'rate',
+                    'serial',
+                    'transform',
+                    'wavelet' ],
+       packages = find_packages( ),
+       python_requires = '>= 3.8',
+       license = 'BSD-3C.  Copyright (c) 2018, Larry Turner, Schneider Electric.  All rights reserved.' )
+
+
