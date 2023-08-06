b89ae98137feb193bc376bac340f4d0c893c3ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:53 +0000

    Add package sbad-distributions/0.1/sbad_distributions-0.1.tar.gz

---
+++ packages/sbad-distributions/sbad_distributions-0.1.tar.gz/sbad_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sbad_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sbad_distributions'],
+      author ="Sergio Badillo",
+      author_email = "sbadilloworks@gmail.com",
+      zip_safe=False)
