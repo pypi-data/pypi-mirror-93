7c7dd0599f86b9a68b23370e6f617f0e21e6d3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:52 +0000

    Add package iolink/0.0.2/iolink-0.0.2.tar.gz

---
+++ packages/iolink/iolink-0.0.2.tar.gz/iolink-0.0.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Setup script for the `iolink` package.
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='iolink',
+    author='Maxim-Trinamic Software Team',
+    author_email='pypi.trinamic@maximintegrated.com',
+    description='IO-Link Adapter Interface',
+    long_description_content_type='text/markdown',
+    url='https://github.com/trinamic/iolink',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+    ],
+    license='MIT',
+)
