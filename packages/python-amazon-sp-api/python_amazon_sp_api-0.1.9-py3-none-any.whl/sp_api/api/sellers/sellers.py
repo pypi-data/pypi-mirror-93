2d1d3df24371f8c554888f15ad545d7192bbbe98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:07 +0000

    Add package python-amazon-sp-api/0.1.9/python_amazon_sp_api-0.1.9-py3-none-any.whl

---
+++ packages/python-amazon-sp-api/python_amazon_sp_api-0.1.9-py3-none-any.whl/sp_api/api/sellers/sellers.py
@@ -0,0 +1,12 @@
+from sp_api.base.helpers import sp_endpoint
+from sp_api.api.sellers.models.get_marketplace_participations_response import GetMarketplaceParticipationsResponse
+from sp_api.base import Client, Marketplaces
+
+
+class Sellers(Client):
+    def __init__(self, marketplace=Marketplaces.US, *, refresh_token=None, account='default', credentials=None):
+        super().__init__(marketplace, refresh_token, account, credentials)
+
+    @sp_endpoint('/sellers/v1/marketplaceParticipations')
+    def get_marketplace_participation(self, **kwargs):
+        return GetMarketplaceParticipationsResponse(**self._request(kwargs.pop('path')).json())
