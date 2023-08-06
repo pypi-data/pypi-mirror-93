9d6714d6275bacbaa65933045d68b540a89f09bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package chat-replay-downloader/0.0.5/chat_replay_downloader-0.0.5-py2.py3-none-any.whl

---
+++ packages/chat-replay-downloader/chat_replay_downloader-0.0.5-py2.py3-none-any.whl/chat_replay_downloader/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for chat-replay-downloader."""
+
+__author__ = 'Joshua Lochner'
+__email__ = 'admin@xenova.com'
+__version__ = '0.0.5'
+__url__ = 'https://github.com/xenova/chat_replay_downloader'
+
+from .cli import main
+
+from .chat_replay_downloader import ChatDownloader
---
+++ packages/chat-replay-downloader/chat_replay_downloader-0.0.5-py2.py3-none-any.whl/chat_replay_downloader/chat_replay_downloader.py
@@ -0,0 +1,4 @@
+"""Main module."""
+
+# Specify all "endpoints"/usable methods here
+from .sites import ChatDownloader
