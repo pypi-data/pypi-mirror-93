b050e248bf7da72675989fe9fd08193dbc5455c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:24 +0000

    Add package asen-6008/1.0.1/asen_6008-1.0.1-py3-none-any.whl

---
+++ packages/asen-6008/asen_6008-1.0.1-py3-none-any.whl/asen_6008/asen_6008.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 27 12:40:48 2021
+
+@author: nbelakovski
+"""
+AU = 1.49597870700e8 # km
+Days_per_year =  365.242189
+
+# Gravitational coefficients (km3/s2)
+mu_sun = 1.32712440018e11
+mu_venus   = 3.24858599e5
+mu_earth   = 3.98600433e5
+mu_mars    = 4.28283100e4
+mu_jupiter = 1.266865361e8
+mu_saturn  = 3.7931208e7
+mu_uranus  = 5.7939513e6
+mu_neptune = 6.835100e6
+mu_pluto   = 8.71e2
+# Planetary radii (km)
+r_venus = 6051.8
+r_earth = 6378.14
+r_mars = 3396.19
+r_jupiter = 71492
+r_saturn  = 60268
+r_uranus  = 25559
+r_neptune = 24764
+r_pluto   = 1188.3
+
+a_mars = 1.523679342
