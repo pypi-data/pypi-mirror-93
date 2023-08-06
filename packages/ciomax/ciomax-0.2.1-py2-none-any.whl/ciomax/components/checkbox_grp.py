c213c7b8da393837dff62036c047cd7545962c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:01 +0000

    Add package ciomax/0.2.1/ciomax-0.2.1-py2-none-any.whl

---
+++ packages/ciomax/ciomax-0.2.1-py2-none-any.whl/ciomax/components/checkbox_grp.py
@@ -0,0 +1,28 @@
+from PySide2 import QtWidgets, QtCore
+
+from ciomax.components import widgets
+
+
+class CheckboxGrp(QtWidgets.QWidget):
+
+    def __init__(self, **kwargs):
+        super(CheckboxGrp, self).__init__()
+        layout = QtWidgets.QHBoxLayout()
+        self.setLayout(layout)
+        num_checkboxes = kwargs.get("checkboxes", 1)
+        sublabels = kwargs.get("sublabels", [])
+
+        if len(sublabels) != num_checkboxes:
+            sublabels = [""] * num_checkboxes
+
+        layout.addWidget(widgets.FormLabel(kwargs.get("label", "")))
+
+        self.checkboxes = []
+
+        for i in range(num_checkboxes):
+            cb = QtWidgets.QCheckBox(sublabels[i])
+            self.checkboxes.append(cb)
+            layout.addWidget(cb)
+
+        layout.addSpacing(85)
+
