944ebd2cacb9cb963084eb1dc66acc76317c48b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package m1keypkg/0.0.1/m1keypkg-0.0.1.tar.gz

---
+++ packages/m1keypkg/m1keypkg-0.0.1.tar.gz/m1keypkg-0.0.1/README.txt
@@ -0,0 +1,1 @@
+Basic introduction to MAAC family (OneML creators)
---
+++ packages/m1keypkg/m1keypkg-0.0.1.tar.gz/m1keypkg-0.0.1/m1keypkg/__init__.py
@@ -0,0 +1,15 @@
+def m1key():
+    print("Hello, this is Mithilesh Tiwari")
+
+def abhay():
+    print("Hello, this is Abhay Katheria")
+
+def rheo():
+    print("Hello, this is Chitransh Anand")
+
+def gangu():
+    print("Hello, this is Anshika Gangwar")
+
+def oneml():
+    print("Hello, its team one-ml")
+    print("visit https://oneml.github.io")
