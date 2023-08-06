2344cf80cb5c060852a32407ea567c8077977a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:50 +0000

    Add package vid2slides/0.0.4/vid2slides-0.0.4-py3-none-any.whl

---
+++ packages/vid2slides/vid2slides-0.0.4-py3-none-any.whl/vid2slides/cli.py
@@ -0,0 +1,19 @@
+import click
+import os
+import shutil
+from .vid2slides import v2s 
+
+"""
+How it works:
+1. ffmpeg writes keyframes to frames folder
+2. checks if adjacent keyframes are the same
+3. write to pdf
+"""
+
+
+@click.command()
+@click.argument('filename')
+@click.option('-o', '--output', default='out.pdf')
+def cli(filename, output):
+    """ Extracts a slideshow from a video presentation """
+    v2s(filename, output)
