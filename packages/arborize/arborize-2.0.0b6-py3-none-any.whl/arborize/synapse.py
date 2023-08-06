c1400cd907d64cc723ca5365ece1efb364a8a9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:02 +0000

    Add package arborize/2.0.0b6/arborize-2.0.0b6-py3-none-any.whl

---
+++ packages/arborize/arborize-2.0.0b6-py3-none-any.whl/arborize/synapse.py
@@ -0,0 +1,25 @@
+import glia as g
+from patch import p
+
+class Synapse:
+
+    def __init__(self, cell, section, point_process_name, attributes = {}, variant=None, type=None):
+        self._cell = cell
+        self._type = type
+        self._section = section
+        self._point_process_name = point_process_name
+        with g.context(pkg=cell.__class__.glia_package):
+            self._point_process_glia_name = g.resolve(point_process_name, variant=variant)
+            self._point_process = g.insert(section, point_process_name, variant=variant)
+        section.__ref__(self)
+        for key, value in attributes.items():
+            setattr(self._point_process, key, value)
+
+    def __neuron__(self):
+        return self._point_process.__neuron__()
+
+    def stimulate(self, *args, **kwargs):
+        return self._point_process.stimulate(*args, **kwargs)
+
+    def record(self):
+        return p.record(self._point_process._ref_i)
