5ea4120b7fac9384a8c0c6a235079db3d9909816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:19 +0000

    Add package lv-probability/0.1/lv_probability-0.1.tar.gz

---
+++ packages/lv-probability/lv_probability-0.1.tar.gz/lv_probability-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='lv_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['lv_probability'],
+      zip_safe=False)
