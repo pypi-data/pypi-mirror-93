991c5f34acb1fa64b7268cb448fac8cb4f313fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:20 +0000

    Add package pyfileindex/0.0.6/pyfileindex-0.0.6.tar.gz

---
+++ packages/pyfileindex/pyfileindex-0.0.6.tar.gz/pyfileindex-0.0.6/setup.py
@@ -0,0 +1,37 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyfileindex',
+    version=versioneer.get_version(),
+    description='pyfileindex - pythonic file system index',
+    long_description='pyfileindex creates a dynamic file system index inside a pandas DataFrame.',
+
+    url='https://github.com/pyfileindex/pyfileindex',
+    author='Jan Janssen',
+    author_email='janssen@mpie.de',
+    license='BSD',
+
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+
+    keywords='pyfileindex',
+    packages=find_packages(exclude=["*tests*", "*binder*", "*notebooks*"]),
+    install_requires=[
+        'pandas>=1.2.1',
+        'scandir>=1.10.0'
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+    )
