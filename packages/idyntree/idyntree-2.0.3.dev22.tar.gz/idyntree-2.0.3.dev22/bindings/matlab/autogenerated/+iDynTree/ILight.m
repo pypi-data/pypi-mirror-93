b9da72e5db8f7fdf320d4aec40c01c36c493a807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:26 +0000

    Add package idyntree/2.0.3.dev22/idyntree-2.0.3.dev22.tar.gz

---
+++ packages/idyntree/idyntree-2.0.3.dev22.tar.gz/idyntree-2.0.3.dev22/bindings/matlab/autogenerated/+iDynTree/ILight.m
@@ -0,0 +1,63 @@
+classdef ILight < SwigRef
+  methods
+    function this = swig_this(self)
+      this = iDynTreeMEX(3, self);
+    end
+    function delete(self)
+      if self.swigPtr
+        iDynTreeMEX(1803, self);
+        self.SwigClear();
+      end
+    end
+    function varargout = getName(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1804, self, varargin{:});
+    end
+    function varargout = setType(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1805, self, varargin{:});
+    end
+    function varargout = getType(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1806, self, varargin{:});
+    end
+    function varargout = setPosition(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1807, self, varargin{:});
+    end
+    function varargout = getPosition(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1808, self, varargin{:});
+    end
+    function varargout = setDirection(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1809, self, varargin{:});
+    end
+    function varargout = getDirection(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1810, self, varargin{:});
+    end
+    function varargout = setAmbientColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1811, self, varargin{:});
+    end
+    function varargout = getAmbientColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1812, self, varargin{:});
+    end
+    function varargout = setSpecularColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1813, self, varargin{:});
+    end
+    function varargout = getSpecularColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1814, self, varargin{:});
+    end
+    function varargout = setDiffuseColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1815, self, varargin{:});
+    end
+    function varargout = getDiffuseColor(self,varargin)
+      [varargout{1:nargout}] = iDynTreeMEX(1816, self, varargin{:});
+    end
+    function self = ILight(varargin)
+      if nargin==1 && strcmp(class(varargin{1}),'SwigRef')
+        if ~isnull(varargin{1})
+          self.swigPtr = varargin{1}.swigPtr;
+        end
+      else
+        error('No matching constructor');
+      end
+    end
+  end
+  methods(Static)
+  end
+end
