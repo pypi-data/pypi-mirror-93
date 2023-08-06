7649e925785ede03fbd53d9d3b6e0f6892603af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:29 +0000

    Add package popex/1.3.1/popex-1.3.1.tar.gz

---
+++ packages/popex/popex-1.3.1.tar.gz/popex-1.3.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# Generic dependencies
+from setuptools import setup, find_packages
+import unittest
+
+
+# Get unittest functions
+def test_suite():
+    test_loader = unittest.TestLoader()
+    return test_loader.discover('tests', pattern='test_*.py')
+
+
+# Long description from the README.md file
+with open("README.md", "r") as file_handle:
+    long_description = file_handle.read()
+
+
+# Generic requirements
+install_requires = ['numpy', 'scipy']
+
+_setup_data = {
+    'name':                 'popex',
+    'version':              '1.3.1',
+    'description':          'PoPEx sampling package',
+    'long_description':      long_description,
+    'long_description_content_type': 'text/markdown',
+
+    'author':               'Christoph Jaeggli',
+    'author_email':         'christoph.jaeggli@gmail.com',
+    'url':                  'https://github.com/randlab/PoPEx',
+
+    'license':              'MIT',
+
+    'packages':             find_packages(),
+    'install_requires':     install_requires,
+}
+
+if __name__ == '__main__':
+    setup(**_setup_data)
