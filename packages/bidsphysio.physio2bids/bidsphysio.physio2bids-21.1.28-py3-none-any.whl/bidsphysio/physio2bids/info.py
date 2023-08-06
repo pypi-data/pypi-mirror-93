b81ca33c8adc926cfdb559517c9567070c8ece15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:10 +0000

    Add package bidsphysio.physio2bids/21.1.28/bidsphysio.physio2bids-21.1.28-py3-none-any.whl

---
+++ packages/bidsphysio.physio2bids/bidsphysio.physio2bids-21.1.28-py3-none-any.whl/bidsphysio/physio2bids/info.py
@@ -0,0 +1,39 @@
+""" NOTE: When bumping up the version number, double-check if you
+need to also bump up the version of the dependencies
+"""
+
+__version__ = "21.1.28"
+__author__ = "Pablo Velasco"
+__author_email__ = "pablo.velasco@nyu.edu"
+__url__ = "https://github.com/cbinyu/bidsphysio"
+__packagename__ = 'bidsphysio.physio2bids'
+__description__ = "Physiology-to-BIDS Converter"
+__license__ = "MIT"
+__longdesc__ = """Converts physio data from a few different files to BIDS physiological recording."""
+
+CLASSIFIERS = [
+    'Environment :: Console',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Topic :: Scientific/Engineering'
+]
+
+PYTHON_REQUIRES = ">=3.6"
+
+REQUIRES = [
+    'bidsphysio >= 4.3.3',
+]
+
+TESTS_REQUIRES = [
+    'pytest'
+]
+
+EXTRA_REQUIRES = {
+    'tests': TESTS_REQUIRES,
+}
+
+# Flatten the lists
+EXTRA_REQUIRES['all'] = sum(EXTRA_REQUIRES.values(), [])
