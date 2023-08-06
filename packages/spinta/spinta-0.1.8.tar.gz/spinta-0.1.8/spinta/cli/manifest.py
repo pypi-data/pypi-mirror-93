1360dd4d1f03fc3d19477f0994237da76adc8d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:00 +0000

    Add package spinta/0.1.8/spinta-0.1.8.tar.gz

---
+++ packages/spinta/spinta-0.1.8.tar.gz/spinta-0.1.8/spinta/cli/manifest.py
@@ -0,0 +1,91 @@
+import pathlib
+from typing import Iterator
+from typing import List
+
+from typer import Argument
+from typer import Context as TyperContext
+from typer import Option
+from typer import Typer
+
+from spinta import commands
+from spinta.components import Context
+from spinta.core.enums import Access
+from spinta.manifests.helpers import load_manifest_nodes
+from spinta.manifests.tabular.helpers import datasets_to_tabular
+from spinta.manifests.tabular.helpers import read_tabular_manifest
+from spinta.manifests.tabular.helpers import write_tabular_manifest
+from spinta.utils.enums import get_enum_by_name
+from spinta.utils.imports import importstr
+
+app = Typer()
+
+
+@app.command(short_help="Copy manifest optionally transforming final copy")
+def copy(
+    ctx: TyperContext,
+    external: bool = Option(True, help=(
+        "Do not copy external data source metadata"
+    )),
+    # TODO: Change `str` to `Access`
+    #       https://github.com/tiangolo/typer/issues/151
+    access: str = Option('private', help=(
+        "Copy properties with at least specified access"
+    )),
+    files: List[pathlib.Path] = Argument(None, help=(
+        "Source manifest files to copy from"
+    )),
+    dest: pathlib.Path = Argument(None, help=(
+        "Target manifest file to save a copy to"
+    )),
+):
+    """Copy models from CSV manifest files into another CSV manifest file"""
+    context: Context = ctx.obj
+    access = get_enum_by_name(Access, access)
+    rows = _read_csv_files(context, files, external=external, access=access)
+    with dest.open('w') as f:
+        write_tabular_manifest(f, rows)
+
+
+def _read_csv_files(
+    context: Context,
+    files: List[pathlib.Path],
+    *,
+    external: bool = True,
+    access: Access = Access.private,
+) -> Iterator[dict]:
+    rc = context.get('rc')
+    for path in files:
+        with context:
+            context.set('rc', rc.fork({
+                'manifest': 'copy',
+                'manifests': {
+                    'copy': {
+                        'type': 'tabular',
+                        'path': path,
+                        'keymap': 'default',
+                    },
+                },
+                'keymaps': {
+                    'default': {
+                        'type': 'sqlalchemy',
+                        'dsn': 'sqlite:///:memory:',
+                    }
+                }
+            }))
+
+            config = context.get('config')
+            commands.load(context, config)
+            commands.check(context, config)
+
+            Store = rc.get('components', 'core', 'store', cast=importstr, required=True)
+            store = Store()
+            context.set('store', store)
+            commands.load(context, store)
+
+            schemas = read_tabular_manifest(path)
+            load_manifest_nodes(context, store.manifest, schemas)
+
+            commands.link(context, store.manifest)
+            commands.check(context, store.manifest)
+
+            yield from datasets_to_tabular(store.manifest, external=external, access=access)
---
+++ packages/spinta/spinta-0.1.8.tar.gz/spinta-0.1.8/spinta/testing/data.py
@@ -0,0 +1,75 @@
+import operator
+from typing import List
+from typing import Union
+
+import requests
+from pprintpp import pformat
+
+from spinta.testing.client import TestClient
+from spinta.utils.data import take
+from spinta.utils.nestedstruct import flatten
+
+
+def listdata(
+    resp: requests.Response,
+    *keys: str,
+    sort: Union[bool, str] = True,
+) -> List[tuple]:
+    if resp.headers['content-type'].startswith('text/html'):
+        data = resp.context
+        assert resp.status_code == 200, pformat(data)
+        assert 'data' in data, pformat(data)
+        assert 'header' in data, pformat(data)
+        keys = keys or [k for k in data['header'] if not k.startswith('_')]
+        data = [
+            {k: v['value'] for k, v in zip(data['header'], row)}
+            for row in data['data']
+        ]
+    else:
+        data = resp.json()
+        assert resp.status_code == 200, pformat(data)
+        assert '_data' in data, pformat(data)
+        data = data['_data']
+        keys = keys or sorted({
+            k
+            for d in flatten(data)
+            for k in d
+            if not k.startswith('_')
+        })
+    if len(keys) == 1:
+        k = keys[0]
+        data = [take(k, row) for row in data]
+    else:
+        data = [tuple(take(k, row) for k in keys) for row in data]
+    if sort is True:
+        data = sorted(data)
+    elif sort:
+        data = sorted(data, key=operator.itemgetter(keys.index(sort)))
+    return data
+
+
+def pushdata(app: TestClient, *args) -> Union[dict, list]:
+    model: str
+    data: Union[list, dict]
+    if len(args) == 2:
+        model, data = args
+    else:
+        model = '/'
+        data, = args
+    multiple = isinstance(data, list)
+    if multiple:
+        data = {'_data': data}
+        resp = app.post(model, json=data)
+    else:
+        resp = app.post(model, json=data)
+    try:
+        resp.raise_for_status()
+    except requests.HTTPError as e:
+        raise Exception(
+            f"pushdata error: model={model}, error={e}, data: {data!r}"
+        )
+    data = resp.json()
+    if multiple:
+        return sorted(data['_data'], key=lambda x: x['_id'])
+    else:
+        return data
