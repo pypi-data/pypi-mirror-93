09236d1ad101b2c2ed4fffd2a1752923a35a7bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:02 +0000

    Add package fairworkflows/0.2.3/fairworkflows-0.2.3-py3-none-any.whl

---
+++ packages/fairworkflows/fairworkflows-0.2.3-py3-none-any.whl/fairworkflows/config.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+PACKAGE_DIR = Path(__file__).parent.absolute()
+ROOT_DIR = PACKAGE_DIR.parent
+TESTS_ROOT = ROOT_DIR / 'tests'
+TESTS_RESOURCES = TESTS_ROOT / 'resources'
+
+# Generated workflow directory names
+PYTHON_DIR = 'scripts'
+PLEX_DIR = 'plex'
+
+DUMMY_FAIRWORKFLOWS_URI = 'http://fairworkflows.org'
