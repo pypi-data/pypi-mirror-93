e3e417bcea56d16405c81d05f31a87c4dc070e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:13 +0000

    Add package ribs/0.2.0/ribs-0.2.0.tar.gz

---
+++ packages/ribs/ribs-0.2.0.tar.gz/ribs-0.2.0/HISTORY.md
@@ -0,0 +1,17 @@
+# History
+
+## 0.2.0 (2021-01-29)
+
+- Alpha release
+
+## 0.1.0 (2020-09-11)
+
+- Test release
+
+### 0.1.1 (2021-01-29)
+
+- Test release
+
+## 0.0.0 (2020-09-11)
+
+- pyribs begins
---
+++ packages/ribs/ribs-0.2.0.tar.gz/ribs-0.2.0/ribs/__init__.py
@@ -0,0 +1,14 @@
+"""A bare-bones quality diversity optimization library."""
+
+__author__ = """ICAROS Lab"""
+__email__ = "tjanaka@usc.edu"
+__version__ = "0.2.0"
+
+from ribs import archives, emitters, factory, optimizers
+
+__all__ = [
+    "archives",
+    "emitters",
+    "optimizers",
+    "factory",
+]
