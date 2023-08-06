aadd77911df8b310a55d2555fae3bb55304b7248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package cdk-fargate-express/0.4.138/cdk_fargate_express-0.4.138-py3-none-any.whl

---
+++ packages/cdk-fargate-express/cdk_fargate_express-0.4.138-py3-none-any.whl/cd_fargate_express/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_ecs._jsii
+import aws_cdk.aws_ecs_patterns._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-fargate-express",
+    "0.4.138",
+    __name__[0:-6],
+    "cdk-fargate-express@0.4.138.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
