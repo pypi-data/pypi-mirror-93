666cdd1e9bdfab0e22d2b15ce35354ece3037a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:04 +0000

    Add package toolforge/5.0.0b1/toolforge-5.0.0b1.tar.gz

---
+++ packages/toolforge/toolforge-5.0.0b1.tar.gz/toolforge-5.0.0b1/README.rst
@@ -0,0 +1,14 @@
+=========
+toolforge
+=========
+
+Toolforge is a Python library that contains helper functions for different
+tasks.
+
+See wikitech_ for
+the full documentation.
+
+Licensed under the GPL v3 or any later version. See COPYING for the full
+details.
+
+.. _wikitech: https://wikitech.wikimedia.org/wiki/User:Legoktm/toolforge_library
---
+++ packages/toolforge/toolforge-5.0.0b1.tar.gz/toolforge-5.0.0b1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='toolforge',
+    version='5.0.0b1',
+    packages=['toolforge'],
+    package_data={'toolforge': ['py.typed']},
+    url='https://wikitech.wikimedia.org/wiki/User:Legoktm/toolforge_library',
+    license='GPL-3.0+',
+    author='Kunal Mehta',
+    author_email='legoktm@member.fsf.org',
+    description='Small library for common tasks on Wikimedia Toolforge',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'requests',
+        'pymysql<0.10;python_version<"3.5"',
+        'pymysql;python_version>="3.5"',
+        'typing;python_version<"3.5"'
+    ],
+    python_requires='>=3.4',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite="tests",
+)
