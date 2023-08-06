cedcf1b685016d5cfbf8e9a98c3383401394c394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package basically-ti-basic/0.1.5/basically-ti-basic-0.1.5.tar.gz

---
+++ packages/basically-ti-basic/basically-ti-basic-0.1.5.tar.gz/basically-ti-basic-0.1.5/basically_ti_basic/__init__.py
@@ -0,0 +1,4 @@
+from .__version__ import __title__, __description__, __url__, __version__, __author__, __author_email__, __license__, __copyright__
+# from basically_ti_basic.compiler.__init__ import PrgmCompiler
+# from basically_ti_basic.files.__init__ import TIPrgmFile
+from basically_ti_basic.main import compile_file, decompile_file
