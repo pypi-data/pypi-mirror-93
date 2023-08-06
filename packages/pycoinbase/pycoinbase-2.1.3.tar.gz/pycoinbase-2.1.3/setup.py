6a384cf86cbe24ab8243582343d7a97f88c069e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:29 +0000

    Add package pycoinbase/2.1.3/pycoinbase-2.1.3.tar.gz

---
+++ packages/pycoinbase/pycoinbase-2.1.3.tar.gz/pycoinbase-2.1.3/setup.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import os
+from setuptools import setup
+import pycoinbase.wallet
+
+README = open(os.path.join(os.path.dirname(__file__), 'PYPIREADME.rst')).read()
+REQUIREMENTS = [
+    line.strip() for line in open(os.path.join(os.path.dirname(__file__),
+                                               'requirements.txt')).readlines()]
+
+setup(
+    name='pycoinbase',
+    version=pycoinbase.wallet.__version__,
+    packages=['pycoinbase', 'pycoinbase.wallet'],
+    include_package_data=True,
+    license='Apache 2.0',
+    description='Updated Coinbase API client library',
+    long_description=README,
+    url='https://github.com/anton-stefanovich/pycoinbase/',
+    keywords=['api', 'coinbase', 'bitcoin', 'oauth2', 'client'],
+    install_requires=REQUIREMENTS,
+    author='Anton Stefanovich',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
