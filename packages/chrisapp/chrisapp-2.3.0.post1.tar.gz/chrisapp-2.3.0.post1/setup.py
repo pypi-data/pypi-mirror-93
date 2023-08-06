1907fca65d4451a5ed5598977f3b32efce388de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:13 +0000

    Add package chrisapp/2.3.0.post1/chrisapp-2.3.0.post1.tar.gz

---
+++ packages/chrisapp/chrisapp-2.3.0.post1.tar.gz/chrisapp-2.3.0.post1/setup.py
@@ -0,0 +1,21 @@
+from os import path
+from setuptools import setup
+
+with open(path.join(path.dirname(path.abspath(__file__)), 'README.rst')) as f:
+    readme = f.read()
+
+setup(
+    name             = 'chrisapp',
+    version          = '2.3.0-1',
+    description      = 'Superclass for Chris plugin apps',
+    long_description = readme,
+    author           = 'FNNDSC',
+    author_email     = 'dev@babymri.org',
+    url              = 'https://github.com/FNNDSC/chrisapp',
+    packages         = ['chrisapp'],
+    test_suite       = 'nose.collector',
+    tests_require    = ['nose==1.3.7'],
+    license          = 'MIT',
+    zip_safe         = False,
+    python_requires = '>=3.7'
+)
