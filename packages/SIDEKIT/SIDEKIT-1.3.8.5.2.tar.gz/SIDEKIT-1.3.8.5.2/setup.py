34a4de1982d10e97799cb672b498cdae89372db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:19 +0000

    Add package SIDEKIT/1.3.8.5.2/SIDEKIT-1.3.8.5.2.tar.gz

---
+++ packages/SIDEKIT/SIDEKIT-1.3.8.5.2.tar.gz/SIDEKIT-1.3.8.5.2/setup.py
@@ -0,0 +1,72 @@
+from distutils.core import setup
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+def get_sidekit_version():
+    with open('sidekit/__init__.py') as f:
+        s = [l.rstrip() for l in f.readlines()]
+        version = None
+        for l in s:
+            if '__version__' in l:
+                version = l.split('=')[-1]
+        if version is None:
+            raise RuntimeError('Can not detect version from sidekit/__init__.py')
+        return eval(version)
+
+setup(
+    name='SIDEKIT',
+    version=get_sidekit_version(),
+    author='Anthony Larcher',
+    author_email='anthony.larcher@univ-lemans.fr',
+    packages=['sidekit', 'sidekit.bosaris', 'sidekit.frontend', 'sidekit.libsvm', 'sidekit.nnet'],
+    url='http://www-lium.univ-lemans.fr/sidekit/',
+    download_url='http://pypi.python.org/pypi/Sidekit/',
+    license='LGPL',
+    platforms=['Linux, Windows', 'MacOS'],
+    description='Speaker, Language Recognition and Diarization package.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy",
+        "pyparsing>=2.4.7",
+        "python-dateutil >=2.8.1",
+        "scipy",
+        "six>=1.15.0",
+        "matplotlib>=3.3.2",
+        "soundfile",
+        "torch",
+        "torchvision",
+        "tqdm>=4.55.1",
+        "pyyaml",
+        "h5py>=2.10.0",
+        "pandas>=1.2.1",
+        "scikit-learn"
+    ],
+    package_data={'sidekit': ['docs/*']},
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'Environment :: MacOS X',
+                 'Environment :: Win32 (MS Windows)',
+                 'Environment :: X11 Applications',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Legal Industry',
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft',
+                 'Operating System :: Other OS',
+                 'Operating System :: POSIX',
+                 'Programming Language :: Python :: 3.8',
+                 'Topic :: Multimedia :: Sound/Audio :: Speech',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence']
+)
+
+
+
+
