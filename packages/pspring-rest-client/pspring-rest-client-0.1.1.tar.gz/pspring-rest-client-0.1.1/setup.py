73acf059bab4648c5753ccae33a8c5e8c60c115b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:51 +0000

    Add package pspring-rest-client/0.1.1/pspring-rest-client-0.1.1.tar.gz

---
+++ packages/pspring-rest-client/pspring-rest-client-0.1.1.tar.gz/pspring-rest-client-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pspring-rest-client',
+    version='0.1.1',
+    license='TBD',
+    author='Vasudevan Palani',
+    author_email='vasudevan.palani@gmail.com',
+    url='https://github.com/vasudevan-palani/pspring-rest-client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['pspringrestclient'],
+    install_requires=["pspring>=0.0.1"],
+    include_package_data=True,
+    description="A rest client for easier life",
+)
