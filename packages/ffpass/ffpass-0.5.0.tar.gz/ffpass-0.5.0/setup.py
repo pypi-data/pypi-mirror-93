b3c83ffe9d22cc55b421d741befd3fe6ff7a7a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package ffpass/0.5.0/ffpass-0.5.0.tar.gz

---
+++ packages/ffpass/ffpass-0.5.0.tar.gz/ffpass-0.5.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="ffpass",
+    version="0.5.0",
+    author="Louis Abraham",
+    license="MIT",
+    author_email="louis.abraham@yahoo.fr",
+    description="Import and Export passwords for Firefox",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/louisabraham/ffpass",
+    packages=["ffpass"],
+    install_requires=["pyasn1", "pycryptodome"],
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["ffpass = ffpass:main"]},
+    classifiers=["Topic :: Utilities", "Topic :: Security :: Cryptography"],
+)
