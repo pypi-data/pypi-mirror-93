39d499706da8bade2ec735f0f0c8c8cb33abf8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:12 +0000

    Add package Jaidev-Chaudhary/0.0.1/Jaidev%20Chaudhary-0.0.1.tar.gz

---
+++ packages/Jaidev-Chaudhary/Jaidev%20Chaudhary-0.0.1.tar.gz/Jaidev Chaudhary-0.0.1/README.txt
@@ -0,0 +1,1 @@
+Basic test for creating packages, addnumbers, multipynumbers, divide
---
+++ packages/Jaidev-Chaudhary/Jaidev%20Chaudhary-0.0.1.tar.gz/Jaidev Chaudhary-0.0.1/calculator/__init__.py
@@ -0,0 +1,11 @@
+def add_numbers(x, y):
+    return x+y
+
+def subtract_numbers(x, y):
+    return x-y
+
+def multipy_numbers(x, y):
+    return x*y
+
+def divide_numbers(x, y):
+    return x//y
