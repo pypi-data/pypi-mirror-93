a341c54975f0da05669f5bfdca84066ed7a40ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:33 +0000

    Add package pgn2data/0.0.1/pgn2data-0.0.1.tar.gz

---
+++ packages/pgn2data/pgn2data-0.0.1.tar.gz/pgn2data-0.0.1/README.md
@@ -0,0 +1,33 @@
+# pgn2data library
+
+A library that converts a chess pgn files into tabulated data sets.
+
+A pgn file can contain one or multiple chess games. The library parses the pgn file and creates two csv files:
+
+- Games file: contains high level information (e.g. date, site, event, score, players etc...)
+
+- Moves file: contains the moves for each game  (e.g. notation, squares, fen position, is in check etc...)
+
+The two files can be mapped together using a GUID which the process inserts into both files.
+
+
+## Implementation
+
+Here is a basic example of how to convert a PGN file:
+
+    import PGNData
+    
+    pgn_data = PGNData("data/pgn/tal_bronstein_1982.pgn")
+    result = pgn_data.export()
+    result.print_summary()
+    
+The Result object is a return value from the process, which allows you to check whether the dataset creation was successful.
+
+To group multiple files into the same output file you can do the following:
+
+    pgn_data = PGNData(["data/pgn/file1.pgn","data/pgn/file/file2.pgn"],"output")
+    result = pgn_data.export()
+    result.print_summary()
+    
+This process the two pgn files in the specified list and exports them to file called "output.csv".
+
---
+++ packages/pgn2data/pgn2data-0.0.1.tar.gz/pgn2data-0.0.1/testing/test.py
@@ -0,0 +1,83 @@
+import unittest
+from converter.board_ref import BoardPieces
+import logging
+from converter.fen import FenStats
+import chess
+from common.common import full_range
+
+
+class BoardRefTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def run_piece_at_square_test(self):
+        bp = BoardPieces()
+        self.__moving_piece("e2", "e4", bp)
+        self.__moving_piece("e7", "e6", bp)
+        self.__moving_piece("g1", "f3", bp)
+
+    def __moving_piece(self, from_square, to_square, bp):
+        from_before = bp.get_piece_at_square(from_square)
+        to_before = bp.get_piece_at_square(to_square)
+        bp.track_move(from_square, to_square)
+        from_after = bp.get_piece_at_square(from_square)
+        to_after = bp.get_piece_at_square(to_square)
+        self.assertEqual(bp.get_piece_at_square(to_square), from_before)
+        self.assertEqual(bp.get_piece_at_square(from_square), "")
+        self.assertNotEqual(to_before, to_after)
+        self.assertNotEqual(from_before, from_after)
+
+
+class FenTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def run_test(self):
+        fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"
+        fs = FenStats(fen)
+        white_total, black_total = fs.get_total_piece_count()
+        self.assertEqual(white_total, 16)
+        self.assertEqual(black_total, 16)
+        capture_white = fs.get_captured_score(chess.WHITE)
+        capture_black = fs.get_captured_score(chess.BLACK)
+        self.assertEqual(capture_white, 0)
+        self.assertEqual(capture_black, 0)
+        piece_count = fs.get_piece_count(chess.ROOK, chess.WHITE)
+        self.assertEqual(piece_count, 2)
+        piece_count = fs.get_piece_count(chess.QUEEN, chess.BLACK)
+        self.assertEqual(piece_count, 1)
+
+        valuations = [(8, 31, 0, 0),
+                      (7, 7, 0, 0),
+                      (0, 0, 0, 0),
+                      (1, 1, 0, 0),
+                      (0, 0, 0, 0),
+                      (0, 0, 0, 0),
+                      (0, 0, 8, 8),
+                      (0, 0, 8, 31)]
+
+        for r in full_range(1, 8):
+            result = fs.get_piece_count_and_value_for_fen_row(r)
+            self.assertEqual(result, valuations[r - 1])
+
+
+def board_test():
+    test_board_ref = BoardRefTestCase()
+    test_board_ref.run_piece_at_square_test()
+
+
+def fen_stat_tests():
+    test_fen = FenTestCase()
+    test_fen.run_test()
+
+
+def run_all_tests():
+    log = logging.getLogger("pgn2data")
+    logging.basicConfig(level=logging.INFO)
+
+    log.info("Start testing")
+    board_test()
+    fen_stat_tests()
+    log.info("End testing")
