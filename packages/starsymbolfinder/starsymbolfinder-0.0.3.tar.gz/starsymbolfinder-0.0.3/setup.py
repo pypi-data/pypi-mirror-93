50ed3369c003b79424728bfc87f736080cfe8870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package starsymbolfinder/0.0.3/starsymbolfinder-0.0.3.tar.gz

---
+++ packages/starsymbolfinder/starsymbolfinder-0.0.3.tar.gz/starsymbolfinder-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='starsymbolfinder',
+  version='0.0.3',
+  description='A package that finds a star symbol for a specified date.',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/DefaultGamingChannel',
+  author='DefaultGamingChannel',
+  author_email='info@picstreme.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='starsymbol',
+  packages=find_packages(),
+  install_requires=['']
+)
