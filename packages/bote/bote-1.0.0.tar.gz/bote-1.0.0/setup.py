5b621f8acc6e57cc20d4ab19bb06f772e5ef4622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:53 +0000

    Add package bote/1.0.0/bote-1.0.0.tar.gz

---
+++ packages/bote/bote-1.0.0.tar.gz/bote-1.0.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bote",
+    version="1.0.0",
+    author="Rüdiger Voigt",
+    author_email="projects@ruediger-voigt.eu",
+    description="Send email messages and enforce encryption.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RuedigerVoigt/bote",
+    package_data={"bote": ["py.typed"]},
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["compatibility>=0.8.0", "userprovided>=0.8.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Monitoring",
+        "Topic :: Communications :: Email",
+        "Topic :: Security :: Cryptography"
+    ],
+)
