9b78eba45f08cd568998bacce7d24a977eb2ceef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package honeybee-core/1.42.2/honeybee-core-1.42.2.tar.gz

---
+++ packages/honeybee-core/honeybee-core-1.42.2.tar.gz/honeybee-core-1.42.2/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="honeybee-core",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    author="Ladybug Tools",
+    author_email="info@ladybug.tools",
+    description="Honeybee is a Python library to create, run and visualize the results of environmental simulation. See extensions (e.g. honeybee_radiance) for specific simulation type.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ladybug-tools/honeybee-core",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={
+        'cli': ['click==7.1.2', "honeybee-schema==1.42.0;python_version>='3.6'"]
+    },
+    entry_points={
+        "console_scripts": ["honeybee = honeybee.cli:main"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent"
+    ],
+    license="AGPL-3.0"
+)
