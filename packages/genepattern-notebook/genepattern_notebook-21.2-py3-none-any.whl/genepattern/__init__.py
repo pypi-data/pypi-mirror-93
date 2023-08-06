5b600ba54cf8ab94cb76e2e7723341a99a5e023d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:35 +0000

    Add package genepattern-notebook/21.2/genepattern_notebook-21.2-py3-none-any.whl

---
+++ packages/genepattern-notebook/genepattern_notebook-21.2-py3-none-any.whl/genepattern/__init__.py
@@ -0,0 +1,18 @@
+"""
+GenePattern Notebook extension for Jupyter
+
+Copyright 2015-2020 Regents of the University of California & Broad Institute
+"""
+
+from .remote_widgets import GPAuthWidget, GPTaskWidget, GPJobWidget, session, register_session, get_session, display
+from nbtools import UIBuilder as GPUIBuilder, UIOutput as GPUIOutput, build_ui, open
+from .local_widgets import GPModuleWidget
+from .jupyter_extensions import (GenePatternMagic, load_ipython_extension, load_jupyter_server_extension,
+                                 _jupyter_server_extension_paths, _jupyter_nbextension_paths)
+
+
+__author__ = 'Thorin Tabor'
+__copyright__ = 'Copyright 2014-2021, Regents of the University of California & Broad Institute'
+__version__ = '21.02'
+__status__ = 'Production/Stable'
+__license__ = 'BSD-3-Clause'
