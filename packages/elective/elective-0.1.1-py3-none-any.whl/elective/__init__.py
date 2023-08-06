8473815bfb320dcf50ceae5b16416ba9b9969f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:52 +0000

    Add package elective/0.1.1/elective-0.1.1-py3-none-any.whl

---
+++ packages/elective/elective-0.1.1-py3-none-any.whl/elective/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# elective, a python program configuration option loader.
+# Copyright (C) 2021 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+"""``elective``:  a CSS naming hierarchy enforcer."""
+
+from .config import Config
