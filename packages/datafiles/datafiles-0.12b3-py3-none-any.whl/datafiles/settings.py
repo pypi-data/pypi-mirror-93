64c1ac7978689de652a2e5cadc99f7153a60018c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:50 +0000

    Add package datafiles/0.12b3/datafiles-0.12b3-py3-none-any.whl

---
+++ packages/datafiles/datafiles-0.12b3-py3-none-any.whl/datafiles/settings.py
@@ -0,0 +1,11 @@
+"""Shared configuration flags."""
+
+HIDE_TRACEBACK_IN_HOOKS = True
+
+HOOKS_ENABLED = True
+
+INDENT_YAML_BLOCKS = True
+
+MINIMIZE_LIST_DIFFS = True
+
+YAML_LIBRARY = 'ruamel.yaml'
