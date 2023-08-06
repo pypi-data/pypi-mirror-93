f0ded157c9ff75778d5815d4dbda61f47d414b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:45 +0000

    Add package hum/0.0.10/hum-0.0.10-py3-none-any.whl

---
+++ packages/hum/hum-0.0.10-py3-none-any.whl/hum/util.py
@@ -0,0 +1,38 @@
+from warnings import warn
+from inspect import getmodule
+
+
+def getmodulename(obj, default=''):
+    """Get name of module of object"""
+    return getattr(getmodule(obj), '__name__', default)
+
+
+class ModuleNotFoundErrorNiceMessage:
+    def __init__(self, msg=None):
+        self.msg = msg
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is ModuleNotFoundError:
+            msg = self.msg or f"""
+It seems you don't have required `{exc_val.name}` package for this Store.
+Try installing it by running:
+
+    pip install {exc_val.name}
+
+in your terminal.
+For more information: https://pypi.org/project/{exc_val.name}
+            """
+            warn(msg)
+
+
+class ModuleNotFoundIgnore:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is ModuleNotFoundError:
+            pass
+        return True
