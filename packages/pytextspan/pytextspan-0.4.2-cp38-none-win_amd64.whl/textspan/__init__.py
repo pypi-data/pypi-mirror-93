cf02fb6eede7a6a305f7320765963ef95187679e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:10 +0000

    Add package pytextspan/0.4.2/pytextspan-0.4.2-cp38-none-win_amd64.whl

---
+++ packages/pytextspan/pytextspan-0.4.2-cp38-none-win_amd64.whl/textspan/__init__.py
@@ -0,0 +1,17 @@
+from textspan.textspan import (
+    get_original_spans,
+    align_spans_by_mapping,
+    align_spans,
+    remove_span_overlaps,
+    lift_spans_index,
+    lift_span_index,
+)
+
+__all__ = [
+    "get_original_spans",
+    "align_spans_by_mapping",
+    "align_spans",
+    "remove_span_overlaps",
+    "lift_span_index",
+    "lift_spans_index",
+]
