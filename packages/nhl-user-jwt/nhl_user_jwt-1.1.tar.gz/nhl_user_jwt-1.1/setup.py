6e658bc4cc77fd587fade5ec7edaff69f5188f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package nhl-user-jwt/1.1/nhl_user_jwt-1.1.tar.gz

---
+++ packages/nhl-user-jwt/nhl_user_jwt-1.1.tar.gz/nhl_user_jwt-1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nhl_user_jwt",
+    version="1.1",
+    author="ligo",
+    author_email="ligocz@dingtalk.com",
+    description="jwt生成和验签",
+    url="https://www.nihuola.vip/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'python-jose>=3.2.0',
+        'passlib>=1.7.4',
+        'redis>=3.5.2'
+    ]
+)
+
+
