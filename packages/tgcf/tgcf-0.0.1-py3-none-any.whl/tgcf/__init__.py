6d34d6fb1fa452cf064ec0f6e0fdda3914d60aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:28 +0000

    Add package tgcf/0.0.1/tgcf-0.0.1-py3-none-any.whl

---
+++ packages/tgcf/tgcf-0.0.1-py3-none-any.whl/tgcf/__init__.py
@@ -0,0 +1,4 @@
+''' A simple script to forward all the messages of one chat (indivisual/group/channel) to another. Made using Telethon. Can be used for backup or cloning.
+'''
+
+__version__ = "0.0.1"
