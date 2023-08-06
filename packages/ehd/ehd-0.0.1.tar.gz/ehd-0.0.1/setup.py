94e34fb7fa6d232cfc6cd0ff1fcd497f8cbbbf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:33 +0000

    Add package ehd/0.0.1/ehd-0.0.1.tar.gz

---
+++ packages/ehd/ehd-0.0.1.tar.gz/ehd-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ehd", # Replace with your own username
+    version="0.0.1",
+    author="Njeru",
+    author_email="njerudenis0@gmail.com",
+    description="To use for my projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Denisnjeru/EHD.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.*',
+)
