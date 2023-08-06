4dc9040884bb3c097bc6a197102eab86210c83e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:29 +0000

    Add package fcutils/1.1.1/fcutils-1.1.1.tar.gz

---
+++ packages/fcutils/fcutils-1.1.1.tar.gz/fcutils-1.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_namespace_packages
+
+requirements = [
+    "numpy",
+    "opencv-python",
+    "pandas",
+    "matplotlib",
+    "seaborn",
+    "scipy",
+    "pyyaml",
+    "statsmodels",
+]
+
+setup(
+    name="fcutils",
+    version="1.1.1",
+    author_email="federicoclaudi@protonmail.com",
+    description="bunch of utility functions",
+    packages=find_namespace_packages(exclude=()),
+    include_package_data=True,
+    url="https://github.com/FedeClaudi/fcutils",
+    author="Federico Claudi",
+    zip_safe=False,
+    install_requires=requirements,
+)
