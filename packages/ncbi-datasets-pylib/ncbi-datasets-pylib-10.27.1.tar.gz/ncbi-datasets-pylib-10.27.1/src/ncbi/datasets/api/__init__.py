654cf6df1ff9c1853dedc4b9be7d65c6dac6bb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:47 +0000

    Add package ncbi-datasets-pylib/10.27.1/ncbi-datasets-pylib-10.27.1.tar.gz

---
+++ packages/ncbi-datasets-pylib/ncbi-datasets-pylib-10.27.1.tar.gz/ncbi-datasets-pylib-10.27.1/src/ncbi/datasets/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ncbi.datasets.api.gene_api import GeneApi
+from ncbi.datasets.api.genome_api import GenomeApi
+from ncbi.datasets.api.version_api import VersionApi
+from ncbi.datasets.api.virus_api import VirusApi
