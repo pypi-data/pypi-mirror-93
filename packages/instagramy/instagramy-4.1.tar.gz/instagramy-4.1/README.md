7afde067c9c2b4ccb6d40928dd1359e999633b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:15 +0000

    Add package instagramy/4.1/instagramy-4.1.tar.gz

---
+++ packages/instagramy/instagramy-4.1.tar.gz/instagramy-4.1/README.md
@@ -0,0 +1,118 @@
+<h1 align="center"> Instagramy </h1>
+
+<p align="center">Python Package for Instagram Without Any external dependencies</p>
+
+<ul>
+</ul>
+
+<p align="center">
+    <a href="https://pypi.org/project/instagramy/">
+    <img alt="PyPi" src="https://img.shields.io/pypi/v/instagramy.svg"/>
+    </a>
+    <a href="https://pepy.tech/project/instagramy">
+    <img alt="Downloads" src="https://pepy.tech/badge/instagramy"/>
+    </a>
+    <a href="https://github.com/yogeshwaran01/instagramy/blob/master/LICENSE.txt">
+    <img alt="GitHub license" src="https://img.shields.io/github/license/yogeshwaran01/instagramy?style=plastic"/>
+    </a>
+    <a href="https://github.com/psf/black">
+    <img alt="Code style" src="https://img.shields.io/badge/codestyle-Black-blue"/>
+    </a>
+    <img alt="GitHub Repo size" src="https://img.shields.io/github/repo-size/yogeshwaran01/instagramy"/>
+</p>
+
+</hr>
+
+<p align="center">
+Scrape Instagram Users Informations, Posts Details, and Hashtags details. This Package scrapes the user's recent posts with some information like likes, comments, captions and etc. No login required and any dependencies.
+</p>
+
+## Download
+
+### Installation
+
+```bash
+
+pip install instagramy
+
+```
+
+### Upgrade
+
+```bash
+
+pip install --upgrade instagramy
+
+```
+## Usage
+
+### Instagram User details
+
+Class `InstagramUser` scrape some of the information related to the user of the Instagram
+
+#### Properties
+
+- biography
+- fullname
+- is_private
+- is_verified
+- number_of_followers
+- number_of_followings
+- number_of_posts
+- other_info
+- posts
+- posts_display_urls
+- profile_picture_url
+- username
+- website
+
+<img src="https://raw.githubusercontent.com/yogeshwaran01/instagramy/master/samples/user.png" width=100% height=100%>
+
+### Instagram Hashtag details
+
+Class `InstagramHashTag`  scrape some of the information related to the hash-tag of the Instagram
+
+#### Properties
+
+- number_of_posts
+- posts_display_urls
+- profile_pic_url
+- tagname
+- top_posts
+
+<img src="https://raw.githubusercontent.com/yogeshwaran01/instagramy/master/samples/hashtag.png" width=100% height=100%>
+
+### Instagram Post details
+
+Class `InstagramPost`  scrape some of the information related to the particular post of Instagram. It takes the post id as the parameter. You can get the post id from the URL of the Instagram posts from the property of `InstagramUser.posts`. or `InstagramHagTag.top_posts`
+
+#### Properties
+
+- author
+- caption
+- description
+- number_of_comments
+- number_of_likes
+- post_detail
+- uploaded_date
+
+<img src="https://raw.githubusercontent.com/yogeshwaran01/instagramy/master/samples/post.png" width=100% height=100%>
+
+## Note
+
+- Don't send multiple request, If you send multiple request, the Instagram redirect to login page, If you send multiple request, reboot your pc or change the ip or try after somtimes.
+- This Package does not work in Remote PC or any Online python Interpreter
+- This Package not scrap all the posts from a certain account, limit of the posts only 12 ( For non-private account)
+- This Package not scrap all the posts of certain hash-tags it only scrapes the top 60 - 70 posts in certain hash-tags.
+
+### Sample-Scripts
+
+Some sample scripts based on this package
+
+- [👦 Download Instagram DP](https://github.com/yogeshwaran01/Python-Scripts/blob/master/Scripts/instadp.py)
+
+- [📊 Analysis Instagram Accounts with Matplotlib](https://github.com/yogeshwaran01/Python-Scripts/blob/master/Scripts/instalysis.py)
+
+- [#️⃣ Bulk Instagram Hashtag Posts Download](https://github.com/yogeshwaran01/Python-Scripts/blob/master/Scripts/instagram_hastags_post.py)
+
+<h3 align="center"> Made with Python ❤️ </h3>
---
+++ packages/instagramy/instagramy-4.1.tar.gz/instagramy-4.1/instagramy/InstagramUser.py
@@ -0,0 +1,193 @@
+"""
+    instagramy.InstagramUser
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This module scrape data
+    of given Instagram User.
+
+    Usage Example
+    -------------
+    ::
+
+        from instagramy.InstagramUser import InstagramUser
+
+        >>> user = InstagramUser('github')
+        >>> user.is_verified
+        >>> user.number_of_followers
+        >>> user.biography
+
+"""
+
+from .core.parser import ParseUser
+from .core.cache import Cache
+from .core.requests import get
+from .core.exceptions import UsernameNotFound
+from .core.exceptions import HTTPError
+
+
+class InstagramUser:
+    """
+    Class InstagramUser scrapes instagram user information
+    >>> instagram_user = InstagramUser("github")
+    >>> instagram_user.is_verified
+    True
+    >>> instagram_user.biography
+    'Built for developers.'
+    """
+
+    def __init__(self, username: str, from_cache=True):
+
+        self.url = f"https://www.instagram.com/{username}/"
+        if from_cache:
+            cache = Cache("user")
+            if cache.is_exists(username):
+                self.user_data = cache.read_cache(username)
+            else:
+                self.user_data = self.get_json()
+                cache.make_cache(username, self.user_data)
+        else:
+            self.user_data = self.get_json()
+
+    def get_json(self) -> dict:
+        """
+        Return a dict of user information
+        """
+        try:
+            html = get(self.url)
+        except HTTPError:
+            raise UsernameNotFound(self.url.split("/")[-2])
+
+        parser = ParseUser()
+        parser.feed(html)
+        return parser.Data
+
+    @property
+    def username(self) -> str:
+        """ Username of the given user """
+        return self.user_data["username"]
+
+    @property
+    def fullname(self) -> str:
+        """ Fullname of the given user """
+        return self.user_data["full_name"]
+
+    @property
+    def biography(self) -> str:
+        """ Biography of the given user """
+        return self.user_data["biography"]
+
+    @property
+    def website(self) -> str:
+        """ Website of the given user """
+        return self.user_data["external_url"]
+
+    @property
+    def number_of_followers(self) -> int:
+        """ No.of Followers of the given user """
+        return self.user_data["edge_followed_by"]["count"]
+
+    @property
+    def number_of_followings(self) -> int:
+        """ No.of Following of the given user """
+        return self.user_data["edge_follow"]["count"]
+
+    @property
+    def number_of_posts(self) -> int:
+        """ No.of Post of the given user """
+        return self.user_data["edge_owner_to_timeline_media"]["count"]
+
+    @property
+    def profile_picture_url(self) -> str:
+        """ Profile picture url of the Given User """
+        return self.user_data["profile_pic_url_hd"]
+
+    @property
+    def is_verified(self) -> bool:
+        """ Verification status of the user """
+        return self.user_data["is_verified"]
+
+    @property
+    def is_private(self) -> bool:
+        """ Account type is Private """
+        return self.user_data["is_private"]
+
+    @property
+    def posts(self) -> list:
+        """
+        Top 12 posts data of the given user
+        """
+
+        posts_lists = []
+        posts_details = self.user_data["edge_owner_to_timeline_media"]["edges"]
+        for i in posts_details:
+            data = {}
+            try:
+                data["likes"] = i["node"]["edge_liked_by"]["count"]
+            except (KeyError, TypeError):
+                data["likes"] = None
+            try:
+                data["comments"] = i["node"]["edge_media_to_comment"]["count"]
+            except (KeyError, TypeError):
+                data["comments"] = None
+            try:
+                data["caption"] = i["node"]["accessibility_caption"]
+            except (KeyError, TypeError):
+                data["caption"] = None
+            try:
+                data["is_video"] = i["node"]["is_video"]
+            except (KeyError, TypeError):
+                data["is_video"] = None
+            try:
+                data["timestamp"] = i["node"]["taken_at_timestamp"]
+            except (KeyError, TypeError):
+                data["timestamp"] = None
+            try:
+                data["location"] = i["node"]["location"]
+            except (KeyError, TypeError):
+                data["location"] = None
+            try:
+                data["shortcode"] = i["node"]["shortcode"]
+            except (KeyError, TypeError):
+                data["shortcode"] = None
+            try:
+                data[
+                    "post_url"
+                ] = f'https://www.instagram.com/p/{i["node"]["shortcode"]}/'
+            except (KeyError, TypeError):
+                data["post_url"] = None
+            try:
+                data["display_url"] = i["node"]["display_url"]
+            except (KeyError, TypeError):
+                data["display_url"] = None
+            posts_lists.append(data)
+        return posts_lists
+
+    @property
+    def posts_display_urls(self) -> list:
+        """
+        Top 12 posts picture url of the given user
+        """
+
+        return [i["display_url"] for i in self.posts]
+
+    @property
+    def other_info(self) -> dict:
+        """
+        Other information about user
+        """
+        return {
+            "is_private": self.user_data["is_private"],
+            "is_verified": self.user_data["is_verified"],
+            "is_business_account": self.user_data["is_business_account"],
+            "is_joined_recently": self.user_data["is_joined_recently"],
+            "has_ar_effects": self.user_data["has_ar_effects"],
+            "has_clips": self.user_data["has_clips"],
+            "has_guides": self.user_data["has_guides"],
+            "has_channel": self.user_data["has_channel"],
+        }
+
+    def __str__(self) -> str:
+        return f"{self.fullname} ({self.username}) -> {self.biography}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}('{self.username}')"
