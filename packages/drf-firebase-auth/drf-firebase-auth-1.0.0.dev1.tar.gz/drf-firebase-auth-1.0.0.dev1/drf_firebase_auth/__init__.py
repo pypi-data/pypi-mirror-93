19b22a6d0f82b3abc8fbe41fbcecb2ad891cd277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:19 +0000

    Add package drf-firebase-auth/1.0.0.dev1/drf-firebase-auth-1.0.0.dev1.tar.gz

---
+++ packages/drf-firebase-auth/drf-firebase-auth-1.0.0.dev1.tar.gz/drf-firebase-auth-1.0.0.dev1/drf_firebase_auth/__init__.py
@@ -0,0 +1,15 @@
+"""Metadata for drf-firebase-auth package."""
+__title__ = 'drf-firebase-auth'
+__version__ = '1.0.0dev1'
+__description__ = (
+    'Custom Django Rest Framework authentication backend for '
+    'parsing Firebase uid tokens and storing as local users.'
+)
+__url__ = 'https://gitlab.com/garyburgmann/drf-firebase-auth'
+__author__ = 'Gary Burgmann'
+__author_email__ = 'garyburgmann@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Gary Burgmann'
+
+# Version synonym
+VERSION = __version__
