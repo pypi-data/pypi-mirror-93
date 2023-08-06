5114734696f4aca9f0c11e1e206a2fcbabbeda3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:10 +0000

    Add package nextgentermux/1.3/nextgentermux-1.3.tar.gz

---
+++ packages/nextgentermux/nextgentermux-1.3.tar.gz/nextgentermux-1.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+
+  name = 'nextgentermux',
+  packages = ['nextgentermux'],
+  #scripts=['nextxgentermux/nextgen']
+  version = '1.3',
+  license='gpl-3.0', 
+  description = 'An one tap termux setup tool',
+  url = 'https://github.com/Ign0r3dH4x0r/NextGenTermux',
+  download_url = 'https://github.com/smsowmik/NextGenTermux/archive/1.1.tar.gz',
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',    
+    'Intended Audience :: Developers', 
+    'Topic :: Adaptive Technologies',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Unix Shell',  
+  ],
+
+
+)
