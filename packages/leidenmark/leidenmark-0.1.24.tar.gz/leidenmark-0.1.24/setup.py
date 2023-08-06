596448797166870d3768ebe415d507e25f93d22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:13 +0000

    Add package leidenmark/0.1.24/leidenmark-0.1.24.tar.gz

---
+++ packages/leidenmark/leidenmark-0.1.24.tar.gz/leidenmark-0.1.24/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name = 'leidenmark',
+    version = '0.1.24',
+    description = 'A markdown extension for converting Leiden+ epigraphic text to TEI Epidoc XML',
+    packages = find_packages(),
+    install_requires = ['markdown', 'lxml>=4.5', 'regex', 'dh_utils'],
+    keywords = 'Leiden+ Markdown TEI Epidoc XML HTML',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/andredelft/leidenmark",
+    python_requires = '>=3.6',
+    author = 'André van Delft',
+    author_email = 'andrevandelft@outlook.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3'
+    ]
+)
