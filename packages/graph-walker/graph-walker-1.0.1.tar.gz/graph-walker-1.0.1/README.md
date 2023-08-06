4f58e1247e280d0dc4897151a916ce3a972d3a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:25 +0000

    Add package graph-walker/1.0.1/graph-walker-1.0.1.tar.gz

---
+++ packages/graph-walker/graph-walker-1.0.1.tar.gz/graph-walker-1.0.1/README.md
@@ -0,0 +1,27 @@
+# How to install
+
+```
+pip install pybind11
+pip install graph-walker
+```
+
+# How to use
+
+```python
+import networkx as nx
+import walker
+
+# create a random graph
+G = nx.random_partition_graph([1000] * 15, .01, .001)
+
+# generate random walks
+X = walker.random_walks(G, n_walks=50, walk_len=25)
+
+# generate random walks according to Node2Vec methodology by specifying p and q
+X = walker.random_walks(G, n_walks=50, walk_len=25, p=.25, q=.25)
+
+# corrupt random walks by randomly changing nodes in random walks
+# `y` matrix has a size (N, walk_len - 1) with:
+# y[i, j] = 1 if nodes X[i, j] and X[i, j + 1] share an edge, 0 otherwise
+X, y = walker.corrupt(G, X, r=.1)
+```
---
+++ packages/graph-walker/graph-walker-1.0.1.tar.gz/graph-walker-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages, Extension
+from glob import glob
+import pybind11
+import os
+
+
+compiler_args = "-Ofast -mavx2 -march=native".split()
+ext_modules = [
+    Extension(
+        '_walker',
+        sorted(glob("src/*.cpp")),
+        language='c++',
+        include_dirs=[
+            pybind11.get_include(),
+            pybind11.get_include(True),],
+        extra_compile_args=compiler_args)
+]
+
+
+setup(
+    name="graph-walker",
+    version="1.0.1",
+    author="Maixent Chenebaux",
+    author_email="max.chbx@gmail.com",
+    description="Fastest library for random walks on graph",
+    url="https://github.com/kerighan/graph-walker",
+    packages=find_packages(),
+    install_requires=["networkx", "numpy", "scipy", "pybind11"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    ext_modules=ext_modules
+)
