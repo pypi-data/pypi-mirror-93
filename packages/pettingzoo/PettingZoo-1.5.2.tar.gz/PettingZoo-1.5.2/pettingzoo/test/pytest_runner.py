3bdaef446877410655d9de2200dd4b0e3e7f855d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:38 +0000

    Add package pettingzoo/1.5.2/PettingZoo-1.5.2.tar.gz

---
+++ packages/pettingzoo/PettingZoo-1.5.2.tar.gz/PettingZoo-1.5.2/pettingzoo/test/pytest_runner.py
@@ -0,0 +1,25 @@
+import pytest
+import pickle
+from .all_modules import all_environments
+from .api_test import api_test
+from .seed_test import seed_test
+from .parallel_test import parallel_play_test
+from .max_cycles_test import max_cycles_test
+import os
+
+
+@pytest.mark.parametrize(("name", "env_module"), list(all_environments.items()))
+def test_module(name, env_module):
+    _env = env_module.env()
+    assert str(_env) == os.path.basename(name)
+    api_test(_env)
+    if "classic/" not in name:
+        parallel_play_test(env_module.parallel_env())
+
+    if "prospector" not in name:
+        seed_test(env_module.env, 50)
+
+    max_cycles_test(env_module, name)
+
+    recreated_env = pickle.loads(pickle.dumps(_env))
+    api_test(recreated_env)
