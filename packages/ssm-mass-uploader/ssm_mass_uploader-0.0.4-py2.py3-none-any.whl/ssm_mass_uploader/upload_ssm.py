94d67af5c08c1df46167356f6c1da80fced5829c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:40 +0000

    Add package ssm-mass-uploader/0.0.4/ssm_mass_uploader-0.0.4-py2.py3-none-any.whl

---
+++ packages/ssm-mass-uploader/ssm_mass_uploader-0.0.4-py2.py3-none-any.whl/ssm_mass_uploader/upload_ssm.py
@@ -0,0 +1,37 @@
+from aws_utils.ssm import SSM
+from aws_utils.kms import KMS
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='Session options for boto3.')
+parser.add_argument('-keyid', '-k', type=str, help='KMS key id.')
+parser.add_argument('--profile', '--p', type=str, default='default',
+                    help='Name of profile in your aws credentails file. Default is "default"')
+parser.add_argument('--region', '--r', type=str, default='us-east-1',
+                    help='Region you want your ssm credentials uploaded to. Default is us-east-1')
+parser.add_argument('-file', '-f', type=str, default='secret.sandbox',
+                    help='File that contains encrypted secrets.')
+parser.add_argument('-stage', '-s', type=str, default='dev',
+                    help='Stage')
+
+args = parser.parse_args()
+
+
+def main(profile, region, keyid, file, stage):
+
+    file_bytes = open(file, 'rb')
+    decrypt = KMS(profile, region, keyid, file_bytes)
+    ssm_upload = SSM(profile, region, stage)
+    secrets = json.loads(decrypt.decrypt_file())['secrets']
+
+    try:
+        for secret in secrets:
+            ssm_upload.put_parameter(secret)
+
+        print('Secrets Uploaded.')
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main(args.profile, args.region, args.keyid, args.file, args.stage)
