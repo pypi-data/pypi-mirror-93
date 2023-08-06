1df046c41049f6b659a19972cddf659f7b269aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:59 +0000

    Add package airflow-django/0.7.0/airflow-django-0.7.0.tar.gz

---
+++ packages/airflow-django/airflow-django-0.7.0.tar.gz/airflow-django-0.7.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['airflow_django']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['apache-airflow>=1.10', 'django>=2.0']
+
+setup_kwargs = {
+    'name': 'airflow-django',
+    'version': '0.7.0',
+    'description': 'A kit for using Django features, like its ORM, in Airflow DAGs.',
+    'long_description': None,
+    'author': 'Paris Kasidiaris',
+    'author_email': 'paris@sourcelair.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
