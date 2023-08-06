4081360796f83706f2c87dd570e16a87da321185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:54 +0000

    Add package kfpxtend/1.1/kfpxtend-1.1.0-py3-none-any.whl

---
+++ packages/kfpxtend/kfpxtend-1.1.0-py3-none-any.whl/kfpxtend/__init__.py
@@ -0,0 +1,17 @@
+import kfp
+from ._client import *
+from ._operators import *
+from ._component_store import *
+from ._components import *
+from ._auth import *
+
+kfp.Client.page_size = 500
+kfp.Client.get_pipeline_by_name = get_pipeline_by_name
+kfp.Client.get_experiment_by_name = get_experiment_by_name
+kfp.Client.get_run_by_name = get_run_by_name
+kfp.Client.get_runs_by_experiment = get_runs_by_experiment
+kfp.Client.delete_pipeline_by_name = delete_pipeline_by_name
+kfp.Client.delete_experiment_by_name = delete_experiment_by_name
+kfp.Client.delete_run_by_name = delete_run_by_name
+kfp.Client.delete_runs_by_experiment = delete_runs_by_experiment
+kfp.Client.upload_pipeline_version = upload_pipeline_version
