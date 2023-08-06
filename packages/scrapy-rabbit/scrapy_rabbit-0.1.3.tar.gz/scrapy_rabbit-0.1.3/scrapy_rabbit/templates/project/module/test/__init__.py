68fd145e11a2b6ecb3629553ee3b533cedd41d39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:21 +0000

    Add package scrapy-rabbit/0.1.3/scrapy_rabbit-0.1.3.tar.gz

---
+++ packages/scrapy-rabbit/scrapy_rabbit-0.1.3.tar.gz/scrapy_rabbit-0.1.3/scrapy_rabbit/templates/project/module/test/__init__.py
@@ -0,0 +1,15 @@
+# encoding = uft-8
+"""
+@ File   :  __init__.py.py
+@ Author :  Eamonn
+@ Data   :  2019/10/30
+"""
+from eamonn import eamonn
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
