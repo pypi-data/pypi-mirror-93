6473332627192c32bbcd4253aa4ef4e03c77ab00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:08 +0000

    Add package gym-idsgame/1.0.8/gym_idsgame-1.0.8-py3-none-any.whl

---
+++ packages/gym-idsgame/gym_idsgame-1.0.8-py3-none-any.whl/gym_idsgame/envs/__init__.py
@@ -0,0 +1,28 @@
+from gym_idsgame.envs.idsgame_env import IdsGameEnv, IdsGameRandomAttackV0Env, \
+    IdsGameRandomDefenseV0Env, IdsGameV0Env, IdsGameRandomAttackV1Env, IdsGameRandomDefenseV1Env, IdsGameV1Env, \
+    IdsGameMinimalDefenseV0Env, IdsGameMinimalDefenseV1Env, IdsGameMaximalAttackV0Env, IdsGameMaximalAttackV1Env, \
+    IdsGameMaximalAttackV2Env, IdsGameMinimalDefenseV2Env, IdsGameRandomDefenseV2Env, IdsGameRandomAttackV2Env, \
+    IdsGameV2Env, IdsGameMaximalAttackV3Env, IdsGameMinimalDefenseV3Env, IdsGameRandomAttackV3Env, \
+    IdsGameRandomDefenseV3Env, IdsGameV3Env, IdsGameRandomDefenseV4Env, IdsGameMaximalAttackV4Env, \
+    IdsGameMinimalDefenseV4Env, IdsGameRandomAttackV4Env, IdsGameV4Env, IdsGameRandomDefenseV5Env, \
+    IdsGameMaximalAttackV5Env, IdsGameMinimalDefenseV5Env, IdsGameRandomAttackV5Env, IdsGameV5Env, \
+    IdsGameMaximalAttackV6Env, IdsGameMinimalDefenseV6Env, IdsGameRandomAttackV6Env, IdsGameRandomDefenseV6Env, \
+    IdsGameV6Env, IdsGameMaximalAttackV7Env, IdsGameMinimalDefenseV7Env, IdsGameRandomAttackV7Env, \
+    IdsGameRandomDefenseV7Env, IdsGameV7Env, IdsGameMaximalAttackV8Env, IdsGameMinimalDefenseV8Env, \
+    IdsGameRandomAttackV8Env, IdsGameRandomDefenseV8Env, IdsGameV8Env, IdsGameMaximalAttackV9Env, \
+    IdsGameMinimalDefenseV9Env, IdsGameRandomAttackV9Env, IdsGameRandomDefenseV9Env, IdsGameV9Env, \
+    IdsGameMaximalAttackV10Env, IdsGameMinimalDefenseV10Env, IdsGameRandomAttackV10Env, IdsGameRandomDefenseV10Env, \
+    IdsGameV10Env, IdsGameMaximalAttackV11Env, IdsGameMinimalDefenseV11Env, IdsGameRandomAttackV11Env, \
+    IdsGameRandomDefenseV11Env, IdsGameV11Env, IdsGameMaximalAttackV12Env, IdsGameMinimalDefenseV12Env, \
+    IdsGameRandomAttackV12Env, IdsGameRandomDefenseV12Env, IdsGameV12Env, IdsGameMaximalAttackV13Env, \
+    IdsGameMinimalDefenseV13Env, IdsGameRandomAttackV13Env, IdsGameRandomDefenseV13Env, IdsGameV13Env, \
+    IdsGameMaximalAttackV14Env, IdsGameMinimalDefenseV14Env, IdsGameRandomAttackV14Env, IdsGameRandomDefenseV14Env, \
+    IdsGameV14Env, IdsGameMaximalAttackV15Env, IdsGameMinimalDefenseV15Env, IdsGameRandomAttackV15Env, \
+    IdsGameRandomDefenseV15Env, IdsGameV15Env, IdsGameMaximalAttackV16Env, IdsGameMinimalDefenseV16Env, \
+    IdsGameRandomAttackV16Env, IdsGameRandomDefenseV16Env, IdsGameV16Env, IdsGameMaximalAttackV17Env, \
+    IdsGameMinimalDefenseV17Env, IdsGameRandomAttackV17Env, IdsGameRandomDefenseV17Env, IdsGameV17Env, \
+    IdsGameMaximalAttackV18Env, IdsGameMinimalDefenseV18Env, IdsGameRandomAttackV18Env, IdsGameRandomDefenseV18Env, \
+    IdsGameV18Env, IdsGameMaximalAttackV19Env, IdsGameMinimalDefenseV19Env, IdsGameRandomAttackV19Env, \
+    IdsGameRandomDefenseV19Env, IdsGameV19Env, IdsGameMaximalAttackV20Env, IdsGameMinimalDefenseV20Env, \
+    IdsGameRandomAttackV20Env, IdsGameRandomDefenseV20Env, IdsGameV20Env, IdsGameMaximalAttackV21Env, \
+    IdsGameMinimalDefenseV21Env, IdsGameRandomAttackV21Env, IdsGameRandomDefenseV21Env, IdsGameV21Env
