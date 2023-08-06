5ffc4a2961456f4e9dd0293b2100f8bd1cee4818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:08 +0000

    Add package tmuxp/1.7.2/tmuxp-1.7.2-py3-none-any.whl

---
+++ packages/tmuxp/tmuxp-1.7.2-py3-none-any.whl/tmuxp/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# flake8: NOQA
+"""tmux session manager.
+
+tmuxp
+~~~~~
+
+:copyright: Copyright 2013-2018 Tony Narlock.
+:license: MIT, see LICENSE for details
+
+"""
+from __future__ import absolute_import, unicode_literals
+
+from . import cli, config, util
+from .__about__ import (
+    __author__,
+    __copyright__,
+    __description__,
+    __email__,
+    __license__,
+    __package_name__,
+    __title__,
+    __version__,
+)
