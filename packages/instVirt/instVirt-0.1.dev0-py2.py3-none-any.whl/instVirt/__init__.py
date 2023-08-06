e39f2800fedbac574d4379a7c5a8856935737758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:49 +0000

    Add package instVirt/0.1.dev0/instVirt-0.1.dev0-py2.py3-none-any.whl

---
+++ packages/instVirt/instVirt-0.1.dev0-py2.py3-none-any.whl/instVirt/__init__.py
@@ -0,0 +1,5 @@
+"""Este aplicativo tem a finalidade de auxiliar professores em saa de aula.
+Também pode ser usado para exercícios e aulas pré estabelecidas com textos.
+No momento eu como educador o utilizo de várias formas.
+Aqui temos uma versão inicial onde estou aberto a colaborações e dicas"""
+__version__="0.1dev0"
