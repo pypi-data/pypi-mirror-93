cc40f10d0168bd65428921d02a46d061136ae5b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:19 +0000

    Add package grblas/1.3.3/grblas-1.3.3-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/grblas/grblas-1.3.3-cp38-cp38-macosx_10_9_x86_64.whl/grblas/backends/suitesparse/build.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from cffi import FFI
+
+is_win = sys.platform.startswith("win")
+
+ffibuilder = FFI()
+
+ffibuilder.set_source(
+    "grblas.backends.suitesparse._suitesparse_grblas",
+    r"""#include "GraphBLAS.h" """,
+    libraries=["graphblas"],
+)
+
+thisdir = os.path.dirname(__file__)
+
+header = "suitesparse_graphblas_4.0.3.h"
+if is_win:
+    header = "suitesparse_graphblas_no_complex_4.0.3.h"
+gb_cdef = open(os.path.join(thisdir, header))
+
+ffibuilder.cdef(gb_cdef.read())
+
+if __name__ == "__main__":
+    ffibuilder.compile(verbose=True)
