e076e64927c04c0258fc518d22cc562f4c87ad27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:22 +0000

    Add package jaraco.nxt/2.1.0/jaraco.nxt-2.1.0.tar.gz

---
+++ packages/jaraco.nxt/jaraco.nxt-2.1.0.tar.gz/jaraco.nxt-2.1.0/examples/locate-device.py
@@ -0,0 +1,10 @@
+from jaraco.nxt import locator
+import logging
+
+
+def run():
+    logging.basicConfig(level=logging.DEBUG)
+    locator.find_brick()
+
+
+__name__ == '__main__' and run()
---
+++ packages/jaraco.nxt/jaraco.nxt-2.1.0.tar.gz/jaraco.nxt-2.1.0/examples/read_input.py
@@ -0,0 +1,54 @@
+"""
+This example demonstrates how one might read sensor values from the brick.
+"""
+
+import sys
+
+from jaraco.nxt import messages
+from jaraco.nxt import locator
+from jaraco.nxt import _enum as enum
+
+
+def run():
+    dev = locator.find_brick()
+
+    # read the values form this port
+    port = enum.InputPort(1)
+
+    # configure the input mode
+    dev.send(
+        messages.SetInputMode(
+            port,
+            messages.SensorType.switch,
+            messages.SensorMode.boolean,
+        )
+    )
+
+    # print out the field names once
+    print(', '.join(field[:4] for field in messages.InputValues.fields))
+
+    try:
+        while True:
+            query_status(dev, port)
+    except KeyboardInterrupt:
+        sys.stdout.write('\n')
+
+
+def query_status(dev, port):
+    """
+    Send the GetInputValues message, then process the
+    jaraco.nxt.messages.InputValues reply, printing each
+    of the fields in a CSV format.
+    """
+
+    # query for the input values and re-write the line
+    dev.send(messages.GetInputValues(port))
+    input_res = dev.receive()
+    # print each of the fields
+    values = ', '.join('%4d' % getattr(input_res, field) for field in input_res.fields)
+    # carriage return but no line feed so we write over the previous line
+    sys.stdout.write('\r')
+    sys.stdout.write(values)
+
+
+__name__ == '__main__' and run()
