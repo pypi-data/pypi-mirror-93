b300be424b0598c8d29a96e164f6a35c5f7469da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:04 +0000

    Add package baizhanSperMath/1.0/baizhanSperMath-1.0.tar.gz

---
+++ packages/baizhanSperMath/baizhanSperMath-1.0.tar.gz/baizhanSperMath-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name = "baizhanSperMath",
+    version = '1.0',
+    description = "这是第一个对外发布的模块，测试哦",
+    author = "gaoqi",
+    author_email = "1621106327@qq.com",
+    py_modules = ["baizhanSperMath.mode01", "baizhanSperMath.mode02"]
+)
