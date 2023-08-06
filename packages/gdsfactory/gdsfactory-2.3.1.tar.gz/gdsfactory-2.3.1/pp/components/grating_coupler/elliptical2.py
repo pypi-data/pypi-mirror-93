bf446a62a8b84357f036c7b91f951f02b0bf451b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:14 +0000

    Add package gdsfactory/2.3.1/gdsfactory-2.3.1.tar.gz

---
+++ packages/gdsfactory/gdsfactory-2.3.1.tar.gz/gdsfactory-2.3.1/pp/components/grating_coupler/elliptical2.py
@@ -0,0 +1,104 @@
+from typing import Callable, Optional, Tuple, Union
+
+import numpy as np
+import picwriter.components as pc
+
+import pp
+from pp.component import Component
+from pp.components.waveguide_template import wg_strip
+from pp.picwriter2component import picwriter2component
+from pp.port import deco_rename_ports
+from pp.types import Layer
+
+
+@deco_rename_ports
+@pp.cell
+def grating_coupler_elliptical2(
+    wgt: Callable = wg_strip,
+    theta: float = np.pi / 4.0,
+    length: float = 30.0,
+    taper_length: float = 10.0,
+    period: float = 1.0,
+    dutycycle: float = 0.7,
+    ridge: bool = True,
+    layer_trenches: Layer = pp.LAYER.WG,
+    layer_core: Layer = pp.LAYER.WGCLAD,
+    layer_cladding: Layer = pp.LAYER.SLAB150,
+    teeth_list: Optional[bool] = None,
+    port: Union[Tuple[float, float], Tuple[int, int]] = (0.0, 0.0),
+    direction: str = "EAST",
+    polarization: str = "te",
+    wavelength: str = 1550,
+    **kwargs
+) -> Component:
+    r""" Returns Grating coupler from Picwriter
+
+    Args:
+        waveguide_template: object or function
+        port (tuple): Cartesian coordinate of the input port
+        direction (string): Direction that the component will point *towards*,
+            can be of type `'NORTH'`, `'WEST'`, `'SOUTH'`, `'EAST'`,
+            OR an angle (float, in radians)
+        theta (float): Angle of the waveguide. Defaults to pi/4.0
+        length (float): Length of the total grating coupler region, measured from the output port.
+        taper_length (float): Length of the taper before the grating coupler.
+        period (float): Grating period.  Defaults to 1.0
+        dutycycle (float): dutycycle, determines the size of the 'gap' by dutycycle=(period-gap)/period.
+        ridge (boolean): If True, adds another layer to the grating coupler that can be used for partial etched gratings
+        layer (tuple): Tuple specifying the layer/datatype of the ridge region.  Defaults to (3,0)
+        teeth_list (list): Can optionally pass a list of (gap, width) tuples to be used as the gap and teeth widths
+            for irregularly spaced gratings.
+            For example, [(0.6, 0.2), (0.7, 0.3), ...] would be a gap of 0.6,
+            then a tooth of width 0.2, then gap of 0.7 and tooth of 0.3, and so on.
+            Overrides *period*, *dutycycle*, and *length*.  Defaults to None.
+
+    .. plot::
+      :include-source:
+
+      import pp
+
+      c = pp.c.grating_coupler_elliptical2()
+      pp.plotgds(c)
+
+    .. code::
+
+                 \  \  \  \
+                  \  \  \  \
+                _|-|_|-|_|-|___
+               |_______________  W0
+
+    """
+
+    c = pc.GratingCoupler(
+        pp.call_if_func(
+            wgt,
+            wg_layer=layer_trenches[0],
+            wg_datatype=layer_trenches[1],
+            clad_layer=layer_cladding[0],
+            clad_datatype=layer_cladding[1],
+            **kwargs
+        ),
+        theta=theta,
+        length=length,
+        taper_length=taper_length,
+        period=period,
+        dutycycle=1 - dutycycle,
+        ridge=ridge,
+        ridge_layers=layer_core,
+        teeth_list=teeth_list,
+        port=port,
+        direction=direction,
+    )
+
+    c = picwriter2component(c)
+    c.polarization = polarization
+    c.wavelength = wavelength
+
+    return c
+
+
+if __name__ == "__main__":
+
+    c = grating_coupler_elliptical2()
+    print(c.ports)
+    c.show()
---
+++ packages/gdsfactory/gdsfactory-2.3.1.tar.gz/gdsfactory-2.3.1/pp/components/ring_single_array.py
@@ -0,0 +1,52 @@
+from typing import Callable, Dict, List
+
+import pp
+from pp.components.ring_single import ring_single
+from pp.components.waveguide import waveguide
+
+
+@pp.cell
+def ring_single_array(
+    ring_function: Callable = ring_single,
+    waveguide_function: Callable = waveguide,
+    spacing: float = 5.0,
+    list_of_dicts: List[Dict[str, float]] = [
+        dict(length_x=10.0, bend_radius=5.0),
+        dict(length_x=20.0, bend_radius=10.0),
+    ],
+) -> pp.Component:
+    """Ring of single bus connected with waveguides.
+
+    .. code::
+
+           ______               ______
+          |      |             |      |
+          |      |  length_y   |      |
+          |      |             |      |
+         --======-- spacing ----==gap==--
+
+          length_x
+
+    """
+    c = pp.Component()
+    settings0 = list_of_dicts[0]
+    ring1 = c << ring_function(**settings0)
+
+    ringp = ring1
+    wg = waveguide_function(length=spacing)
+
+    for settings in list_of_dicts[1:]:
+        ringi = c << ring_function(**settings)
+        wgi = c << wg
+        wgi.connect("W0", ringp.ports["E0"])
+        ringi.connect("W0", wgi.ports["E0"])
+        ringp = ringi
+
+    c.add_port("W0", port=ring1.ports["W0"])
+    c.add_port("E0", port=ringi.ports["E0"])
+    return c
+
+
+if __name__ == "__main__":
+    c = ring_single_array()
+    c.show()
