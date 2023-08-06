21500ca322aafe92c088a902c7bfae94af798872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:51 +0000

    Add package nester2708/1.1.5/nester2708-1.1.5.zip

---
+++ packages/nester2708/nester2708-1.1.5.zip/nester2708-1.1.5/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester2708',
+    version='1.1.5',
+    py_modules=['nester2708'],
+    author='Anthony Bui',
+    author_email='builetuananh2708@gmail.com',
+    # url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
