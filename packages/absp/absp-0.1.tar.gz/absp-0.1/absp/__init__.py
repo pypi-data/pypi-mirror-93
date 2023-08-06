560c41f9d149a0cffc4ccc7e1679b7072690055e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:06 +0000

    Add package absp/0.1/absp-0.1.tar.gz

---
+++ packages/absp/absp-0.1.tar.gz/absp-0.1/absp/__init__.py
@@ -0,0 +1,12 @@
+class hellow:
+     def __init__(self,name):
+        self.name = name
+     def welcome(self):
+          return f'welcome to my module test {self.name}'
+'''
+use : 
+
+name = hellow('ishan')
+print(name.welcome())
+
+'''
