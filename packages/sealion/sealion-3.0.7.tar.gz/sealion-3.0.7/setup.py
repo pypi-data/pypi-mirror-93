5ffcecf58f97aaf674ba1e513bed9cc3cb4c6364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:02 +0000

    Add package sealion/3.0.7/sealion-3.0.7.tar.gz

---
+++ packages/sealion/sealion-3.0.7.tar.gz/sealion-3.0.7/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+
+non_python_files = ['cython_decision_tree_functions.pyx', 'cython_tsne.pyx', 'cython_unsupervised_clustering.pyx',  'cython_naive_bayes.pyx',  'cython_ensemble_learning.pyx',  'cython_knn.pyx']
+
+setup(
+  name = 'sealion',
+  packages = setuptools.find_packages(),
+  package_data = {'' : non_python_files},
+  include_package_data=True,
+  version = '3.0.7',
+  license='MIT',
+  description='SeaLion is a comprehensive machine learning and data science library for beginners and ml-engineers alike.',
+  author = 'Anish Lakkapragada',
+  author_email = 'anish.lakkapragada@gmail.com',
+  url = 'https://github.com/anish-lakkapragada/SeaLion',
+  download_url = 'https://github.com/anish-lakkapragada/SeaLion/archive/v3.0.7.tar.gz',
+  keywords = ['Machine Learning', 'Data Science', 'Python'],
+  install_requires=[
+          'numpy',
+          'joblib',
+          'pandas',
+          'scipy',
+          'tqdm',
+          'multiprocess'
+      ],
+  long_description="README.md",
+  python_requires='>=3',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
