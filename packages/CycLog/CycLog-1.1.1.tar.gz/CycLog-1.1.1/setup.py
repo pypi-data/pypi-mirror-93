e0dcb76aeda575ce22a9db68fb9c3e46dee6c2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:21 +0000

    Add package CycLog/1.1.1/CycLog-1.1.1.tar.gz

---
+++ packages/CycLog/CycLog-1.1.1.tar.gz/CycLog-1.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", encoding="u8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="CycLog",
+    version="1.1.1",
+    author="Jonatan",
+    author_email="pybots.il@gmail.com",
+    description="A python module to handle cyclic logs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonatan1609/CycleLog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Logging",
+    ],
+    python_requires=">=3"
+)
