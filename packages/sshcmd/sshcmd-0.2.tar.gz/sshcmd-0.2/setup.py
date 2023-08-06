6fd9ecb1002ce5a4a99da39cff964ab536080d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:56 +0000

    Add package sshcmd/0.2/sshcmd-0.2.tar.gz

---
+++ packages/sshcmd/sshcmd-0.2.tar.gz/sshcmd-0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+setuptools.setup(
+    name="sshcmd", 
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version="0.2",
+    license='MIT',
+    author="lxl",
+    author_email="xliang.lee@qq.com",
+    description="ssh cmd",
+    long_description="ssh cmd",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords=['ssh', 'cmd'],
+    python_requires='>=2.6',
+    install_requires=[
+        'paramiko',
+    ],
+ )
+
