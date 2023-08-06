3a953083d4213d30de36ae6e6c8e7e6dee93a4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:36 +0000

    Add package ntpl/0.0.4/ntpl-0.0.4.tar.gz

---
+++ packages/ntpl/ntpl-0.0.4.tar.gz/ntpl-0.0.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ntpl",
+    version="0.0.4",
+    author="Chris McCormick",
+    author_email="chris@mccormick.cx",
+    packages=["ntpl"],
+    license="LICENSE.txt",
+    description="Minimal library to modify and render HTML.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chr15m/ntpl",
+    install_requires=[
+        "beautifulsoup4==4.9.1",
+        "pyhiccup==0.1",
+    ],
+)
