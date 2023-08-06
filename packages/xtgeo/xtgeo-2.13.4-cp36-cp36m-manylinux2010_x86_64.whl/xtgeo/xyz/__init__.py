d5e5ffcc8b2e5d0614defc42605f66ff61ee00ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:01 +0000

    Add package xtgeo/2.13.4/xtgeo-2.13.4-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/xtgeo/xtgeo-2.13.4-cp36-cp36m-manylinux2010_x86_64.whl/xtgeo/xyz/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""The XTGeo xyz (points and polygons) package."""
+from __future__ import division, absolute_import
+from __future__ import print_function
+
+# flake8: noqa
+from xtgeo.xyz._xyz import XYZ
+from xtgeo.xyz.points import Points
+from xtgeo.xyz.polygons import Polygons
