7f2bc0169b1d40b6d21d02bb480b3c1c361829c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package flask-social-login/0.2.1/flask_social_login-0.2.1.tar.gz

---
+++ packages/flask-social-login/flask_social_login-0.2.1.tar.gz/flask_social_login-0.2.1/flask_social_login/__init__.py
@@ -0,0 +1,12 @@
+"""
+    flask_social_login
+    ~~~~~~~~~~~~~~~~~~
+
+    :copyright: (c) 2021 by Pyunghyuk Yoo
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "__version__ = '0.2.1'"
+
+from .core import Social
+from .datastore import SQLAlchemyConnectionDatastore
