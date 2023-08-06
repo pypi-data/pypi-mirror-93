a3ddc1297ac7de80af8f351d49c7851e1d30a192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:14 +0000

    Add package Lido-vsmelov/0.2.1/Lido-vsmelov-0.2.1.tar.gz

---
+++ packages/Lido-vsmelov/Lido-vsmelov-0.2.1.tar.gz/Lido-vsmelov-0.2.1/setup.py
@@ -0,0 +1,25 @@
+"""Setup script for Lido"""
+
+import os.path
+from setuptools import setup, find_namespace_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+setup(
+    name="Lido-vsmelov",
+    version="0.2.1",
+    description="Network helpers for Lido",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lidofinance/lido-python",
+    author="Lido",
+    author_email="info@lido.fi",
+    license="MIT",
+    packages=find_namespace_packages(),
+    include_package_data=True,
+    install_requires=["web3", "py_ecc", "ssz", "requests"],
+    python_requires=">=3.0,<4",
+)
