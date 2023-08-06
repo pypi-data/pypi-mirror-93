500d183de8b4e2bf7bd716d9a6860da51bfbe3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:15 +0000

    Add package honeybadger/0.4.2/honeybadger-0.4.2-py2-none-any.whl

---
+++ packages/honeybadger/honeybadger-0.4.2-py2-none-any.whl/honeybadger/contrib/__init__.py
@@ -0,0 +1,4 @@
+from honeybadger.contrib.flask import FlaskHoneybadger
+from honeybadger.contrib.django import DjangoHoneybadgerMiddleware
+
+__all__ = ['FlaskHoneybadger', 'DjangoHoneybadgerMiddleware']
