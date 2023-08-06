3990e64144b1bb054d0e47947e882b1334fd07bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:35 +0000

    Add package grabutils/0.0.29/grabutils-0.0.29-py3-none-any.whl

---
+++ packages/grabutils/grabutils-0.0.29-py3-none-any.whl/grabutils/jspo.py
@@ -0,0 +1,36 @@
+from functools import reduce
+
+import jmespath
+
+
+class JsReader:
+    def __init__(self, value: any):
+        self.jsview = value
+
+
+class JsPath:
+    def __init__(self, path: str):
+        self.js_path = path
+        self._init = False
+        self._value = None
+        self._proc = list()
+
+    def map(self, fn):
+        def mmap(value):
+            return fn(value)
+
+        self._proc.append(mmap)
+        return self
+
+    def flatmap(self, fn):
+        def mflatmap(value):
+            return map(fn, value)
+
+        self._proc.append(mflatmap)
+        return self
+
+    def __get__(self, obj: JsReader, objtype=None):
+        if self._init:
+            return self._value
+        self._value = reduce(lambda memo, item: item(memo), self._proc, jmespath.search(self.js_path, obj.jsview))
+        return self._value
