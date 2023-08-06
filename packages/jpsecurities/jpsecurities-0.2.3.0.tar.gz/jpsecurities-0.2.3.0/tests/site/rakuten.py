b66163c4a1ef79b3d3d26bd44d89e3c6f8484b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:13 +0000

    Add package jpsecurities/0.2.3.0/jpsecurities-0.2.3.0.tar.gz

---
+++ packages/jpsecurities/jpsecurities-0.2.3.0.tar.gz/jpsecurities-0.2.3.0/tests/site/rakuten.py
@@ -0,0 +1,31 @@
+import unittest
+from jpsecurities.site.rakuten import Rakuten
+from selenium.webdriver.chrome.options import Options
+import time
+import logging
+import sys
+import os
+from logging import Formatter, handlers, StreamHandler, getLogger, DEBUG
+
+logging.getLogger().setLevel(DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+class TestRakuten(unittest.TestCase):
+    chrome_options = Options()
+    chrome_options.binary_location = os.environ['CHROME_BIN_LOCATION']
+    #chrome_options.headless = True
+    executable_path = os.environ['CHROME_EXE_PATH']
+    username = os.environ['RAKUTEN_USERNAME']
+    pwd = os.environ['RAKUTEN_PWD']
+
+    def test(self):
+        with Rakuten(executable_path=self.executable_path, chrome_options=self.chrome_options,
+                     username=self.username, pwd=self.pwd) as rakuten:
+            df = rakuten.get_spot_margin_transaction_info()
+            df.to_csv('/tmp/get_spot_margin_transaction_info.csv')
+            #time.sleep(10000)
+            
+
+if __name__ == "__main__":
+    unittest.main()
