05550169eb7be4a3a16d890a534ae6eb43a67cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:50 +0000

    Add package discord-menu/0.9.0/discord-menu-0.9.0.tar.gz

---
+++ packages/discord-menu/discord-menu-0.9.0.tar.gz/discord-menu-0.9.0/discordmenu/emoji/emoji_cache.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from discord import Emoji as Emoji
+from discord.ext.commands import Bot
+
+
+class EmojiCache:
+    def __init__(self, guild_ids: List[int]):
+        self.guild_ids = guild_ids
+        self.custom_emojis: List[Emoji] = []
+
+    def set_guild_ids(self, guild_ids: List[int]):
+        self.guild_ids = guild_ids
+
+    def refresh_from_discord_bot(self, bot: Bot):
+        self.custom_emojis = [e for g in bot.guilds if g.id in self.guild_ids for e in g.emojis]
+
+    def refresh_from_emojis(self, emojis: List[Emoji]):
+        self.custom_emojis = emojis
+
+    def get_by_name(self, name):
+        res = [e for e in self.custom_emojis if e.name == name]
+        return res[0] if len(res) > 0 else name
+
+    def get_emoji(self, name):
+        for e in self.custom_emojis:
+            if e.name == name:
+                return str(e)
+        return ":{}:".format(name)
+
+
+emoji_cache = EmojiCache([])
