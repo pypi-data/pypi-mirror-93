329ca365638b6489f25b221ab5b9185d3413eeed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package vicNeuralNetwork/0.0.2/vicNeuralNetwork-0.0.2-py3-none-any.whl

---
+++ packages/vicNeuralNetwork/vicNeuralNetwork-0.0.2-py3-none-any.whl/neural_network/nn/core.py
@@ -0,0 +1,238 @@
+import numpy as np
+import random
+
+
+class Node:
+    def __init__(self, inputs=[]):
+        self.inputs = inputs
+        self.outputs = []
+
+        for n in self.inputs:
+            n.outputs.append(self)
+            # set 'self' node as inbound_nodes's outbound_nodes
+
+        self.value = None
+
+        self.gradients = {}
+        # keys are the inputs to this node, and their
+        # values are the partials of this node with
+        # respect to that input.
+        # \partial{node}{input_i}
+
+    def forward(self):
+        '''
+        Forward propagation.
+        Compute the output value vased on 'inbound_nodes' and store the
+        result in self.value
+        '''
+
+        raise NotImplemented
+
+    def backward(self):
+        raise NotImplemented
+
+
+class Placeholder(Node):
+    def __init__(self):
+        '''
+        An Input node has no inbound nodes.
+        So no need to pass anything to the Node instantiator.
+        '''
+        Node.__init__(self)
+
+    def forward(self, value=None):
+        '''
+        Only input node is the node where the value may be passed
+        as an argument to forward().
+        All other node implementations should get the value of the
+        previous node from self.inbound_nodes
+
+        Example:
+        val0: self.inbound_nodes[0].value
+        '''
+        if value is not None:
+            self.value = value
+            ## It's is input node, when need to forward, this node initiate self's value.
+
+        # Input subclass just holds a value, such as a data feature or a model parameter(weight/bias)
+
+    def backward(self):
+        self.gradients = {self: 0}
+        for n in self.outputs:
+            grad_cost = n.gradients[self]
+            self.gradients[self] = grad_cost * 1
+
+        # input N --> N1, N2
+        # \partial L / \partial N
+        # ==> \partial L / \partial N1 * \ partial N1 / \partial N
+
+
+class Add(Node):
+    def __init__(self, *nodes):
+        Node.__init__(self, nodes)
+
+    def forward(self):
+        self.value = sum(map(lambda n: n.value, self.inputs))
+        ## when execute forward, this node caculate value as defined.
+
+
+class Linear(Node):
+    def __init__(self, nodes, weights, bias):
+        Node.__init__(self, [nodes, weights, bias])
+
+    def forward(self):
+        inputs = self.inputs[0].value
+        weights = self.inputs[1].value
+        bias = self.inputs[2].value
+
+        self.value = np.dot(inputs, weights) + bias
+
+    def backward(self):
+        # initial a partial for each of the inbound_nodes.
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inputs}
+
+        for n in self.outputs:
+            # Get the partial of the cost w.r.t this node.
+            grad_cost = n.gradients[self]
+
+            self.gradients[self.inputs[0]] = np.dot(grad_cost, self.inputs[1].value.T)
+            self.gradients[self.inputs[1]] = np.dot(self.inputs[0].value.T, grad_cost)
+            self.gradients[self.inputs[2]] = np.sum(grad_cost, axis=0, keepdims=False)
+
+        # WX + B / W ==> X
+        # WX + B / X ==> W
+
+
+class Sigmoid(Node):
+    def __init__(self, node):
+        Node.__init__(self, [node])
+
+    def _sigmoid(self, x):
+        return 1. / (1 + np.exp(-1 * x))
+
+    def forward(self):
+        self.x = self.inputs[0].value
+        self.value = self._sigmoid(self.x)
+
+    def backward(self):
+        self.partial = self._sigmoid(self.x) * (1 - self._sigmoid(self.x))
+
+        # y = 1 / (1 + e^-x)
+        # y' = 1 / (1 + e^-x) (1 - 1 / (1 + e^-x))
+
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inputs}
+
+        for n in self.outputs:
+            grad_cost = n.gradients[self]  # Get the partial of the cost with respect to this node.
+
+            self.gradients[self.inputs[0]] = grad_cost * self.partial
+            # use * to keep all the dimension same!.
+
+
+class MSE(Node):
+    def __init__(self, y, a):
+        Node.__init__(self, [y, a])
+
+    def forward(self):
+        y = self.inputs[0].value.reshape(-1, 1)
+        a = self.inputs[1].value.reshape(-1, 1)
+        assert (y.shape == a.shape)
+
+        self.m = self.inputs[0].value.shape[0]
+        self.diff = y - a
+
+        self.value = np.mean(self.diff ** 2)
+
+    def backward(self):
+        self.gradients[self.inputs[0]] = (2 / self.m) * self.diff
+        self.gradients[self.inputs[1]] = (-2 / self.m) * self.diff
+
+
+def forward_and_backward(outputnode, graph):
+    # execute all the forward method of sorted_nodes.
+
+    ## In practice, it's common to feed in mutiple data example in each forward pass rather than just 1. Because the examples can be processed in parallel. The number of examples is called batch size.
+    for n in graph:
+        n.forward()
+        ## each node execute forward, get self.value based on the topological sort result.
+
+    for n in graph[::-1]:
+        n.backward()
+
+    # return outputnode.value
+
+
+###   v -->  a -->  C
+##    b --> C
+##    b --> v -- a --> C
+##    v --> v ---> a -- > C
+
+def toplogic(graph):
+    sorted_node = []
+
+    while len(graph) > 0:
+
+        all_inputs = []
+        all_outputs = []
+
+        for n in graph:
+            all_inputs += graph[n]
+            all_outputs.append(n)
+
+        all_inputs = set(all_inputs)
+        all_outputs = set(all_outputs)
+
+        need_remove = all_outputs - all_inputs  # which in all_inputs but not in all_outputs
+
+        if len(need_remove) > 0:
+            node = random.choice(list(need_remove))
+
+            need_to_visited = [node]
+
+            if len(graph) == 1: need_to_visited += graph[node]
+
+            graph.pop(node)
+            sorted_node += need_to_visited
+
+            for _, links in graph.items():
+                if node in links: links.remove(node)
+        else:  # have cycle
+            break
+
+    return sorted_node
+
+
+from collections import defaultdict
+
+
+def convert_feed_dict_to_graph(feed_dict):
+    computing_graph = defaultdict(list)
+
+    nodes = [n for n in feed_dict]
+
+    while nodes:
+        n = nodes.pop(0)
+
+        if isinstance(n, Placeholder):
+            n.value = feed_dict[n]
+
+        if n in computing_graph: continue
+
+        for m in n.outputs:
+            computing_graph[n].append(m)
+            nodes.append(m)
+
+    return computing_graph
+
+
+def topological_sort_feed_dict(feed_dict):
+    graph = convert_feed_dict_to_graph(feed_dict)
+
+    return toplogic(graph)
+
+
+def optimize(trainables, learning_rate=1e-2):
+    # there are so many other update / optimization methods
+    # such as Adam, Mom,
+    for t in trainables:
+        t.value += -1 * learning_rate * t.gradients[t]
