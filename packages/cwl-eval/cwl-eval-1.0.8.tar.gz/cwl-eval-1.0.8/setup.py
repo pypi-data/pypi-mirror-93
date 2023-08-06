402c255191bba55e82d44ce8c19a623cb6cc9299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:00 +0000

    Add package cwl-eval/1.0.8/cwl-eval-1.0.8.tar.gz

---
+++ packages/cwl-eval/cwl-eval-1.0.8.tar.gz/cwl-eval-1.0.8/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='cwl-eval',
+    version='1.0.8',
+    
+    scripts=['cwl-eval'],
+    
+    author='Leif Azzopardi, Paul Thomas, Alistair Moffat',
+    author_email='leifos@acm.org, pathom@microsoft.com, ammoffat@unimelb.edu.au',
+    
+    description='An information retrieval evaluation script based on the C/W/L framework '
+                'that is TREC Compatible and provides a replacement for INST_EVAL, RBP_EVAL, '
+                'TBG_EVAL, UMeasure and TREC_EVAL scripts. All measurements are reported in '
+                'the same units making all metrics directly comparable.',
+    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    url='https://github.com/ireval/cwl',
+
+    packages=setuptools.find_packages(),
+    
+    python_requires='>=3',
+
+    install_requires=[
+        'numpy',
+    ],
+    
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+
+    ],
+
+)
