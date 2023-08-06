c2cc9039a11bfa30538618d730b84b50bf5109a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:29 +0000

    Add package buycoins-python/0.3/buycoins_python-0.3-py3-none-any.whl

---
+++ packages/buycoins-python/buycoins_python-0.3-py3-none-any.whl/buycoins/client.py
@@ -0,0 +1,63 @@
+from decouple import config
+from python_graphql_client import GraphqlClient
+from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError, ConnectionError
+
+from buycoins.exceptions import QueryError, ClientError
+
+auth_key = config("auth_key")
+
+
+class BuyCoinsClient:
+    def __init__(self):
+        self.__endpoint = "https://backend.buycoins.tech/api/graphql"
+        self.__username = ""
+        self.__password = ""
+        self.__auth_key = auth_key
+
+    def _split_auth_key(self):
+        self.__username, self.__password = self.__auth_key.split(":")
+
+    def _initiate_client(self):
+        self._split_auth_key()
+        try:
+            self.__auth = HTTPBasicAuth(self.__username, self.__password)
+            self.__client = GraphqlClient(self.__endpoint, auth=self.__auth)
+        except (HTTPError, ConnectionError) as e:
+            return e
+        else:
+            return self.__client
+
+    # def check_request(self, request):
+    #     try:
+    #         if type(request) == ConnectionError:
+    #             print("Stage I: ", request)
+    #             raise ClientError("Connection Error", 404)
+    #
+    #         elif type(request) == HTTPError:
+    #             print("Stage II: ", request)
+    #             raise ClientError("HTTP Error", 404)
+    #     except ClientError as e:
+    #         return e.response
+    #     else:
+    #         print("AHAN!")
+    #         # return request
+
+    def _execute_request(self, query: str, variables: dict = {}):
+        if not query or query == "":
+            raise QueryError("Invalid query passed!")
+
+        try:
+            self._initiate_client()
+            request = self.__client.execute(query=query, variables=variables)
+
+        except ClientError as e:
+            return e.response
+        except HTTPError as e:
+            return e
+        except QueryError as e:
+            return e.response
+        except ConnectionError as e:
+            return e
+        else:
+            return request
