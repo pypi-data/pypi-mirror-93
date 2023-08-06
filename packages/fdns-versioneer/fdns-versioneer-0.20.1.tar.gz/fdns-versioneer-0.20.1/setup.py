6f985bbb6f45db7d1d9d856dbeb21064071f4121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:16 +0000

    Add package fdns-versioneer/0.20.1/fdns-versioneer-0.20.1.tar.gz

---
+++ packages/fdns-versioneer/fdns-versioneer-0.20.1.tar.gz/fdns-versioneer-0.20.1/setup.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+import os, base64, tempfile, io
+from os import path
+from pathlib import Path
+from setuptools import setup, Command
+from setuptools.command.build_py import build_py
+from setuptools.dist import Distribution as _Distribution
+
+LONG = Path.read_text(Path(__file__).parent / "README.md")
+
+# as nice as it'd be to versioneer ourselves, that sounds messy.
+VERSION = "0.20.1"
+
+
+def ver(s):
+    return s.replace("@VERSIONEER-VERSION@", VERSION)
+
+def get(fn, add_ver=False, unquote=False, do_strip=False, do_readme=False):
+    with open(fn) as f:
+        text = f.read()
+
+    if add_ver:
+        text = ver(text)
+    if unquote:
+        text = text.replace("%", "%%")
+    if do_strip:
+        text = "".join(line for line in text.splitlines(keepends=True)
+                         if not line.endswith("# --STRIP DURING BUILD\n"))
+    if do_readme:
+        text = text.replace("@README@", get("README.md"))
+    return text
+
+def get_vcs_list():
+    project_path = path.join(path.abspath(path.dirname(__file__)), 'src')
+    return [filename
+            for filename
+            in os.listdir(project_path)
+            if path.isdir(path.join(project_path, filename)) and filename != "__pycache__"]
+
+def generate_long_version_py(VCS):
+    s = io.StringIO()
+    s.write(get(f"src/{VCS}/long_header.py", add_ver=True, do_strip=True))
+    for piece in ["src/subprocess_helper.py",
+                  "src/from_parentdir.py",
+                  f"src/{VCS}/from_keywords.py",
+                  f"src/{VCS}/from_vcs.py",
+                  "src/render.py",
+                  f"src/{VCS}/long_get_versions.py"]:
+        s.write(get(piece, unquote=True, do_strip=True))
+    return s.getvalue()
+
+def generate_versioneer_py():
+    s = io.StringIO()
+    s.write(get("src/header.py", add_ver=True, do_readme=True))
+    s.write(get("src/subprocess_helper.py", do_strip=True))
+
+    for VCS in get_vcs_list():
+        s.write(f"LONG_VERSION_PY['{VCS}'] = r'''\n")
+        s.write(generate_long_version_py(VCS))
+        s.write("'''\n")
+        s.write("\n\n")
+
+        s.write(get(f"src/{VCS}/from_keywords.py", do_strip=True))
+        s.write(get(f"src/{VCS}/from_vcs.py", do_strip=True))
+
+        s.write(get(f"src/{VCS}/install.py", do_strip=True))
+
+    s.write(get("src/from_parentdir.py", do_strip=True))
+    s.write(get("src/from_file.py", add_ver=True, do_strip=True))
+    s.write(get("src/render.py", do_strip=True))
+    s.write(get("src/get_versions.py", do_strip=True))
+    s.write(get("src/cmdclass.py", do_strip=True))
+    s.write(get("src/setupfunc.py", do_strip=True))
+
+    return s.getvalue().encode("utf-8")
+
+
+class make_versioneer(Command):
+    description = "create standalone versioneer.py"
+    user_options = []
+    boolean_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        with open("versioneer.py", "w") as f:
+            f.write(generate_versioneer_py().decode("utf8"))
+        return 0
+
+class make_long_version_py_git(Command):
+    description = "create standalone _version.py (for git)"
+    user_options = []
+    boolean_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        assert os.path.exists("versioneer.py")
+        long_version = generate_long_version_py("git")
+        with open("git_version.py", "w") as f:
+            f.write(long_version %
+                    {"DOLLAR": "$",
+                     "STYLE": "pep440",
+                     "TAG_PREFIX": "tag-",
+                     "PARENTDIR_PREFIX": "parentdir_prefix",
+                     "VERSIONFILE_SOURCE": "versionfile_source",
+                     })
+        return 0
+
+class my_build_py(build_py):
+    def run(self):
+        v = generate_versioneer_py()
+        v_b64 = base64.b64encode(v).decode("ascii")
+        lines = [v_b64[i:i+60] for i in range(0, len(v_b64), 60)]
+        v_b64 = "\n".join(lines)+"\n"
+
+        with open("src/installer.py") as f:
+            s = f.read()
+        s = ver(s.replace("@VERSIONEER-INSTALLER@", v_b64))
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            installer = os.path.join(tempdir, "versioneer.py")
+            with open(installer, "w") as f:
+                f.write(s)
+
+            self.py_modules = [os.path.splitext(os.path.basename(installer))[0]]
+            self.package_dir.update({'': os.path.dirname(installer)})
+            rc = build_py.run(self)
+        return rc
+
+
+# python's distutils treats module-less packages as binary-specific (not
+# "pure"), so "setup.py bdist_wheel" creates binary-specific wheels. Override
+# this so we get cross-platform wheels instead. More info at:
+# https://bitbucket.org/pypa/wheel/issue/116/packages-with-only-filesdata_files-get
+class Distribution(_Distribution):
+    def is_pure(self): return True
+
+setup(
+    name = "fdns-versioneer",
+    license = "public domain",
+    version = VERSION,
+    description = "Easy VCS-based management of project version strings",
+    author = "Felipe Espinoza",
+    author_email = "github-contact@espinoza.dev",
+    url = "https://github.com/fdns/python-versioneer",
+    # "fake" is replaced with versioneer-installer in build_scripts. We need
+    # a non-empty list to provoke "setup.py build" into making scripts,
+    # otherwise it skips that step.
+    py_modules = ["fake"],
+    entry_points={
+        'console_scripts': [
+            'versioneer = versioneer:main',
+        ],
+    },
+    long_description=LONG,
+    long_description_content_type="text/markdown",
+    distclass=Distribution,
+    cmdclass = { "build_py": my_build_py,
+                 "make_versioneer": make_versioneer,
+                 "make_long_version_py_git": make_long_version_py_git,
+                 },
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        ],
+    )
---
+++ packages/fdns-versioneer/fdns-versioneer-0.20.1.tar.gz/fdns-versioneer-0.20.1/src/cmdclass.py
@@ -0,0 +1,209 @@
+import os, sys # --STRIP DURING BUILD
+LONG_VERSION_PY = {} # --STRIP DURING BUILD
+def get_version(): pass # --STRIP DURING BUILD
+def get_versions(): pass # --STRIP DURING BUILD
+def get_root(): pass # --STRIP DURING BUILD
+def get_config_from_root(): pass # --STRIP DURING BUILD
+def write_to_version_file(): pass # --STRIP DURING BUILD
+
+
+def get_cmdclass(cmdclass=None):
+    """Get the custom setuptools/distutils subclasses used by Versioneer.
+
+    If the package uses a different cmdclass (e.g. one from numpy), it
+    should be provide as an argument.
+    """
+    if "versioneer" in sys.modules:
+        del sys.modules["versioneer"]
+        # this fixes the "python setup.py develop" case (also 'install' and
+        # 'easy_install .'), in which subdependencies of the main project are
+        # built (using setup.py bdist_egg) in the same python process. Assume
+        # a main project A and a dependency B, which use different versions
+        # of Versioneer. A's setup.py imports A's Versioneer, leaving it in
+        # sys.modules by the time B's setup.py is executed, causing B to run
+        # with the wrong versioneer. Setuptools wraps the sub-dep builds in a
+        # sandbox that restores sys.modules to it's pre-build state, so the
+        # parent is protected against the child's "import versioneer". By
+        # removing ourselves from sys.modules here, before the child build
+        # happens, we protect the child from the parent's versioneer too.
+        # Also see https://github.com/python-versioneer/python-versioneer/issues/52
+
+    cmds = {} if cmdclass is None else cmdclass.copy()
+
+    # we add "version" to both distutils and setuptools
+    from distutils.core import Command
+
+    class cmd_version(Command):
+        description = "report generated version string"
+        user_options = []
+        boolean_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            vers = get_versions(verbose=True)
+            print("Version: %s" % vers["version"])
+            print(" full-revisionid: %s" % vers.get("full-revisionid"))
+            print(" dirty: %s" % vers.get("dirty"))
+            print(" date: %s" % vers.get("date"))
+            if vers["error"]:
+                print(" error: %s" % vers["error"])
+    cmds["version"] = cmd_version
+
+    # we override "build_py" in both distutils and setuptools
+    #
+    # most invocation pathways end up running build_py:
+    #  distutils/build -> build_py
+    #  distutils/install -> distutils/build ->..
+    #  setuptools/bdist_wheel -> distutils/install ->..
+    #  setuptools/bdist_egg -> distutils/install_lib -> build_py
+    #  setuptools/install -> bdist_egg ->..
+    #  setuptools/develop -> ?
+    #  pip install:
+    #   copies source tree to a tempdir before running egg_info/etc
+    #   if .git isn't copied too, 'git describe' will fail
+    #   then does setup.py bdist_wheel, or sometimes setup.py install
+    #  setup.py egg_info -> ?
+
+    # we override different "build_py" commands for both environments
+    if 'build_py' in cmds:
+        _build_py = cmds['build_py']
+    elif "setuptools" in sys.modules:
+        from setuptools.command.build_py import build_py as _build_py
+    else:
+        from distutils.command.build_py import build_py as _build_py
+
+    class cmd_build_py(_build_py):
+        def run(self):
+            root = get_root()
+            cfg = get_config_from_root(root)
+            versions = get_versions()
+            _build_py.run(self)
+            # now locate _version.py in the new build/ directory and replace
+            # it with an updated value
+            if cfg.versionfile_build:
+                target_versionfile = os.path.join(self.build_lib,
+                                                  cfg.versionfile_build)
+                print("UPDATING %s" % target_versionfile)
+                write_to_version_file(target_versionfile, versions)
+    cmds["build_py"] = cmd_build_py
+
+    if 'build_ext' in cmds:
+        _build_ext = cmds['build_ext']
+    elif "setuptools" in sys.modules:
+        from setuptools.command.build_ext import build_ext as _build_ext
+    else:
+        from distutils.command.build_ext import build_ext as _build_ext
+
+    class cmd_build_ext(_build_ext):
+        def run(self):
+            root = get_root()
+            cfg = get_config_from_root(root)
+            versions = get_versions()
+            _build_ext.run(self)
+            if self.inplace:
+                # build_ext --inplace will only build extensions in
+                # build/lib<..> dir with no _version.py to write to.
+                # As in place builds will already have a _version.py
+                # in the module dir, we do not need to write one.
+                return
+            # now locate _version.py in the new build/ directory and replace
+            # it with an updated value
+            target_versionfile = os.path.join(self.build_lib,
+                                              cfg.versionfile_build)
+            print("UPDATING %s" % target_versionfile)
+            write_to_version_file(target_versionfile, versions)
+    cmds["build_ext"] = cmd_build_ext
+
+    if "cx_Freeze" in sys.modules:  # cx_freeze enabled?
+        from cx_Freeze.dist import build_exe as _build_exe
+        # nczeczulin reports that py2exe won't like the pep440-style string
+        # as FILEVERSION, but it can be used for PRODUCTVERSION, e.g.
+        # setup(console=[{
+        #   "version": versioneer.get_version().split("+", 1)[0], # FILEVERSION
+        #   "product_version": versioneer.get_version(),
+        #   ...
+
+        class cmd_build_exe(_build_exe):
+            def run(self):
+                root = get_root()
+                cfg = get_config_from_root(root)
+                versions = get_versions()
+                target_versionfile = cfg.versionfile_source
+                print("UPDATING %s" % target_versionfile)
+                write_to_version_file(target_versionfile, versions)
+
+                _build_exe.run(self)
+                os.unlink(target_versionfile)
+                with open(cfg.versionfile_source, "w") as f:
+                    LONG = LONG_VERSION_PY[cfg.VCS]
+                    f.write(LONG %
+                            {"DOLLAR": "$",
+                             "STYLE": cfg.style,
+                             "TAG_PREFIX": cfg.tag_prefix,
+                             "PARENTDIR_PREFIX": cfg.parentdir_prefix,
+                             "VERSIONFILE_SOURCE": cfg.versionfile_source,
+                             })
+        cmds["build_exe"] = cmd_build_exe
+        del cmds["build_py"]
+
+    if 'py2exe' in sys.modules:  # py2exe enabled?
+        from py2exe.distutils_buildexe import py2exe as _py2exe
+
+        class cmd_py2exe(_py2exe):
+            def run(self):
+                root = get_root()
+                cfg = get_config_from_root(root)
+                versions = get_versions()
+                target_versionfile = cfg.versionfile_source
+                print("UPDATING %s" % target_versionfile)
+                write_to_version_file(target_versionfile, versions)
+
+                _py2exe.run(self)
+                os.unlink(target_versionfile)
+                with open(cfg.versionfile_source, "w") as f:
+                    LONG = LONG_VERSION_PY[cfg.VCS]
+                    f.write(LONG %
+                            {"DOLLAR": "$",
+                             "STYLE": cfg.style,
+                             "TAG_PREFIX": cfg.tag_prefix,
+                             "PARENTDIR_PREFIX": cfg.parentdir_prefix,
+                             "VERSIONFILE_SOURCE": cfg.versionfile_source,
+                             })
+        cmds["py2exe"] = cmd_py2exe
+
+    # we override different "sdist" commands for both environments
+    if 'sdist' in cmds:
+        _sdist = cmds['sdist']
+    elif "setuptools" in sys.modules:
+        from setuptools.command.sdist import sdist as _sdist
+    else:
+        from distutils.command.sdist import sdist as _sdist
+
+    class cmd_sdist(_sdist):
+        def run(self):
+            versions = get_versions()
+            self._versioneer_generated_versions = versions
+            # unless we update this, the command will keep using the old
+            # version
+            self.distribution.metadata.version = versions["version"]
+            return _sdist.run(self)
+
+        def make_release_tree(self, base_dir, files):
+            root = get_root()
+            cfg = get_config_from_root(root)
+            _sdist.make_release_tree(self, base_dir, files)
+            # now locate _version.py in the new base_dir directory
+            # (remembering that it may be a hardlink) and replace it with an
+            # updated value
+            target_versionfile = os.path.join(base_dir, cfg.versionfile_source)
+            print("UPDATING %s" % target_versionfile)
+            write_to_version_file(target_versionfile,
+                                  self._versioneer_generated_versions)
+    cmds["sdist"] = cmd_sdist
+
+    return cmds
