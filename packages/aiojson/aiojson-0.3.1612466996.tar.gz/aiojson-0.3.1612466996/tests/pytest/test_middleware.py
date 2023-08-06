5d40eff8499fbd8a3460589b50a77fb1505998c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:16 +0000

    Add package aiojson/0.3.1612466996/aiojson-0.3.1612466996.tar.gz

---
+++ packages/aiojson/aiojson-0.3.1612466996.tar.gz/aiojson-0.3.1612466996/tests/pytest/test_middleware.py
@@ -0,0 +1,14 @@
+import aiohttp.web
+import pytest
+
+from aiojson.middleware import error_middleware
+
+
+async def test_good404(aiohttp_client):
+
+    app = aiohttp.web.Application(middlewares=[error_middleware])
+    client = await aiohttp_client(app)
+    resp = await client.get('/test3123')
+    assert resp.status == 404
+    body = await resp.json()
+    assert {"error": True, "reason": {"text": "Not Found", "code": "NotFound"}} == body
