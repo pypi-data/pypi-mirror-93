7ea4c36325fb14ad0d37583783f8a6f17bb15036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:30 +0000

    Add package robloxpy/0.2.11/robloxpy-0.2.11.tar.gz

---
+++ packages/robloxpy/robloxpy-0.2.11.tar.gz/robloxpy-0.2.11/setup.py
@@ -0,0 +1,21 @@
+#from setuptools import setup
+import setuptools
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='robloxpy',
+    version='0.2.11',
+    author="Kristan Smout",
+    author_email="python@kristansmout.com",
+    description="Wrapper for roblox Web API's",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KristanSmout/RobloxPyOfficial",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Attribution Assurance License",
+        "Operating System :: OS Independent",
+    ],
+)
+    
