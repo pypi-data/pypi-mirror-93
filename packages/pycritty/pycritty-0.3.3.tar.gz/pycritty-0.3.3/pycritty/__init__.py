6b1e561fb275621061cde94fd4fbc760aec5e662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:59 +0000

    Add package pycritty/0.3.3/pycritty-0.3.3.tar.gz

---
+++ packages/pycritty/pycritty-0.3.3.tar.gz/pycritty-0.3.3/pycritty/__init__.py
@@ -0,0 +1,7 @@
+"""Automated tools for managing alacritty configurations"""
+
+__version__ = "0.3.3"
+
+
+class PycrittyError(Exception):
+    pass
---
+++ packages/pycritty/pycritty-0.3.3.tar.gz/pycritty-0.3.3/pycritty/main.py
@@ -0,0 +1,26 @@
+# Pycritty
+# Antonio Sarosi
+# December 2020
+
+from . import PycrittyError
+from .io import log
+from .commands import subcommands, Pycritty
+from .cli import parser
+
+
+def main():
+    args = vars(parser.parse_args())
+    if args['subcommand'] is None:
+        command_receiver = Pycritty
+    else:
+        command_receiver = subcommands[args['subcommand']]
+    args.pop('subcommand')
+    try:
+        command_receiver().execute(args)
+    except PycrittyError as e:
+        log.err(e)
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
