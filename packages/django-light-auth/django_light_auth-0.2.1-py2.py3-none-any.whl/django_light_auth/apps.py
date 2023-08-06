5d5c2433393ba7964d761d245faa09da70bd4285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:06 +0000

    Add package django-light-auth/0.2.1/django_light_auth-0.2.1-py2.py3-none-any.whl

---
+++ packages/django-light-auth/django_light_auth-0.2.1-py2.py3-none-any.whl/django_light_auth/apps.py
@@ -0,0 +1,51 @@
+from uuid import uuid4
+from datetime import datetime, timedelta
+from typing import Union
+
+from django.conf import settings
+from django.apps import AppConfig
+
+
+class DjangoLightAuthConfig(AppConfig):
+    name = 'django_light_auth'
+
+    login_path = '/login'
+    logout_path = '/logout'
+    success_path = '/'
+
+    validate_func = 'django_light_auth.light_auth_validate_func'
+
+    allow_paths = set()
+
+    token = uuid4().hex
+    # https://docs.djangoproject.com/en/3.1/topics/http/sessions/
+    expiry: Union[None, int, datetime, timedelta] = 3600  # 1 hour
+
+    def ready(self):
+        # func
+        value = getattr(settings, 'LIGHT_AUTH_VALIDATE_FUNC', '')
+        if value:
+            self.validate_func = value
+
+        # path
+        value = getattr(settings, 'LIGHT_AUTH_LOGIN_PATH', '')
+        if value:
+            self.login_path = value.rstrip('/')
+        value = getattr(settings, 'LIGHT_AUTH_LOGOUT_PATH', '')
+        if value:
+            self.logout_path = value.rstrip('/')
+
+        self.allow_paths.add(self.login_path)
+        self.allow_paths.add(self.logout_path)
+
+        # expiry
+        value = getattr(settings, 'LIGHT_AUTH_EXPIRY', None)
+        if value is None:
+            # https://docs.djangoproject.com/en/3.1/topics/http/sessions/
+            # "If value is None,
+            # the session reverts to using the global session expiry policy."
+            self.expiry = None
+        elif isinstance(value, int) and value >= 0:
+            self.expiry = value
+        elif isinstance(value, (datetime, timedelta)):
+            self.expiry = value
---
+++ packages/django-light-auth/django_light_auth-0.2.1-py2.py3-none-any.whl/django_light_auth/runtimes.py
@@ -0,0 +1,124 @@
+import logging
+from typing import Dict, Any, Optional, Callable
+
+from django.apps import apps
+from django.http import HttpRequest, HttpResponseRedirect
+
+logger = logging.getLogger(__file__)
+
+TOKEN_KEY = 'light_auth_token'
+
+
+def _get_session_from_request(request: HttpRequest):
+    try:
+        session = request.__getattribute__('session')
+    except AttributeError:
+        raise Exception(
+            'Missed django.contrib.sessions.middleware.SessionMiddleware, '
+            'django-light-auth depend it.'
+        )
+    return session
+
+
+def do_login(request: HttpRequest) -> str:
+    session = _get_session_from_request(request)
+    session[TOKEN_KEY] = apps.get_app_config(
+        'django_light_auth'
+    ).token
+    expiry = apps.get_app_config('django_light_auth').expiry
+    if expiry is not None:
+        # https://docs.djangoproject.com/en/3.1/topics/http/sessions/
+        # "If value is None,
+        # the session reverts to using the global session expiry policy."
+        session.set_expiry(
+            apps.get_app_config('django_light_auth').expiry
+        )
+
+    return apps.get_app_config('django_light_auth').success_path
+
+
+def do_logout(request: HttpRequest) -> str:
+    session = _get_session_from_request(request)
+    session[TOKEN_KEY] = False
+
+    return apps.get_app_config('django_light_auth').login_path
+
+
+def _import_func(func_name: str, father_model=None) -> Optional[Callable]:
+    try:
+        index = func_name.find('.')
+        if index == -1:
+            func = getattr(father_model, func_name[index + 1:])
+            return func
+
+        else:
+            if father_model is None:
+                module = __import__(func_name[:index])
+
+            else:
+                module = getattr(father_model, func_name[:index])
+
+            return _import_func(func_name[index + 1:], module)
+
+    except (ImportError, AttributeError, ValueError) as e:
+        logger.error(e)
+        logger.error('func_name: {}, father_model:{}'.format(
+            func_name, father_model
+        ))
+        return None
+
+
+def do_validate(data: Dict[str, Any]) -> bool:
+    func_name = apps.get_app_config('django_light_auth').validate_func
+
+    func = _import_func(func_name)
+    if func is None:
+        raise ImportError('Can import: {}'.format(func_name))
+
+    if func(data):
+        return True
+
+    return False
+
+
+def light_auth_validate_func(data: Dict[str, Any]) -> bool:
+    """demo for setting.LIGHT_AUTH_VALIDATE_FUNC"""
+    if data.get('username', None) == data.get('password', None):
+        return True
+
+    return False
+
+
+def validate_request(request: HttpRequest) -> bool:
+    path = request.path.rstrip('/')
+    if path in apps.get_app_config('django_light_auth').allow_paths:
+        return True
+
+    session = _get_session_from_request(request)
+    token = session.get(TOKEN_KEY, '')
+    if token == apps.get_app_config('django_light_auth').token:
+        return True
+
+    return False
+
+
+class LightAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request: HttpRequest):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+
+        response = self.get_response(request)
+
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        if not validate_request(request):
+            return HttpResponseRedirect(
+                apps.get_app_config('django_light_auth').login_path
+            )
+
+        return response
