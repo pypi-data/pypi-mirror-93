0116b6c7b7d58a9d4f3cbd6ac183c2cef6118c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:46 +0000

    Add package global-logger/0.3.21/global_logger-0.3.21-py2.py3-none-any.whl

---
+++ packages/global-logger/global_logger-0.3.21-py2.py3-none-any.whl/global_logger/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Global Logger."""
+
+__author__ = """Alexey Rubasheff"""
+__email__ = 'alexey.rubasheff@gmail.com'
+__version__ = '0.3.21'
+
+from .global_logger import Log   # noqa # pylint: disable=unused-import
