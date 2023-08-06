f3a4a9f674fc9160ec9a1794d422fa2f4d429554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:50 +0000

    Add package omniblack.logging/0.1.3/omniblack.logging-0.1.3-py3-none-any.whl

---
+++ packages/omniblack.logging/omniblack.logging-0.1.3-py3-none-any.whl/omniblack/logging/logConfig.py
@@ -0,0 +1,45 @@
+import logging
+from sys import stdout
+from os import environ
+
+
+def config(disable_rich=False):
+    """
+    Configure logging to use a nice format.
+
+    Args:
+        disable_rich: Disable the 'rich' library activation.
+            This will also disable color output.
+            'rich' activation can also disable by the NO_COLOR or
+            NO_RICH environment variables.
+
+    """
+    handlers = None
+    format = '%(levelname)s %(asctime)s: %(message)s'
+    rich_disabled = 'NO_RICH' in environ or disable_rich
+    if 'NO_COLOR' not in environ and not rich_disabled:
+        try:
+            from rich.logging import RichHandler
+            from rich.traceback import install
+            install()
+            # We change the format string when rich is enabled, because
+            # it will auto color the level when for us
+            format = '%(asctime)s: %(message)s'
+            handlers = (
+                RichHandler(
+                    rich_tracebacks=True,
+                    show_time=False,
+                ),
+            )
+        except ImportError:
+            # Rich is optional
+            pass
+
+    if handlers is None:
+        handlers = (logging.StreamHandler(stream=stdout), )
+
+    logging.basicConfig(
+        format=format,
+        datefmt='%m/%d/%Y %I:%M:%S %p',
+        handlers=handlers,
+    )
