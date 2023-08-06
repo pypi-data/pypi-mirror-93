73f90a23e693e2fb67a88cbde3f99d4d0563af76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:32 +0000

    Add package wsgi-lineprof/0.13.0/wsgi_lineprof-0.13.0-cp37-cp37m-win32.whl

---
+++ packages/wsgi-lineprof/wsgi_lineprof-0.13.0-cp37-cp37m-win32.whl/wsgi_lineprof/types.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from datetime import datetime
+from types import CodeType
+from typing import Dict, TextIO
+import uuid
+
+from typing_extensions import TypedDict
+
+from wsgi_lineprof.extensions import LineTiming
+
+
+CodeTiming = Dict[int, LineTiming]
+Measurement = Dict[CodeType, CodeTiming]
+RequestMeasurement = TypedDict("RequestMeasurement", {
+    "id": uuid.UUID,
+    "started_at": datetime,
+    "elapsed": float,
+    "unit": float,
+    "results": Measurement,
+    "request_method": str,
+    "path_info": str,
+    "query_string": str,
+})
+
+# TODO: Improve definition
+Stream = TextIO
