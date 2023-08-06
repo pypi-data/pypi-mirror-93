bb1db15e85f52030a7a1fbb1c9d38b9dcbf8720c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:31 +0000

    Add package jurigged/0.1.0/jurigged-0.1.0.tar.gz

---
+++ packages/jurigged/jurigged-0.1.0.tar.gz/jurigged-0.1.0/jurigged/__init__.py
@@ -0,0 +1,4 @@
+from .codefile import CodeFile, Definition, conform, dig
+from .live import Watcher, watch
+from .recode import Recoder, function_recoder, module_recoder, virtual_file
+from .register import registry
