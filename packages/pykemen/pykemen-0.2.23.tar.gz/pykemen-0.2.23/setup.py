7953bcabf4096236e13bfaf462fb61e6d22bf15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:14 +0000

    Add package pykemen/0.2.23/pykemen-0.2.23.tar.gz

---
+++ packages/pykemen/pykemen-0.2.23.tar.gz/pykemen-0.2.23/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+setup(
+    name='pykemen',
+    version='0.2.23',
+    description='Added file attatchemn support to emails',
+    license='Eiup',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'pykemen': ['google/*.py', '*']},
+    author='Ayyoub Marroun',
+    author_email='ayyoub@metriplica.com',
+    keywords=['google apis', 'utilities'],
+    url='https://github.com/ayyoubmarroun/pykemen',
+    download_url='https://github.com/ayyoubmarroun/pykemen/archive/0.2.23.tar.gz',
+    install_requires=[
+        'httplib2',
+        'pandas',
+        'google-api-python-client',
+        'oauth2client',
+        'google-cloud',
+        'google-cloud-core',
+        'google-cloud-bigquery',
+        'future'
+    ]
+)
