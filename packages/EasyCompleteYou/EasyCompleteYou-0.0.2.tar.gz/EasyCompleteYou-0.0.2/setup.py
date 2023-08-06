8b9135f2f7b5586c58bb9642f2548bc682cf51e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:36 +0000

    Add package EasyCompleteYou/0.0.2/EasyCompleteYou-0.0.2.tar.gz

---
+++ packages/EasyCompleteYou/EasyCompleteYou-0.0.2.tar.gz/EasyCompleteYou-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+# with open("readme.md", "r") as fh:
+#     long_description = fh.read()
+
+long_description = "sdfsdf"
+setuptools.setup(
+    name="EasyCompleteYou",
+    version="0.0.2",
+    author="jimmy huang",
+    author_email="1902161621@qq.com",
+    description="for ECY",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas>=0.20.0',  #所需要包的版本號
+    ])
