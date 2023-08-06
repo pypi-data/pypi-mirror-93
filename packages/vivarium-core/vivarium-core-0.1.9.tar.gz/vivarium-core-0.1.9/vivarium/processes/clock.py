52f5f66b1f50981ba727a7de579ca2ce2c86eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package vivarium-core/0.1.9/vivarium-core-0.1.9.tar.gz

---
+++ packages/vivarium-core/vivarium-core-0.1.9.tar.gz/vivarium-core-0.1.9/vivarium/processes/clock.py
@@ -0,0 +1,24 @@
+"""
+=============
+Clock Process
+=============
+"""
+from vivarium.core.process import Process
+
+class Clock(Process):
+    """ Clock
+
+    A process for tracking global time
+    """
+    name = 'clock'
+    defaults = {
+        'time_step': 1.0}
+
+    def ports_schema(self):
+        return {
+            'global_time': {
+                '_default': 0.0,
+                '_updater': 'accumulate'}}
+
+    def next_update(self, timestep, states):
+        return {'global_time': timestep}
