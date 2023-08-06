962b9bdc584e4ef7d576eda6c1ea578ec75f2c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:16 +0000

    Add package vic-NeuralNetwork/0.0.2/vic_NeuralNetwork-0.0.2.tar.gz

---
+++ packages/vic-NeuralNetwork/vic_NeuralNetwork-0.0.2.tar.gz/vic_NeuralNetwork-0.0.2/README.md
@@ -0,0 +1,20 @@
+## About
+
+This is a self-designed deep learning framework created by victor @Hangzhou
+
+
+## Features
++ Define Dynamic Computing Graph
++ Define Neural Network Operator
++ define Linear, Sigmoid, L2_loss
++ Auto_Diff Computing
++ Auto-Feedforward and Backward
+
+## Remain
++ Classificaiton
++ Cross-Entropy
++ CNN, RNN
+
+# Connection
+whatchat:alwayslearn
+Mail:2534535@qq.com
---
+++ packages/vic-NeuralNetwork/vic_NeuralNetwork-0.0.2.tar.gz/vic_NeuralNetwork-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vic_NeuralNetwork", # Replace with your own username
+    version="0.0.2",
+    author="Victor",
+    author_email="25333456@qq.com",
+    description="This is self-defined neural network framework for self-learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
