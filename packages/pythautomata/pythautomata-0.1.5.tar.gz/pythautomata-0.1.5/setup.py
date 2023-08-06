f39eda9fe259add45264ea6bbc09485d716a5312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:32 +0000

    Add package pythautomata/0.1.5/pythautomata-0.1.5.tar.gz

---
+++ packages/pythautomata/pythautomata-0.1.5.tar.gz/pythautomata-0.1.5/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pythautomata',
+ 'pythautomata.abstract',
+ 'pythautomata.automata',
+ 'pythautomata.automata.wheighted_automaton_definition',
+ 'pythautomata.base_types',
+ 'pythautomata.exceptions',
+ 'pythautomata.model_comparators',
+ 'pythautomata.model_exporters',
+ 'pythautomata.utilities']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graphviz', 'numpy']
+
+setup_kwargs = {
+    'name': 'pythautomata',
+    'version': '0.1.5',
+    'description': "ORT's implementation of various kinds of automata",
+    'long_description': None,
+    'author': 'Federico',
+    'author_email': 'fedevilensky@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
