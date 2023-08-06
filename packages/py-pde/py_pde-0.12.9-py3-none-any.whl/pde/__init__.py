d09dbdcd433ba612d1c06addeb7c76dd5d98e10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:35 +0000

    Add package py-pde/0.12.9/py_pde-0.12.9-py3-none-any.whl

---
+++ packages/py-pde/py_pde-0.12.9-py3-none-any.whl/pde/__init__.py
@@ -0,0 +1,15 @@
+"""
+The py-pde package provides classes and methods for solving partial differential
+equations.
+"""
+
+from .fields import *  # @UnusedWildImport
+from .grids import *  # @UnusedWildImport
+from .pdes import *  # @UnusedWildImport
+from .solvers import *  # @UnusedWildImport
+from .storage import *  # @UnusedWildImport
+from .tools.misc import environment
+from .tools.parameters import Parameter
+from .trackers import *  # @UnusedWildImport
+from .version import __version__
+from .visualization import *  # @UnusedWildImport
---
+++ packages/py-pde/py_pde-0.12.9-py3-none-any.whl/pde/tools/__init__.py
@@ -0,0 +1,22 @@
+"""
+Package containing several tools required in py-pde
+
+.. autosummary::
+   :nosignatures:
+
+   cache
+   cuboid
+   docstrings
+   expressions
+   math
+   misc
+   numba
+   output
+   parameters
+   parse_duration
+   plotting
+   spectral
+   spherical
+   
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de> 
+"""
