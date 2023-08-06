1313c37837ad506bbbacb8c8dae365e42b2595a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:11 +0000

    Add package get-me-that-dashboard/0.0.1/get_me_that_dashboard-0.0.1.tar.gz

---
+++ packages/get-me-that-dashboard/get_me_that_dashboard-0.0.1.tar.gz/get_me_that_dashboard-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='get_me_that_dashboard',
+      version='0.0.1',
+      description='simple utility to import grafana dashboards into helm charts',
+      author='Eyal Firstenberg',
+      author_email='efirstenberg@paloaltonetworks.com',
+      classifiers=['Development Status :: 4 - Beta'],
+      packages=[],
+      scripts=['bin/get_me_that_dashboard'])
