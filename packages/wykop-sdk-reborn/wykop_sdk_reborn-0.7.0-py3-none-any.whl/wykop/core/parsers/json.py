bffca9dae00181feca530ba74432279a342e2037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:31 +0000

    Add package wykop-sdk-reborn/0.7.0/wykop_sdk_reborn-0.7.0-py3-none-any.whl

---
+++ packages/wykop-sdk-reborn/wykop_sdk_reborn-0.7.0-py3-none-any.whl/wykop/core/parsers/json.py
@@ -0,0 +1,36 @@
+"""Wykop API JSON praser module."""
+from __future__ import absolute_import
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from wykop.core.parsers.base import BaseParser, Error
+
+
+class JSONParser(BaseParser):
+
+    def __init__(self, exception_resolver, **json_kwargs):
+        super(JSONParser, self).__init__(exception_resolver)
+        self.json_kwargs = json_kwargs
+
+    def _get_response(self, data):
+        return json.loads(data, **self.json_kwargs)
+
+    def _get_error(self, response):
+        if not isinstance(response, dict):
+            return
+
+        error_data = response.get('error')
+
+        if error_data is None:
+            return
+
+        code = error_data.get('code')
+        message = error_data.get('message')
+
+        if message is None:
+            # try english message
+            message = error_data.get('message_en')
+
+        return Error(code, message)
