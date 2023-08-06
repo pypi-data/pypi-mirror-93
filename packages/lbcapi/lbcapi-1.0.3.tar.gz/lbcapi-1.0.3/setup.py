88fd17c1cdf3d956c5b4945731d7aa0a79df51b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:59 +0000

    Add package lbcapi/1.0.3/lbcapi-1.0.3.tar.gz

---
+++ packages/lbcapi/lbcapi-1.0.3.tar.gz/lbcapi-1.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='lbcapi',
+    version='1.0.3',
+    packages=['lbcapi'],
+    include_package_data=True,
+    license='MIT License',
+    description='Make API calls to LocalBitcoins API.',
+    author='LocalBitcoins Oy',
+    url='https://github.com/LocalBitcoins/lbcapi',
+    install_requires=[
+        'requests',
+    ],
+)
