2c280172c4a44fb0c44cd88212572535b4d4628b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package antenny-py/1.3.0/antenny-py-1.3.0.tar.gz

---
+++ packages/antenny-py/antenny-py-1.3.0.tar.gz/antenny-py-1.3.0/antenny/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from antenny.api.client_api import ClientApi
+from antenny.api.events_api import EventsApi
+from antenny.api.registration_api import RegistrationApi
+from antenny.api.subscription_api import SubscriptionApi
