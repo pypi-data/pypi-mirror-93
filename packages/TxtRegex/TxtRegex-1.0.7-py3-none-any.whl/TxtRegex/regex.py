3a7cb1b5e31fba6f2e3165a6014d06ec2b08e90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:14 +0000

    Add package TxtRegex/1.0.7/TxtRegex-1.0.7-py3-none-any.whl

---
+++ packages/TxtRegex/TxtRegex-1.0.7-py3-none-any.whl/TxtRegex/regex.py
@@ -0,0 +1,23 @@
+import re
+
+
+def convert(op, content, rep):
+    op = op.upper()
+    tmp = content.splitlines()
+    tmp = list(filter(None, tmp))
+    temp = tmp
+    for i in range(len(tmp)):
+        if op == "EMAIL":
+            temp[i] = re.sub(r'([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)', rep, temp[i]).strip()
+        elif op == "URL":
+            temp[i] = re.sub(r'(http|ftp|https):\S+', rep, temp[i]).strip()
+        elif op == "NUMBER":
+            temp[i] = re.sub('[0-9]+-[0-9]+-[0-9]+(-[0-9]+)?', rep, temp[i]).strip()
+        elif op == "BRACKET":
+            temp[i] = re.sub(r'[<{(【「〖\[].*[>)}】」〗\]]', rep, temp[i]).strip()
+        elif op == "HTML":
+            temp[i] = re.sub(r'<[^>]+>', '', temp[i]).strip()
+        elif op == "ONLY":
+            temp[i] = re.sub(r'[^ .,A-Za-z0-9가-힣]', '', temp[i]).strip()
+
+    return list(filter(None, temp))
