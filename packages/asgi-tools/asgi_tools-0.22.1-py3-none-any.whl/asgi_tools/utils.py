7f749463b8393631853bae599a06baff98d43d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:32 +0000

    Add package asgi-tools/0.22.1/asgi_tools-0.22.1-py3-none-any.whl

---
+++ packages/asgi-tools/asgi_tools-0.22.1-py3-none-any.whl/asgi_tools/utils.py
@@ -0,0 +1,34 @@
+"""ASGI-Tools Utils."""
+
+from functools import wraps
+from inspect import iscoroutinefunction, isasyncgenfunction
+import typing as t
+
+from multidict import CIMultiDict
+
+from ._types import ScopeHeaders
+
+
+F = t.TypeVar('F', bound=t.Callable[..., t.Any])
+
+
+def is_awaitable(fn: t.Callable) -> bool:
+    """Check than the given function is awaitable."""
+    return iscoroutinefunction(fn) or isasyncgenfunction(fn)
+
+
+def to_awaitable(fn: F) -> F:
+    """Convert the given function to a coroutine function if it isn't"""
+    if is_awaitable(fn):
+        return fn
+
+    @wraps(fn)
+    async def coro(*args, **kwargs):
+        return fn(*args, **kwargs)
+
+    return t.cast(F, coro)
+
+
+def parse_headers(headers: ScopeHeaders) -> CIMultiDict:
+    """Decode the given headers list."""
+    return CIMultiDict([(n.decode('latin-1'), v.decode('latin-1')) for n, v in headers])
