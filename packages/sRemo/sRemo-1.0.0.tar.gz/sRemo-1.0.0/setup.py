8a868329c869a98fcbedf2e04dc1c3e5bdf06291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:02 +0000

    Add package sRemo/1.0.0/sRemo-1.0.0.tar.gz

---
+++ packages/sRemo/sRemo-1.0.0.tar.gz/sRemo-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("LICENSE", "r") as f:
+    license = f.readline()
+
+setup(
+    name="sRemo",
+    version="1.0.0",
+    author="zunda(@zunda_pixel)",
+    author_email="zunda.pixel@gmail.com",
+    description="Python client for sRemo(sCloud) API",
+    url="https://github.com/zunda-pixel/sRemo",
+    license="MIT",
+    packages=find_packages(exclude="sRemo")
+)
