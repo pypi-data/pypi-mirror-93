a72ce2a712db5f99a4807439a1a4c639b21f6612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:02 +0000

    Add package super-py/0.2.0/super-py-0.2.0.tar.gz

---
+++ packages/super-py/super-py-0.2.0.tar.gz/super-py-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['super_py']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'super-py',
+    'version': '0.2.0',
+    'description': 'Features that Python should have in the standard library',
+    'long_description': '# SuperPy\n\nDescription follows (Or look into the files, its not that complicated)',
+    'author': 'Marcel Kröker',
+    'author_email': 'kroeker.marcel@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
