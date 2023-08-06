0ae97e45a17224d9bb055d27e7391d29a71264a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:43 +0000

    Add package ctfbox/1.4.2/ctfbox-1.4.2-py3-none-any.whl

---
+++ packages/ctfbox/ctfbox-1.4.2-py3-none-any.whl/ctfbox/exceptions/exceptions.py
@@ -0,0 +1,23 @@
+
+class CtfboxError(Exception):
+    pass
+
+
+class FlaskSessionHelperError(CtfboxError):
+    pass
+
+
+class ProvideArgumentError(CtfboxError):
+    pass
+
+
+class HashAuthArgumentError(CtfboxError):
+    pass
+
+
+class GeneratePayloadError(CtfboxError):
+    pass
+
+
+class HttprawError(CtfboxError):
+    pass
