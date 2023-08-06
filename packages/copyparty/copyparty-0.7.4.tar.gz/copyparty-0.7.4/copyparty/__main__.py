152ee0a8ebd9a4c7fec77aed0712ae51163439ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:11 +0000

    Add package copyparty/0.7.4/copyparty-0.7.4.tar.gz

---
+++ packages/copyparty/copyparty-0.7.4.tar.gz/copyparty-0.7.4/copyparty/__main__.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+"""copyparty: http file sharing hub (py2/py3)"""
+__author__ = "ed <copyparty@ocv.me>"
+__copyright__ = 2019
+__license__ = "MIT"
+__url__ = "https://github.com/9001/copyparty/"
+
+import os
+import time
+import shutil
+import filecmp
+import locale
+import argparse
+from textwrap import dedent
+
+from .__init__ import E, WINDOWS, VT100
+from .__version__ import S_VERSION, S_BUILD_DT, CODENAME
+from .svchub import SvcHub
+from .util import py_desc
+
+
+class RiceFormatter(argparse.HelpFormatter):
+    def _get_help_string(self, action):
+        """
+        same as ArgumentDefaultsHelpFormatter(HelpFormatter)
+        except the help += [...] line now has colors
+        """
+        fmt = "\033[36m (default: \033[35m%(default)s\033[36m)\033[0m"
+        if not VT100:
+            fmt = " (default: %(default)s)"
+
+        help = action.help
+        if "%(default)" not in action.help:
+            if action.default is not argparse.SUPPRESS:
+                defaulting_nargs = [argparse.OPTIONAL, argparse.ZERO_OR_MORE]
+                if action.option_strings or action.nargs in defaulting_nargs:
+                    help += fmt
+        return help
+
+    def _fill_text(self, text, width, indent):
+        """same as RawDescriptionHelpFormatter(HelpFormatter)"""
+        return "".join(indent + line + "\n" for line in text.splitlines())
+
+
+def ensure_locale():
+    for x in [
+        "en_US.UTF-8",
+        "English_United States.UTF8",
+        "English_United States.1252",
+    ]:
+        try:
+            locale.setlocale(locale.LC_ALL, x)
+            print("Locale:", x)
+            break
+        except:
+            continue
+
+
+def ensure_cert():
+    """
+    the default cert (and the entire TLS support) is only here to enable the
+    crypto.subtle javascript API, which is necessary due to the webkit guys
+    being massive memers (https://www.chromium.org/blink/webcrypto)
+
+    i feel awful about this and so should they
+    """
+    cert_insec = os.path.join(E.mod, "res/insecure.pem")
+    cert_cfg = os.path.join(E.cfg, "cert.pem")
+    if not os.path.exists(cert_cfg):
+        shutil.copy2(cert_insec, cert_cfg)
+
+    try:
+        if filecmp.cmp(cert_cfg, cert_insec):
+            print(
+                "\033[33m  using default TLS certificate; https will be insecure."
+                + "\033[36m\n  certificate location: {}\033[0m\n".format(cert_cfg)
+            )
+    except:
+        pass
+
+    # speaking of the default cert,
+    # printf 'NO\n.\n.\n.\n.\ncopyparty-insecure\n.\n' | faketime '2000-01-01 00:00:00' openssl req -x509 -sha256 -newkey rsa:2048 -keyout insecure.pem -out insecure.pem -days $((($(printf %d 0x7fffffff)-$(date +%s --date=2000-01-01T00:00:00Z))/(60*60*24))) -nodes && ls -al insecure.pem && openssl x509 -in insecure.pem -text -noout
+
+
+def main():
+    time.strptime("19970815", "%Y%m%d")  # python#7980
+    if WINDOWS:
+        os.system("rem")  # enables colors
+
+    desc = py_desc().replace("[", "\033[1;30m[")
+
+    f = '\033[36mcopyparty v{} "\033[35m{}\033[36m" ({})\n{}\033[0m\n'
+    print(f.format(S_VERSION, CODENAME, S_BUILD_DT, desc))
+
+    ensure_locale()
+    ensure_cert()
+
+    ap = argparse.ArgumentParser(
+        formatter_class=RiceFormatter,
+        prog="copyparty",
+        description="http file sharing hub v{} ({})".format(S_VERSION, S_BUILD_DT),
+        epilog=dedent(
+            """
+            -a takes username:password,
+            -v takes src:dst:permset:permset:cflag:cflag:...
+               where "permset" is accesslevel followed by username (no separator)
+               and "cflag" is config flags to set on this volume
+            
+            list of cflags:
+              cnodupe rejects existing files (instead of symlinking them)
+
+            example:\033[35m
+              -a ed:hunter2 -v .::r:aed -v ../inc:dump:w:aed:cnodupe  \033[36m
+              mount current directory at "/" with
+               * r (read-only) for everyone
+               * a (read+write) for ed
+              mount ../inc at "/dump" with
+               * w (write-only) for everyone
+               * a (read+write) for ed
+               * reject duplicate files  \033[0m
+            
+            if no accounts or volumes are configured,
+            current folder will be read/write for everyone
+
+            consider the config file for more flexible account/volume management,
+            including dynamic reload at runtime (and being more readable w)
+
+            values for --urlform:
+              "stash" dumps the data to file and returns length + checksum
+              "save,get" dumps to file and returns the page like a GET
+              "print,get" prints the data in the log and returns GET
+              (leave out the ",get" to return an error instead)
+            """
+        ),
+    )
+    # fmt: off
+    ap.add_argument("-c", metavar="PATH", type=str, action="append", help="add config file")
+    ap.add_argument("-i", metavar="IP", type=str, default="0.0.0.0", help="ip to bind (comma-sep.)")
+    ap.add_argument("-p", metavar="PORT", type=str, default="3923", help="ports to bind (comma/range)")
+    ap.add_argument("-nc", metavar="NUM", type=int, default=64, help="max num clients")
+    ap.add_argument("-j", metavar="CORES", type=int, default=1, help="max num cpu cores")
+    ap.add_argument("-a", metavar="ACCT", type=str, action="append", help="add account")
+    ap.add_argument("-v", metavar="VOL", type=str, action="append", help="add volume")
+    ap.add_argument("-q", action="store_true", help="quiet")
+    ap.add_argument("-ed", action="store_true", help="enable ?dots")
+    ap.add_argument("-emp", action="store_true", help="enable markdown plugins")
+    ap.add_argument("-e2d", action="store_true", help="enable up2k database")
+    ap.add_argument("-e2s", action="store_true", help="enable up2k db-scanner")
+    ap.add_argument("-mcr", metavar="SEC", type=int, default=60, help="md-editor mod-chk rate")
+    ap.add_argument("-nw", action="store_true", help="disable writes (benchmark)")
+    ap.add_argument("-nih", action="store_true", help="no info hostname")
+    ap.add_argument("-nid", action="store_true", help="no info disk-usage")
+    ap.add_argument("--no-sendfile", action="store_true", help="disable sendfile")
+    ap.add_argument("--urlform", type=str, default="print,get", help="how to handle url-forms")
+    al = ap.parse_args()
+    # fmt: on
+
+    al.i = al.i.split(",")
+    try:
+        if "-" in al.p:
+            lo, hi = [int(x) for x in al.p.split("-")]
+            al.p = list(range(lo, hi + 1))
+        else:
+            al.p = [int(x) for x in al.p.split(",")]
+    except:
+        raise Exception("invalid value for -p")
+
+    SvcHub(al).run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/copyparty/copyparty-0.7.4.tar.gz/copyparty-0.7.4/copyparty/httpconn.py
@@ -0,0 +1,137 @@
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+import os
+import sys
+import ssl
+import time
+import socket
+
+try:
+    import jinja2
+except ImportError:
+    print(
+        """\033[1;31m
+  you do not have jinja2 installed,\033[33m
+  choose one of these:\033[0m
+   * apt install python-jinja2
+   * python3 -m pip install --user jinja2
+   * (try another python version, if you have one)
+   * (try copyparty.sfx instead)
+"""
+    )
+    sys.exit(1)
+
+from .__init__ import E
+from .util import Unrecv
+from .httpcli import HttpCli
+
+
+class HttpConn(object):
+    """
+    spawned by HttpSrv to handle an incoming client connection,
+    creates an HttpCli for each request (Connection: Keep-Alive)
+    """
+
+    def __init__(self, sck, addr, hsrv):
+        self.s = sck
+        self.addr = addr
+        self.hsrv = hsrv
+
+        self.args = hsrv.args
+        self.auth = hsrv.auth
+        self.cert_path = hsrv.cert_path
+
+        self.t0 = time.time()
+        self.nbyte = 0
+        self.workload = 0
+        self.log_func = hsrv.log
+        self.set_rproxy()
+
+        env = jinja2.Environment()
+        env.loader = jinja2.FileSystemLoader(os.path.join(E.mod, "web"))
+        self.tpl_mounts = env.get_template("splash.html")
+        self.tpl_browser = env.get_template("browser.html")
+        self.tpl_msg = env.get_template("msg.html")
+        self.tpl_md = env.get_template("md.html")
+        self.tpl_mde = env.get_template("mde.html")
+
+    def set_rproxy(self, ip=None):
+        if ip is None:
+            color = 36
+            ip = self.addr[0]
+            self.rproxy = None
+        else:
+            color = 34
+            self.rproxy = ip
+
+        self.ip = ip
+        self.log_src = "{} \033[{}m{}".format(ip, color, self.addr[1]).ljust(26)
+        return self.log_src
+
+    def respath(self, res_name):
+        return os.path.join(E.mod, "web", res_name)
+
+    def log(self, msg):
+        self.log_func(self.log_src, msg)
+
+    def run(self):
+        method = None
+        self.sr = None
+        if self.cert_path:
+            try:
+                method = self.s.recv(4, socket.MSG_PEEK)
+            except socket.timeout:
+                return
+            except AttributeError:
+                # jython does not support msg_peek; forget about https
+                method = self.s.recv(4)
+                self.sr = Unrecv(self.s)
+                self.sr.buf = method
+
+                # jython used to do this, they stopped since it's broken
+                # but reimplementing sendall is out of scope for now
+                if not getattr(self.s, "sendall", None):
+                    self.s.sendall = self.s.send
+
+            if len(method) != 4:
+                err = "need at least 4 bytes in the first packet; got {}".format(
+                    len(method)
+                )
+                self.log(err)
+                self.s.send(b"HTTP/1.1 400 Bad Request\r\n\r\n" + err.encode("utf-8"))
+                return
+
+        if method not in [None, b"GET ", b"HEAD", b"POST", b"PUT ", b"OPTI"]:
+            if self.sr:
+                self.log("\033[1;31mTODO: cannot do https in jython\033[0m")
+                return
+
+            self.log_src = self.log_src.replace("[36m", "[35m")
+            try:
+                self.s = ssl.wrap_socket(
+                    self.s, server_side=True, certfile=self.cert_path
+                )
+            except Exception as ex:
+                em = str(ex)
+
+                if "ALERT_BAD_CERTIFICATE" in em:
+                    # firefox-linux if there is no exception yet
+                    self.log("client rejected our certificate (nice)")
+
+                elif "ALERT_CERTIFICATE_UNKNOWN" in em:
+                    # chrome-android keeps doing this
+                    pass
+
+                else:
+                    self.log("\033[35mhandshake\033[0m " + em)
+
+                return
+
+        if not self.sr:
+            self.sr = Unrecv(self.s)
+
+        while True:
+            cli = HttpCli(self)
+            if not cli.run():
+                return
