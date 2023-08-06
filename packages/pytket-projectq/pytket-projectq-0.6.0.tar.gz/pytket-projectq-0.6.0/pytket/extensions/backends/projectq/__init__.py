372f7dd21cb939fca79d849338dfe46f70378e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:52 +0000

    Add package pytket-projectq/0.6.0/pytket-projectq-0.6.0.tar.gz

---
+++ packages/pytket-projectq/pytket-projectq-0.6.0.tar.gz/pytket-projectq-0.6.0/pytket/extensions/backends/projectq/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2019-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+"""Backend for utilising the ProjectQ simulator directly from pytket"""
+
+# _metadata.py is copied to the folder after installation.
+from ._metadata import __extension_version__, __extension_name__  # type: ignore
+from .projectq_backend import ProjectQBackend
---
+++ packages/pytket-projectq/pytket-projectq-0.6.0.tar.gz/pytket-projectq-0.6.0/pytket/extensions/projectq/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2019-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+"""Module for conversion between ProjectQ and tket primitives."""
+from .projectq_convert import tk_to_projectq, tketBackendEngine, tketOptimiser
