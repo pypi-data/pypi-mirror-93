83b48ece68d4a9084ed0400f06763ddc23ca8723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:33 +0000

    Add package fairdatapoint/0.7.2/fairdatapoint-0.7.2-py3-none-any.whl

---
+++ packages/fairdatapoint/fairdatapoint-0.7.2-py3-none-any.whl/fdp/fdp.py
@@ -0,0 +1,22 @@
+import connexion
+from fdp import config
+
+def create_app(host, port, graph_endpoint=None):
+    config.init_fairgraph(host, port, graph_endpoint)
+
+    app = connexion.FlaskApp(__name__, specification_dir='openapi/', debug=True, options={"swagger_url": ""})
+    options = {"swagger_ui": True}
+    app.add_api('openapi.yaml',
+            options=options,
+            arguments={'title': 'FDP server'},
+            strict_validation=True,
+            validate_responses=True
+            )
+
+    return app.app
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    port = 80
+    app = create_app(host, port)
+    app.run(host=host, port=port, debug=True)
