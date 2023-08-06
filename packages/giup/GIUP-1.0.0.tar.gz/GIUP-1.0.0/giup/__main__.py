0e96e0a01c34c02d9387b4e7482adb5e9e4b33c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package giup/1.0.0/GIUP-1.0.0.tar.gz

---
+++ packages/giup/GIUP-1.0.0.tar.gz/GIUP-1.0.0/giup/__main__.py
@@ -0,0 +1,63 @@
+# Copyright 2021 Siphalor
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -*- coding: utf-8 -*-
+import argparse
+import asyncio
+import sys
+
+import colorama
+from termcolor import cprint
+
+from giup import util, __version__
+from giup.project import Project
+
+colorama.init()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="GIUP",
+        description="Git Interactive Update and Publish - "
+                    "Interactively hierarchically merge, update and publish your projects."
+    )
+    parser.add_argument(
+        "project",
+        type=str,
+        nargs="?",
+        default=".giup",
+        help="the project configuration to use. \".giup\" is the default"
+    )
+    parser.add_argument(
+        "-f", "--fail",
+        action="store_true",
+        help="quit the run on first error"
+    )
+    parser.add_argument(
+        "-v", "--version",
+        action="version",
+        version=__version__.__version__
+    )
+    args = parser.parse_args()
+    try:
+        event_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
+        project: Project = event_loop.run_until_complete(Project.read(args.project))
+        project.fail_on_error = args.fail
+        event_loop.run_until_complete(project.run())
+    except util.ParseError as error:
+        cprint("Failed to parse project configuration file:\n" + str(error), color="red", file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/giup/GIUP-1.0.0.tar.gz/GIUP-1.0.0/giup/git.py
@@ -0,0 +1,56 @@
+# Copyright 2021 Siphalor
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# -*- coding: utf-8 -*-
+import os
+import shlex
+
+from .util import async_run_command_result, async_run_command_output
+
+
+class GitError(BaseException):
+    pass
+
+
+class GitBranchError(GitError):
+    branch_name: str
+
+    def __init__(self, __branch_name: str):
+        self.branch_name = __branch_name
+
+
+async def check_branch_name(branch_name: str) -> bool:
+    if await async_run_command_result("git check-ref-format --branch " + shlex.quote(branch_name),
+                                      stdout=False, stderr=False):
+        raise GitBranchError(branch_name)
+    else:
+        return True
+
+
+async def get_current_branch() -> str:
+    (code, (stdout, stderr)) = await async_run_command_output("git branch --show-current", stderr=False)
+    if code:
+        raise GitBranchError("Unknown")
+    else:
+        return os.fsdecode(stdout).strip()
+
+
+async def switch(branch_name: str):
+    if await async_run_command_result("git switch --quiet " + shlex.quote(branch_name)):
+        raise GitBranchError(branch_name)
+
+
+async def merge(branch_name: str):
+    if await async_run_command_result("git merge " + shlex.quote(branch_name)):
+        raise GitBranchError(branch_name)
