030b28e0858065d3e4b5a396c7be2493d46cc992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package NHXDB/1.3.0/NHXDB-1.3.0.tar.gz

---
+++ packages/NHXDB/NHXDB-1.3.0.tar.gz/NHXDB-1.3.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="NHXDB",
+    version="v1.3.0",
+    author="NHXTech",
+    author_email="chmuhammadsohaib@gmail.com",
+    description="A lightweight Database Module with a blend of SQL-like Language and ORM syntax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NHXTech/NHXDB",
+    packages=setuptools.find_packages(),
+    keywords = 'NHXDB NHXTech chmuhammadsohaib NHX Database DBMS',
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
