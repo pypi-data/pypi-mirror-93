1b06577b9561c3cfafd848292b7aeb7268f810a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package aitur/0.0.1/aitur-0.0.1.tar.gz

---
+++ packages/aitur/aitur-0.0.1.tar.gz/aitur-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name="aitur",
+    version="0.0.1",
+    packages=['aitur'],
+    description="Artificial Intelligence for Turkish",
+    long_description=open('README.md', encoding="utf8").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/msaidzengin/aitur",
+    author="M. Said Zengin",
+    author_email="msaidzengin@gmail.com",
+    maintainer="M. Said Zengin",
+    maintainer_email="msaidzengin@gmail.com",
+    keywords=['ai', 'turkish', 'tur', 'artificial intelligence', 'yapay zeka', 'makine ögrenmesi'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
