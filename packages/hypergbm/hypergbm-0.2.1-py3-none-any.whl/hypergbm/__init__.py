cacac4d484ea6de4008edee89c02e1a48732eb01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:21 +0000

    Add package hypergbm/0.2.1/hypergbm-0.2.1-py3-none-any.whl

---
+++ packages/hypergbm/hypergbm-0.2.1-py3-none-any.whl/hypergbm/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+__author__ = 'yangjian'
+"""
+
+"""
+
+from .hyper_gbm import HyperGBM, HyperGBMEstimator, HyperGBMExplainer, HyperEstimator, HyperModel
+from .experiment import CompeteExperiment, make_experiment
+
+__version__ = '0.2.1'
