ef9448fb8e9537ea69d88685755903fed3d3d94d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:05 +0000

    Add package aesim.simba/2021.1.31/aesim.simba-2021.1.31.tar.gz

---
+++ packages/aesim.simba/aesim.simba-2021.1.31.tar.gz/aesim.simba-2021.1.31/aesim/simba/__init__.py
@@ -0,0 +1,15 @@
+#%% Load modules...
+import clr, sys, os
+
+foldername = os.path.dirname(os.path.abspath(__file__)) + r'\Resources'
+sys.path.append(foldername)
+clr.AddReference(foldername+'\\Simba.Data.dll')
+from Simba.Data.Repository import ProjectRepository
+from Simba.Data import License, Design, Circuit, DesignExamples
+import Simba.Data
+Simba.Data.FunctionsAssemblyResolver.RedirectAssembly()
+Simba.Data.DoubleArrayPythonEncoder.Register()
+Simba.Data.ParameterToPythonEncoder.Register()
+Simba.Data.PythonToParameterDecoder.Register()
+if os.environ.get('SIMBA_DEPLOYMENT_KEY') is not None:
+    License.Activate(os.environ.get('SIMBA_DEPLOYMENT_KEY'))
---
+++ packages/aesim.simba/aesim.simba-2021.1.31.tar.gz/aesim.simba-2021.1.31/setup.py
@@ -0,0 +1,45 @@
+#from distutils.core import setup
+from setuptools import setup
+from datetime import datetime
+    
+with open("README.md", "r",encoding="utf8") as fh:
+    long_description = fh.read()
+    
+setup(
+    # Application name:
+    name="aesim.simba",
+
+    # Version number:
+    version="2021.01.31",
+
+    # Application author details:
+    author="AESIM.tech",
+    author_email="contact@aesim.tech",
+    
+    # Packages
+    packages=["aesim.simba"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://simba.io",
+
+    license="By using aesim.simba, you agree to the license agreement available here: https://www.simba.io/static/EULA.pdf",
+    
+    description="Power Electronics and Motor Drive Simulation",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "wheel",
+        "pythonnet",
+    ],
+    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
