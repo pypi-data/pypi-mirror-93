d53a60e6e02932b242cd716a266d700530c5239c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package dbustiker/0.0.1/dbustiker-0.0.1-py3-none-any.whl

---
+++ packages/dbustiker/dbustiker-0.0.1-py3-none-any.whl/dbustiker.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+
+def get_stiket(ID):
+    try:
+        if int(ID) > 0:
+            gets = requests.get('https://paladinbot.pythonanywhere.com/dbu/?method=get_stiker&id={}'.format(ID)).json()
+            return gets
+        else:
+            'DBU_EROR: user_id должен быть больше 0'
+    except Exception as e:
+        print(e)
+        return 'DBU_EROR: user_id должен быть числом.'
+
+print(get_stiket(396886945))
