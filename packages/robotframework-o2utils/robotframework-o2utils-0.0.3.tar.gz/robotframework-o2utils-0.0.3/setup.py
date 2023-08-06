c02a044bd9f48d02d37942f378ed1b63a10b71fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:13 +0000

    Add package robotframework-o2utils/0.0.3/robotframework-o2utils-0.0.3.tar.gz

---
+++ packages/robotframework-o2utils/robotframework-o2utils-0.0.3.tar.gz/robotframework-o2utils-0.0.3/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='robotframework-o2utils',  # Required
+
+    version='0.0.3',  # Required
+
+    description='Package for testing needs',  # Optional
+
+    long_description=long_description,  # Optional
+
+    long_description_content_type='text/markdown',  # Optional
+
+    # url='https://github.com/pypa/sampleproject',  # Optional
+
+    author='Eduard Farkaš',  # Optional
+
+    # author_email='',  # Optional
+
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        # 'Intended Audience :: Testers',
+        # 'Topic :: Software Testing :: Test Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    keywords='robot framework, testing',  # Optional
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+    package_dir={'': 'src'},  # Optional
+
+    python_requires='>=3.6, <4',
+
+    # project_urls={  # Optional
+    #     'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+    #     'Funding': 'https://donate.pypi.org',
+    #     'Say Thanks!': 'http://saythanks.io/to/example',
+    #     'Source': 'https://github.com/pypa/sampleproject/',
+    # },
+)
