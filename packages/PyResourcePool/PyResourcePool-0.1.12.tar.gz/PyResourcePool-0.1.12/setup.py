1aecbdee2484c9ee0179327a9b3a298f4843cc8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:21 +0000

    Add package PyResourcePool/0.1.12/PyResourcePool-0.1.12.tar.gz

---
+++ packages/PyResourcePool/PyResourcePool-0.1.12.tar.gz/PyResourcePool-0.1.12/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PyResourcePool',
+    version='0.1.12',
+    author='Jason Milen',
+    author_email='jpmilen@gmail.com',
+    url='http://pypi.python.org/pypi/PyResourcePool/',
+    license='LICENSE.txt',
+    description='Provides a thread safe resource pool',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+)
