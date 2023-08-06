9a1e73174c00153ab533ee6fc03640601ae609cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:40 +0000

    Add package spintest/0.4.0/spintest-0.4.0-py3-none-any.whl

---
+++ packages/spintest/spintest-0.4.0-py3-none-any.whl/spintest/__init__.py
@@ -0,0 +1,46 @@
+"""Initialization of `spintest` package."""
+
+import asyncio
+import colorlog
+import logging
+
+from typing import List, Dict, Optional
+
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(
+    colorlog.ColoredFormatter("%(log_color)s%(asctime)s - %(levelname)s - %(message)s")
+)
+logger = colorlog.getLogger("spintest")
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+
+from spintest.manager import TaskManager  # noqa: E402
+
+
+def spintest(
+    urls: List[str],
+    tasks: List[Dict[str, str]],
+    token: str = None,
+    parallel: bool = False,
+    verify: bool = True,
+    generate_report: Optional[str] = None,
+):
+    """Programmatic wrapper for spintest."""
+    loop = asyncio.new_event_loop()
+    task_manager = TaskManager(
+        urls,
+        tasks,
+        token=token,
+        parallel=parallel,
+        verify=verify,
+        generate_report=generate_report,
+    )
+    result = loop.run_until_complete(task_manager.run())
+    loop.close()
+    return result
+
+
+__all__ = ["spintest", "TaskManager"]
+__version__ = "0.4.0"
