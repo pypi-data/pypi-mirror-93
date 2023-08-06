de81f39a68350bf5e3f50bedd270cda9e1454fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:38 +0000

    Add package covasim/2.0.2/covasim-2.0.2-py3-none-any.whl

---
+++ packages/covasim/covasim-2.0.2-py3-none-any.whl/covasim/version.py
@@ -0,0 +1,9 @@
+'''
+Version and license information.
+'''
+
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__ = '2.0.2'
+__versiondate__ = '2020-02-01'
+__license__ = f'Covasim {__version__} ({__versiondate__}) — © 2021 by IDM'
