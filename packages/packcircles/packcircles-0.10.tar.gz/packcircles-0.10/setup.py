6fe0af0ce1dee54dc7e0cb3ce4ace3019732e5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:37 +0000

    Add package packcircles/0.10/packcircles-0.10.tar.gz

---
+++ packages/packcircles/packcircles-0.10.tar.gz/packcircles-0.10/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='packcircles',
+    version='0.10',
+    description='A pure Python implementation of a circle packing algorithm',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/mhtchan/packcircles',
+    author='mhtchan',
+    author_email='mhtchan@outlook.com',
+    license='MIT',
+    packages=['packcircles'],
+    install_requires=['pyllist >= 0.3'])
