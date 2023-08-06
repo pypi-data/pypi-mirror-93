6e452208ab3ab3a25feb23f48e95c7b8514b2af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package ratsnlp/0.0.955/ratsnlp-0.0.955-py3-none-any.whl

---
+++ packages/ratsnlp/ratsnlp-0.0.955-py3-none-any.whl/ratsnlp/nlpbook/generation/corpus.py
@@ -0,0 +1,28 @@
+import csv
+import logging
+from transformers import InputExample
+
+
+logger = logging.getLogger(__name__)
+
+
+class KoreanChatCorpus:
+
+    @classmethod
+    def _read_corpus(cls, input_file, quotechar='"'):
+        with open(input_file, "r", encoding="utf-8") as f:
+            return list(csv.reader(f, delimiter="\t", quotechar=quotechar))
+
+    def get_labels(self):
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = line[1]
+            label = None if set_type == "test" else line[2]
+            examples.append(InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
