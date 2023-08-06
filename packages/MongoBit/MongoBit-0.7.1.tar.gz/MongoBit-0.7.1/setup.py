8f82e73a9df718e09ec77d72f691fcd228c98b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:22 +0000

    Add package MongoBit/0.7.1/MongoBit-0.7.1.tar.gz

---
+++ packages/MongoBit/MongoBit-0.7.1.tar.gz/MongoBit-0.7.1/setup.py
@@ -0,0 +1,31 @@
+"""
+MongoBit
+--------
+
+Simple pymongo orm
+"""
+from setuptools import setup
+
+setup(
+    name="MongoBit",
+    version="0.7.1",
+    url="https://github.com/lixxu/mongobit",
+    license="BSD",
+    author="Lix Xu",
+    author_email="xuzenglin@gmail.com",
+    description="Simple pymongo orm",
+    long_description=__doc__,
+    packages=["mongobit"],
+    zip_safe=False,
+    platforms="any",
+    install_requires=["pymongo", "six"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
