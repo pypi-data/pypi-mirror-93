067868ae10ed66a8a6cc1ce086a1cd18385fd6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:04 +0000

    Add package circuitpython-nrf24l01/1.3.0/circuitpython-nrf24l01-1.3.0.tar.gz

---
+++ packages/circuitpython-nrf24l01/circuitpython-nrf24l01-1.3.0.tar.gz/circuitpython-nrf24l01-1.3.0/docs/index.rst
@@ -0,0 +1,56 @@
+.. include:: greetings.rst
+
+.. toctree::
+    :caption: Introduction
+    :hidden:
+
+    greetings
+
+.. toctree::
+    :caption: Examples
+    :hidden:
+
+    examples
+
+.. toctree::
+    :caption: API Reference
+    :hidden:
+
+    basic_api
+    advanced_api
+    configure_api
+    ble_api
+
+.. toctree::
+    :caption: Troubleshooting
+    :hidden:
+
+    troubleshooting
+
+
+.. toctree::
+    :caption: Store Links
+    :hidden:
+
+    2.4GHz Transceiver IC - nRF24L01+ <https://www.sparkfun.com/products/690>
+    SparkFun Transceiver Breakout - nRF24L01+ <https://www.sparkfun.com/products/691>
+    SparkFun Transceiver Breakout - nRF24L01+ (RP-SMA) <https://www.sparkfun.com/products/705>
+
+.. toctree::
+    :caption: Other Links
+    :hidden:
+
+    Download <https://github.com/2bndy5/CircuitPython_nRF24L01/releases/latest>
+    CircuitPython Reference Documentation <https://circuitpython.readthedocs.io>
+    CircuitPython Support Forum <https://forums.adafruit.com/viewforum.php?f=60>
+    Discord Chat <https://adafru.it/discord>
+    Adafruit Learning System <https://learn.adafruit.com>
+    Adafruit Blog <https://blog.adafruit.com>
+    Adafruit Store <https://www.adafruit.com>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/circuitpython-nrf24l01/circuitpython-nrf24l01-1.3.0.tar.gz/circuitpython-nrf24l01-1.3.0/setup.py
@@ -0,0 +1,58 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from os import path
+from setuptools import setup
+
+# To use a consistent encoding
+from codecs import open
+
+REPO = "https://github.com/2bndy5/CircuitPython_nRF24L01"
+
+# Get the long description from the README file
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="circuitpython-nrf24l01",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Circuitpython driver library for the nRF24L01 transceiver",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # Author details
+    author="Brendan Doherty",
+    author_email="2bndy5@gmail.com",
+    install_requires=["Adafruit-Blinka", "adafruit-circuitpython-busdevice"],
+    # Choose your license
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+    # What does your project relate to?
+    keywords="adafruit blinka circuitpython micropython nrf24l01 nRF24L01+"
+    " raspberry pi driver radio transceiver",
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
+    #       CHANGE `py_modules=['...']` TO `packages=['...']`
+    packages=["circuitpython_nrf24l01"],
+    # Specifiy your homepage URL for your project here
+    url=REPO,
+    # Extra links for the sidebar on pypi
+    project_urls={"Documentation": "http://circuitpython-nrf24l01.rtfd.io",},
+    download_url="{}/releases".format(REPO),
+)
