8778498ef04844c84519f8652385a6b5730ad14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:22 +0000

    Add package mothman/2.3.0b0/mothman-2.3.0b0-py3-none-any.whl

---
+++ packages/mothman/mothman-2.3.0b0-py3-none-any.whl/mothman/depictions.py
@@ -0,0 +1,295 @@
+# coding: utf8
+"""Generate Cydia/Sileo depictions (from deb packages) for Debian repos.
+"""
+
+import json
+import re
+from datetime import datetime
+from xml.etree import ElementTree as etree
+
+# regexes
+RE_DEPENDS = re.compile(r"^([a-z0-9+\-\.]+)(?: *\(([<>=]{1,2}) *(.*)?\))?$")
+
+
+def dict_to_xml(data: dict, rootname: str = "root") -> etree.Element:
+    """Convert a dictionary to XML.
+    All objects are converted to keys, and sub-dictionaries are treated as sub-elements.
+
+    Args:
+        data: The dictionary.
+        rootname: The root element tag.
+    """
+
+    root = etree.Element(rootname)
+
+    for key, value in data.items():
+
+        if isinstance(value, dict):
+            root.append(dict_to_xml(value, rootname=key))
+
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, dict):
+                    root.append(dict_to_xml(item, rootname=key))
+
+                else:
+                    etree.SubElement(root, key).text = str(item)
+
+        else:
+            etree.SubElement(root, key).text = str(value)
+
+    return root
+
+
+class Generic:
+    """A generic depiction.
+    To create new representations, subclass this and override the .build() method,
+    which should output the depiction representation as a string.
+
+    Also, you should pass *args and **kwargs to super().__init__.
+
+    Args:
+        control: The Debian control headers as a dictionary.
+        other_info: A dictionary of package info that does not appear in the
+            control file. The dictionary should be in this format:
+            {
+                "price": "Free",  # price of package
+                "header_image": "..."  # direct url to a banner
+                "screenshots": [  # direct url(s) to screenshot images
+                    "direct_url1",
+                    "direct_url2",
+                    ...
+                ]
+            }
+            where price is the price, header_image is the direct url to a image
+            to use as a banner, and screenshots is a list of URLs to images.
+            (Price and header_image is used only by Sileo.)
+            This is optional.
+
+    Attributes:
+        control: See Args.
+        other_info: See Args.
+    """
+
+    def __init__(self, control: dict, other_info: dict = {}) -> None:
+        self.control = control
+        self.other_info = other_info
+
+    def build(self) -> str:
+        raise NotImplementedError
+
+
+class Cydia(Generic):
+    """A Cydia depiction.
+
+    See GenericDepiction for args.
+    """
+
+    # map of the XML fields to Debian control file fields
+    # most fields are one-to-one, some fields have multiple values
+    # so they will be handled later.
+    XML_ELEMENTS = {"id": "Package", "name": "Name", "version": "Version"}
+
+    # template
+    XML_DICT: dict = {
+        "id": "",
+        "name": "",
+        "version": "",
+        "compatibility": {"firmware": {}},
+        "dependencies": {"package": []},
+        "shortDescription": "",
+        "descriptionlist": {"description": []},
+        "screenshots": {"screenshot": []},
+        "changelog": {"change": ["1.0.0", "Inital release."]},
+        "links": {
+            "link": [
+                {
+                    "name": "/r/jailbreak",
+                    "url": "https://www.reddit.com/r/jailbreak",
+                    "iconclass": "fa fa-reddit",
+                }
+            ]
+        },
+    }
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        # add control info to xml
+        for k, v in self.XML_ELEMENTS.items():
+            self.XML_DICT[k] = self.control[v]
+
+    def build(self) -> str:
+        """Export the depiction as an XML representation (for use in Web depiction),
+        i.e in Reposi3/repo.me repo templates.
+
+        Returns:
+            The XML tree.
+        """
+
+        description = self.control["Description"].splitlines()
+        self.XML_DICT["shortDescription"] = description[0]
+        self.XML_DICT["descriptionlist"]["description"] = description
+
+        dependencies = self.control["Depends"].split(", ")
+        self.XML_DICT["dependencies"]["package"] = dependencies
+
+        for dep in dependencies:
+
+            if not dep.startswith("firmware"):
+                continue
+
+            _, operator, version = RE_DEPENDS.findall(dep)[0]
+
+            # FIXME: No way to specify strictly less/more than for iOS version number
+            if operator == "<<" or operator == "<=":
+                firmware = {"maxiOS": version}
+
+            elif operator == "=":
+                firmware = {"miniOS": version, "maxiOS": version}
+
+            elif operator == ">=" or operator == ">>":
+                firmware = {"miniOS": version}
+
+            self.XML_DICT["compatibility"]["firmware"] = firmware
+
+        screenshots = self.other_info.get("screenshots")
+        if screenshots is not None:
+            for count, url in enumerate(screenshots, 1):
+                self.XML_DICT["screenshots"]["screenshot"].append(
+                    {"description": f"Screenshot {count}", "image": url}
+                )
+
+        return etree.tostring(
+            dict_to_xml(self.XML_DICT, rootname="package"), encoding="unicode"
+        )
+
+
+class Sileo(Generic):
+    # dictionary for sileo views
+    # each view is an item in the 'views' list.
+    SILEO_DICT: dict = {
+        "minVersion": "0.1",
+        "headerImage": "headerImage",
+        "class": "DepictionTabView",
+        "tintColor": "#0657bb",
+        "tabs": [
+            {
+                "tabname": "Details",
+                "views": [],
+                "class": "DepictionStackView",
+            }
+        ],
+    }
+
+    def __init__(self, *args, **kwargs):
+        self._views = []
+        super().__init__(*args, **kwargs)
+
+    def add_view(self, viewclass: str, properties: dict = {}) -> None:
+        """Add a subview to the depiction root.
+
+        Args:
+            viewclass: The class name of the view.
+            properties: The subview's properties.
+        """
+
+        properties["class"] = viewclass
+        self.SILEO_DICT["tabs"][0]["views"].append(properties)
+
+    def add_spacer(self) -> None:
+        """Add a spacer view (to seperate depiction entries)."""
+
+        self.add_view("DepictionSpacerView", {"spacing": 8})
+
+    def build(self) -> str:
+        """Export the depiction as an native representation (for use in Sileo depiction).
+
+        Returns:
+            The JSON as a string.
+        """
+
+        # package name
+        self.add_view(
+            "DepictionSubheaderView",
+            {
+                "title": self.control["Name"],
+                "useBoldText": True,
+                "useBottomMargin": False,
+            },
+        )
+
+        # description (short)
+        self.add_view(
+            "DepictionMarkdownView",
+            {
+                "markdown": self.control["Description"].partition("\n")[0],
+                "useSpacing": True,
+            },
+        )
+
+        self.add_spacer()
+
+        # screenshots (if any)
+        screenshots = []
+
+        if "screenshots" in self.other_info:
+            for count, url in enumerate(self.other_info["screenshots"], 1):
+                screenshots.append(
+                    {"accessibilityText": f"Screenshot{count}", "url": url}
+                )
+
+            self.add_view(
+                "DepictionScreenshotsView",
+                {
+                    "itemCornerRadius": 6,
+                    "itemSize": "{160, 275.41333333333336}",
+                    "screenshots": screenshots,
+                },
+            )
+
+        # description (long)
+        self.add_view(
+            "DepictionMarkdownView",
+            {
+                "title": "markdown-description",
+                "markdown": self.control["Description"],
+                "useBoldText": True,
+                "useBottomMargin": False,
+            },
+        )
+
+        self.add_spacer()
+
+        # version
+        self.add_view(
+            "DepictionTableTextView",
+            {"title": "Version", "text": self.control["Version"]},
+        )
+
+        # date released
+        self.add_view(
+            "DepictionTableTextView",
+            {"title": "Released", "text": datetime.today().strftime("%m-%d-%Y")},
+        )
+
+        # price (if any)
+        self.add_view(
+            "DepictionTableTextView",
+            {"title": "Price", "text": self.other_info.get("price") or "Free"},
+        )
+
+        self.add_spacer()
+
+        # author
+        self.add_view(
+            "DepictionTableTextView",
+            {"title": "Developer", "text": self.control["Author"]},
+        )
+
+        # header image (if any)
+        header = self.other_info.get("header_info")
+        if header is not None:
+            self.SILEO_DICT["headerImage"] = header
+
+        return json.dumps(self.SILEO_DICT, indent=4)
---
+++ packages/mothman/mothman-2.3.0b0-py3-none-any.whl/mothman/repo.py
@@ -0,0 +1,125 @@
+# coding: utf8
+"""Cydia/Sileo repository stuff.
+If you want a 'classical' Debian repository, see mothman.tree.DebianTree.
+"""
+
+import email
+import email.message
+import json
+import logging
+import re
+from typing import Generator, Optional
+
+from mothman import depictions, tree
+
+__all__ = ["Repository"]
+
+_log = logging.getLogger("mothman")
+
+CONFIG_NAME = "mothman.json"
+# config for repo templates (paths to depictions, etc.)
+# all urls are relative to the root.
+TEMPLATES = {
+    "repo.me": {
+        "github": "syns/repo.me",
+        "Depiction": {
+            "path": "depictions/web/{package}/info.xml",
+            "url": "{host}/depictions/web/?p={package}",
+        },
+        "SileoDepiction": {
+            "path": "depictions/native/{package}/depiction.json",
+            "url": "{host}/depictions/native/{package}/depiction.json",
+        },
+        # where the debian packages are
+        "deb_path": "debians",
+        # apt config (if any)
+        "apt.conf": "assets/repo/repo.conf",
+        # files/folders that are not needed
+        "exclude": [
+            "Packages*",
+            "debians/me.syns.sample.deb",
+            "depictions/web/me.syns.samplepackage",
+            "depictions/native/me.syns.samplepackage",
+            # user-generated files, should be customised by repo maintainer
+            "assets/repo/Banners/*",
+            "assets/repo/Screenshots/*",
+            "sileo-featured.json",
+        ],
+    },
+    "Reposi3": {
+        "github": "supermamon/Reposi3",
+        "Depiction": {
+            "path": "depictions/{package}/info.xml",
+            "url": "{host}/depictions/?p={package}",
+        },
+        # Reposi3 doesn't support Sileo depictions ._.
+        "deb_path": "debs",
+        "exclude": [
+            "debs/com.supermamon.*",
+            "depictions/com.supermamon.*",
+            "Packages*",
+            "Release",
+        ],
+    },
+}
+
+DEPICTIONS = {"Depiction": depictions.Cydia, "SileoDepiction": depictions.Sileo}
+
+# for parsing repo.conf (in the syns/repo.me repo template).
+RE_DECL = re.compile(r"^\s*([a-zA-Z]+) (\".+\"|.+);$", re.MULTILINE)
+
+
+class Repository(tree.DebianTree):
+    """A Cydia/Sileo repository.
+
+    Args:
+        host: The website host URL, i.e 'username.github.io/repo'.
+        *args: Passed to super().__init__.
+        template: The repo template as a dict (see TEMPLATES for an example).
+            If None, template will be loaded from mothman.json
+            (in the repo root).
+        **kwargs: Passed to super().__init__
+    """
+
+    def __init__(self, host: str, *args, template: Optional[dict] = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if template is None:
+            with (self.root / CONFIG_NAME).open() as f:
+                self._template = json.load(f)
+        else:
+            self._template = template
+
+        self.deb_path = self.root / self._template["deb_path"]
+
+        self._host = host
+        self._depictions = {k: v for k, v in DEPICTIONS.items() if k in self._template}
+
+    def _build(self, package: str) -> Generator[email.message.Message, None, None]:
+        versions = super()._build(package)
+        latest = next(versions)
+
+        # only build depiction for latest version
+        self._build_depiction(latest)
+        yield latest
+
+        for version in versions:
+            yield version
+
+    def _build_depiction(self, debinfo: email.message.Message) -> None:
+        for dep, dep_class in self._depictions.items():
+            _log.debug("[%s] making %s depiction", debinfo["Package"], dep)
+            dep_path = self.root / self._template[dep]["path"].format(
+                package=debinfo["Package"]
+            )
+
+            # make parent directories, if it does not exist yet
+            (dep_path.parent).mkdir(parents=True, exist_ok=True)
+
+            # write actual depiction
+            with dep_path.open("w") as f:
+                f.write(dep_class(debinfo).build())
+
+            # add depiction field to debinfo
+            debinfo[dep] = self._template[dep]["url"].format(
+                host=self._host, package=debinfo["Package"]
+            )
