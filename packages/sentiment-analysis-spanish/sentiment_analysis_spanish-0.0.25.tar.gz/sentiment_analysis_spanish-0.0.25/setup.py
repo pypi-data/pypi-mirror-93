d1b432ad2164179975d0026f12e7d8becc07c98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package sentiment-analysis-spanish/0.0.25/sentiment_analysis_spanish-0.0.25.tar.gz

---
+++ packages/sentiment-analysis-spanish/sentiment_analysis_spanish-0.0.25.tar.gz/sentiment_analysis_spanish-0.0.25/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sentiment_analysis_spanish",
+    version="0.0.25",
+    author="Hugo J. Bello",
+    author_email="hjbello.wk@gmail.com",
+    description="Sentiment analysis for sentences in spanish",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sentiment-analysis-spanish/sentiment-spanish",
+    packages=setuptools.find_packages(),
+    package_data={'sentiment_analysis_spanish': ['saved_models/*']},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
