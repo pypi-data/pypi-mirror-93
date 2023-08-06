f5e262da2c7537c878e8e854c52aa9d9fd3064a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:29 +0000

    Add package distkv-akumuli/0.3.2/distkv_akumuli-0.3.2.tar.gz

---
+++ packages/distkv-akumuli/distkv_akumuli-0.3.2.tar.gz/distkv_akumuli-0.3.2/distkv_ext/akumuli/config.py
@@ -0,0 +1,7 @@
+from distkv.util import attrdict, Path
+
+CFG = attrdict(
+    prefix=Path(".distkv", "akumuli"),
+    # There is no "server" entry, that's stored in the DistKV node
+    server_default=attrdict(port=8282,),
+)
