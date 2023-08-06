d7bcd8756e5ba6ca2eba6963db00fa0a15388400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:50 +0000

    Add package autoforecast/0.0.3/autoforecast-0.0.3.tar.gz

---
+++ packages/autoforecast/autoforecast-0.0.3.tar.gz/autoforecast-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="autoforecast", # Replace with your own username
+    version="0.0.3",
+    author="Guillaume Simo",
+    author_email="guillaume.simo@hotmail.fr",
+    description="AutoML time series forecasting",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GuillaumeSimo/autoforecast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
