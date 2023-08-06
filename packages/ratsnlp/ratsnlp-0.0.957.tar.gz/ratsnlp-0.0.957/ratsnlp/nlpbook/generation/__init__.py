cb3b8b45fabf73d2209ebd90422b8b6223846d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:49 +0000

    Add package ratsnlp/0.0.957/ratsnlp-0.0.957.tar.gz

---
+++ packages/ratsnlp/ratsnlp-0.0.957.tar.gz/ratsnlp-0.0.957/ratsnlp/nlpbook/generation/__init__.py
@@ -0,0 +1,4 @@
+from .corpus import GenerationDataset, NsmcCorpus
+from .task import GenerationTask
+from .tokenizer import KoGPT2Tokenizer
+from .arguments import GenerationTrainArguments, GenerationDeployArguments
