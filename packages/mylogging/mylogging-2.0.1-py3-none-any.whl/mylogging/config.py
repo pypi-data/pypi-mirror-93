269f012cb1fde238c2822ea7383dc43d3c4074c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:08 +0000

    Add package mylogging/2.0.1/mylogging-2.0.1-py3-none-any.whl

---
+++ packages/mylogging/mylogging-2.0.1-py3-none-any.whl/mylogging/config.py
@@ -0,0 +1,6 @@
+TO_FILE = False  # Whether log to file. Setup str path (or pathlib.Path) of file with .log suffix (create if not exist). Print to console then doesn't work
+COLOR = 'auto'  # Whether colorize results - mostly python syntax in tracebacks. If _TO_FILE is configured, colorize is ignored.
+
+
+# Do not edit, internal variables
+__DEBUG = 1
