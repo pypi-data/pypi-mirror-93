f77fbb9e8bbe061ae7b463631c70c48ba8b10462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:37 +0000

    Add package alpha-vantage-proxy/0.0.3/alpha_vantage_proxy-0.0.3.tar.gz

---
+++ packages/alpha-vantage-proxy/alpha_vantage_proxy-0.0.3.tar.gz/alpha_vantage_proxy-0.0.3/alpha_vantage_proxy/models.py
@@ -0,0 +1,31 @@
+
+import enum
+
+class TimeSeriesFunction(enum.Enum):
+   
+    INTRADAY = "TIME_SERIES_INTRADAY"
+    INTRADAY_EXTENDED = "TIME_SERIES_INTRADAY_EXTENDED"
+    DAILY = "TIME_SERIES_DAILY"
+    DAILY_ADJUSTED = "TIME_SERIES_DAILY_ADJUSTED"
+    WEEKLY = "TIME_SERIES_WEEKLY"
+    WEEKLY_ADJUSTED = "TIME_SERIES_WEEKLY_ADJUSTED"
+    MONTHLY = "TIME_SERIES_MONTHLY"
+    MONTHLY_ADJUSTED = "TIME_SERIES_MONTHLY_ADJUSTED"
+
+class TimeSeriesOutputSize(enum.Enum):
+
+    COMPACT = "compact"
+    FULL = "full"
+
+class TimeSeriesDataType(enum.Enum):
+
+    JSON = "json"
+    CSV = "csv"
+
+class IntradayInterval(enum.Enum):
+
+    ONE_MINUTE = "1min"
+    FIVE_MINUTE = "5min"
+    FIFTEEN_MINUTE = "15min"
+    THIRTY_MINUTE = "30min"
+    SIXTY_MINUTE = "60min"
