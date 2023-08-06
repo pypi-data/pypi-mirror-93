a7c99b40c6d57bc41c62f9f7496e5f38f7a9216e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package restfx/0.7.6/restfx-0.7.6-py3-none-any.whl

---
+++ packages/restfx/restfx-0.7.6-py3-none-any.whl/restfx/http/response.py
@@ -0,0 +1,39 @@
+import json
+
+from werkzeug.exceptions import InternalServerError
+from werkzeug.wrappers import Response
+
+
+class HttpResponse(Response):
+    def __init__(self, content=None, content_type='text/html;charset=utf8', status=200, headers=None, **kwargs):
+        super().__init__(content, content_type=content_type, status=status, headers=headers, **kwargs)
+
+
+class JsonResponse(HttpResponse):
+    def __init__(self, obj, encoder=None):
+        content = json.dumps(obj, ensure_ascii=True, cls=encoder)
+        super().__init__(content, content_type='application/json;charset=utf8')
+
+
+class HttpBadRequest(HttpResponse):
+    def __init__(self, content=None):
+        super().__init__(content, status=400)
+
+
+class HttpUnauthorized(HttpResponse):
+    def __init__(self, content=None):
+        super().__init__(content, status=401)
+
+
+class HttpNotFound(HttpResponse):
+    def __init__(self, content=None):
+        super().__init__(content, status=404)
+
+
+class HttpServerError(HttpResponse, InternalServerError):
+    def __init__(self, content=None):
+        e = None
+        if isinstance(content, Exception):
+            e = content
+            content = None
+        super().__init__(content, None, e)
