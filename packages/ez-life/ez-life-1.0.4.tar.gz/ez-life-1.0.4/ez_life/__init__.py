a7140543e51cc7b86bf78772b64a5b5a72c3ef7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:14 +0000

    Add package ez-life/1.0.4/ez-life-1.0.4.tar.gz

---
+++ packages/ez-life/ez-life-1.0.4.tar.gz/ez-life-1.0.4/ez_life/__init__.py
@@ -0,0 +1,14 @@
+
+#================================================================
+#
+#   File name   : jt_misc.__init__.py
+#   Author      : Josiah Tan
+#   Created date: 28/11/2020
+#   Description : module for miscalleneous stuff
+#
+#================================================================
+
+#================================================================
+
+from .param2attr import Param2attr
+from .jt_property import JTProperty
