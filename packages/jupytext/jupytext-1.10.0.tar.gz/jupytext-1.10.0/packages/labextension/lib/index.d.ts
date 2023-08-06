b511f2a2a859407c8ebfd81de5a89b0887645350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:44 +0000

    Add package jupytext/1.10.0/jupytext-1.10.0.tar.gz

---
+++ packages/jupytext/jupytext-1.10.0.tar.gz/jupytext-1.10.0/packages/labextension/lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from "@jupyterlab/application";
+/**
+ * Initialization data for the jupyterlab-jupytext extension.
+ */
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
