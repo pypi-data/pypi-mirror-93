6b3b5104c501adde9d8a989a915d1b9fc0e17e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package bank1/0.0.1/bank1-0.0.1.tar.gz

---
+++ packages/bank1/bank1-0.0.1.tar.gz/bank1-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+with open('README.md','r') as file:
+    long_description=file.read()
+    
+setup(
+    name='bank1',
+    version='0.0.1',
+    description='Say bank',
+    py_modules=["deposit_amount","home_loan","personal_loan","vehicle_loan","withdraw_amount"],
+    package_dir={'':'naga_upputuri_bank'},
+
+    classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.9",
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Operating System :: OS Independent",
+        ],
+
+     long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "blessings~=1.7",
+        ],
+    extras_require={
+        "dev":["pytest>=3.7",],},
+
+    url="https://github.com/naga99552/naga_upputuri_bank",
+    author="Nagababu",
+    author_email="nagababuupputuri52@gmail.com",
+    )
