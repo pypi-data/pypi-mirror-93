55c3051cd0e541472b1eeccd50d30f7862848db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:32 +0000

    Add package scrapfly-sdk/0.7.1/scrapfly_sdk-0.7.1-py3-none-any.whl

---
+++ packages/scrapfly-sdk/scrapfly_sdk-0.7.1-py3-none-any.whl/scrapfly/__init__.py
@@ -0,0 +1,43 @@
+__version__ = '0.7.1'
+
+from typing import Tuple
+from .errors import ScrapflyError
+from .errors import ScrapflyAspError
+from .errors import ScrapflyProxyError
+from .errors import ScrapflyScheduleError
+from .errors import ScrapflyScrapeError
+from .errors import ScrapflySessionError
+from .errors import ScrapflyThrottleError
+from .errors import ScrapflyWebhookError
+from .errors import EncoderError
+from .errors import ErrorFactory
+from .errors import HttpError
+from .errors import UpstreamHttpClientError
+from .errors import UpstreamHttpServerError
+from .errors import ApiHttpClientError
+from .errors import ApiHttpServerError
+from .api_response import ScrapeApiResponse, ResponseBodyHandler
+from .client import ScrapflyClient
+from .scrape_config import ScrapeConfig
+
+__all__:Tuple[str, ...] = (
+    'ScrapflyError',
+    'ScrapflyAspError',
+    'ScrapflyProxyError',
+    'ScrapflyScheduleError',
+    'ScrapflyScrapeError',
+    'ScrapflySessionError',
+    'ScrapflyThrottleError',
+    'ScrapflyWebhookError',
+    'UpstreamHttpClientError',
+    'UpstreamHttpServerError',
+    'ApiHttpClientError',
+    'ApiHttpServerError',
+    'EncoderError',
+    'ScrapeApiResponse',
+    'ErrorFactory',
+    'HttpError',
+    'ScrapflyClient',
+    'ResponseBodyHandler',
+    'ScrapeConfig'
+)
