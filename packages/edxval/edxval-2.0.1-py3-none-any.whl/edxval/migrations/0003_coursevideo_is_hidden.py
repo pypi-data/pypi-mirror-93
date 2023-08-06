113913234f6da7499252677c357e4e35f423081e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package edxval/2.0.1/edxval-2.0.1-py3-none-any.whl

---
+++ packages/edxval/edxval-2.0.1-py3-none-any.whl/edxval/migrations/0003_coursevideo_is_hidden.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('edxval', '0002_data__default_profiles'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='coursevideo',
+            name='is_hidden',
+            field=models.BooleanField(default=False, help_text='Hide video for course.'),
+        ),
+    ]
