80eefa064bfaea2b22ca8231fcf86796e1e69721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:04 +0000

    Add package tradingview-ta/3.2.1/tradingview_ta-3.2.1.tar.gz

---
+++ packages/tradingview-ta/tradingview_ta-3.2.1.tar.gz/tradingview_ta-3.2.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tradingview_ta',
+    version='3.2.1',
+    description="A python module to get TradingView's technical analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/deathlyface/python-tradingview-ta',
+    author='deathlyface',
+    author_email='bri4nong@gmail.com',
+    packages=['tradingview_ta'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'requests',
+    ],
+    python_requires='>=3.5')
