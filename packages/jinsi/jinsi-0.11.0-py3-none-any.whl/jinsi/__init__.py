7cf56c027888bf7470f9e93d1a3d93d8b17133a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:58 +0000

    Add package jinsi/0.11.0/jinsi-0.11.0-py3-none-any.whl

---
+++ packages/jinsi/jinsi-0.11.0-py3-none-any.whl/jinsi/__init__.py
@@ -0,0 +1,22 @@
+from .api import \
+    load_file, \
+    load_string, \
+    load1f, \
+    load1s, \
+    render_file, \
+    render_string, \
+    render1f, \
+    render1s
+
+from .__pkginfo__ import version as __version__
+
+__all__ = [
+    'load_file',
+    'load_string',
+    'load1f',
+    'load1s',
+    'render_file',
+    'render_string',
+    'render1f',
+    'render1s',
+]
