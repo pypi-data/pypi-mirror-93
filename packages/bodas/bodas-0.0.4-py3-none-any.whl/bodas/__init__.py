1ae6cff1300657de4ba4ec4da59fdbc0fcdbf171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:34 +0000

    Add package bodas/0.0.4/bodas-0.0.4-py3-none-any.whl

---
+++ packages/bodas/bodas-0.0.4-py3-none-any.whl/bodas/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+#  _               _
+# | |__   ___   __| | __ _ ___
+# | '_ \ / _ \ / _` |/ _` / __|
+# | |_) | (_) | (_| | (_| \__ \
+# |_.__/ \___/ \__,_|\__,_|___/
+
+
+"""
+Bodas library
+~~~~~~~~~~~~~~~~~~~~~
+
+Bodas is a library written in Python, sitting on top of SymPy, for asymptotic Bode plots.
+Basic usage:
+
+   >>> import bodas
+   >>> bodas.plot('1 / (1+s/5000) ')
+
+:copyright: (c) 2021 by Francesco Urbani <https://urbanij.github.io>
+:license: see LICENSE for more details.
+"""
+
+__title__ = 'bodas'
+__version__ = '0.0.4'
+__author__ = u'Francesco Urbani'
+
+from .main import plot
