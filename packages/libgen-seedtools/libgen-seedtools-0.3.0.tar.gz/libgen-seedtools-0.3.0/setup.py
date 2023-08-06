33f685ba6441c14a6b835d33fb408c88229c25ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:47 +0000

    Add package libgen-seedtools/0.3.0/libgen-seedtools-0.3.0.tar.gz

---
+++ packages/libgen-seedtools/libgen-seedtools-0.3.0.tar.gz/libgen-seedtools-0.3.0/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import find_packages, setup
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="libgen-seedtools",
+    author="Brandon Davis",
+    version="0.3.0",
+    author_email="libgen-seedtools@subdavis.com",
+    url="https://github.com/subdavis/libgen-seedtools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "click",
+        "click-aliases",
+        "colorama",
+        "humanfriendly",
+        "ipfshttpclient",
+        "pydantic",
+        "pyjwt",
+        "transmission-rpc",
+        "requests",
+        "requests-toolbelt",
+        "torrentool",
+        "torrent-tracker-scraper",
+    ],
+    license="MIT",
+    entry_points={
+        "console_scripts": ["libgen-seedtools = libgen_seedtools.cli:cli"],
+    },
+    python_requires=">=3.6",
+    setup_requires=["setuptools-git"],
+)
