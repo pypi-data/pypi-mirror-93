985afd0c506d0370c1f73941b145a07b7c44aad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:47 +0000

    Add package pyrogis/0.1.3/pyrogis-0.1.3-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/pyrogis/pyrogis-0.1.3-cp38-cp38-macosx_10_15_x86_64.whl/pyrogis/__init__.py
@@ -0,0 +1,24 @@
+from .ingredients import Dish
+from .ingredients import Ingredient
+from .ingredients import Recipe
+from .ingredients import Pierogi
+from .ingredients import Quantize
+from .ingredients import SpatialQuantize
+from .ingredients import Sort
+from .ingredients import Threshold
+
+from .chef import Chef
+
+__version__ = '0.1.3'
+
+__all__ = [
+    'Dish',
+    'Ingredient',
+    'Recipe',
+    'Pierogi',
+    'Quantize',
+    'SpatialQuantize',
+    'Sort',
+    'Threshold',
+    'Chef'
+]
