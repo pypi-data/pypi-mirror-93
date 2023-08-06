e7fc692d40c586fa04b8066acfa43e96ddb9690d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:32 +0000

    Add package circle-python/0.0.10/circle-python-0.0.10.tar.gz

---
+++ packages/circle-python/circle-python-0.0.10.tar.gz/circle-python-0.0.10/circle/object_classes.py
@@ -0,0 +1,7 @@
+from circle import resources
+
+OBJECT_CLASSES = {
+    resources.Ach.OBJECT_NAME: resources.Ach,
+    resources.Card.OBJECT_NAME: resources.Card,
+    resources.Wire.OBJECT_NAME: resources.Wire,
+}
