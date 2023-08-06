c1facccacff1b56a5f378acc0dbc8d804a020ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:02 +0000

    Add package more-dfply/0.1.0/more_dfply-0.1.0-py3-none-any.whl

---
+++ packages/more-dfply/more_dfply-0.1.0-py3-none-any.whl/more_dfply/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.1.0'
+
+from .more_dfply import fix_name, fix_names, maybe_tile, maybe_eval, any_intention, tiled_where, cond_eval, ifelse, maybe_combine, combine_all, arg_eval_and_combine, coalesce, case_when, col_zip, union_all
