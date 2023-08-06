c02ccd0de188aa21a881b32e1178fb5bb22bef0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package flytekit/0.16.0b3/flytekit-0.16.0b3.tar.gz

---
+++ packages/flytekit/flytekit-0.16.0b3.tar.gz/flytekit-0.16.0b3/flytekit/__init__.py
@@ -0,0 +1,38 @@
+import flytekit.plugins  # noqa: F401
+from flytekit.annotated.base_sql_task import SQLTask
+from flytekit.annotated.base_task import PythonTask, TaskMetadata, kwtypes
+from flytekit.annotated.condition import conditional
+from flytekit.annotated.container_task import ContainerTask
+from flytekit.annotated.context_manager import ExecutionParameters, FlyteContext
+from flytekit.annotated.dynamic_workflow_task import dynamic
+from flytekit.annotated.interface import Interface
+from flytekit.annotated.launch_plan import LaunchPlan
+from flytekit.annotated.map_task import maptask
+from flytekit.annotated.notification import Email, PagerDuty, Slack
+from flytekit.annotated.reference import get_reference_entity
+from flytekit.annotated.reference_entity import LaunchPlanReference, TaskReference, WorkflowReference
+from flytekit.annotated.resources import Resources
+from flytekit.annotated.schedule import CronSchedule, FixedRate
+from flytekit.annotated.task import reference_task, task
+from flytekit.annotated.testing import patch, task_mock
+from flytekit.annotated.type_engine import TypeEngine, TypeTransformer
+from flytekit.annotated.workflow import WorkflowFailurePolicy, reference_workflow, workflow
+from flytekit.loggers import logger
+
+__version__ = "0.16.0b3"
+
+
+def current_context() -> ExecutionParameters:
+    """
+    Use this method to get a handle of specific parameters available in a flyte task.
+
+    Usage
+
+    .. code-block::
+
+        flytekit.current_context().logging.info(...)
+
+    Available params are documented in :py:class:`flytekit.annotated.context_manager.ExecutionParams`.
+    There are some special params, that should be available
+    """
+    return FlyteContext.current_context().user_space_params
