7cca1005e662c0bacbca60f2bef0018ad4975ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:32 +0000

    Add package easytorch/2.4.4/easytorch-2.4.4.tar.gz

---
+++ packages/easytorch/easytorch-2.4.4.tar.gz/easytorch-2.4.4/easytorch/utils/logger.py
@@ -0,0 +1,21 @@
+import math as _math
+
+
+def error(msg):
+    print(f"####   [Error!]   ####: {msg}")
+
+
+def warn(msg):
+    print(f"****  [Warning!]  ****: {msg}")
+
+
+def info(msg):
+    print(f"{msg}")
+
+
+def success(msg):
+    print(f"----  [Success!] ----: {msg}")
+
+
+def lazy_debug(x, add=1):
+    return x % int(_math.log(x + 1) + add) == 0
