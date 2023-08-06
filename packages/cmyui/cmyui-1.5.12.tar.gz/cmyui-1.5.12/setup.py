6e4c584f3f71fa5fdbc2ec6604ac11d404f3e7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package cmyui/1.5.12/cmyui-1.5.12.tar.gz

---
+++ packages/cmyui/cmyui-1.5.12.tar.gz/cmyui-1.5.12/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name = 'cmyui',
+    version = '1.5.12',
+    author = 'cmyui',
+    author_email = 'cmyuiosu@gmail.com',
+    description = 'Some generic tools I find myself using often (web, mysql/pg, logging, utils, etc).',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/cmyui/cmyui_pkg',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires = '>=3.9',
+    package_data = {
+        'cmyui': ['py.typed'],
+    }
+)
