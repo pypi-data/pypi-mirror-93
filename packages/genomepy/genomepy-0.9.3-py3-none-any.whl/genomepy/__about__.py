5f9d68c02382e162b00b44cb5fb4a56f3016e1f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:17 +0000

    Add package genomepy/0.9.3/genomepy-0.9.3-py3-none-any.whl

---
+++ packages/genomepy/genomepy-0.9.3-py3-none-any.whl/genomepy/__about__.py
@@ -0,0 +1,3 @@
+"""Metadata"""
+__version__ = "0.9.3"
+__author__ = "Simon van Heeringen"
---
+++ packages/genomepy/genomepy-0.9.3-py3-none-any.whl/genomepy/exceptions.py
@@ -0,0 +1,8 @@
+"""Exceptions for the genomepy module."""
+
+
+class GenomeDownloadError(Exception):
+
+    """Error while downloading genome."""
+
+    pass
