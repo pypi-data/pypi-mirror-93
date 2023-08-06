dabf7c6a4bc7ccaddd9fc597465d312067acb212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:10 +0000

    Add package fluffy-id/0.1.0/fluffy_id-0.1.0.tar.gz

---
+++ packages/fluffy-id/fluffy_id-0.1.0.tar.gz/fluffy_id-0.1.0/fluffy_id/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for GUID Generator."""
+
+__author__ = """Pyunghyuk Yoo"""
+__email__ = 'yoophi@gmail.com'
+__version__ = '0.1.0'
+
+from fluffy_id.fluffy_id import gen_guid
+
+__all__ = ["gen_guid"]
---
+++ packages/fluffy-id/fluffy_id-0.1.0.tar.gz/fluffy_id-0.1.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Pyunghyuk Yoo",
+    author_email='yoophi@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Quickly and easily generate individual or bulk sets of globally unique identifiers (GUIDs).",
+    entry_points={
+        'console_scripts': [
+            'fluffy_id=fluffy_id.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='fluffy_id',
+    name='fluffy_id',
+    packages=find_packages(include=['fluffy_id', 'fluffy_id.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/yoophi/fluffy_id',
+    version='0.1.0',
+    zip_safe=False,
+)
