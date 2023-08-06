2d462aac14eef490bdf77de20c3878289221aeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:10 +0000

    Add package atcom/0.1.15/atcom-0.1.15.tar.gz

---
+++ packages/atcom/atcom-0.1.15.tar.gz/atcom-0.1.15/atcom/__version__.py
@@ -0,0 +1,3 @@
+VERSION = (0, 1, 15)
+
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/atcom/atcom-0.1.15.tar.gz/atcom-0.1.15/atcom/modems.py
@@ -0,0 +1,25 @@
+modems = {
+    "2c7c": { # Quectel VENDOR_ID
+        "EC25": { # Modem Name
+            "0125": ["02"] # USB_COMPOSITION_ID
+        },
+        "EC21": { # Modem Name
+            "0121": ["02"] # USB_COMPOSITION_ID
+        },
+        "BG96": { # Modem Name
+            "0296": ["02"] # USB_COMPOSITION_ID
+        },
+        "BG95": { # Modem Name
+            "0700": ["02"] # USB_COMPOSITION_ID
+        }
+    },
+    "1bc7": { # Telit VENDOR_ID
+        "LE910": { # Modem Name
+            "1201": ["04"], # USB_COMPOSITION_ID
+            "1206": ["05"] # USB_COMPOSITION_ID
+        },
+        "ME910": {
+            "1101": ["01"]
+        }
+    }
+}
