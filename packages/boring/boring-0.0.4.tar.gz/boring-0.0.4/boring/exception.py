083d08e902a5e6171f36335af35da7077670aac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:50 +0000

    Add package boring/0.0.4/boring-0.0.4.tar.gz

---
+++ packages/boring/boring-0.0.4.tar.gz/boring-0.0.4/boring/exception.py
@@ -0,0 +1,47 @@
+from boring import __version__
+from boring.utils import http_date
+
+HTML_ERROR = """<html>
+      <head>
+        <title>{reason}</title>
+      </head>
+      <body>
+        <h1><p>{reason}</p></h1>
+        {body}
+      </body>
+    </html>
+"""
+
+
+class HttpException(Exception):
+
+    code = 500
+    reason = "Internal Server Error"
+    body = "Internal Server Error"
+
+    def __init__(self, code=None, reason=""):
+        self.headers = [("Server", "Boring-Server/%s" % __version__)]
+        self.code = code or self.code
+        self.reason = reason or self.reason
+
+    def write_error(self, conn, req=None, resp=None):
+        error = HTML_ERROR.format(reason=self.reason, body=self.body)
+        status = "HTTP/1.1 %s %s\r\n" % (self.code, self.reason)  # encode()
+        status = status.encode()
+        self.headers.extend([("Content-Length", len(error)),
+                             ("Date", http_date()), ("Connection", 'close')])
+        headers = ["%s: %s\r\n" % (k, v) for k, v in self.headers]
+        headers.append("\r\n")
+        conn.send(status)
+        conn.send("".join(headers).encode())
+        conn.send(error.encode())
+
+
+class BadRequest(HttpException):
+    body = 'Bad Request'
+    code = 400
+    reason = "Bad Request"
+
+
+class InvalidHeader(BadRequest):
+    pass
---
+++ packages/boring/boring-0.0.4.tar.gz/boring-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+file = open('README.md')
+description = file.read()
+file.close()
+setuptools.setup(name="boring",
+                 version="0.0.4",
+                 author="Horlarwumhe",
+                 author_email="amachiever4real@gmail.com",
+                 description="A small HTTP web server for wsgi compatible apps",
+                 long_description=description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/horlarwumhe/boring",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.7',
+                 entry_points='''
+                 [console_scripts]
+                 boring = boring.cli:main''')
