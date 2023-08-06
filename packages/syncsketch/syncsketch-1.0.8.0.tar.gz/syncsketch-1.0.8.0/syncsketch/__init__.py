d1b415213b043a7744bffad8b4749b3cd2a94314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:54 +0000

    Add package syncsketch/1.0.8.0/syncsketch-1.0.8.0.tar.gz

---
+++ packages/syncsketch/syncsketch-1.0.8.0.tar.gz/syncsketch-1.0.8.0/syncsketch/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: yafes
+# @Date:   2018-11-20 17:39:54
+# @Last Modified by:   yafes
+# @Last Modified time: 2018-11-20 17:42:49
+
+from __future__ import absolute_import
+
+from .syncsketch import SyncSketchAPI
+
+__version__ = "1.0.8.0"
+__author__ = "SyncSketch Dev Team"
+__credits__ = "Philip Floetotto, Yafes Sahin, Brady Endres"
