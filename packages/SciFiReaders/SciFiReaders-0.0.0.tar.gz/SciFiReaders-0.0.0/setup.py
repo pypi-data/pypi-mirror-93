bda768ed2d0c57bcc4cde2888abf14ec3ae39f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:39 +0000

    Add package SciFiReaders/0.0.0/SciFiReaders-0.0.0.tar.gz

---
+++ packages/SciFiReaders/SciFiReaders-0.0.0.tar.gz/SciFiReaders-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='SciFiReaders',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/pycroscopy',
+    author='Pycroscopy',
+    author_email='pycroscopy@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
