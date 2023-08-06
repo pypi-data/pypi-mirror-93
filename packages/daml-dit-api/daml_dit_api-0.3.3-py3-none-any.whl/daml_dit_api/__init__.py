c693f01ef6a9ea8844e364c2b822b4a405ce4573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:03 +0000

    Add package daml-dit-api/0.3.3/daml_dit_api-0.3.3-py3-none-any.whl

---
+++ packages/daml-dit-api/daml_dit_api-0.3.3-py3-none-any.whl/daml_dit_api/__init__.py
@@ -0,0 +1,19 @@
+from typing import  Optional
+
+from .package_metadata import \
+    IntegrationTypeFieldInfo, \
+    IntegrationTypeInfo, \
+    CatalogInfo, \
+    PackageMetadata, \
+    DABL_META_NAME
+
+from .integration_runtime_spec import \
+    METADATA_COMMON_RUN_AS_PARTY, \
+    METADATA_TRIGGER_NAME, \
+    METADATA_INTEGRATION_ID, \
+    METADATA_INTEGRATION_TYPE_ID, \
+    METADATA_INTEGRATION_COMMENT, \
+    METADATA_INTEGRATION_ENABLED, \
+    METADATA_INTEGRATION_RUN_AS_PARTY, \
+    METADATA_INTEGRATION_RUNTIME, \
+    IntegrationRuntimeSpec
