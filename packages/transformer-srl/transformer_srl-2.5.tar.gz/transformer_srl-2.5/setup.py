4433a4ae2fe3c4f86e8366ee1d6bfb9378d41e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:03 +0000

    Add package transformer-srl/2.5/transformer_srl-2.5.tar.gz

---
+++ packages/transformer-srl/transformer_srl-2.5.tar.gz/transformer_srl-2.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="transformer_srl",  # Replace with your own username
+    version="2.5",
+    author="Riccardo Orlando",
+    author_email="orlandoricc@gmail.com",
+    description="SRL Transformer model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Riccorl/transformer-srl",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "allennlp>=2.0,<2.1",
+        "allennlp_models>=2.0,<2.1",
+        "spacy>=2.3,<2.4"
+    ],
+    python_requires=">=3.6",
+)
