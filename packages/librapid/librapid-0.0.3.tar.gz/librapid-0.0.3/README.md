63b6cab9b9ef2bec8c9f0c92e5d15b782c112af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package librapid/0.0.3/librapid-0.0.3.tar.gz

---
+++ packages/librapid/librapid-0.0.3.tar.gz/librapid-0.0.3/README.md
@@ -0,0 +1,24 @@
+[![PyPI version fury.io](https://badge.fury.io/py/librapid.svg)](https://pypi.python.org/pypi/librapid/)
+[![PyPI license](https://img.shields.io/pypi/l/librapid.svg)](https://pypi.python.org/pypi/librapid/)
+[![Downloads per month](https://img.shields.io/pypi/dm/librapid.svg)](https://pypi.python.org/pypi/librapid/)
+
+# RAPID
+
+Rapid is a pyton library with its backend written in CUDA. This allows calculations to be performed on the GPU, often faster than they could on the CPU.
+
+## Installation
+
+To install rapid from source, run the following commands
+
+```shell
+git clone https://github.com/Pencilcaseman/Rapid.git
+cd Rapid
+python setup.py sdist
+pip install .
+```
+
+To install rapid via ```pip```, run this command
+
+```shell
+pip install librapid
+```
---
+++ packages/librapid/librapid-0.0.3.tar.gz/librapid-0.0.3/setup.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+from setuptools import Extension
+from distutils.errors import CCompilerError
+import os
+from setuptools import setup, find_packages
+import shutil
+from pathlib import Path
+
+# On Windows, we need the original PATH without Anaconda's compiler in it:
+PATH = os.environ.get("PATH")
+
+forceBuild = [Extension("rapidForceBuild.forceBuild", ["rapid/src/forceBuild.c"])]
+
+extensionFiles = ["rapid/src/ndarray/rapid_ndarray.cu"]
+options = [
+    "/Ox",
+    "/MT",
+    "-lcublas",
+    "-lkernel32",
+    "-luser32",
+    "-lgdi32",
+    "-lwinspool",
+    "-lcomdlg32",
+    "-ladvapi32",
+    "-lshell32",
+    "-lole32",
+    "-loleaut32",
+    "-luuid",
+    "-lodbc32",
+    "-lodbccp32"
+]
+
+def buildExtensions(files, options=[]):
+    cwd = os.path.dirname(__file__) or os.path.curdir
+
+    os.system("nvcc --version")
+    for ext in files:
+        filename = ext[-ext[::-1].find("/"):]
+
+        print(filename)
+        print(filename[:filename.find(".")])
+
+        os.system("cd rapid && mkdir bin")
+        os.system("nvcc -o rapid/lib{}.dll --shared {} {}".format(filename[:filename.find(".")], ext, " ".join(options)))
+
+        # Move the files that were created to the lib folder"
+        toMove = []
+
+        for file in os.listdir(cwd + "/rapid"):
+            if file.startswith("lib{}".format(filename[:filename.find(".")])):
+                toMove.append(file)
+
+        if len(toMove) < 1:
+            raise CCompilerError("Unable to compile CUDA code")
+
+        for file in toMove:
+            shutil.move("rapid/{}".format(file), "rapid/bin/{}".format(file))
+
+        os.listdir(cwd)
+
+buildExtensions(extensionFiles)
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
+
+setup(name="librapid",
+      version="0.0.3",
+      description="General purpose library optimized to run on the GPU",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      ext_modules = forceBuild,
+      packages=["rapid"] + ["rapid." + mod for mod in find_packages("rapid", exclude=["examples", "test"])],
+      include_package_data=True,
+      author="Toby Davis",
+      author_email="pencilcaseman@gmail.com",
+      url="https://github.com/Pencilcaseman/Rapid",
+      license="MIT Licences",
+      keywords="cuda math array ndarray matrix gpu",
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Topic :: Utilities",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Education",
+          "License :: OSI Approved :: MIT License"
+      ]
+      )
