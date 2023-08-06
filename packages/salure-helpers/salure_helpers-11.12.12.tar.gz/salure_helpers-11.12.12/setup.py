d2392812447ecaccf64e288610ba2fcc76e07af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:29 +0000

    Add package salure-helpers/11.12.12/salure_helpers-11.12.12.tar.gz

---
+++ packages/salure-helpers/salure_helpers-11.12.12.tar.gz/salure_helpers-11.12.12/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='salure_helpers',
+    version='11.12.12',
+    description='Files with helpful code, developed by Salure',
+    url='https://bitbucket.org/salurebi/salure_helpers/',
+    author='Salure',
+    author_email='bi@salure.nl',
+    license='Salure License',
+    packages=['salure_helpers'],
+    package_data={'salure_helpers': ['templates/*', 'datasets/*', 'helpers/*']},
+    install_requires=[
+        'aiohttp>=3,<4',
+        'pandas>=1,<2',
+        'mandrill-really-maintained>=1,<2',
+        'pymysql>=1,<2',
+        'requests>=2,<3',
+        'pysftp>=0,<1',
+        'pyarrow>=0,<1',
+        'twine>=3,<4',
+        'clickhouse-driver>=0,<1',
+        'fs>=2,<3',
+        'python-gnupg>=0,<1',
+        'xmltodict>=0,<1',
+        'zeep>=4,<5'
+    ],
+    zip_safe=False
+)
