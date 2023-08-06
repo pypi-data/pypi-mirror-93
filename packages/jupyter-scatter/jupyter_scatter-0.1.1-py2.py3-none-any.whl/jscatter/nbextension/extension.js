bdc769ae28cdd328febe03be515880b1557841b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:14 +0000

    Add package jupyter-scatter/0.1.1/jupyter_scatter-0.1.1-py2.py3-none-any.whl

---
+++ packages/jupyter-scatter/jupyter_scatter-0.1.1-py2.py3-none-any.whl/jscatter/nbextension/extension.js
@@ -0,0 +1,53 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+define(() => /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./extension.js":
+/*!**********************!*\
+  !*** ./extension.js ***!
+  \**********************/
+/***/ ((module) => {
+
+eval("// This file contains the javascript that is run when the notebook is loaded.\n// It contains some requirejs configuration and the `load_ipython_extension`\n// which is required for any notebook extension.\n\n// Configure requirejs\nif (window.require) {\n  window.require.config({\n    map: {\n      \"*\" : {\n        \"jupyter-scatter\": \"nbextensions/jupyter-scatter/index\",\n      }\n    }\n  });\n}\n\n// Export the required load_ipython_extension\nmodule.exports = {\n  load_ipython_extension: function() {}\n};\n\n\n//# sourceURL=webpack://jupyter-scatter/./extension.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__("./extension.js");
+/******/ })()
+);;
