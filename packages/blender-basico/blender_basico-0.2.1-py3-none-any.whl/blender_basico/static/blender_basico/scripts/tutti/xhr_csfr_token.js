dcce97f75ec68192a437edbeb8af0caa45706725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:12 +0000

    Add package blender-basico/0.2.1/blender_basico-0.2.1-py3-none-any.whl

---
+++ packages/blender-basico/blender_basico-0.2.1-py3-none-any.whl/blender_basico/static/blender_basico/scripts/tutti/xhr_csfr_token.js
@@ -0,0 +1,11 @@
+var csrftoken = Cookies.get('csrftoken');
+function csrfSafeMethod(method) {
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+	beforeSend: function (xhr, settings) {
+		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		}
+	}
+});
