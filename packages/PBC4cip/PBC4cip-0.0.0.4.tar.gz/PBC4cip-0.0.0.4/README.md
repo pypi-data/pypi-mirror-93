11b1d6bbfec250da0931c1097487745b930fdb2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:30 +0000

    Add package PBC4cip/0.0.0.4/PBC4cip-0.0.0.4.tar.gz

---
+++ packages/PBC4cip/PBC4cip-0.0.0.4.tar.gz/PBC4cip-0.0.0.4/README.md
@@ -0,0 +1,13 @@
+# PBC4cip
+PBC4cip is a machine learning package used to handle class imbalanced prediction problems.
+
+## Built With
+
+* [Pandas](https://pandas.pydata.org) - DataFrame framework
+* [Numpy](https://numpy.org) - Numpy Library
+* [tqdm](https://tqdm.github.io/) - Progress bar helper
+
+## Getting Started
+To have an idea on how to run the program just see the example located in PBC4cip/TestProg.py
+
+There are various command line arguments that can be passed to the program, these can be found by running python src/PBC4cip.CLI.py --help.
---
+++ packages/PBC4cip/PBC4cip-0.0.0.4.tar.gz/PBC4cip-0.0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PBC4cip",
+    version="0.0.0.4",
+    author="Ernesto Ramírez-Sáyago, Miguel Angel Medina-Pérez, Octavio Loyola-González",
+    author_email="A00513925@itesm.mx, migue@tec.mx, octavioloyola@tec.mx",
+    description="PBC4cip classifier for class imbalance problems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/octavioloyola/PBC4cip",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'pandas', 'scikit-learn', 'tqdm'],
+    python_requires='>=3.6',
+)
