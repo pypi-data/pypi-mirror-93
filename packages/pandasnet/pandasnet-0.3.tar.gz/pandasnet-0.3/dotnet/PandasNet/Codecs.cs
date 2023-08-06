fed7513b0a06568d45124f80d22218a473f161a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package pandasnet/0.3/pandasnet-0.3.tar.gz

---
+++ packages/pandasnet/pandasnet-0.3.tar.gz/pandasnet-0.3/dotnet/PandasNet/Codecs.cs
@@ -0,0 +1,75 @@
+using Python.Runtime;
+using System;
+using System.Collections.Generic;
+
+namespace PandasNet
+{
+    public class Codecs : IPyObjectDecoder, IPyObjectEncoder
+    {
+        private static readonly Codecs instance = new Codecs();
+
+        public static void Initialize(bool force = false) => instance.Setup(force);
+
+        private bool _isInitialized = false;
+        private readonly Dictionary<string, object> _decoders = new Dictionary<string, object>();
+        private readonly Dictionary<Type, Func<object, PyObject>> _encoders = new Dictionary<Type, Func<object, PyObject>>();
+
+        private Codecs() 
+        {
+            Setup(); 
+        }
+
+        private void Setup(bool force = false)
+        {
+            if (_isInitialized && !force) return;
+            _isInitialized = true;
+
+            DateTimeCodec.Initialize(this);
+            NumpyCodec.Initialize(this);
+            PandasCodec.Initialize(this);
+
+            PyObjectConversions.RegisterEncoder(this);
+            PyObjectConversions.RegisterDecoder(this);
+        }
+
+        public void Register<T>(Func<T, PyObject> encoder) => _encoders[typeof(T)] = p => encoder((T)p);
+
+        public void Register<T>(string pyType, Func<PyObject, T> decoder) => _decoders[pyType] = decoder;
+
+        public bool CanDecode(PyObject objectType, Type targetType)
+        {
+            var type = objectType.ToString();
+            return _decoders.ContainsKey(type);
+        }
+
+        public bool TryDecode<T>(PyObject pyObj, out T value)
+        {
+            var type = pyObj.GetPythonType().ToString();
+            if (!_decoders.TryGetValue(type, out var decoder))
+            {
+                value = default;
+                return false;
+            }
+            
+            value = ((Func<PyObject, T>)decoder)(pyObj);
+            return true;
+        }
+
+        public bool CanEncode(Type type)
+        {
+            return _encoders.ContainsKey(type);
+        }
+
+        public PyObject TryEncode(object value)
+        {
+            if (value == null) return null; // Todo: Should I return None instead ?
+
+            if (!_encoders.TryGetValue(value.GetType(), out var encoder))
+                return null; // Todo: Should I return None instead ?
+
+            return encoder(value);
+        }
+
+        public static PyObject Encode(object value) => instance.TryEncode(value);
+    }
+}
