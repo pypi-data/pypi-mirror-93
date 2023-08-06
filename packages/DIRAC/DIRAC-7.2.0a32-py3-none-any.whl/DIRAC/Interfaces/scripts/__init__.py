2b52c81960e9c2df146b8180cd4f445ced6e0556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package DIRAC/7.2.0a32/DIRAC-7.2.0a32-py3-none-any.whl

---
+++ packages/DIRAC/DIRAC-7.2.0a32-py3-none-any.whl/DIRAC/Interfaces/scripts/__init__.py
@@ -0,0 +1,12 @@
+############################################################
+# $HeadURL$
+############################################################
+
+"""
+   DIRAC.Interfaces.scripts package
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+__RCSID__ = "$Id$"
