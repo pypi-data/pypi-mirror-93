e77895d603915a05cbd1f3bc3445689a73b82ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:11 +0000

    Add package benchling-api-client/0.10.8a0/benchling-api-client-0.10.8a0.tar.gz

---
+++ packages/benchling-api-client/benchling-api-client-0.10.8a0.tar.gz/benchling-api-client-0.10.8a0/benchling_api_client/models/automation_file.py
@@ -0,0 +1,75 @@
+from typing import Any, Dict, Union
+
+import attr
+
+from ..models.automation_file_automation_file_config import AutomationFileAutomationFileConfig
+from ..models.automation_file_file import AutomationFileFile
+from ..types import UNSET, Unset
+
+
+@attr.s(auto_attribs=True)
+class AutomationFile:
+    """  """
+
+    assay_run_id: Union[Unset, str] = UNSET
+    automation_file_config: Union[Unset, AutomationFileAutomationFileConfig] = UNSET
+    file: Union[Unset, None, AutomationFileFile] = UNSET
+    id: Union[Unset, str] = UNSET
+    status: Union[Unset, str] = UNSET
+
+    def to_dict(self) -> Dict[str, Any]:
+        assay_run_id = self.assay_run_id
+        automation_file_config: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self.automation_file_config, Unset):
+            automation_file_config = self.automation_file_config.to_dict()
+
+        file: Union[Unset, None, Dict[str, Any]] = UNSET
+        if not isinstance(self.file, Unset):
+            file = self.file.to_dict() if self.file else None
+
+        id = self.id
+        status = self.status
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update({})
+        if assay_run_id is not UNSET:
+            field_dict["assayRunId"] = assay_run_id
+        if automation_file_config is not UNSET:
+            field_dict["automationFileConfig"] = automation_file_config
+        if file is not UNSET:
+            field_dict["file"] = file
+        if id is not UNSET:
+            field_dict["id"] = id
+        if status is not UNSET:
+            field_dict["status"] = status
+
+        return field_dict
+
+    @staticmethod
+    def from_dict(src_dict: Dict[str, Any]) -> "AutomationFile":
+        d = src_dict.copy()
+        assay_run_id = d.pop("assayRunId", UNSET)
+
+        automation_file_config: Union[Unset, AutomationFileAutomationFileConfig] = UNSET
+        _automation_file_config = d.pop("automationFileConfig", UNSET)
+        if not isinstance(_automation_file_config, Unset):
+            automation_file_config = AutomationFileAutomationFileConfig.from_dict(_automation_file_config)
+
+        file = None
+        _file = d.pop("file", UNSET)
+        if _file is not None and not isinstance(_file, Unset):
+            file = AutomationFileFile.from_dict(_file)
+
+        id = d.pop("id", UNSET)
+
+        status = d.pop("status", UNSET)
+
+        automation_file = AutomationFile(
+            assay_run_id=assay_run_id,
+            automation_file_config=automation_file_config,
+            file=file,
+            id=id,
+            status=status,
+        )
+
+        return automation_file
---
+++ packages/benchling-api-client/benchling-api-client-0.10.8a0.tar.gz/benchling-api-client-0.10.8a0/benchling_api_client/models/schema_summary.py
@@ -0,0 +1,40 @@
+from typing import Any, Dict, Union
+
+import attr
+
+from ..types import UNSET, Unset
+
+
+@attr.s(auto_attribs=True)
+class SchemaSummary:
+    """  """
+
+    id: Union[Unset, str] = UNSET
+    name: Union[Unset, str] = UNSET
+
+    def to_dict(self) -> Dict[str, Any]:
+        id = self.id
+        name = self.name
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update({})
+        if id is not UNSET:
+            field_dict["id"] = id
+        if name is not UNSET:
+            field_dict["name"] = name
+
+        return field_dict
+
+    @staticmethod
+    def from_dict(src_dict: Dict[str, Any]) -> "SchemaSummary":
+        d = src_dict.copy()
+        id = d.pop("id", UNSET)
+
+        name = d.pop("name", UNSET)
+
+        schema_summary = SchemaSummary(
+            id=id,
+            name=name,
+        )
+
+        return schema_summary
