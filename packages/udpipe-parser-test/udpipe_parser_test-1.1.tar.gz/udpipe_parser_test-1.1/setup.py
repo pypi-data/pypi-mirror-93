ef49b158626ec03277970bcff3b6c9033639f9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:00 +0000

    Add package udpipe-parser-test/1.1/udpipe_parser_test-1.1.tar.gz

---
+++ packages/udpipe-parser-test/udpipe_parser_test-1.1.tar.gz/udpipe_parser_test-1.1/setup.py
@@ -0,0 +1,25 @@
+import os
+import setuptools
+
+with open(r"C:\projects\udpipe_parser\README.txt") as readme_file:
+    README = readme_file.read()
+
+setuptools.setup(
+     name='udpipe_parser_test',  
+     version='1.1',
+     license='MIT',
+     author="Constantin Werner",
+     author_email="const.werner@gmail.com",
+     description="Analyzer brings Universal Dependencies trees in more practical form.",
+     include_package_data=True,
+     long_description=README,
+     keywords=['analyzer', 'Universal Dependencies', 'NLP', 'russian','syntax'],
+     url="https://github.com/constantin50/udpipe_analyzer",
+     packages=setuptools.find_packages(),
+     install_requires=["morpholog","conllu","separatrice","nltk"],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
