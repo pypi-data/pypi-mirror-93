765576e991e04e0da92e7d99661ddff5c8d370ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:30 +0000

    Add package mpf/0.54.6/mpf-0.54.6-cp34-none-any.whl

---
+++ packages/mpf/mpf-0.54.6-cp34-none-any.whl/mpf/platforms/interfaces/segment_display_platform_interface.py
@@ -0,0 +1,67 @@
+"""Support for physical segment displays."""
+import abc
+from typing import Any
+
+
+class SegmentDisplayPlatformInterface(metaclass=abc.ABCMeta):
+
+    """Interface for a segment display in hardware platforms."""
+
+    __slots__ = ["number"]
+
+    def __init__(self, number: Any) -> None:
+        """Remember the number."""
+        self.number = number
+
+    @abc.abstractmethod
+    def set_text(self, text: str, flashing: bool) -> None:
+        """Set a text to the display.
+
+        This text will be right aligned in case the text is shorter than the display.
+        If it is too long it will be cropped on the left.
+        """
+        raise NotImplementedError
+
+
+class SegmentDisplaySoftwareFlashPlatformInterface(SegmentDisplayPlatformInterface):
+
+    """SegmentDisplayPlatformInterface with software emulation for flashing."""
+
+    __slots__ = ["_flash_on", "_flashing", "_text"]
+
+    def __init__(self, number: Any) -> None:
+        """Remember the number."""
+        super().__init__(number)
+        self._flash_on = True
+        self._flashing = False
+        self._text = ""
+
+    def set_software_flash(self, state):
+        """Set software flashing state."""
+        self._flash_on = state
+
+        if not self._flashing:
+            return
+
+        # do not flash empty text
+        if not self._text:
+            return
+
+        if state:
+            self._set_text(self._text)
+        else:
+            self._set_text("")
+
+    def set_text(self, text: str, flashing: bool) -> None:
+        """Set a text to the display."""
+        self._text = text
+        self._flashing = flashing
+        if not self._flashing:
+            self._flash_on = True
+        if not flashing or self._flash_on or not text:
+            self._set_text(text)
+
+    @abc.abstractmethod
+    def _set_text(self, text: str) -> None:
+        """Set a text to the display."""
+        raise NotImplementedError
---
+++ packages/mpf/mpf-0.54.6-cp34-none-any.whl/mpf/tests/test_Tilt.py
@@ -0,0 +1,312 @@
+from mpf.tests.MpfTestCase import MpfTestCase, test_config
+
+
+class TestTilt(MpfTestCase):
+
+    def get_config_file(self):
+        return 'config.yaml'
+
+    def get_machine_path(self):
+        return 'tests/machine_files/tilt/'
+
+    def get_platform(self):
+        return 'smart_virtual'
+
+    def _tilted(self, **kwargs):
+        del kwargs
+        self._is_tilted = True
+
+    @test_config("config_system_11_trough.yaml")
+    def test_tilt_in_outhole(self):
+        """Test that the ball does not stay in the outhole."""
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.hit_switch_and_run('s_ball_switch1', 0)
+        self.hit_switch_and_run('s_ball_switch2', 0)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.hit_and_release_switch("s_start")
+        self.advance_time_and_run(10)
+
+        self.assertBallsOnPlayfield(0)
+        self.assertAvailableBallsOnPlayfield(1)
+        self.assertEqual(1, self.machine.ball_devices["bd_plunger"].balls)
+
+        # ball ejects and ends up on playfield
+        self.release_switch_and_run("s_plunger", 10)
+        self.assertBallsOnPlayfield(1)
+        self.assertAvailableBallsOnPlayfield(1)
+
+        # machine tilts
+        self.assertFalse(self._is_tilted)
+        self.hit_and_release_switch("s_tilt")
+        self.advance_time_and_run(10)
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+        self.assertEqual(0, self.machine.ball_devices["bd_outhole"].balls)
+        self.assertEqual(0, self.machine.ball_devices["bd_plunger"].balls)
+        self.assertBallsOnPlayfield(1)
+        self.assertAvailableBallsOnPlayfield(1)
+
+        # ball drains and gets kicked to trough
+        self.machine.switch_controller.process_switch('s_outhole', 1)
+        self.advance_time_and_run(20)
+        self.assertEqual(0, self.machine.ball_devices["bd_outhole"].balls)
+
+        self.assertEqual(False, self.machine.game.tilted)
+
+    @test_config("config_mechanical_eject.yaml")
+    def test_mechanical_eject(self):
+        """Test that tilt triggers auto launch."""
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(10)
+
+        self.assertBallsOnPlayfield(0)
+        self.assertAvailableBallsOnPlayfield(1)
+        self.assertEqual(1, self.machine.ball_devices["bd_launcher"].balls)
+
+        self.assertFalse(self._is_tilted)
+        self.machine.switch_controller.process_switch('s_tilt', 1)
+        self.machine.switch_controller.process_switch('s_tilt', 0)
+        self.advance_time_and_run(10)
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+
+        # ball ejects
+        self.assertBallsOnPlayfield(1)
+        self.assertAvailableBallsOnPlayfield(1)
+        self.assertEqual(0, self.machine.ball_devices["bd_launcher"].balls)
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+
+        self.assertEqual(False, self.machine.game.tilted)
+
+    def test_simple_tilt(self):
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(10)
+
+        # flipper actived
+        self.assertTrue(self.machine.flippers["f_test"]._enabled)
+
+        self.assertTrue(self.machine.mode_controller.is_active('tilt'))
+        self.assertNotEqual(None, self.machine.game)
+
+        # scoring should work
+        self.post_event("test_scoring")
+        self.assertPlayerVarEqual(100, "score")
+
+        self.assertFalse(self._is_tilted)
+        self.machine.switch_controller.process_switch('s_tilt', 1)
+        self.machine.switch_controller.process_switch('s_tilt', 0)
+        self.advance_time_and_run(1)
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+
+        # flipper deactived
+        self.assertFalse(self.machine.flippers["f_test"]._enabled)
+
+        # scoring should no longer work
+        self.assertPlayerVarEqual(100, "score")
+        self.post_event("test_scoring")
+        self.assertPlayerVarEqual(100, "score")
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+
+        self.assertEqual(False, self.machine.game.tilted)
+
+    def test_tilt_event(self):
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(10)
+
+        self.assertTrue(self.machine.mode_controller.is_active('tilt'))
+        self.assertNotEqual(None, self.machine.game)
+
+        self.assertFalse(self._is_tilted)
+        self.machine.events.post("tilt_event")
+        self.advance_time_and_run(1)
+        self.machine.events.post("tilt_event")
+        self.advance_time_and_run(1)
+        self.machine.events.post("tilt_event")
+        self.advance_time_and_run(1)
+
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+
+        self.assertEqual(False, self.machine.game.tilted)
+
+    def test_simple_tilt_ball_not_on_pf_yet(self):
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(1)
+
+        self.assertTrue(self.machine.mode_controller.is_active('tilt'))
+        self.assertNotEqual(None, self.machine.game)
+
+        self.assertFalse(self._is_tilted)
+        self.machine.switch_controller.process_switch('s_tilt', 1)
+        self.machine.switch_controller.process_switch('s_tilt', 0)
+        self.advance_time_and_run(.1)
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+
+        self.assertEqual(False, self.machine.game.tilted)
+
+    def test_tilt_warning(self):
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(10)
+
+        self.assertTrue(self.machine.mode_controller.is_active('tilt'))
+        self.assertNotEqual(None, self.machine.game)
+
+        self.assertFalse(self._is_tilted)
+
+        # multiple hits in 300ms window
+        self.machine.switch_controller.process_switch('s_tilt_warning', 1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 0)
+        self.advance_time_and_run(.1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 0)
+        self.advance_time_and_run(.1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 0)
+        self.advance_time_and_run(1)
+        self.assertFalse(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+
+        self.machine.switch_controller.process_switch('s_tilt_warning', 1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 0)
+        self.advance_time_and_run(1)
+        self.assertFalse(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+
+        self.machine.switch_controller.process_switch('s_tilt_warning', 1)
+        self.machine.switch_controller.process_switch('s_tilt_warning', 0)
+        self.advance_time_and_run(1)
+        self.assertTrue(self._is_tilted)
+        self.assertNotEqual(None, self.machine.game)
+        self.assertEqual(True, self.machine.game.tilted)
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+        self.assertNotEqual(None, self.machine.game)
+
+        # wait for settle time (5s) since last s_tilt_warning hit
+        self.advance_time_and_run(3.5)
+        self.assertEqual(False, self.machine.game.tilted)
+
+    def test_slam_tilt(self):
+        self._is_tilted = False
+        self.machine.events.add_handler("tilt", self._tilted)
+
+        self.machine.ball_controller.num_balls_known = 0
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.machine.switch_controller.process_switch('s_ball_switch2', 1)
+        self.advance_time_and_run(2)
+
+        self.assertEqual(None, self.machine.game)
+        self.assertEqual(2, self.machine.ball_controller.num_balls_known)
+        self.assertEqual(2, self.machine.ball_devices["bd_trough"].balls)
+        self.machine.switch_controller.process_switch('s_start', 1)
+        self.machine.switch_controller.process_switch('s_start', 0)
+        self.advance_time_and_run(10)
+
+        # flipper actived
+        self.assertTrue(self.machine.flippers["f_test"]._enabled)
+
+        self.assertTrue(self.machine.mode_controller.is_active('tilt'))
+        self.assertNotEqual(None, self.machine.game)
+
+        self.assertFalse(self._is_tilted)
+        self.machine.switch_controller.process_switch('s_slam_tilt', 1)
+        self.machine.switch_controller.process_switch('s_slam_tilt', 0)
+        self.advance_time_and_run(1)
+        self.assertNotEqual(None, self.machine.game)
+
+        # flipper deactived
+        self.assertFalse(self.machine.flippers["f_test"]._enabled)
+
+        self.machine.switch_controller.process_switch('s_ball_switch1', 1)
+        self.advance_time_and_run(1)
+
+        self.assertEqual(None, self.machine.game)
+
+        # test that it does not crash outside the game
+        self.post_event("tilt_reset_warnings")
+        self.advance_time_and_run()
