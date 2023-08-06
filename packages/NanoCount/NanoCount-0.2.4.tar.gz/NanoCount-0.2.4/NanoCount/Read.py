505438aacffbf3af0461d210d83ba50f553e21c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:52 +0000

    Add package NanoCount/0.2.4/NanoCount-0.2.4.tar.gz

---
+++ packages/NanoCount/NanoCount-0.2.4.tar.gz/NanoCount-0.2.4/NanoCount/Read.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+#~~~~~~~~~~~~~~IMPORTS~~~~~~~~~~~~~~#
+# Standard library imports
+
+# Third party imports
+import pysam
+
+# Local imports
+
+#~~~~~~~~~~~~~~CLASS READ~~~~~~~~~~~~~~#
+class Read (object):
+    """
+    Represent a read from the original fastq file associated with alignments found
+    by minimap2
+    """
+
+    #~~~~~~~~~~~~~~MAGIC METHODS~~~~~~~~~~~~~~#
+    def __init__(self):
+        self.alignment_list = []
+
+    def __repr__(self):
+        m = ""
+        for r in self.alignment_list:
+            m +="\t\t{}\n".format(r)
+        return (m)
+
+    #~~~~~~~~~~~~~~PROPERTY METHODS~~~~~~~~~~~~~~#
+    @property
+    def n_alignment (self):
+        return len(self.alignment_list)
+
+    #~~~~~~~~~~~~~~PUBLIC METHODS~~~~~~~~~~~~~~#
+    def get_best_alignment (self, primary_score="primary"):
+        primary_alignment_idx = self._get_primary_idx(primary_score=primary_score)
+        return self.alignment_list[primary_alignment_idx]
+
+    def get_secondary_alignments_list (self, primary_score="primary"):
+        primary_alignment_idx = self._get_primary_idx(primary_score=primary_score)
+        return [alignment for i, alignment in enumerate(self.alignment_list) if i != primary_alignment_idx]
+
+    def add_pysam_alignment (self, pysam_aligned_segment, **kwargs):
+        self.alignment_list.append (Alignment (pysam_aligned_segment))
+
+    def add_alignment (self, alignment, **kwargs):
+        self.alignment_list.append (alignment)
+
+    #~~~~~~~~~~~~~~PRIVATE METHODS~~~~~~~~~~~~~~#
+    def _get_primary_idx (self, primary_score="primary"):
+        idx = 0
+        if primary_score == "primary":
+            for i, alignment in enumerate(self.alignment_list):
+                if not alignment.secondary:
+                    idx = i
+        elif primary_score == "align_score":
+            best_align_score = -1
+            for i, alignment in enumerate(self.alignment_list):
+                if alignment.align_score > best_align_score:
+                    best_align_score = alignment.align_score
+                    idx = i
+        elif primary_score == "align_len":
+            best_align_len = -1
+            for i, alignment in enumerate(self.alignment_list):
+                if alignment.align_len > best_align_len:
+                    best_align_len = alignment.align_len
+                    idx = i
+        return idx
+
+#~~~~~~~~~~~~~~CLASS HIT~~~~~~~~~~~~~~#
+class Alignment ():
+    """
+    Helper class to extract relevant fields from the a pysam alignedSegment object
+    """
+
+    #~~~~~~~~~~~~~~MAGIC METHODS~~~~~~~~~~~~~~#
+    def __init__(self, pysam_aligned_segment):
+        """
+        """
+        self.qname = pysam_aligned_segment.query_name
+        self.rname = pysam_aligned_segment.reference_name
+        self.qlen = int (pysam_aligned_segment.query_length)
+        self.align_len = int (pysam_aligned_segment.query_alignment_length)
+        self.align_score = int (pysam_aligned_segment.get_tag("AS"))
+        self.secondary = pysam_aligned_segment.is_secondary or pysam_aligned_segment.is_supplementary
+
+    def __repr__(self):
+        return "Query:{} | Reference:{} | Query len:{} | Alignment len:{} | Align Score:{} | Secondary:{}".format(
+            self.qname, self.rname, self.qlen, self.align_len, self.align_score, self.secondary)
+
+    #~~~~~~~~~~~~~~PROPERTY METHODS~~~~~~~~~~~~~~#
+    @property
+    def query_fraction_aligned (self):
+        return self.align_len/self.qlen
---
+++ packages/NanoCount/NanoCount-0.2.4.tar.gz/NanoCount-0.2.4/NanoCount/__main__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#~~~~~~~~~~~~~~IMPORTS~~~~~~~~~~~~~~#
+
+# Standard library imports
+import argparse
+
+# Local imports
+import NanoCount as pkg
+from NanoCount.NanoCount import NanoCount as nc
+from NanoCount.common import *
+
+#~~~~~~~~~~~~~~MAIN PARSER ENTRY POINT~~~~~~~~~~~~~~#
+
+def main(args=None):
+
+    # Define parser
+    parser = argparse.ArgumentParser (description=pkg.__description__)
+    parser.add_argument("--version", action="version", version="{} v{}".format(pkg.__name__, pkg.__version__))
+
+    parser_io = parser.add_argument_group("Input/Output options")
+    arg_from_docstr(parser=parser_io, func=nc, arg_name="alignment_file", short_name="i")
+    arg_from_docstr(parser=parser_io, func=nc, arg_name="count_file", short_name="o")
+
+    parser_ms = parser.add_argument_group("Misc options")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="min_read_length", short_name="l")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="min_query_fraction_aligned", short_name="f")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="equivalent_threshold", short_name="t")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="scoring_value", short_name="s")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="convergence_target", short_name="c")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="max_em_rounds", short_name="e")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="extra_tx_info", short_name="x")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="primary_score", short_name="p")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="discard_suplementary", short_name="a")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="max_dist_3_prime", short_name="3")
+    arg_from_docstr(parser=parser_ms, func=nc, arg_name="max_dist_5_prime", short_name="5")
+
+    parser_vb = parser.add_argument_group("Verbosity options")
+    arg_from_docstr(parser=parser_vb, func=nc, arg_name="verbose", short_name="v")
+    arg_from_docstr(parser=parser_vb, func=nc, arg_name="quiet", short_name="q")
+
+    # Parse args and run main function
+    args = parser.parse_args()
+    nanocount = nc (**vars(args))
+
+# execute only if run as a script
+if __name__ == "__main__":
+    main()
