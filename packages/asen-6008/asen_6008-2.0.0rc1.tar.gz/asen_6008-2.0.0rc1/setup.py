91f9fadb5b66a641784de14b51c4550104fe10ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:04 +0000

    Add package asen-6008/2.0.0rc1/asen_6008-2.0.0rc1.tar.gz

---
+++ packages/asen-6008/asen_6008-2.0.0rc1.tar.gz/asen_6008-2.0.0rc1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md") as f:
+    long_desc = f.read()
+
+setuptools.setup(
+        name="asen_6008",
+        version="2.0.0-rc.1",
+        author="Nickolai Belakovski",
+        description="Constants for CU Boulder's ASEN 6008",
+        long_description=long_desc,
+        long_description_content_type="text/markdown",
+        url="https://github.com/nbelakovski/asen_6008",
+        packages=['asen_6008'],
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free For Educational Use",
+        "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+        )
