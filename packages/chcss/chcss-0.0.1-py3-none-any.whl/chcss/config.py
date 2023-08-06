5d4412baf19d893a1f309104bc7b7335c1f3f243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:47 +0000

    Add package chcss/0.0.1/chcss-0.0.1-py3-none-any.whl

---
+++ packages/chcss/chcss-0.0.1-py3-none-any.whl/chcss/config.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# chcss, a CSS naming hierarchy enforcer.
+# Copyright (C) 2021 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+"""chcss configuration functions."""
+
+
+class Config:
+    """Load and manage configuration data."""
+
+    def __init__(self):
+        """Initialize a Config() object."""
+        pass
