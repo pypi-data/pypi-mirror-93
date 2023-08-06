e03d4b898a96834bdd39f19c8b2f9e31ac6c1e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:43 +0000

    Add package akerbp.mlops/0.20210201210825/akerbp.mlops-0.20210201210825.tar.gz

---
+++ packages/akerbp.mlops/akerbp.mlops-0.20210201210825.tar.gz/akerbp.mlops-0.20210201210825/src/akerbp/mlops/training/handler.py
@@ -0,0 +1,21 @@
+# handler.py
+import warnings
+warnings.simplefilter("ignore")
+
+from akerbp.mlops.training.service import service
+from akerbp.mlops.utils import logger
+from akerbp.mlops import __version__ as version
+
+logging=logger.get_logger()
+
+logging.debug(f"MLOps framework version {version}")
+
+def handle(data, secrets):
+   try:
+      return service(data, secrets)
+   except Exception as error:
+      error_type = type(error).__name__
+      error_message = f" Training service failed. {error_type}: {error}"
+      logging.critical(error_message)
+      return dict(status='error', error_message=error_message)
+    
