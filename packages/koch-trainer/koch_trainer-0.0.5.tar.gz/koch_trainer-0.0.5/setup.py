65be79603e55556b29978cf403f6025ef66c1549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:36 +0000

    Add package koch-trainer/0.0.5/koch_trainer-0.0.5.tar.gz

---
+++ packages/koch-trainer/koch_trainer-0.0.5.tar.gz/koch_trainer-0.0.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pathlib
+
+required_modules = [
+	'audiogen-p3',
+	'PyAudio',
+]
+
+here				= pathlib.Path(__file__).parent.resolve()
+long_description	= ( here / 'README.md' ).read_text( encoding='utf-8' )
+
+setup(
+	name='koch_trainer',
+	version='0.0.5',
+	description='Koch method Morse code training program with Word Mode, Callsign Mode, and Random Character Mode',
+	author='Ryan Guisewite',
+	author_email='ryan.guisewite@gmail.com',
+	url='https://github.com/rguisewite/koch_trainer',
+
+	packages=find_packages(),
+	install_requires=required_modules,
+	include_package_data=True,
+
+	entry_points={
+		'console_scripts': [
+			'koch_trainer = koch_trainer.koch_trainer:main'
+		]
+	},
+
+	long_description=long_description,
+	classifiers=[
+		'Environment :: Console',
+		'Topic :: Multimedia :: Sound/Audio',
+		'Topic :: Communications :: Ham Radio',
+		'Programming Language :: Python :: 3.9',
+	]
+)
