ed44c28ef66a4ac46b7708f90a7ee449beb0e996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:12 +0000

    Add package artemis-dataset/0.0/artemis_dataset-0.0.tar.gz

---
+++ packages/artemis-dataset/artemis_dataset-0.0.tar.gz/artemis_dataset-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='artemis_dataset',
+      version='0.0',
+      description='ArtEmis: Affective Language for Art',            
+      license='MIT',
+      packages=['artemis_dataset'],
+      zip_safe=False)
