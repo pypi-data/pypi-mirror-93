00d5ee63a79d1e6b37680f64fc1cb32e7cadbc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:32 +0000

    Add package py-repo-root/1.1.1/py-repo-root-1.1.1.tar.gz

---
+++ packages/py-repo-root/py-repo-root-1.1.1.tar.gz/py-repo-root-1.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+      name='py-repo-root',
+      version='1.1.1',
+      license='MIT',
+      description='Python utility for cleaner handling of paths',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/pashminacameron/py-repo-root',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.7.0',
+      include_package_data=True,
+      author='Pashmina Cameron, Henry Jackson Flux',
+      author_email='pashabhi@yahoo.com',
+      install_requires=[],
+      zip_safe=False)
