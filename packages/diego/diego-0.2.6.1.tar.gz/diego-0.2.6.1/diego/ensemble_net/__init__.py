c705d54a6f2aeaabde1324a23f06d55ad94b95a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:48 +0000

    Add package diego/0.2.6.1/diego-0.2.6.1.tar.gz

---
+++ packages/diego/diego-0.2.6.1.tar.gz/diego-0.2.6.1/diego/ensemble_net/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+conan.__init__.py was created on 2017/10/17.
+Author: Charles_Lai
+Email: lai.bluejay@gmail.com
+"""
+
+from diego.ensemble_net.base import Ensemble, EnsembleClassifier
+from diego.ensemble_net.stacker import EnsembleStack, EnsembleStackClassifier
+from diego.ensemble_net.combination import Combiner
