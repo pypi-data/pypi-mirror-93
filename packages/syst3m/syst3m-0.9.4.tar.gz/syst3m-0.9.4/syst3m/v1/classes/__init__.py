ce31c4548f738477b04b7199fe055aa284cecbf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:43 +0000

    Add package syst3m/0.9.4/syst3m-0.9.4.tar.gz

---
+++ packages/syst3m/syst3m-0.9.4.tar.gz/syst3m-0.9.4/syst3m/v1/classes/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from syst3m.v1.classes.system import User, Group
+from syst3m.v1.classes.color import color, symbol
+from syst3m.v1.classes.defaults import defaults
+from syst3m.v1.classes.env import env
+from syst3m.v1.classes import utils, console, objects
+#from syst3m.v1.classes.screen import screen
+#from syst3m.v1.classes.keyboard import keyboard
+#from syst3m.v1.classes.mouse import mouse
