109d61897ea634aaa73e49e4be25514ca2957d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:55 +0000

    Add package essential/0.0.1/essential-0.0.1.tar.gz

---
+++ packages/essential/essential-0.0.1.tar.gz/essential-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="essential",
+    version="0.0.1",
+    author="Michael Hekmati",
+    author_email="micheal.hekmati@gmail.com",
+    packages=find_packages()
+)
