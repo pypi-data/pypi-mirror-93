028f017bec3ff7c27a3443a05aaa5ef3108d8bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:00 +0000

    Add package snapmap-archiver/1.1.0/snapmap-archiver-1.1.0.tar.gz

---
+++ packages/snapmap-archiver/snapmap-archiver-1.1.0.tar.gz/snapmap-archiver-1.1.0/README.md
@@ -0,0 +1,37 @@
+# snapmap-archiver
+
+A tool written in Python to download all Snapmaps content from a specific geolocation.
+
+![snapmap-archiver splash](/.github/img/Splash.png)
+
+## Setup
+
+`pip3 install snapmap-archiver`
+
+[View on PyPI](https://pypi.org/project/snapmap-archiver/)
+
+Install dependencies with `pip3`.
+
+```sh
+pip3 install -r requirements.txt
+```
+
+Install [aria2c](http://aria2.github.io/)
+
+## Usage
+
+```sh
+python3 -m snapmap_archiver -o [OUTPUT DIR] -g="[LATITUDE],[LONGITUDE]"
+```
+
+Unfortunately you have to use the arbitrary `-g="lat,lon"` rather than just `-g "lat,lon"` when parsing negative numbers as `argsparse` interprets said numbers as extra arguments.
+
+### Optional Arguments
+
+#### Export JSON
+
+You can export a JSON file with info about downloaded snaps with the `--write-json` argument, which will contain information like the time the Snap was posted, and the Snap location.
+
+#### Snap Radius
+
+The radius from the coordinates you provide that will be included for downloads. `-r 20000` will download all Snaps within a 20km radius of your coordinates.
---
+++ packages/snapmap-archiver/snapmap-archiver-1.1.0.tar.gz/snapmap-archiver-1.1.0/snapmap_archiver/get_data.py
@@ -0,0 +1,44 @@
+import requests, random, json, pathlib, sys
+
+from requests.api import get
+
+req_headers = {
+    'User-Agent': random.choice(json.loads(open(f'{pathlib.Path(__file__).parent.absolute()}/utils/user-agents.json', 'r').read())),
+    'Host': 'ms.sc-jpl.com',
+    'Accept': '*/*',
+    'Accept-Language': 'en-US',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://map.snapchat.com/@0,0,7.67z',
+    'Content-Type': 'application/json',
+    'Origin': 'https://map.snapchat.com',
+    'Content-Length': '2',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Sec-GPC': '1',
+    'TE': 'Trailers',
+}
+
+def get_epoch():
+    for entry in json.loads(requests.post('https://ms.sc-jpl.com/web/getLatestTileSet', headers=req_headers, json={}).text)['tileSetInfos']:
+        if(entry['id']['type'] == 'HEAT'):
+            return(entry['id']['epoch'])
+
+def api_query(lat, lon, zl=5, max_radius=10000):
+    available_snaps = []
+    current_iteration = max_radius
+    _epoch = get_epoch()
+    try:
+        while current_iteration != 1:
+            payload = {"requestGeoPoint":{"lat":lat,"lon":lon},"zoomLevel":zl,"tileSetId":{"flavor":"default","epoch":_epoch,"type":1},"radiusMeters":current_iteration,"maximumFuzzRadius":0}
+            req_headers['Content-Length'] = str(len(str(payload)))
+            api_data = json.loads(requests.post('https://ms.sc-jpl.com/web/getPlaylist', headers=req_headers, json=payload).text)
+            available_snaps = available_snaps + api_data['manifest']['elements']
+            if(current_iteration > 2000):
+                current_iteration = current_iteration - 2000
+            elif(current_iteration > 1000):
+                current_iteration = current_iteration - 100
+            else:
+                current_iteration = 1
+    except:
+        sys.exit("You seem to have been rate limited, please wait and try again.")
+    return [i for n, i in enumerate(available_snaps) if i not in available_snaps[n + 1:]]
