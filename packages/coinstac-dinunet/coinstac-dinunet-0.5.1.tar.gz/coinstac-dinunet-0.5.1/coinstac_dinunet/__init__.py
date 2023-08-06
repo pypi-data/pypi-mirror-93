b9114b2e54a8c9fd9e37c262bfe86602569c804d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:47 +0000

    Add package coinstac-dinunet/0.5.1/coinstac-dinunet-0.5.1.tar.gz

---
+++ packages/coinstac-dinunet/coinstac-dinunet-0.5.1.tar.gz/coinstac-dinunet-0.5.1/coinstac_dinunet/__init__.py
@@ -0,0 +1,4 @@
+from .data import COINNDataLoader, COINNDataset
+from .nodes import COINNLocal, COINNRemote
+from .trainer import COINNTrainer
+from .pool import PooledTrainer
---
+++ packages/coinstac-dinunet/coinstac-dinunet-0.5.1.tar.gz/coinstac-dinunet-0.5.1/coinstac_dinunet/nodes/__init__.py
@@ -0,0 +1,3 @@
+from .local import COINNLocal
+from .remote import COINNRemote
+
