f1cd0fb70fe082e597f6934896ca457c539e906b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:53 +0000

    Add package pylotoncycle/0.4.0/pylotoncycle-0.4.0.tar.gz

---
+++ packages/pylotoncycle/pylotoncycle-0.4.0.tar.gz/pylotoncycle-0.4.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pylotoncycle',
+    version='0.4.0',
+    description='Module to access your Peloton workout data',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/justmedude/pylotoncycle',
+    author='Vikram Adukia',
+    author_email='github@fireitup.net',
+    license='BSD',
+    packages=['pylotoncycle'],
+    install_requires=['requests'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3'
+    ]
+)
