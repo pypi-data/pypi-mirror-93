ed25a35b62441564f00a6adf53ab62fcd8685caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:49 +0000

    Add package Email-bot/1/Email-bot-1.tar.gz

---
+++ packages/Email-bot/Email-bot-1.tar.gz/Email-bot-1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="Email-bot",
+    version="1",
+    packages=["email-bot"],
+    author="Pranav",
+    description="Helps you send GMAIL and Outlook Emails.",
+    py_requires='>3.8'
+)
