d3e33f0ba0b1f6901689de728a505a4ad7332547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:42 +0000

    Add package staticjinja/1.0.4/staticjinja-1.0.4-py3-none-any.whl

---
+++ packages/staticjinja/staticjinja-1.0.4-py3-none-any.whl/staticjinja/__init__.py
@@ -0,0 +1,24 @@
+"""
+staticjinja
+===========
+
+staticjinja is a library that makes it easy to build static sites using Jinja2.
+
+Many static site generators are complex, with long manuals and unnecessary
+features. But using template engines to build static websites is really useful.
+staticjinja is designed to be lightweight (under 500 lines of source code), and
+to be easy to use, learn, and extend, enabling you to focus on making your site.
+
+Documentation is available at https://staticjinja.readthedocs.org.
+
+Please file bugs, view source code, and contribute at
+https://github.com/staticjinja/staticjinja/
+"""
+
+# flake8: noqa
+# This needs to match what is in pyproject.toml
+__version_info__ = (1, 0, 4)
+__version__ = ".".join(map(str, __version_info__))
+
+from .reloader import Reloader
+from .staticjinja import make_site, Site
---
+++ packages/staticjinja/staticjinja-1.0.4-py3-none-any.whl/staticjinja/cli.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""staticjinja
+
+Usage:
+  staticjinja build [options]
+  staticjinja watch [options]
+  staticjinja -h | --help
+  staticjinja --version
+
+Commands:
+  build      Render the site
+  watch      Render the site, and re-render on changes to <srcpath>
+
+Options:
+  --srcpath=<srcpath>   Directory in which to build from [default: ./templates]
+  --outpath=<outpath>   Directory in which to build to [default: ./]
+  --static=<a,b,c>      Directory(s) within <srcpath> containing static files
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+import os
+import sys
+
+from docopt import docopt
+
+from .staticjinja import Site
+from staticjinja import __version__
+
+
+def render(args):
+    """
+    Render a site.
+
+    :param args:
+        A map from command-line options to their values. For example:
+
+            {
+                '--help': False,
+                '--outpath': './',
+                '--srcpath': './templates',
+                '--static': None,
+                '--version': False,
+                'build': True,
+                'watch': False
+            }
+    """
+
+    def resolve(path):
+        if not os.path.isabs(path):
+            path = os.path.join(os.getcwd(), path)
+        return os.path.normpath(path)
+
+    srcpath = resolve(args["--srcpath"])
+    if not os.path.isdir(srcpath):
+        print("The templates directory '{}' is invalid.".format(srcpath))
+        sys.exit(1)
+
+    outpath = resolve(args["--outpath"])
+
+    staticdirs = args["--static"]
+    staticpaths = None
+    if staticdirs:
+        staticpaths = staticdirs.split(",")
+        for path in staticpaths:
+            path = os.path.join(srcpath, path)
+            if not os.path.isdir(path):
+                print("The static files directory '{}' is invalid.".format(path))
+                sys.exit(1)
+
+    site = Site.make_site(searchpath=srcpath, outpath=outpath, staticpaths=staticpaths)
+    site.render(use_reloader=args["watch"])
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    render(docopt(__doc__, argv=argv, version=__version__))
+
+
+if __name__ == "__main__":
+    main()
