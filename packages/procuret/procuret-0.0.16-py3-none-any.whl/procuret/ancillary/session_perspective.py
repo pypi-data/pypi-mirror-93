f798c4fe3a9f44bb36ac7f3e9f46b080f276ca52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:20 +0000

    Add package procuret/0.0.16/procuret-0.0.16-py3-none-any.whl

---
+++ packages/procuret/procuret-0.0.16-py3-none-any.whl/procuret/ancillary/session_perspective.py
@@ -0,0 +1,12 @@
+"""
+Procuret Python
+Session Perspective Module
+author: hugh@blinkybeach.com
+"""
+from enum import Enum
+
+
+class Perspective(Enum):
+    SUPPLIER = 1
+    BUSINESS = 2
+    ADMINISTRATOR = 3
