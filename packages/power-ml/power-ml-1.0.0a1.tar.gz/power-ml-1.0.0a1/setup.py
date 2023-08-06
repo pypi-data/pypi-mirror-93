1bd8397026f5120801b78f1981ab8552d2f9506b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:40 +0000

    Add package power-ml/1.0.0a1/power-ml-1.0.0a1.tar.gz

---
+++ packages/power-ml/power-ml-1.0.0a1.tar.gz/power-ml-1.0.0a1/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['power_ml',
+ 'power_ml.ai',
+ 'power_ml.ai.predictor',
+ 'power_ml.ai.selection',
+ 'power_ml.data',
+ 'power_ml.platform',
+ 'power_ml.stats',
+ 'power_ml.util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.16.63,<2.0.0',
+ 'joblib>=1.0.0,<2.0.0',
+ 'lightgbm>=3.1.1,<4.0.0',
+ 'numpy>=1.19.5,<2.0.0',
+ 'pandas>=1.2.1,<2.0.0',
+ 'psutil>=5.8.0,<6.0.0',
+ 'scikit-learn>=0.24.1,<0.25.0',
+ 'tinydb>=4.3.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'power-ml',
+    'version': '1.0.0a1',
+    'description': 'Empowerment Python Machine Learing Stack.',
+    'long_description': '# power-ml\n\nEmpowerment Python Machine Learing Stack.\n\n## Install\n\n* Requirements\n   * Poetry\n   * pyenv\n\n```sh\npoetry install --no-dev\n```\n\n## Development\n\n```sh\n# Setup\npoetry install\n\n# Run lint and test.\nbash scripts/report.bash\n```\n',
+    'author': 'Takeru Saito',
+    'author_email': 'takelushi@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/takelushi/power-ml',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
