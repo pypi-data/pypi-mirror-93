82bf8b50ad43601954417b7933e090a9579938ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:16 +0000

    Add package inewave/0.0.12/inewave-0.0.12.tar.gz

---
+++ packages/inewave/inewave-0.0.12.tar.gz/inewave-0.0.12/inewave/config.py
@@ -0,0 +1,30 @@
+SUBMERCADOS = ["SUDESTE", "SUL", "NORDESTE", "NORTE"]
+REES = ["SUDESTE",
+        "SUL",
+        "NORDESTE",
+        "NORTE",
+        "ITAIPU",
+        "MADEIRA",
+        "TPIRES",
+        "BMONTE",
+        "MAN-AP",
+        "PARANA",
+        "IGUACU",
+        "PRNPANEMA"]
+NUM_CENARIOS = 2000
+MAX_ANOS_ESTUDO = 30
+NUM_PATAMARES = 3
+NUM_VARIAVEIS_MEDIAS = 52
+NUM_VARIAVEIS_CUSTO_PMO = 16
+MESES = ["JANEIRO",
+         "FEVEREIRO",
+         "MARCO",
+         "ABRIL",
+         "MAIO",
+         "JUNHO",
+         "JULHO",
+         "AGOSTO",
+         "SETEMBRO",
+         "OUTUBRO",
+         "NOVEMBRO",
+         "DEZEMBRO"]
