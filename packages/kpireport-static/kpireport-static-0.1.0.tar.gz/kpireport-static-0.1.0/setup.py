46f7dc0bb89dd0cb577f921b456dc09ba9e14373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:44 +0000

    Add package kpireport-static/0.1.0/kpireport-static-0.1.0.tar.gz

---
+++ packages/kpireport-static/kpireport-static-0.1.0.tar.gz/kpireport-static-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="kpireport-static",
+    version="0.1.0",
+    author="KPI Reporter LLC",
+    author_email="dev@kpireporter.com",
+    url="https://kpireporter.com",
+    license="Prosperity Public License",
+    packages=["kpireport_static"],
+    install_requires=[
+        "kpireport",
+        "imgkit",
+    ],
+    entry_points={
+        "kpireport.output": ["static = kpireport_static:StaticOutputDriver"],
+    },
+)
