381d1e79a705117f7b7c509a278a4a73ae7d8a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package pipefun/0.0.3/pipefun-0.0.3.tar.gz

---
+++ packages/pipefun/pipefun-0.0.3.tar.gz/pipefun-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pipefun", # Replace with your own username
+    version="0.0.3",
+    author="starskyC",
+    author_email="cl2004_0123@aliyun.com",
+    description="A lightweight package that contains pipe operators and functional tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/starskyC/pipefun",
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
