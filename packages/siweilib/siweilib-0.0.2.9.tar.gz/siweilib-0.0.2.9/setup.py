9058bd9897ca949101fcc99f263a556fdacb2717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:47 +0000

    Add package siweilib/0.0.2.9/siweilib-0.0.2.9.tar.gz

---
+++ packages/siweilib/siweilib-0.0.2.9.tar.gz/siweilib-0.0.2.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import upgrade_version
+
+# f = open('VERSION.txt')
+# version_str = f.read()
+# f.close()
+version_str = upgrade_version.get_version()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="siweilib",
+    version=version_str,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=("tests",)),
+    python_requires='>=3.5',
+    install_requires=[
+        "termcolor",
+        "pytz",
+        "argparse",
+        "cryptography",
+    ]
+)
---
+++ packages/siweilib/siweilib-0.0.2.9.tar.gz/siweilib-0.0.2.9/siweilib/filez/__init__.py
@@ -0,0 +1,3 @@
+# from siweilib.filez.lib import *
+from .lib import *
+from .chash import *
