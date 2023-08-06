01a52d667e95a21e0cafbde6e2530add8375cce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:41 +0000

    Add package JasonUtils/1.0.0/JasonUtils-1.0.0.tar.gz

---
+++ packages/JasonUtils/JasonUtils-1.0.0.tar.gz/JasonUtils-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='JasonUtils',
+    version='1.0.0',
+    packages=['JasonUtils'],
+    url='https://github.com/I-Good-Vegetable/JasonUtils',
+    license='MIT',
+    author='Jason',
+    author_email='jinxin.liu.ca@gmail.com',
+    description='Jason\'s Utils'
+)
