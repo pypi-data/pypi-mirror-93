08fbd1a30772f76358c526a2d2369f8d3f5a9387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:05 +0000

    Add package madlibspy/2.0/madlibspy-2.0.tar.gz

---
+++ packages/madlibspy/madlibspy-2.0.tar.gz/madlibspy-2.0/madlibspy/__init__.py
@@ -0,0 +1,20 @@
+__version__ = "2.0"
+
+import requests, json
+
+class Madlibs:
+  def get(path=None):
+    if path:
+      with open(path, "r") as f:
+        data = json.load(path)
+    else:
+      data = requests.get("https://api.bytestobits.dev/madlibs").json()
+    
+    return data
+  
+  def convert(answers:list, questions:int, text:str):
+    en = 0
+    for i in range(questions):
+      text = text.replace("{" + str(en) + "}", answers[en])
+      en += 1
+    return text
