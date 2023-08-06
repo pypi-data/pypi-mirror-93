978a9a730f366125046392f7b2bf8c544f7bca35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:48 +0000

    Add package TextFeatureSelection/0.0.12/TextFeatureSelection-0.0.12.tar.gz

---
+++ packages/TextFeatureSelection/TextFeatureSelection-0.0.12.tar.gz/TextFeatureSelection-0.0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+import sys
+
+if sys.version_info[0] < 3:
+    with open('README.rst') as f:
+        long_description = f.read()
+else:
+    with open('README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+
+setup(
+    name='TextFeatureSelection',
+    version='0.0.12',
+    description='Python library for feature selection for text features. It has filter method and genetic algorithm for improving text classification models. Helps improve your machine learning models',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    author='StatguyUser',
+    url='https://github.com/StatguyUser/TextFeatureSelection',
+    install_requires=['numpy','pandas','scikit-learn'],
+    download_url='https://github.com/StatguyUser/TextFeatureSelection.git',
+    py_modules=["TextFeatureSelection"],
+    package_dir={'':'src'},
+)
