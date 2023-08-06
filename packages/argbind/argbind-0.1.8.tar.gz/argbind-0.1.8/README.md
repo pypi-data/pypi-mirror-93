9ec50c8af2b91940047d8922be9d2989be1d48d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:34 +0000

    Add package argbind/0.1.8/argbind-0.1.8.tar.gz

---
+++ packages/argbind/argbind-0.1.8.tar.gz/argbind-0.1.8/README.md
@@ -0,0 +1,311 @@
+# ArgBind
+
+**Build CLIs via docstrings and type annotations, with YAML support.**
+
+![Build](https://github.com/pseeth/argbind/workflows/Build/badge.svg) 
+[![PyPI version](https://badge.fury.io/py/argbind.svg)](https://badge.fury.io/py/argbind)
+[![codecov](https://codecov.io/gh/pseeth/argbind/branch/main/graph/badge.svg?token=BWI0FHZI5H)](undefined)
+![Lines of code](https://img.shields.io/tokei/lines/github/pseeth/argbind)
+[![Downloads](https://pepy.tech/badge/argbind)](https://pepy.tech/project/argbind)
+
+*ArgBind is a simple way to bind arguments to functions to the command line or to .yml files!* It supports scoping of arguments, similar to other frameworks like 
+[Hydra](https://github.com/facebookresearch/hydra) and
+[gin-config](https://github.com/google/gin-config).
+ArgBind is *very* small (only ~300 lines of code, in one file), can be used to make complex and well-documented command line programs, and allows 
+you to configure program execution from .yml files.
+
+Scroll down to see some [examples](#examples). Please also look at the 
+current known [limitations](#limitations) of ArgBind.
+
+## Why ArgBind?
+
+I built ArgBind mostly to help me configure my machine learning experiments. ML experiment
+configuration is often highly nested, and can get out of hand quick. I didn't want to switch
+my workflow around too much to accommodate a new framework. Instead, I wanted the scripts
+that I've written to be easily adapted so that I could achieve a few goals:
+
+1. Configure scripts using `.yml` files. Be able to save `.yml` files that can be used to rerun scripts the exact same way twice.
+2. Spend time writing actual functions needed to run experiments, not argument parsers.
+3. Be able to run my experiment code from other Python scripts, notebooks, or the command line.
+4. Be able to specify arguments from the command line directly to various functions.
+5. Be able to use scoping patterns, so I can run a function inside of a `train` scope and `test` scope, with different results (e.g. for getting a train dataset and a test dataset).
+
+Nothing out there really fit the bill, so I wrote ArgBind. If you have 
+an `argparse` based script, converting it to ArgBind should be very quick! ArgBind is simple, 
+small, and easy to use. To get a feel for how it works, check out [usage](#usage), [design](#design), and [examples](#examples)!
+
+## Installation
+
+Install via `pip`:
+
+```
+python -m pip install argbind
+```
+
+Or from source:
+
+```
+git clone https://github.com/pseeth/argbind.git
+cd argbind
+python -m pip install -e .
+```
+
+## Examples
+
+- [Example 1: Hello World](./examples/hello_world/)
+- [Example 2: Scope patterns](./examples/scoping/)
+- [Example 3: Typing](./examples/typing/)
+- [Example 4: MNIST Script](./examples/mnist/)
+- [Example 5: Loading, saving, and using .yml files](./examples/yaml)
+- [Example 6: Multi-stage programs](./examples/multistage)
+- [Example 7: Mimic more traditional CLI, without `func.arg` notation](./examples/without_prefix)
+- [Example 8: Debug mode](./examples/debug)
+
+## Usage
+
+There are six main functions.
+
+- `bind`: Binds a functions typed keyword arguments to ArgBind.
+- `parse_args`: Actually parses the arguments into a dictionary.
+- `scope`: Context manager that scopes a dictionary containing function arguments to be used by the functions.
+- `dump_args`: Dumps the args dictionary to a `.yml` file. Used internally when program is called with `--args.save path/to/save.yml`.
+- `load_args`: Loads args from a `.yml` file. Used internally when program is called with `--args.load path/to/load.yml`.
+- `get_used_args`: Gets arguments that have actually been used by call functions up to this point.
+
+Your code with ArgBind generally follows this pattern:
+
+1. Write a function with a good docstring, and typed keyword arguments.
+2. Bind it via `bind`.
+3. When program is called, parse the arguments via `parse_args`.
+4. Scope the arguments, and call the bound function within the context block.
+5. Optionally call program with `--args.save` to save the current execution configuration to a `.yml` file or `--args.load` to load arguments from a prior saved execution configuration to run it the same way twice.
+6. Optionally, run your script with `--args.debug=1` to see exactly how every bound function is called.
+
+In your program, you can call `get_used_args` to inspect the state of the argument dictionary. Here's a minimal example:
+
+```python
+import argbind
+
+@argbind.bind()
+def hello(
+    name : str = 'world'
+):
+    """Say hello to someone.
+
+    Parameters
+    ----------
+    name : str, optional
+        Who you're saying hello to, by default 'world'
+    """
+    print("Hello " + name)
+
+if __name__ == "__main__":
+    # Arguments for CLI automatically generated from bound functions under the pattern
+    # function_name.function_arg.
+    args = argbind.parse_args()
+    # When called within a scope, the keyword arguments map to those from CLI or 
+    # from defaults.
+    with argbind.scope(args):
+        hello()
+```
+
+Help text is automatically generated from the docstring:
+
+```
+❯ python examples/hello_world/with_argbind.py -h
+usage: with_argbind.py [-h] [--args.save ARGS.SAVE] [--args.load ARGS.LOAD] [--args.debug ARGS.DEBUG] [--hello.name HELLO.NAME]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --args.save ARGS.SAVE
+                        Path to save all arguments used to run script to.
+  --args.load ARGS.LOAD
+                        Path to load arguments from, stored as a .yml file.
+  --args.debug ARGS.DEBUG
+                        Print arguments as they are passed to each function.
+
+Generated arguments for function hello:
+  Say hello to someone.
+
+  --hello.name HELLO.NAME
+                        Who you're saying hello to, by default 'world'
+```
+
+Execution of this could look like:
+
+```
+# Default arguments
+❯ python examples/hello_world/with_argbind.py
+Hello world
+# Binding name from the command line and saving the args.
+❯ python examples/hello_world/with_argbind.py --hello.name=you --args.save=/tmp/args.yml
+Hello you
+# Loading saved arguments.
+❯ python examples/hello_world/with_argbind.py --args.load=/tmp/args.yml
+Hello you
+# Loading saved arguments, and overriding via command line.
+❯ python examples/hello_world/with_argbind.py --args.load=/tmp/args.yml --hello.name=me
+Hello me
+# See how each function is called with args.debug=1.
+❯ python examples/hello_world/with_argbind.py --args.load=/tmp/args.yml --args.debug=1
+hello(
+  name : str = you
+)
+Hello you
+```
+
+You can also run the `hello` function from another Python script or a Jupyter notebook:
+
+```python
+import argbind
+# Import the bound function
+from .hello_world import hello 
+# Load the args
+args = argbind.load_args('/tmp/args.yml')
+# Scope the args
+with argbind.scope(args):
+    # Run the bound function
+    hello() # Prints 'Hello you'.
+hello() # Prints 'Hello world', as it's outside scope.
+# Can edit the args before scoping again.
+args['hello.name'] = 'me'
+with argbind.scope(args):
+    hello() # Prints 'Hello me'.
+```
+
+You'll notice that ArgBind forces you to document and type your 
+function arguments, which is always a good idea! 
+Please check out the [examples](#examples) for more details!
+
+
+## Design
+
+ArgBind is designed around a decorator that can be used on
+functions the user wants to expose to command line or to a .yml file.
+The typed keyword arguments to that function are 
+then bound to a dictionary. When the function is called, 
+each keyword argument is looked up in the dictionary and its
+value is replaced with the corresponding value in the dictionary. The
+dictionary that the function looks for values in is controlled by
+`scope`:
+
+```python
+import argbind 
+
+@argbind.bind()
+def func(arg : str = 'default'):
+    print(arg)
+
+dict1 = {
+    'func.arg': 1,
+}
+dict2 = {
+    'func.arg': 2
+}
+
+with argbind.scope(dict1):
+    func() # prints 1
+with argbind.scope(dict2):
+    func() # prints 2
+func(arg=3) # prints 3.
+```
+
+The function arguments are bound to the command line. Continuing the 
+simple program from above:
+
+```python
+if __name__ == "__main__":
+    args = argbind.parse_args()
+    with argbind.scope(args):
+        func()
+    with argbind.scope(args):
+        func(arg=3)
+```
+
+You can call this function like so:
+
+```bash
+❯ python examples/readme_example.py --func.arg 5
+1 # Looks up `arg` in dict1
+2 # Looks up `arg` in dict2
+3 # arg is passed in on python call `func(arg=3)`
+5 # Looks up `arg` from command line call `--func.arg 5`
+3 # arg is passed in from two places: `func(arg=3)` and `--func.arg 5`. Former overrides the latter.
+```
+
+The logic here is that arguments that are bound that are closer to the actual function call get priority. From highest priority, to lowest, it goes:
+
+1. Bound explicitly in Python code
+2. Bound via command line
+3. Bound via .yml file
+4. Bound via default for kwarg
+
+### Note!
+
+The catch is that the function's keyword argument MUST be typed.
+This is required so that ArgBind knows how to parse it from the
+command line.
+
+# Limitations
+
+There are some limitations to ArgBind, some due to how Python function decorator works,
+and others out of a desire to keep ArgBind's code simple and straightforward.
+
+## Boolean keyword arguments
+
+If a boolean is flipped to True in a `.yml` file, there's no
+way to override it from the command line. If you want a flag to
+be flippable, make the argument an int instead of a bool and use
+0 and 1 for True and False. Then you can override from command
+line like `--func.arg 0` or `--func.arg 1`.
+
+## Only typed keyword arguments are bound
+
+Only keyword arguments that have types associated with them are bound 
+in functions. Positional arguments are not bound. Untyped keyword
+arguments cannot be bound, because ArgBind won't know what type to 
+parse for.
+
+## Bound function names should be unique
+
+Functions that are bound must be unique, even if they are in different files. The 
+function name is resolved in the argument parser only using the immediate name, not
+a path to the function etc. 
+
+## Supported docstring formats
+
+ArgBind uses [docstring-parser](https://github.com/rr-/docstring_parser), and so
+the only supported styles are: ReST, Google, and Numpydoc-style docstrings.
+
+## Not all types are supported
+
+ArgBind supports most types that might pop up in your script, but not all. The supported types can be seen in the [typing example](./examples/typing/).
+
+# Releasing
+
+Do the following steps:
+
+```
+python setup.py sdist
+```
+
+Upload it to test PyPI:
+
+```
+pip install twine
+twine upload --repository testpypi dist/*
+pip install -U --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple -U argbind
+```
+
+Make sure you can install it and it works (e.g. run the examples). Now upload
+to actual PyPI:
+
+```
+twine upload dist/*
+```
+
+# Issues? Questions?
+
+If you've run into some issues with ArgBind, or have some questions, please ask 
+via Github Issues. Projects like ArgBind are pretty tricky to get right, so there
+are surely some edge cases that have been missed.
---
+++ packages/argbind/argbind-0.1.8.tar.gz/argbind-0.1.8/argbind/argbind.py
@@ -0,0 +1,368 @@
+import inspect
+from contextlib import contextmanager
+import argparse
+from typing import List, Dict, Tuple
+import docstring_parser
+import textwrap
+import yaml
+import sys
+import os
+from pathlib import Path
+import ast
+from functools import wraps
+import warnings
+
+PARSE_FUNCS = {}
+ARGS = {}
+USED_ARGS = {}
+PATTERN = None
+DEBUG = False
+HELP_WIDTH = 60
+
+@contextmanager
+def scope(parsed_args, pattern=''):
+    """
+    Context manager to put parsed arguments into 
+    a state.
+    """
+    parsed_args = parsed_args.copy()
+    remove_keys = []
+    matched = {}
+
+    global ARGS
+    global PATTERN
+
+    old_args = ARGS
+    old_pattern = PATTERN
+
+    for key in parsed_args:
+        if '/' in key:
+            if key.split('/')[0] == pattern:
+                matched[key.split('/')[-1]] = parsed_args[key]
+            remove_keys.append(key)
+    
+    parsed_args.update(matched)
+    for key in remove_keys:
+        parsed_args.pop(key)
+    ARGS = parsed_args
+    PATTERN = pattern
+    yield
+
+    ARGS = old_args
+    PATTERN = old_pattern
+
+def _format_func_debug(func_name, func_kwargs, scope=None):
+    formatted = [f"{func_name}("]
+    if scope is not None:
+        formatted.append(f"  # scope = {scope}")
+    for key, val in func_kwargs.items():
+        formatted.append(f"  {key} : {type(val).__name__} = {val}")
+    formatted.append(")")
+    return '\n'.join(formatted)
+
+def bind(*patterns, without_prefix=False):
+    """
+    Wrap the function so it looks in ARGS (managed 
+    by the scope context manager) for keyword 
+    arguments.
+    """
+
+    def decorator(func):
+        PARSE_FUNCS[func.__name__] = (func, patterns, without_prefix)
+        
+        @wraps(func)
+        def cmd_func(*args, **kwargs):
+            prefix = func.__name__
+            sig = inspect.signature(func)
+            cmd_kwargs = {}
+
+            for key, val in sig.parameters.items():
+                arg_type = val.annotation
+                arg_val = val.default
+                if arg_val is not inspect.Parameter.empty:
+                    arg_name = f'{prefix}.{key}' if not without_prefix else f'{key}'
+                    if arg_name in ARGS and key not in kwargs:
+                        cmd_kwargs[key] = ARGS[arg_name]
+                        use_key = arg_name
+                        if PATTERN:
+                            use_key = f'{PATTERN}/{use_key}'
+                        USED_ARGS[use_key] = ARGS[arg_name]
+            
+            kwargs.update(cmd_kwargs)
+            if 'args.debug' not in ARGS: ARGS['args.debug'] = False
+            if ARGS['args.debug'] or DEBUG:
+                if PATTERN: 
+                    scope = PATTERN
+                else:
+                    scope = None
+                print(_format_func_debug(prefix, kwargs, scope))
+            return func(*args, **kwargs)
+        return cmd_func
+    
+    return decorator
+
+# Backwards compat.
+# For scripts written with argbind<=0.1.3.
+bind_to_parser = bind
+
+def get_used_args():
+    """
+    Gets the args that have been used so far
+    by the script (e.g. their function they target
+    was actually called).
+    """
+    return USED_ARGS
+
+def dump_args(args, output_path):
+    """
+    Dumps the provided arguments to a
+    file.
+    """
+    path = Path(output_path)
+    os.makedirs(path.parent, exist_ok=True)
+    with open(path, 'w') as f:
+        yaml.Dumper.ignore_aliases = lambda *args : True
+        x = yaml.dump(args, Dumper=yaml.Dumper)
+        prev_line = None
+        output = []
+        for line in x.split('\n'):
+            cur_line = line.split('.')[0].strip()
+            if not cur_line.startswith('-'):
+                if cur_line != prev_line and prev_line:
+                    line = f'\n{line}'
+                prev_line = line.split('.')[0].strip()
+            output.append(line)
+        f.write('\n'.join(output))
+
+def load_args(input_path):
+    """
+    Loads arguments from a given input path. If $include key is in
+    the args, you can include other y
+    """
+    with open(input_path, 'r') as f:
+        data = yaml.load(f, Loader=yaml.Loader)
+    
+    if '$include' in data:
+        include_files = data.pop('$include')
+        include_args = {}
+        for include_file in include_files:
+            with open(include_file, 'r') as f:
+                _include_args = yaml.load(f, Loader=yaml.Loader)
+            include_args.update(_include_args)
+        include_args.update(data)
+        data = include_args
+
+    _vars = os.environ.copy()
+    if '$vars' in data:
+        _vars.update(data.pop('$vars'))
+    
+    for key, val in data.items():
+        # Check if string starts with $.
+        if isinstance(val, str): 
+            if val.startswith('$'):
+                lookup = val[1:]
+                if lookup in _vars:
+                    data[key] = _vars[lookup]
+        
+        elif isinstance(val, list):
+            new_list = []
+            for subval in val:
+                if subval.startswith('$'):
+                    lookup = subval[1:]
+                    if lookup in _vars:
+                        new_list.append(_vars[lookup])
+                    else:
+                        new_list.append(subval)
+                else:
+                    new_list.append(subval)
+            data[key] = new_list
+
+    if 'args.debug' not in data:
+        data['args.debug'] = DEBUG
+    return data
+
+class str_to_list():
+    def __init__(self, _type):
+        self._type = _type
+    def __call__(self, values):
+        _values = values.split(' ')
+        _values = [self._type(v) for v in _values]
+        return _values
+
+class str_to_tuple():
+    def __init__(self, _type_list):
+        self._type_list = _type_list
+    def __call__(self, values):
+        _values = values.split(' ')
+        _values = [self._type_list[i](v) for i, v in enumerate(_values)]
+        return tuple(_values)
+
+class str_to_dict():
+    def __init__(self):
+        pass
+
+    def _guess_type(self, s):
+        try:
+            value = ast.literal_eval(s)
+        except ValueError:
+            return s
+        else:
+            return value
+
+    def __call__(self, values):
+        values = values.split(' ')
+        _values = {}
+
+        for elem in values:
+            key, val = elem.split('=', 1)
+            key = self._guess_type(key)
+            val = self._guess_type(val)
+            _values[key] = val
+
+        return _values
+
+def parse_args():
+    """
+    Goes through all detected functions that are
+    bound and adds them to the argument parser,
+    along with their scopes. Then parses the
+    command line and returns a dictionary.
+    """
+    p = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    p.add_argument('--args.save', type=str, required=False, 
+        help="Path to save all arguments used to run script to.")
+    p.add_argument('--args.load', type=str, required=False,
+        help="Path to load arguments from, stored as a .yml file.")
+    p.add_argument('--args.debug', type=int, required=False, default=0, 
+        help="Print arguments as they are passed to each function.")
+
+    # Add kwargs from function to parser
+    for func_name in PARSE_FUNCS:
+        func, patterns, without_prefix = PARSE_FUNCS[func_name]
+        sig = inspect.signature(func)
+        prefix = func.__name__
+
+        docstring = docstring_parser.parse(func.__doc__)
+        parameter_help = docstring.params
+        parameter_help = {
+            x.arg_name: x.description for x in parameter_help
+        }
+
+        f = p.add_argument_group(
+            title=f"Generated arguments for function {prefix}",
+        )
+
+        for key, val in sig.parameters.items():
+            arg_type = val.annotation
+            arg_val = val.default
+
+            if arg_val is not inspect.Parameter.empty:
+                arg_names = []
+                arg_help = {}
+                help_text = ''
+                if key in parameter_help:
+                    help_text = textwrap.fill(parameter_help[key], width=HELP_WIDTH)
+                if not without_prefix:
+                    arg_names.append(f'--{prefix}.{key}')
+                else:
+                    arg_names.append(f'--{key}')
+                arg_help[arg_names[-1]] = help_text
+                for pattern in patterns:
+                    if not without_prefix:
+                        arg_names.append(f'--{pattern}/{prefix}.{key}')
+                    else:
+                        arg_names.append(f'--{pattern}/{key}')
+                    arg_help[arg_names[-1]] = argparse.SUPPRESS
+                for arg_name in arg_names:
+                    inner_types = [str, int, float, bool]
+                    list_types = [List[x] for x in inner_types]
+
+                    if arg_type is bool:
+                        f.add_argument(arg_name, action='store_true', 
+                            help=arg_help[arg_name])
+                    elif arg_type in list_types:
+                        _type = inner_types[list_types.index(arg_type)]
+                        f.add_argument(arg_name, type=str_to_list(_type), 
+                            default=arg_val, help=arg_help[arg_name])
+                    elif arg_type is Dict:
+                        f.add_argument(arg_name, type=str_to_dict(), 
+                            default=arg_val, help=arg_help[arg_name])
+                    elif hasattr(arg_type, '__origin__'):
+                        if arg_type.__origin__ is tuple:
+                            _type_list = arg_type.__args__
+                            f.add_argument(arg_name, type=str_to_tuple(_type_list), 
+                                default=arg_val, help=arg_help[arg_name])
+                    else:
+                        f.add_argument(arg_name, type=arg_type, 
+                            default=arg_val, help=arg_help[arg_name])
+            
+        desc = docstring.short_description
+        if desc is None: desc = ''
+
+        if patterns:
+            if not without_prefix:
+                scope_pattern = f"--{patterns[0]}/{prefix}.{key}"
+            else:
+                scope_pattern = f"--{patterns[0]}/{key}"
+        
+            desc += (
+                f" Additional scope patterns: {', '.join(list(patterns))}. "
+                "Use these by prefacing any of the args below with one "
+                "of these patterns. For example: "
+                f"{scope_pattern} VALUE."
+            )
+
+        desc = textwrap.fill(desc, width=HELP_WIDTH)
+        f.description = desc
+    
+    used_args = [x.replace('--', '').split('=')[0] for x in sys.argv if x.startswith('--')]
+    used_args.extend(['args.save', 'args.load'])
+
+    args = vars(p.parse_args())
+    load_args_path = args.pop('args.load')
+    save_args_path = args.pop('args.save')
+    debug_args = args.pop('args.debug')
+    
+    pattern_keys = [key for key in args if '/' in key]
+    top_level_args = [key for key in args if '/' not in key]
+
+    for key in pattern_keys:
+        # If the top-level arguments were altered but the ones
+        # in patterns were not, change the scoped ones to
+        # match the top-level (inherit arguments from top-level).
+        pattern, arg_name = key.split('/')
+        if key not in used_args:
+            args[key] = args[arg_name]
+    
+    if load_args_path:
+        loaded_args = load_args(load_args_path)
+        # Overwrite defaults with things in loaded arguments.
+        # except for things that came from the command line.
+        for key in loaded_args:
+            if key not in used_args:
+                args[key] = loaded_args[key]
+        for key in pattern_keys:
+            pattern, arg_name = key.split('/')
+            if key not in loaded_args and key not in used_args:
+                if arg_name in loaded_args:
+                    args[key] = args[arg_name]
+                
+    for key in top_level_args:
+        if key in used_args:
+            for pattern_key in pattern_keys:
+                pattern, arg_name = pattern_key.split('/')
+                if key == arg_name and pattern_key not in used_args:
+                    args[pattern_key] = args[key]
+
+    if save_args_path:
+        dump_args(args, save_args_path)
+
+    # Put them back in case the script wants to use them
+    args['args.load'] = load_args_path
+    args['args.save'] = save_args_path
+    args['args.debug'] = debug_args
+    
+    return args
