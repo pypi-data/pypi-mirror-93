a6296dca61207be7a19a8a88a4e0f0f789fda12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:53 +0000

    Add package artella-dccs-standalone/1.0.2/artella-dccs-standalone-1.0.2.tar.gz

---
+++ packages/artella-dccs-standalone/artella-dccs-standalone-1.0.2.tar.gz/artella-dccs-standalone-1.0.2/tests/test_client.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains tests for Artella Standalone implementation
+"""
+
+import pytest
+
+
+def test_general():
+    assert True
