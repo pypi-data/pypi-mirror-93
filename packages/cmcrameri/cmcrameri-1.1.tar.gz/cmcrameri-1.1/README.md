169399720d6965b16cc26dde4ad8b8c80677feac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:21 +0000

    Add package cmcrameri/1.1/cmcrameri-1.1.tar.gz

---
+++ packages/cmcrameri/cmcrameri-1.1.tar.gz/cmcrameri-1.1/README.md
@@ -0,0 +1,62 @@
+![Python package](https://github.com/callumrollo/cmcrameri/workflows/Python%20package/badge.svg)
+
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)]()
+[![Python 3.7](https://img.shields.io/badge/python-3.7-blue.svg)]()
+[![Python 3.8](https://img.shields.io/badge/python-3.8-blue.svg)]()
+[![Python 3.9](https://img.shields.io/badge/python-3.9-blue.svg)]()
+
+[![Downloads](https://pepy.tech/badge/cmcrameri)](https://pepy.tech/project/cmcrameri)     (PyPI)
+
+[![Conda Downloads](https://img.shields.io/conda/dn/conda-forge/cmcrameri.svg)](https://anaconda.org/conda-forge/cmcrameri) (conda-forge)
+
+# cmcrameri
+
+This is a Python wrapper around Fabio Crameri's perceptually uniform colour maps
+
+http://www.fabiocrameri.ch/colourmaps.php
+
+All credit for creating the colourmaps to Fabio. Any errors in the Python implementation of colourmaps are my own.
+
+This version is based on Scientific Colourmaps Version 6.0.4 (06.01.2020)
+
+### Install
+
+With pip:
+
+`pip install cmcrameri`
+
+With conda:
+
+```
+conda config --add channels conda-forge
+conda install cmcrameri
+```
+### Usage example
+
+```python
+from cmcrameri import cm
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 100, 100)[None, :]  
+plt.imshow(x, aspect='auto', cmap=cm.batlow) # or any of the other colourmaps made by Fabio Crameri
+plt.axis('off')
+plt.show()
+```
+### Extra instructions
+You can access all the core colourmaps from Fabio Crameri's list by `cm.<colormapname>`
+
+You can use tab autocompletion on `cm` if your editor supports it
+
+For a reversed colourmap, append `_r` to the colourmap name
+
+Categorical colormaps have the suffix `S`
+
+For an image of all the available colourmaps without leaving the comfort of your Python session
+```python
+from cmcrameri.cm import show_cmaps 
+show_cmaps()
+```
+
+### License
+This work is licensed under an [MIT license](https://mit-license.org/).
+
---
+++ packages/cmcrameri/cmcrameri-1.1.tar.gz/cmcrameri-1.1/cmcrameri/cm.py
@@ -0,0 +1,55 @@
+"""
+Perceptually uniform colourmaps for geosciences
+
+Packaging of colourmaps created by Fabio Crameri http://www.fabiocrameri.ch/colourmaps.php
+
+Created by Callum Rollo
+2020-05-06
+"""
+
+import numpy as np
+from pathlib import Path
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+import os
+# Find the colormap text files and make a list of the paths
+text_file_folder = os.path.join(os.path.dirname(__file__), 'cmaps')
+paths = list(Path(text_file_folder).glob('*.txt'))
+crameri_cmaps = dict()
+crameri_cmaps_r = dict()
+crameri_cmaps_s = dict()
+for cmap_path in paths:
+    # Name of colour map taken from text file
+    cmap_name = os.path.split(cmap_path)[1][:-4]
+    cm_data = np.loadtxt(str(cmap_path))
+    # Make a linear segmented colour map
+    if cmap_name[-1] == 'S':
+        crameri_cmaps_s[cmap_name] = LinearSegmentedColormap.from_list(cmap_name, cm_data)
+        continue
+    crameri_cmaps[cmap_name] = LinearSegmentedColormap.from_list(cmap_name, cm_data)
+    # reverse the colour map and add this to the dictionary crameri_cmaps_r, mpt fpr categorical maps
+    crameri_cmaps_r[cmap_name + '_r'] = LinearSegmentedColormap.from_list(cmap_name + '_r', cm_data[::-1, :])
+
+
+def show_cmaps():
+    """
+    A rough function for a quick plot of the colourmaps. Nowhere near as pretty as the original
+    see http://www.fabiocrameri.ch/colourmaps.php
+    :return:
+    """
+    x = np.linspace(0, 100, 100)[None, :]
+    fig, axs = plt.subplots(int(np.ceil(len(crameri_cmaps) / 7)), 7, figsize=(22, 10))
+    fig.subplots_adjust(hspace=.8, wspace=.08)
+    axs = axs.ravel()
+    for ax in axs:
+        ax.axis('off')
+    for c, cmap_selected in enumerate(sorted(crameri_cmaps.keys())):
+        colourmap = crameri_cmaps[cmap_selected]
+        axs[c].pcolor(x, cmap=colourmap)
+        axs[c].text(5, -0.3, cmap_selected, fontsize=30)
+
+
+# So colourmaps can be called in other programs
+locals().update(crameri_cmaps)
+locals().update(crameri_cmaps_r)
+locals().update(crameri_cmaps_s)
