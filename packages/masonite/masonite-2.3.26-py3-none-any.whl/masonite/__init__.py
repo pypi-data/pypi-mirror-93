7067755f80b5fcdd1d6cbed3bd4871fbae7b2d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:23 +0000

    Add package masonite/2.3.26/masonite-2.3.26-py3-none-any.whl

---
+++ packages/masonite/masonite-2.3.26-py3-none-any.whl/masonite/__init__.py
@@ -0,0 +1,23 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+
+from .managers.BroadcastManager import Broadcast
+from .managers.CacheManager import Cache
+from .managers.MailManager import Mail
+from .managers.QueueManager import Queue
+from .managers.SessionManager import Session
+from .managers.UploadManager import Upload
+from .environment import env
+from .__version__ import (
+    __title__,
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __licence__,
+)
+
+_file_source = "masonite"
