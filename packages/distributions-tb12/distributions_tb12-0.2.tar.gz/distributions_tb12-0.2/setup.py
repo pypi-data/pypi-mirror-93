a0b4b4bebd7f8b3654025536677ac4265703768c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:12 +0000

    Add package distributions-tb12/0.2/distributions_tb12-0.2.tar.gz

---
+++ packages/distributions-tb12/distributions_tb12-0.2.tar.gz/distributions_tb12-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_tb12',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_tb12'],
+      zip_safe=False)
