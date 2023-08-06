43ea6e5cee5bee6911fbee49c3413707fcb574e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package spihole/0.0.1/spihole-0.0.1.tar.gz

---
+++ packages/spihole/spihole-0.0.1.tar.gz/spihole-0.0.1/setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(use_scm_version=True)
---
+++ packages/spihole/spihole-0.0.1.tar.gz/spihole-0.0.1/src/spihole/__init__.py
@@ -0,0 +1,3 @@
+from spihole._version import version
+
+__version__ = version
