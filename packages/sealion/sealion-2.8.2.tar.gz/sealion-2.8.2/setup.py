d6735e1b996e84c555629bdac809db0be6d4f23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:34 +0000

    Add package sealion/2.8.2/sealion-2.8.2.tar.gz

---
+++ packages/sealion/sealion-2.8.2.tar.gz/sealion-2.8.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name = 'sealion',
+  packages = setuptools.find_packages(),
+  include_package_data=True,
+  version = '2.8.2',
+  license='MIT',
+  description = 'SeaLion is a simple machine learning and data science library for beginners and ml-engineers alike.',
+  author = 'Anish Lakkapragada',
+  author_email = 'anish.lakkapragada@gmail.com',
+  url = 'https://github.com/anish-lakkapragada/SeaLion',
+  download_url = 'https://github.com/anish-lakkapragada/SeaLion/archive/v2.8.2.tar.gz',
+  keywords = ['Machine Learning', 'Data Science', 'Python'],
+  install_requires=[
+          'numpy',
+          'joblib',
+          'pandas',
+          'scipy',
+          'tqdm',
+          'multiprocess'
+      ],
+  long_description=open('README.txt', 'r').read(),
+  python_requires='>=3.0',
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
