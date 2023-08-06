3cfc1be4d7379868b28efd36984357f9fb8cfd1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package rodder/0.1.3/rodder-0.1.3.tar.gz

---
+++ packages/rodder/rodder-0.1.3.tar.gz/rodder-0.1.3/README.md
@@ -0,0 +1,7 @@
+# rodder
+A distro-independent (or distro-agonistic if you wanna be fancy), non-system package manager with custom repos, similar to Homebrew
+
+# FAQ
+
+## Why "rodder"?
+Because a fishing rod grabs fish, similar to how a package manager grabs packages.
---
+++ packages/rodder/rodder-0.1.3.tar.gz/rodder-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rodder']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rodder',
+    'version': '0.1.3',
+    'description': 'A distro-independant package manager for Linux',
+    'long_description': '# rodder\nA distro-independent (or distro-agonistic if you wanna be fancy), non-system package manager with custom repos, similar to Homebrew\n\n# FAQ\n\n## Why "rodder"?\nBecause a fishing rod grabs fish, similar to how a package manager grabs packages.\n',
+    'author': 'Drake',
+    'author_email': 'mdrakea3@tutanota.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Ruthenic/rodder',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
