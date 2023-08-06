6a3bb1f4b4d269eb5d072ac6a460037ffc3d6546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:00 +0000

    Add package pyais/1.3.1/pyais-1.3.1.tar.gz

---
+++ packages/pyais/pyais-1.3.1.tar.gz/pyais-1.3.1/pyais/__init__.py
@@ -0,0 +1,12 @@
+from pyais.messages import NMEAMessage, AISMessage
+from pyais.stream import TCPStream, FileReaderStream
+
+__license__ = 'MIT'
+__version__ = '1.3.1'
+
+__all__ = (
+    'NMEAMessage',
+    'AISMessage',
+    'TCPStream',
+    'FileReaderStream'
+)
