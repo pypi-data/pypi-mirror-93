c3ab2bdeafff8673030c98147604f4d23677c2d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:07 +0000

    Add package geotagging-alexr96/0.0.23/geotagging-alexr96-0.0.23.tar.gz

---
+++ packages/geotagging-alexr96/geotagging-alexr96-0.0.23.tar.gz/geotagging-alexr96-0.0.23/dpl_geodb/queries.py
@@ -0,0 +1,23 @@
+import psycopg2
+import typing
+from dpl_geodb.geo_ops import config
+
+
+def thumb_querie(id:int) -> tuple:
+    connection = None
+        db_parameters = config()
+        connection = psycopg2.connect(**db_parameters)
+        try:
+            cursor = connection.cursor()
+            select_command = "SELECT * FROM picturereference WHERE id=%(int)s;"
+            cursor.execute(select_command, id)
+            pic_ref = cursor.fetchone()[0]
+        except:
+            return tuple(("error"))
+        finally:
+            if connection is not None:
+                connection.close()
+        return pic_ref
+        
+    
+
