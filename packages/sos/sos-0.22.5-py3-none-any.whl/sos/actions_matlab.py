c0fed4361a8364f89298ff4fccfb309fa1a2ecf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:40 +0000

    Add package sos/0.22.5/sos-0.22.5-py3-none-any.whl

---
+++ packages/sos/sos-0.22.5-py3-none-any.whl/sos/actions_matlab.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
+# Distributed under the terms of the 3-clause BSD License.
+
+from sos.actions import SoS_Action, SoS_ExecuteScript
+
+
+@SoS_Action(acceptable_args=["script", "args"])
+def matlab(
+    script,
+    args='''-nojvm -nodisplay -nosplash -nodesktop -r "try, run('{filename}'), catch me, fprintf('%s / %s\\n',me.identifier,me.message), exit(1), end, exit(0);"''',
+    **kwargs
+):
+    """Execute specified script with command Matlab, with default options
+    "-nojvm -nodisplay -nosplash -nodesktop -r". This action accepts common action arguments such as input,
+    active, workdir, docker_image and args. In particular, content of one or more
+    files  specified by option input would be
+    prepended before the specified script.
+    """
+    return SoS_ExecuteScript(script, "matlab", ".m", args).run(**kwargs)
+
+
+@SoS_Action(acceptable_args=["script", "args"])
+def octave(script, args="", **kwargs):
+    """Execute specified script with command Matlab, with default options
+    "-nodisplay -r". This action accepts common action arguments such as input,
+    active, workdir, docker_image and args. In particular, content of one or more
+    files  specified by option input would be
+    prepended before the specified script.
+    """
+    return SoS_ExecuteScript(script, "octave", ".m", args).run(**kwargs)
