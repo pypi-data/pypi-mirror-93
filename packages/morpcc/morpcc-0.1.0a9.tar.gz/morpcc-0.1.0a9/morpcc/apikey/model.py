792f051e632392357add70ae360d25bbec22844b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:37 +0000

    Add package morpcc/0.1.0a9/morpcc-0.1.0a9.tar.gz

---
+++ packages/morpcc/morpcc-0.1.0a9.tar.gz/morpcc-0.1.0a9/morpcc/apikey/model.py
@@ -0,0 +1,19 @@
+from ..crud.model import CollectionUI, ModelUI
+
+
+class APIKeyModelUI(ModelUI):
+
+    edit_include_fields = ["name"]
+
+
+class APIKeyCollectionUI(CollectionUI):
+
+    modelui_class = APIKeyModelUI
+
+    create_include_fields = ["name"]
+
+    columns = [
+        {"title": "Name", "name": "name"},
+        {"title": "Identity", "name": "api_identity"},
+        {"title": "Actions", "name": "structure:buttons"},
+    ]
