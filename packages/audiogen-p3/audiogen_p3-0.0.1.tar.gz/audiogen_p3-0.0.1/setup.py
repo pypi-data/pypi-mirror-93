207585fb0350d91470867aa4f28496cf9aba87d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:34 +0000

    Add package audiogen-p3/0.0.1/audiogen_p3-0.0.1.tar.gz

---
+++ packages/audiogen-p3/audiogen_p3-0.0.1.tar.gz/audiogen_p3-0.0.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import pathlib
+
+required_modules = []
+extras_require = {
+	'soundcard_playback': ['pyaudio'],
+}
+
+here				= pathlib.Path(__file__).parent.resolve()
+long_description	= ( here / 'README.rst' ).read_text( encoding='utf-8' )
+
+setup(
+	name="audiogen_p3",
+	version="0.0.1",
+	description="Generator based tools for working with audio clips.",
+	author="Ryan Guisewite",
+	author_email="ryan.guisewite@gmail.com",
+	url="https://github.com/rguisewite/audiogen_p3",
+
+	packages=find_packages(exclude='tests'),
+	install_requires=required_modules,
+	extras_require=extras_require,
+
+	tests_require=["nose"],
+	test_suite="nose.collector",
+
+	long_description=long_description,
+	classifiers=[
+		"License :: OSI Approved :: BSD License",
+		"Programming Language :: Python :: 3.9",
+		"Intended Audience :: Developers",
+		"Topic :: Multimedia :: Sound/Audio",
+	],
+    python_requires='>=3.6',
+)
+
