c10ef5c441cc003b20219aece2fe6b1e1b500ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:19 +0000

    Add package setu/0.5.3/setu-0.5.3.tar.gz

---
+++ packages/setu/setu-0.5.3.tar.gz/setu-0.5.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
+setup(
+    name='setu',
+    version='0.5.3',
+    url='https://github.com/SetuHQ/setu-python-sdk',
+    author='Setu',
+    author_email='dev.support@setu.co',
+    description="SDK to help Pythonistas integrate with Setu's APIs",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    install_requires=["requests", "uuid", "PyJWT"],
+    packages=['setu'],
+    zip_safe=False
+)
