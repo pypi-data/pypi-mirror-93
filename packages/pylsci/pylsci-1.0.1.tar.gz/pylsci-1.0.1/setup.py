b8f3a3d19c2c58c68b817c4949c6f4a679e0393e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:44 +0000

    Add package pylsci/1.0.1/pylsci-1.0.1.tar.gz

---
+++ packages/pylsci/pylsci-1.0.1.tar.gz/pylsci-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# build
+# python setup.py sdist bdist_wheel
+# twine upload --repository testpypi dist/* or twine upload dist/*
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pylsci',
+    version='1.0.1',
+    author='Pascal Keilbach',
+    author_email='python@pk17.org',
+    description='Python Package for Laser Speckle Contrast Imaging',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pkeilbach/pylsci',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
