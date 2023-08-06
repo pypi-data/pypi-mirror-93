229ff3edeeffe58bb3e94c5fe10a036d22933d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package hg-git/0.10.0/hg_git-0.10.0-py2-none-any.whl

---
+++ packages/hg-git/hg_git-0.10.0-py2-none-any.whl/hggit/gitrepo.py
@@ -0,0 +1,162 @@
+from __future__ import absolute_import, print_function
+
+from .util import isgitsshuri
+from mercurial import (
+    error,
+    util
+)
+
+from . import compat
+
+peerapi = False
+try:
+    from mercurial.interfaces.repository import peer as peerrepository
+    peerapi = True
+except ImportError:
+    try:
+        from mercurial.repository import peer as peerrepository
+        peerapi = True
+    except ImportError:
+        from mercurial.peer import peerrepository
+
+
+class basegitrepo(peerrepository):
+    def __init__(self, ui, path, create, intents=None, **kwargs):
+        if create:  # pragma: no cover
+            raise error.Abort(b'Cannot create a git repository.')
+        self._ui = ui
+        self.path = path
+        self.localrepo = None
+
+    _peercapabilities = [b'lookup']
+
+    def _capabilities(self):
+        return self._peercapabilities
+
+    def capabilities(self):
+        return self._peercapabilities
+
+    @property
+    def ui(self):
+        return self._ui
+
+    def url(self):
+        return self.path
+
+    def lookup(self, key):
+        assert isinstance(key, bytes)
+        # FIXME: this method is supposed to return a 20-byte node hash
+        return key
+
+    def local(self):
+        if not self.path:
+            raise error.RepoError
+
+    def heads(self):
+        return []
+
+    def listkeys(self, namespace):
+        if namespace == b'namespaces':
+            return {b'bookmarks': b''}
+        elif namespace == b'bookmarks':
+            if self.localrepo is not None:
+                handler = self.localrepo.githandler
+                result = handler.fetch_pack(self.path, heads=[])
+                # map any git shas that exist in hg to hg shas
+                stripped_refs = {
+                    ref[11:]: handler.map_hg_get(val) or val
+                    for ref, val in compat.iteritems(result.refs)
+                    if ref.startswith(b'refs/heads/')
+                }
+                return stripped_refs
+        return {}
+
+    def pushkey(self, namespace, key, old, new):
+        return False
+
+    if peerapi:
+        def branchmap(self):
+            raise NotImplementedError
+
+        def canpush(self):
+            return True
+
+        def close(self):
+            pass
+
+        def debugwireargs(self):
+            raise NotImplementedError
+
+        def getbundle(self):
+            raise NotImplementedError
+
+        def iterbatch(self):
+            raise NotImplementedError
+
+        def known(self):
+            raise NotImplementedError
+
+        def peer(self):
+            return self
+
+        def stream_out(self):
+            raise NotImplementedError
+
+        def unbundle(self):
+            raise NotImplementedError
+
+try:
+    from mercurial.wireprotov1peer import (
+        batchable,
+        future,
+        peerexecutor,
+    )
+except ImportError:
+    # compat with <= hg-4.8
+    gitrepo = basegitrepo
+else:
+    class gitrepo(basegitrepo):
+
+        @batchable
+        def lookup(self, key):
+            f = future()
+            yield {}, f
+            yield super(gitrepo, self).lookup(key)
+
+        @batchable
+        def heads(self):
+            f = future()
+            yield {}, f
+            yield super(gitrepo, self).heads()
+
+        @batchable
+        def listkeys(self, namespace):
+            f = future()
+            yield {}, f
+            yield super(gitrepo, self).listkeys(namespace)
+
+        @batchable
+        def pushkey(self, namespace, key, old, new):
+            f = future()
+            yield {}, f
+            yield super(gitrepo, self).pushkey(key, old, new)
+
+        def commandexecutor(self):
+            return peerexecutor(self)
+
+        def _submitbatch(self, req):
+            for op, argsdict in req:
+                yield None
+
+        def _submitone(self, op, args):
+            return None
+
+instance = gitrepo
+
+
+def islocal(path):
+    if isgitsshuri(path):
+        return True
+
+    u = util.url(path)
+    return not u.scheme or u.scheme == b'file'
---
+++ packages/hg-git/hg_git-0.10.0-py2-none-any.whl/hggit/verify.py
@@ -0,0 +1,100 @@
+# verify.py - verify Mercurial revisions
+#
+# Copyright 2014 Facebook.
+#
+# This software may be used and distributed according to the terms
+# of the GNU General Public License, incorporated herein by reference.
+
+from __future__ import absolute_import, print_function
+
+import stat
+
+from mercurial import error
+from mercurial.i18n import _
+
+from dulwich import diff_tree
+from dulwich.objects import Commit, S_IFGITLINK
+
+from . import compat
+
+
+def verify(ui, repo, hgctx):
+    '''verify that a Mercurial rev matches the corresponding Git rev
+
+    Given a Mercurial revision that has a corresponding Git revision in the
+    map, this attempts to answer whether that revision has the same contents as
+    the corresponding Git revision.
+
+    '''
+    handler = repo.githandler
+
+    gitsha = handler.map_git_get(hgctx.hex())
+    if not gitsha:
+        # TODO deal better with commits in the middle of octopus merges
+        raise error.Abort(_(b'no git commit found for rev %s') % hgctx,
+                          hint=_(b'if this is an octopus merge, '
+                                 b'verify against the last rev'))
+
+    try:
+        gitcommit = handler.git.get_object(gitsha)
+    except KeyError:
+        raise error.Abort(_(b'git equivalent %s for rev %s not found!') %
+                          (gitsha, hgctx))
+    if not isinstance(gitcommit, Commit):
+        raise error.Abort(_(b'git equivalent %s for rev %s is not a commit!') %
+                          (gitsha, hgctx))
+
+    ui.status(_(b'verifying rev %s against git commit %s\n') % (hgctx, gitsha))
+    failed = False
+
+    # TODO check commit message and other metadata
+
+    dirkind = stat.S_IFDIR
+
+    hgfiles = set(hgctx)
+    # TODO deal with submodules
+    hgfiles.discard(b'.hgsubstate')
+    hgfiles.discard(b'.hgsub')
+    gitfiles = set()
+
+    with compat.makeprogress(ui, b'verify', total=len(hgfiles)) as progress:
+        for gitfile, dummy in diff_tree.walk_trees(handler.git.object_store,
+                                                   gitcommit.tree, None):
+            if gitfile.mode == dirkind:
+                continue
+            # TODO deal with submodules
+            if (gitfile.mode == S_IFGITLINK or gitfile.path == b'.hgsubstate' or
+                gitfile.path == b'.hgsub'):
+                continue
+            progress.increment()
+            gitfiles.add(gitfile.path)
+
+            try:
+                fctx = hgctx[gitfile.path]
+            except error.LookupError:
+                # we'll deal with this at the end
+                continue
+
+            hgflags = fctx.flags()
+            gitflags = handler.convert_git_int_mode(gitfile.mode)
+            if hgflags != gitflags:
+                ui.write(_(b"file has different flags: %s (hg '%s', git '%s')\n") %
+                         (gitfile.path, hgflags, gitflags))
+                failed = True
+            if fctx.data() != handler.git[gitfile.sha].data:
+                ui.write(_(b'difference in: %s\n') % gitfile.path)
+                failed = True
+
+    if hgfiles != gitfiles:
+        failed = True
+        missing = gitfiles - hgfiles
+        for f in sorted(missing):
+            ui.write(_(b'file found in git but not hg: %s\n') % f)
+        unexpected = hgfiles - gitfiles
+        for f in sorted(unexpected):
+            ui.write(_(b'file found in hg but not git: %s\n') % f)
+
+    if failed:
+        return 1
+    else:
+        return 0
