4dc21f7e53023b65469d126f10a9e78b1a2d972c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package nabg/0.0.1/nabg-0.0.1.tar.gz

---
+++ packages/nabg/nabg-0.0.1.tar.gz/nabg-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nabg",
+    version="0.0.1",
+    description="A customizable bullshit generator based on Seb Pearce's 'New-Age Bullshit Generator'",
+    py_modules=["nabg", "patterns", "vocabulary"],
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["click > 7.0"],
+    extras_require={"dev": ["pytest>=6.2.2"]},
+    url="https://github.com/naveen-u/nabg",
+    author="Naveen Unnikrishnan",
+    author_email="naveenunnikrishnan98@gmail.com",
+    entry_points={
+        "console_scripts": [
+            "nabg=nabg:main",
+        ]
+    },
+)
