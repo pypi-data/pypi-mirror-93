45e9202916691e2ce2579a96353b55f3f6e13438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:56 +0000

    Add package jaraco.develop/7.6.0/jaraco.develop-7.6.0.tar.gz

---
+++ packages/jaraco.develop/jaraco.develop-7.6.0.tar.gz/jaraco.develop-7.6.0/jaraco/develop/add-github-secrets.py
@@ -0,0 +1,35 @@
+import autocommand
+import keyring
+import getpass
+
+import jaraco.context
+
+from . import github
+
+
+@jaraco.context.suppress(Exception)
+def _safe_getuser():
+    """
+    getuser chokes by design in some environments.
+    """
+    return getpass.getuser()
+
+
+secret_sources = {
+    'PYPI_TOKEN': dict(
+        username='__token__',
+        service_name='https://upload.pypi.org/legacy/',
+    ),
+    'TIDELIFT_TOKEN': dict(
+        username=_safe_getuser(),
+        service_name='https://api.tidelift.com/external-api/',
+    ),
+}
+
+
+@autocommand.autocommand(__name__)
+def run(project: github.Repo = github.Repo.detect()):
+    for name in project.find_needed_secrets():
+        source = secret_sources[name]
+        value = keyring.get_password(**source)
+        project.add_secret(name, value)
