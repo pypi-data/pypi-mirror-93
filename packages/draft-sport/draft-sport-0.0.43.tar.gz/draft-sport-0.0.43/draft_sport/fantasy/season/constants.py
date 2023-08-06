a2f75774196d2595b0d835f7df38562958d16870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package draft-sport/0.0.43/draft-sport-0.0.43.tar.gz

---
+++ packages/draft-sport/draft-sport-0.0.43.tar.gz/draft-sport-0.0.43/draft_sport/fantasy/season/constants.py
@@ -0,0 +1,25 @@
+"""
+Draft Sport Python
+Season Constants Module
+author: hugh@blinkybeach.com
+"""
+from draft_sport.fantasy.season.season import Season
+from draft_sport.fantasy.competition.constants import CompetitionConstants
+from nozomi import NozomiDate
+
+
+class SeasonConstants:
+
+    DRAFT_RUGBY_2020 = Season(
+        public_id='draft_rugby_2020',
+        name='Draft Rugby 2020',
+        start_date=NozomiDate(year=2020, month=7, day=4),
+        competition=CompetitionConstants.DRAFT_RUGBY_2020
+    )
+
+    DRAFT_RUGBY_2020_PRE_COVID = Season(
+        public_id='draft_rugby_2020_1',
+        name='Draft Rugby 2020 (Pre-Covid)',
+        start_date=NozomiDate(year=2020, month=2, day=5),
+        competition=CompetitionConstants.DRAFT_RUGBY_2020_PRE_COVID
+    )
