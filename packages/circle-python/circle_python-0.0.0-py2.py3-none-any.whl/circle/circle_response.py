fb8060c9b9e514fb3f5406d395c07a2256adb07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:24 +0000

    Add package circle-python/0.0.0/circle_python-0.0.0-py2.py3-none-any.whl

---
+++ packages/circle-python/circle_python-0.0.0-py2.py3-none-any.whl/circle/circle_response.py
@@ -0,0 +1,17 @@
+import json
+
+
+class CircleResponse:
+    def __init__(self, body, code, headers):
+        self.body = body
+        self.code = code
+        self.headers = headers
+
+        self.data = json.loads(body)
+
+    @property
+    def idempotency_key(self):
+        try:
+            return self.headers["idempotency-key"]
+        except KeyError:
+            return None
