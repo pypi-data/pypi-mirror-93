4925aad9abf6a3c0098b0cc6dbc79839261f5f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:56 +0000

    Add package distest/0.5.1/distest-0.5.1.tar.gz

---
+++ packages/distest/distest-0.5.1.tar.gz/distest-0.5.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="distest",
+    version="0.5.1",
+    description="Automate the testing of discord bots... With discord bots!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/JakeCover/distest",
+    author="Jake Cover",
+    author_email="python@jakecover.me",
+    license="MIT",
+    packages=["distest", "distest.TestInterface"],
+    install_requires=["discord.py>=1.5.0"],
+    zip_safe=False,
+    classifiers=["Topic :: Software Development :: Testing :: Unit"],
+    keywords=[
+        "Discord",
+        "Discord.py",
+        "Unit Test",
+        "Test",
+        "Distest",
+        "Discord Testing",
+    ],
+)
