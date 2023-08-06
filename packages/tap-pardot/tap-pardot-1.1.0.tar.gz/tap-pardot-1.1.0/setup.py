9ea05fa1806cb70739bf151900839e327b6e3903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:23 +0000

    Add package tap-pardot/1.1.0/tap-pardot-1.1.0.tar.gz

---
+++ packages/tap-pardot/tap-pardot-1.1.0.tar.gz/tap-pardot-1.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-pardot",
+    version="1.1.0",
+    description="Singer.io tap for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_pardot"],
+    install_requires=["singer-python==5.8.0", "requests==2.22.0", "backoff==1.8.0"],
+    entry_points="""
+    [console_scripts]
+    tap-pardot=tap_pardot:main
+    """,
+    packages=["tap_pardot"],
+    package_data={"schemas": ["tap_pardot/schemas/*.json"]},
+    include_package_data=True,
+)
