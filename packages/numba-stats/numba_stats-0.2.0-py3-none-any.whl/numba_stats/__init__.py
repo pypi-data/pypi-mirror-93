cd5dfdd7461f6b621d41c5361c924b94d51997e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package numba-stats/0.2.0/numba_stats-0.2.0-py3-none-any.whl

---
+++ packages/numba-stats/numba_stats-0.2.0-py3-none-any.whl/numba_stats/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.2.0"
+
+from .stats import (  # noqa
+    norm_pdf,
+    norm_cdf,
+    norm_ppf,
+    poisson_pmf,
+    poisson_cdf,
+    expon_pdf,
+    expon_cdf,
+    expon_ppf,
+    t_pdf,
+    t_cdf,
+    t_ppf,
+)
