ed2dc2611a606733967cec580ee161a9b858e6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:42 +0000

    Add package auto-labeling-pipeline/0.1.5/auto_labeling_pipeline-0.1.5-py3-none-any.whl

---
+++ packages/auto-labeling-pipeline/auto_labeling_pipeline-0.1.5-py3-none-any.whl/auto_labeling_pipeline/labels.py
@@ -0,0 +1,42 @@
+import abc
+from typing import Dict, List, Optional, Set
+
+from auto_labeling_pipeline.label import Label
+
+
+class Labels(metaclass=abc.ABCMeta):
+
+    def __init__(self, labels: List[Label]):
+        self.labels = labels
+
+    def filter_by_name(self, stop_labels: Optional[Set[str]] = None) -> 'Labels':
+        if not stop_labels:
+            return self
+        labels = [label for label in self.labels if not label.included(stop_labels)]
+        return self.__class__(labels)
+
+    def replace_label(self, mapping: Optional[Dict[str, str]] = None) -> 'Labels':
+        if not mapping:
+            return self
+        self.labels = [label.replace(mapping) for label in self.labels]
+        return self.__class__(self.labels)
+
+    def dict(self) -> List[dict]:
+        return [label.dict() for label in self.labels]
+
+
+class ClassificationLabels(Labels):
+    pass
+
+
+class SequenceLabels(Labels):
+    pass
+
+
+class Seq2seqLabels(Labels):
+
+    def filter_by_name(self, stop_labels: Optional[Set[str]] = None) -> Labels:
+        return self
+
+    def replace_label(self, mapping: Optional[Dict[str, str]] = None) -> Labels:
+        return self
