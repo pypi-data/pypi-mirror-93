236625ad41c9cb1130da9f7bc249f02d79a69c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:56 +0000

    Add package python-opsramp/1.2.7/python_opsramp-1.2.7-py2.py3-none-any.whl

---
+++ packages/python-opsramp/python_opsramp-1.2.7-py2.py3-none-any.whl/opsramp/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#
+# (c) Copyright 2019-2021 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import warnings
+import sys
+if sys.version_info < (3,):
+    warnings.warn(
+        "Python 2 reached the end of its life at the end of 2019.\n"
+        "Please move to Python 3 because this module will drop support "
+        "on {eod}.".format(
+            eod='31-Jan-2021'
+        ), DeprecationWarning)
