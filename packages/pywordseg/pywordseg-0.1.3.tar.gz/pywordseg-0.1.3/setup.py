cc7d8da32214da6e9035ac28c735393dca114d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:57 +0000

    Add package pywordseg/0.1.3/pywordseg-0.1.3.tar.gz

---
+++ packages/pywordseg/pywordseg-0.1.3.tar.gz/pywordseg-0.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+  name="pywordseg",
+  version="0.1.3",
+  author="Jexus Chuang",
+  description="Open source state-of-the-art Chinese word segmentation toolkit",
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url="https://github.com/voidism/pywordseg",
+  packages=setuptools.find_packages(),
+  license='MIT',
+  install_requires=[
+    "torch",
+    "h5py",
+    "numpy",
+    "overrides",
+  ],
+  classifiers=[
+    "Development Status :: 2 - Pre-Alpha",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.6",
+  ],
+  exclude_package_date={'':['.gitignore','.git','models','ELMoForManyLangs','CharEmb']}
+)
