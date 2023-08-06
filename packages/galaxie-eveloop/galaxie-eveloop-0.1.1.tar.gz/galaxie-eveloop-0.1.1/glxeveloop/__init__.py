3a8144f4027ba5cd44f240cad788c0dd0a1a1c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:50 +0000

    Add package galaxie-eveloop/0.1.1/galaxie-eveloop-0.1.1.tar.gz

---
+++ packages/galaxie-eveloop/galaxie-eveloop-0.1.1.tar.gz/galaxie-eveloop-0.1.1/glxeveloop/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# It script it publish under GNU GENERAL PUBLIC LICENSE
+# http://www.gnu.org/licenses/gpl-3.0.en.html
+# Author: the Galaxie EveLoop Team, all rights reserved
+
+APPLICATION_VERSION = "0.1.1"
+APPLICATION_AUTHORS = ["Tuxa", "Mo"]
+APPLICATION_NAME = "Galaxie EveLoop"
+APPLICATION_COPYRIGHT = "2016-2021 - Galaxie EveLoop Team all right reserved"
+__all__ = [
+    "EventList",
+    "EventBus",
+    "MainLoop",
+]
+from glxeveloop.event_list import EventList
+from glxeveloop.event_bus import EventBus
+from glxeveloop.main_loop import MainLoop
