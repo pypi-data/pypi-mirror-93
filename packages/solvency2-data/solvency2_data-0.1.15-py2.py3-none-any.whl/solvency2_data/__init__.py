7a4ab336c3e618dfc780c7b9b4ae3fe297a217b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:12 +0000

    Add package solvency2-data/0.1.15/solvency2_data-0.1.15-py2.py3-none-any.whl

---
+++ packages/solvency2-data/solvency2_data-0.1.15-py2.py3-none-any.whl/solvency2_data/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for solvency2-data."""
+
+__author__ = """Willem Jan Willemse"""
+__email__ = 'w.j.willemse@xs4all.nl'
+__version__ = '0.1.15'
+
+from .rfr import *
