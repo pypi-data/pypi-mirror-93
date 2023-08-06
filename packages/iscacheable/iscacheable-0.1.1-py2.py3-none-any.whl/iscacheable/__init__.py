decaa11d768a9a95b798b20ab8c63bf213be8cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:53 +0000

    Add package iscacheable/0.1.1/iscacheable-0.1.1-py2.py3-none-any.whl

---
+++ packages/iscacheable/iscacheable-0.1.1-py2.py3-none-any.whl/iscacheable/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#
+# iscacheable - A simple tool to determine if a URL is cacheable or not.
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: MIT
+#
+
+from iscacheable.cli import determineCacheability
+from iscacheable.__version__ import (
+    __title__,
+    __version__,
+    __license__,
+    __author__,
+    __contact__,
+    __url__,
+    __description__)
