df6122b4c44107462cf6854758c5f4dfb24ab2fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:13 +0000

    Add package tezign/0.1.2/tezign-0.1.2.tar.gz

---
+++ packages/tezign/tezign-0.1.2.tar.gz/tezign-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='tezign',
+    version='0.1.2',
+    description='Common python libs by Tezign',
+    author='Anoyi',
+    author_email='anoyi@qq.com',
+    py_modules=['tezign'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
