f1fc778ff81b131eaa9d5e41e6ffefa7a8153f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:25 +0000

    Add package FortiCare/1.0.0/FortiCare-1.0.0.tar.gz

---
+++ packages/FortiCare/FortiCare-1.0.0.tar.gz/FortiCare-1.0.0/setup-forticare.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("FortiCare/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("VERSION", "r", encoding="utf-8") as fh:
+    version = fh.read()
+
+setuptools.setup(
+    name="FortiCare",
+    version=version,
+    author="See \"Authors\" section",
+    author_email="",
+    description="FortiCare management package for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://test.pypi.org/project/FortiCare/",
+    packages=["FortiCare"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+       'requests',
+       'python-dateutil',
+       'PyPDF2',
+       'tabulate',
+       'filelock',
+       'validators'
+   ]
+)
+
+#  python3 setup.py sdist bdist_wheel
