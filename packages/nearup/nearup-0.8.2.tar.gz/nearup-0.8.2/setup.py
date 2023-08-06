dfd47f5f5d93f5eceba6383cf058f06cd42ae46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:26 +0000

    Add package nearup/0.8.2/nearup-0.8.2.tar.gz

---
+++ packages/nearup/nearup-0.8.2.tar.gz/nearup-0.8.2/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("nearuplib/VERSION", "r") as fh:
+    version = fh.read().strip()
+
+setuptools.setup(
+    name="nearup",  # Replace with your own username
+    version=version,
+    author="Near Inc",
+    author_email="hello@near.org",
+    description="Public scripts to launch near blockchain nodes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/near/nearup",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha", "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux", "Topic :: System :: Clustering",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Networking"
+    ],
+    install_requires=['boto3', 'click', 'psutil'],
+    python_requires='>=3.6',
+    include_package_data=True,
+    scripts=['nearup', 'watcher'],
+)
