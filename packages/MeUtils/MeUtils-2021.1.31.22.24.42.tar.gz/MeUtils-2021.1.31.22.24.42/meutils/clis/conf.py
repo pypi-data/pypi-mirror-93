52b6a9c8b8d19384120ef563c09b1a6685cba2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:47 +0000

    Add package MeUtils/2021.1.31.22.24.42/MeUtils-2021.1.31.22.24.42.tar.gz

---
+++ packages/MeUtils/MeUtils-2021.1.31.22.24.42.tar.gz/MeUtils-2021.1.31.22.24.42/meutils/clis/conf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : conf
+# @Time         : 2021/1/31 10:20 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.pipe import *
+
+
+class TrainConf(BaseConfig):
+    epoch = 10
+    batch_size = 128
+
+
+def train(**kwargs):
+    logger.info("开始训练")
+    time.sleep(3)
+
+
+def run(**kwargs):
+    logger.info(f"kwargs: {kwargs}")
+    c = TrainConf.parse_obj(kwargs)
+    train(**c.dict())
+
+
+conf_cli = lambda: fire.Fire(run)
