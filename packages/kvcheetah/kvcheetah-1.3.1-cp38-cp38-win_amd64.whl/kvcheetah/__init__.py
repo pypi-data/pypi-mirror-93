77cd350637be0fd415dd8946eb0d7a9391de1309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:31 +0000

    Add package kvcheetah/1.3.1/kvcheetah-1.3.1-cp38-cp38-win_amd64.whl

---
+++ packages/kvcheetah/kvcheetah-1.3.1-cp38-cp38-win_amd64.whl/kvcheetah/__init__.py
@@ -0,0 +1,16 @@
+"""kvcheetah
+
+A lightweight game engine based on Kivy.
+"""
+
+#Globals
+#==============================================================================
+__author__ = "Eric Snyder"
+__copyright__ = "Copyright (c) 2021 by Eric Snyder"
+__license__ = "MIT"
+__version__ = "1.3.1"
+
+
+#Make sure widget classes get imported and initialized
+#==============================================================================
+import joystick
