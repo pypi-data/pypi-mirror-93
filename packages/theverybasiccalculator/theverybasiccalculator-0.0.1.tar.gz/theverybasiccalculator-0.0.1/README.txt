4f1236ae41b6e86afe42af088180b793588263c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package theverybasiccalculator/0.0.1/theverybasiccalculator-0.0.1.tar.gz

---
+++ packages/theverybasiccalculator/theverybasiccalculator-0.0.1.tar.gz/theverybasiccalculator-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is very simple calculator can execute the 4 basic operations
---
+++ packages/theverybasiccalculator/theverybasiccalculator-0.0.1.tar.gz/theverybasiccalculator-0.0.1/__init__.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    return x+y
+
+def substract(x,y):
+    x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+print(add(3,9))
