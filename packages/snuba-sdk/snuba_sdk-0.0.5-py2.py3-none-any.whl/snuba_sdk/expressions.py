475909ea702dc70fe05d59f14b255613abac9be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:53 +0000

    Add package snuba-sdk/0.0.5/snuba_sdk-0.0.5-py2.py3-none-any.whl

---
+++ packages/snuba-sdk/snuba_sdk-0.0.5-py2.py3-none-any.whl/snuba_sdk/expressions.py
@@ -0,0 +1,211 @@
+import re
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import date, datetime
+from enum import Enum
+from typing import Any, List, Optional, Sequence, Set, Union
+
+from snuba_sdk.snuba import check_array_type, is_aggregation_function
+
+
+class InvalidExpression(Exception):
+    pass
+
+
+class Expression(ABC):
+    def __post_init__(self) -> None:
+        self.validate()
+
+    @abstractmethod
+    def validate(self) -> None:
+        raise NotImplementedError
+
+
+# For type hinting
+ScalarLiteralType = Union[None, bool, str, bytes, float, int, date, datetime]
+ScalarSequenceType = Sequence[ScalarLiteralType]
+ScalarType = Union[ScalarLiteralType, ScalarSequenceType]
+
+# For type checking
+Scalar: Set[type] = {
+    type(None),
+    bool,
+    str,
+    bytes,
+    float,
+    int,
+    date,
+    datetime,
+}
+
+
+class InvalidArray(Exception):
+    def __init__(self, value: List[Any]) -> None:
+        value_str = f"{value}"
+        if len(value_str) > 10:
+            value_str = f"{value_str[:10]}...]"
+
+        super().__init__(
+            f"invalid array {value_str}: arrays must have the same data type or None, perhaps use a tuple instead"
+        )
+
+
+def is_scalar(value: Any) -> bool:
+    if isinstance(value, tuple(Scalar)):
+        return True
+    elif isinstance(value, tuple):
+        if not all(is_scalar(v) for v in value):
+            raise InvalidExpression("tuple must contain only scalar values")
+        return True
+    elif isinstance(value, list):
+        if not check_array_type(value):
+            raise InvalidArray(value)
+
+        return True
+
+    return False
+
+
+column_name_re = re.compile(r"^[a-zA-Z][a-zA-Z_.]+$")
+
+
+def _validate_int_literal(
+    name: str, literal: int, minn: Optional[int], maxn: Optional[int]
+) -> None:
+    if not isinstance(literal, int):
+        raise InvalidExpression(f"{name} '{literal}' must be an integer")
+    if minn is not None and literal < minn:
+        raise InvalidExpression(f"{name} '{literal}' must be at least {minn:,}")
+    elif maxn is not None and literal > maxn:
+        raise InvalidExpression(f"{name} '{literal}' is capped at {maxn:,}")
+
+
+@dataclass(frozen=True)
+class Limit(Expression):
+    limit: int
+
+    def validate(self) -> None:
+        _validate_int_literal("limit", self.limit, 1, 10000)
+
+
+@dataclass(frozen=True)
+class Offset(Expression):
+    offset: int
+
+    def validate(self) -> None:
+        _validate_int_literal("offset", self.offset, 0, None)
+
+
+@dataclass(frozen=True)
+class Granularity(Expression):
+    granularity: int
+
+    def validate(self) -> None:
+        _validate_int_literal("granularity", self.granularity, 1, None)
+
+
+@dataclass(frozen=True)
+class Totals(Expression):
+    totals: bool = False
+
+    def validate(self) -> None:
+        if not isinstance(self.totals, bool):
+            raise InvalidExpression("totals must be a boolean")
+
+
+@dataclass(frozen=True)
+class Column(Expression):
+    name: str
+
+    def validate(self) -> None:
+        if not isinstance(self.name, str):
+            raise InvalidExpression(f"column '{self.name}' must be a string")
+            self.name = str(self.name)
+        if not column_name_re.match(self.name):
+            raise InvalidExpression(
+                f"column '{self.name}' is empty or contains invalid characters"
+            )
+
+
+@dataclass(frozen=True)
+class Function(Expression):
+    function: str
+    parameters: Sequence[Union[ScalarType, Column, "Function"]]
+    alias: Optional[str] = None
+
+    def is_aggregate(self) -> bool:
+        if is_aggregation_function(self.function):
+            return True
+
+        for param in self.parameters:
+            if isinstance(param, Function) and param.is_aggregate():
+                return True
+
+        return False
+
+    def validate(self) -> None:
+        if not isinstance(self.function, str):
+            raise InvalidExpression(f"function '{self.function}' must be a string")
+        if self.function == "":
+            # TODO: Have a whitelist of valid functions to check, maybe even with more
+            # specific parameter type checking
+            raise InvalidExpression("function cannot be empty")
+        if not column_name_re.match(self.function):
+            raise InvalidExpression(
+                f"function '{self.function}' contains invalid characters"
+            )
+
+        if self.alias is not None:
+            if not isinstance(self.alias, str) or self.alias == "":
+                raise InvalidExpression(
+                    f"alias '{self.alias}' of function {self.function} must be None or a non-empty string"
+                )
+            if not column_name_re.match(self.alias):
+                raise InvalidExpression(
+                    f"alias '{self.alias}' of function {self.function} contains invalid characters"
+                )
+
+        for param in self.parameters:
+            if not isinstance(param, (Column, Function, *Scalar)):
+                assert not isinstance(param, bytes)  # mypy
+                raise InvalidExpression(
+                    f"parameter '{param}' of function {self.function} is an invalid type"
+                )
+
+    def __eq__(self, other: object) -> bool:
+        # Don't use the alias to compare equality
+        if not isinstance(other, Function):
+            return False
+
+        return self.function == other.function and self.parameters == other.parameters
+
+
+class Direction(Enum):
+    ASC = "ASC"
+    DESC = "DESC"
+
+
+@dataclass(frozen=True)
+class OrderBy(Expression):
+    exp: Union[Column, Function]
+    direction: Direction
+
+    def validate(self) -> None:
+        if not isinstance(self.exp, (Column, Function)):
+            raise InvalidExpression("OrderBy expression must be a Column or Function")
+        if not isinstance(self.direction, Direction):
+            raise InvalidExpression("OrderBy direction must be a Direction")
+
+
+@dataclass(frozen=True)
+class LimitBy(Expression):
+    column: Column
+    count: int
+
+    def validate(self) -> None:
+        if not isinstance(self.column, Column):
+            raise InvalidExpression("LimitBy can only be used on a Column")
+        if not isinstance(self.count, int) or self.count <= 0 or self.count > 10000:
+            raise InvalidExpression(
+                "LimitBy count must be a positive integer (max 10,000)"
+            )
---
+++ packages/snuba-sdk/snuba_sdk-0.0.5-py2.py3-none-any.whl/snuba_sdk/visitors.py
@@ -0,0 +1,165 @@
+import re
+from abc import ABC, abstractmethod
+from datetime import date, datetime
+from typing import Generic, TypeVar
+
+from snuba_sdk.entity import Entity
+from snuba_sdk.conditions import Condition
+from snuba_sdk.expressions import (
+    Column,
+    Expression,
+    Function,
+    Granularity,
+    InvalidExpression,
+    is_scalar,
+    Limit,
+    LimitBy,
+    Offset,
+    OrderBy,
+    Scalar,
+    ScalarType,
+    Totals,
+)
+
+
+# validation regexes
+unescaped_quotes = re.compile(r"(?<!\\)'")
+unescaped_newline = re.compile(r"(?<!\\)\n")
+
+
+def _stringify_scalar(value: ScalarType) -> str:
+    if value is None:
+        return "NULL"
+    elif isinstance(value, bool):
+        return "TRUE" if value else "FALSE"
+    if isinstance(value, (str, bytes)):
+        if isinstance(value, bytes):
+            decoded = value.decode()
+        else:
+            decoded = value
+
+        decoded = unescaped_quotes.sub("\\'", decoded)
+        decoded = unescaped_newline.sub("\\\\n", decoded)
+        return f"'{decoded}'"
+    elif isinstance(value, (int, float)):
+        return f"{value}"
+    elif isinstance(value, datetime):
+        # Snuba expects naive UTC datetimes, so convert to that
+        if value.tzinfo is not None:
+            delta = value.utcoffset()
+            assert delta is not None
+            value = value - delta
+            value = value.replace(tzinfo=None)
+        return f"toDateTime('{value.isoformat()}')"
+    elif isinstance(value, date):
+        return f"toDateTime('{value.isoformat()}')"
+    elif isinstance(value, list):
+        is_scalar(value)  # Throws on an invalid array
+        return f"array({', '.join([_stringify_scalar(v) for v in value])})"
+    elif isinstance(value, tuple):
+        is_scalar(value)  # Throws on an invalid tuple
+        return f"tuple({', '.join([_stringify_scalar(v) for v in value])})"
+
+    raise InvalidExpression(f"'{value}' is not a valid scalar")
+
+
+TVisited = TypeVar("TVisited")
+
+
+class ExpressionVisitor(ABC, Generic[TVisited]):
+    def visit(self, node: Expression) -> TVisited:
+        if isinstance(node, Column):
+            return self._visit_column(node)
+        elif isinstance(node, Function):
+            return self._visit_function(node)
+        elif isinstance(node, Entity):
+            return self._visit_entity(node)
+        elif isinstance(node, Condition):
+            return self._visit_condition(node)
+        elif isinstance(node, OrderBy):
+            return self._visit_orderby(node)
+        elif isinstance(node, Limit):
+            return self._visit_int_literal(node.limit)
+        elif isinstance(node, Offset):
+            return self._visit_int_literal(node.offset)
+        elif isinstance(node, LimitBy):
+            return self._visit_limitby(node)
+        elif isinstance(node, Granularity):
+            return self._visit_int_literal(node.granularity)
+        elif isinstance(node, Totals):
+            return self._visit_totals(node)
+
+        assert False, f"Unhandled Expression: {node}"
+
+    @abstractmethod
+    def _visit_column(self, column: Column) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_function(self, func: Function) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_int_literal(self, literal: int) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_entity(self, entity: Entity) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_condition(self, cond: Condition) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_orderby(self, orderby: OrderBy) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_limitby(self, limitby: LimitBy) -> TVisited:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _visit_totals(self, totals: Totals) -> TVisited:
+        raise NotImplementedError
+
+
+class Translation(ExpressionVisitor[str]):
+    def _visit_column(self, column: Column) -> str:
+        return column.name
+
+    def _visit_function(self, func: Function) -> str:
+        alias = "" if func.alias is None else f" AS {func.alias}"
+        params = []
+        for param in func.parameters:
+            if isinstance(param, (Column, Function)):
+                params.append(self.visit(param))
+            elif isinstance(param, tuple(Scalar)):
+                params.append(_stringify_scalar(param))
+
+        return f"{func.function}({', '.join(params)}){alias}"
+
+    def _visit_int_literal(self, literal: int) -> str:
+        return f"{literal:d}"
+
+    def _visit_entity(self, entity: Entity) -> str:
+        return f"({entity.name})"
+
+    def _visit_condition(self, cond: Condition) -> str:
+        rhs = None
+        if isinstance(cond.rhs, (Column, Function)):
+            rhs = self.visit(cond.rhs)
+        elif is_scalar(cond.rhs):
+            rhs = _stringify_scalar(cond.rhs)
+
+        assert rhs is not None
+        return f"{self.visit(cond.lhs)} {cond.op.value} {rhs}"
+
+    def _visit_orderby(self, orderby: OrderBy) -> str:
+        return f"{self.visit(orderby.exp)} {orderby.direction.value}"
+
+    def _visit_limitby(self, limitby: LimitBy) -> str:
+        return f"{limitby.count} BY {self.visit(limitby.column)}"
+
+    def _visit_totals(self, totals: Totals) -> str:
+        return str(totals.totals)
