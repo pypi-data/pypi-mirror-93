c8269d75c8cbf29690910ac564b4dc8b752a2afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:54 +0000

    Add package craft-ml/0.2.1/craft_ml-0.2.1.tar.gz

---
+++ packages/craft-ml/craft_ml-0.2.1.tar.gz/craft_ml-0.2.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+
+setup(
+    name="craft_ml",
+    description="",
+    version="0.2.1",
+    package_dir={"": "src"},
+    zip_safe=True,
+    packages=["craft_ml"],
+    install_requires=[
+        "numpy==1.18.1",
+        "scikit_learn==0.24.1",
+        "matplotlib==3.1.3",
+        "pandas==1.0.1",
+        "streamlit==0.75.0",
+    ],
+    python_requires='>=3.7',
+    setup_requires=[],
+    entry_points={
+        "console_scripts": [
+            "craft-ml=craft_ml:main"
+        ]
+    },
+)
---
+++ packages/craft-ml/craft_ml-0.2.1.tar.gz/craft_ml-0.2.1/src/craft_ml/__main__.py
@@ -0,0 +1,6 @@
+import craft_ml
+
+
+if __name__ == '__main__':
+    craft_ml.default_pipeline()
+    print('OK')
