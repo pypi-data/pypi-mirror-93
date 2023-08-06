ee02fd46800742f31c7ce1f3741b16bbbf26241b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:42 +0000

    Add package neqsim/2.2.35/neqsim-2.2.35.tar.gz

---
+++ packages/neqsim/neqsim-2.2.35.tar.gz/neqsim-2.2.35/src/neqsim/javaGateway.py
@@ -0,0 +1,26 @@
+import os
+from os import path
+from py4j.java_gateway import JavaGateway
+import pkg_resources
+gateway = None
+local_os_name = os.name
+colon = ':'
+if local_os_name == 'nt':
+    colon = ';'
+
+port = 0
+
+def create_classpath(jars):
+    resources_dir = pkg_resources.resource_filename('neqsim', 'lib/')
+    return colon.join([path.join(resources_dir, jar) for jar in jars])
+
+
+def start_server():
+    global gateway
+    jars = ['NeqSim.jar']
+    classpath = create_classpath(jars)
+    if port==0:
+       return JavaGateway.launch_gateway(classpath=classpath, die_on_exit=True)
+    else:
+       return JavaGateway.launch_gateway(port=port,classpath=classpath, die_on_exit=True)
+
