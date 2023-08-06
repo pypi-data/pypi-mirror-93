c55921ecdccef8d96d29a23d4b5a4e0bd4ccd06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:24 +0000

    Add package pyki3oom/0.0.1/pyki3oom-0.0.1.tar.gz

---
+++ packages/pyki3oom/pyki3oom-0.0.1.tar.gz/pyki3oom-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pyki3oom',
+    version='0.0.1',
+    description='python wrapper for Kiwoom Open API+ (Korea Stock Market)',
+    url='',
+    author='ador hylas',
+    author_email='null@gmail.com',
+    install_requires=['pandas'],
+    license='MIT',
+    packages=['pyki3oom'],
+    zip_safe=False
+)
