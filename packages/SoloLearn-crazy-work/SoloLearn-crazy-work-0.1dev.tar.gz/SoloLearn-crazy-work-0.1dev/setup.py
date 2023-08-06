76351c0e7eb086c19fad3540c96ebad177903636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:29 +0000

    Add package SoloLearn-crazy-work/0.1dev/SoloLearn-crazy-work-0.1dev.tar.gz

---
+++ packages/SoloLearn-crazy-work/SoloLearn-crazy-work-0.1dev.tar.gz/SoloLearn-crazy-work-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+   name='SoloLearn-crazy-work', 
+   version='0.1dev',
+   packages=['sololearn',],
+   license='MIT', 
+   long_description=open('README.txt').read(),
+)
