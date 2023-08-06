5aea91dc6671a8f730c368845a93b2d383fa1d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:37 +0000

    Add package dphon/2.0.0b0/dphon-2.0.0b0.tar.gz

---
+++ packages/dphon/dphon-2.0.0b0.tar.gz/dphon-2.0.0b0/dphon/cli.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""dphon - a tool for old chinese phonetic analysis
+ 
+Usage:
+    dphon -h | --help
+    dphon --version
+    dphon <path>... [options]
+ 
+Global Options:
+    -h, --help                  Show this help text.
+    -v, --version               Show program version.
+    -o <file>, --output <file>  Write output to a file instead of stdout.
+    -f <fmt>, --format <fmt>    Set input file type. Currently, plaintext (.txt) and json-lines (.jsonl)
+                                files are supported. [default: txt]
+
+Matching Options:
+    -n <n>, --ngram-order <n>   Order of n-grams used to seed matches. Higher means decreased execution
+                                time at the expense of total result count. [default: 4]
+    -k <k>, --threshold <k>     Similarity threshold below which matches will not be retained. Higher
+                                means shorter matches and fewer total results. [default: 0.7]
+    -l <l>, --len-limit <l>     Limit on number of tokens to compare to obtain similarity score. Higher
+                                means longer matches at the expense of execution time. [default: 50]
+
+Filtering Options:
+    -a, --all                   Allow matches without graphic variation. By default, only matches
+                                containing at least one token with shared phonemes but differing graphemes 
+                                are shown.
+    --min <min>                 Limit to matches with total tokens >= min. Has no effect if less than
+                                the value for "--ngram-order", above.
+    --max <max>                 Limit to matches with total tokens <= max. Must be equal to or greater
+                                than the value for "--ngram-order", above.
+
+Examples:
+    dphon texts/*.txt --min 8 --output matches.txt
+    dphon file1.txt file2.txt -n 8 -k 0.8
+    dphon docs.jsonl --format jsonl
+ 
+Help:
+    For more information on using this tool, visit the Github repository:
+    https://github.com/direct-phonology/direct
+"""
+
+import logging
+from itertools import combinations
+from pathlib import Path
+from typing import Dict
+
+import spacy
+from docopt import docopt
+from rich import traceback
+from rich.console import Console
+from rich.logging import RichHandler
+from spacy.language import Language
+from spacy.tokens import Doc
+
+from . import __version__
+from .align import SmithWatermanPhoneticAligner
+from .extend import LevenshteinPhoneticExtender
+from .fmt import DEFAULT_THEME, SimpleFormatter
+from .index import Index
+from .io import CorpusLoader, JsonLinesCorpusLoader, PlaintextCorpusLoader
+from .match import Match
+from .ngrams import Ngrams
+from .phonemes import Phonemes, get_sound_table_json
+from .reuse import MatchGraph
+
+
+def run() -> None:
+    """CLI entrypoint."""
+    args = docopt(__doc__, version=__version__)
+
+    # install logging and exception handlers
+    logging.basicConfig(level="INFO", format="%(message)s",
+                        datefmt="[%X]", handlers=[RichHandler()])
+    logging.captureWarnings(True)
+    traceback.install()
+
+    # setup pipeline
+    nlp = setup(args)
+
+    # process all texts
+    graph = process(nlp, args)
+    results = list(graph.matches)
+    logging.info(f"{len(results)} total results matching query")
+
+    # sort results by highest total score
+    results = sorted(results, key=lambda m: m.weight, reverse=True)
+
+    # set up formatting - colorize for terminal but not for files
+    console = Console(theme=DEFAULT_THEME)
+    format = SimpleFormatter(gap_char="　")
+
+    # write to a file if requested; otherwise write to stdout
+    if args["--output"]:
+        outpath = Path(args["--output"])
+        with outpath.open(mode="w", encoding="utf8") as file:
+            for match in results:
+                file.write(format(match) + "\n\n")
+        logging.info(f"wrote {outpath.resolve()}")
+    else:
+        for match in results:
+            console.print((format(match) + "\n"), soft_wrap=False)
+
+    # teardown pipeline
+    teardown(nlp)
+
+
+def setup(args: Dict) -> Language:
+    """Set up the spaCy processing pipeline."""
+    # get sound table
+    sound_table = get_sound_table_json(Path("./dphon/data/sound_table_v2.json"))
+
+    # add Doc metadata
+    Doc.set_extension("id", default="")
+
+    # setup spaCy model
+    nlp = spacy.blank(
+        "zh", meta={"tokenizer": {"config": {"use_jieba": False}}})
+    nlp.add_pipe(Phonemes(nlp, sound_table=sound_table), first=True)
+    nlp.add_pipe(Ngrams(nlp, n=int(args["--ngram-order"])), after="phonemes")
+    nlp.add_pipe(Index(nlp, val_fn=lambda doc: doc._.ngrams,
+                       filter_fn=lambda ngram: ngram.text.isalpha(),
+                       key_fn=lambda ngram: "".join(ngram._.phonemes)))
+    logging.info("loaded default spaCy model")
+    return nlp
+
+
+def process(nlp: Language, args: Dict) -> MatchGraph:
+    """Run the spaCy processing pipeline."""
+    # set up graph and loader
+    graph = MatchGraph()
+    load_texts: CorpusLoader
+    if args["--format"] == "jsonl":
+        load_texts = JsonLinesCorpusLoader()
+    else:
+        load_texts = PlaintextCorpusLoader()
+
+    # load and index all documents
+    for doc, context in nlp.pipe(load_texts(args["<path>"]), as_tuples=True):
+        doc._.id = context["id"]
+        graph.add_doc(context["id"], doc)
+        logging.debug(f"indexed doc \"{doc._.id}\"")
+    logging.info(f"indexing completed")
+
+    # drop all ngrams from index that only occur once
+    groups = nlp.get_pipe("index").filter(lambda g: len(g[1]) > 1)
+
+    # create initial pairwise matches from seed groups; perfect score of 1.0
+    for _seed, locations in groups:
+        for utxt, vtxt in combinations(locations, 2):
+            if utxt.doc != vtxt.doc:  # skip same-doc matches
+                graph.add_match(
+                    Match(utxt.doc._.id, vtxt.doc._.id, utxt, vtxt, 1.0))
+
+    # limit to seeds with graphic variants if requested
+    if not args["--all"]:
+        graph.filter(nlp.get_pipe("phonemes").has_variant)
+
+    # extend all matches
+    graph.extend(LevenshteinPhoneticExtender(
+        threshold=float(args["--threshold"]),
+        len_limit=int(args["--len-limit"])
+    ))
+
+    # align all matches
+    graph.align(SmithWatermanPhoneticAligner())
+
+    # limit via min and max lengths if requested
+    if args["--min"]:
+        graph.filter(lambda m: len(m) >= int(args["--min"]))
+    if args["--max"]:
+        graph.filter(lambda m: len(m) <= int(args["--max"]))
+
+    # return completed reuse graph
+    return graph
+
+
+def teardown(nlp: Language) -> None:
+    """Unregister spaCy extensions to prevent name collisions."""
+    Doc.remove_extension("id")
+
+    # iterate over all pipeline components and call teardown() if it exists
+    for _name, component in nlp.pipeline:
+        if hasattr(component, "teardown"):
+            component.teardown()
+
+
+if __name__ == "__main__":
+    run()
---
+++ packages/dphon/dphon-2.0.0b0.tar.gz/dphon-2.0.0b0/dphon/phonemes.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""SpaCy pipeline component for converting Token graphemes to phonemes."""
+
+import json
+import logging
+from pathlib import Path
+from typing import Iterable, Iterator, Mapping, Optional, Tuple
+
+from spacy.language import Language
+from spacy.lookups import Lookups, Table
+from spacy.tokens import Doc, Span, Token
+
+from dphon.match import Match
+
+# register the pipeline component factory; see:
+# https://spacy.io/usage/processing-pipelines#custom-components-factories
+Language.factories["phonemes"] = lambda nlp, **cfg: Phonemes(nlp, **cfg)
+
+# private use unicode char that represents phonemes for OOV tokens
+OOV_PHONEMES = "\ue000"
+
+# types for sound tables: map a string to a tuple of syllable phonemes
+Phonemes_T = Tuple[Optional[str], ...]
+SoundTable_T = Mapping[str, Phonemes_T]
+
+
+class Phonemes():
+    """A spaCy pipeline component that enables converting Tokens to phonemes."""
+
+    name = "phonemes"   # will appear in spaCy pipeline
+    attr = "phonemes"   # name for getter for phonemes, e.g. doc._.phonemes
+    table: Table        # reference to sound table in spaCy lookups
+    lookups: Lookups    # reference to all LUTs for language model
+
+    def __init__(self, nlp: Language, sound_table: SoundTable_T, name: str = None, attr: str = None):
+        """Initialize the phonemes component."""
+        # store the attr accessor & name that will appear in the pipeline
+        self.name = name if name else self.name
+        self.attr = attr if attr else self.attr
+
+        # infer the syllable segmentation and map it to an empty phoneme set
+        self.syllable_parts = len(next(iter(sound_table.values())))
+        self.empty_phonemes = tuple(
+            None for part in range(self.syllable_parts))
+
+        # register attribute getters with customizable names; see:
+        # https://spacy.io/usage/processing-pipelines#custom-components-best-practices
+        Doc.set_extension(self.attr, getter=self.get_all_phonemes)
+        Span.set_extension(self.attr, getter=self.get_all_phonemes)
+        Token.set_extension(self.attr, getter=self.get_token_phonemes)
+        Token.set_extension("is_oov", getter=self.is_token_oov)
+
+        # store the sound table in the vocab's Lookups using the attr name
+        self.lookups = nlp.vocab.lookups
+        self.table = self.lookups.add_table(self.attr, sound_table)
+        logging.info(f"using {self.__class__} with name={self.name}")
+
+    def teardown(self) -> None:
+        """Unregister the sound table and attributes to prevent collisions."""
+        self.lookups.remove_table(self.attr)
+        Doc.remove_extension(self.attr)
+        Span.remove_extension(self.attr)
+        Token.remove_extension(self.attr)
+        Token.remove_extension("is_oov")
+
+    def __call__(self, doc: Doc) -> Doc:
+        """Return the Doc unmodified."""
+        return doc
+
+    def is_token_oov(self, token: Token) -> bool:
+        """Check if a token has a phonetic entry in the sound table."""
+        return token.text not in self.table
+
+    def has_variant(self, match: Match) -> bool:
+        """True if a seed contains a graphic variant.
+        
+        This is designed to be called on seeds that are of the same length,
+        so that the match doesn't need to be aligned for it to work."""
+        
+        # compare each token pairwise, True if we find a variant, else False
+        for i in range(len(match)):
+            if self.are_graphic_variants(match.utxt[i], match.vtxt[i]):
+                return True
+        return False
+
+    def are_graphic_variants(self, *tokens: Token) -> bool:
+        """Check if a set of tokens are graphic variants of the same word.
+
+        - If any tokens are not in the sound table, returns False.
+        - If any tokens are non-voiced, returns False.
+        - If any tokens have identical text (graphs), returns False.
+        - If any tokens have differing phonemes, returns False.
+        """
+
+        # O(n) implementation: compare all tokens against first one
+        base_text = tokens[0].text
+        base_phon = self.get_token_phonemes(tokens[0])
+        if base_phon == self.empty_phonemes or base_phon == (OOV_PHONEMES,):
+            return False
+        for token in tokens[1:]:
+            phonemes = self.get_token_phonemes(token)
+            if phonemes == self.empty_phonemes or \
+               phonemes == (OOV_PHONEMES,) or \
+               phonemes != base_phon or \
+               token.text == base_text:
+                return False
+        return True
+
+    def get_all_phonemes(self, tokens: Iterable[Token]) -> Iterator[str]:
+        """Return a flattened iterator over all phonemes in a Span or Doc.
+
+        - Skips parts of the syllable that are not used (stored as None).
+        - Skips non-voiced tokens, such as punctuation.
+        - Skips tokens with no phonetic entry in the sound table.
+        """
+
+        for token in tokens:
+            for phoneme in self.get_token_phonemes(token):
+                if phoneme and phoneme != OOV_PHONEMES:
+                    yield phoneme
+
+    def get_token_phonemes(self, token: Token) -> Phonemes_T:
+        """Return a Token's phonemes as an n-tuple.
+
+        - If a Token is non-alphanumeric, all elements of the tuple will be None.
+        - If a Token is not in the sound table, all elements of the tuple will
+        use a special marker token (OOV_PHONEMES).
+        - If some parts of the syllable are not present, their corresponding
+        elements in the tuple will be None.
+        """
+
+        if not token.is_alpha and not token.like_num:
+            return self.empty_phonemes
+        elif token._.is_oov:
+            # logging.warning(f"no entry for token in sound table: {token.text}")
+            return (OOV_PHONEMES,)
+        else:
+            return self.table[token.text]
+
+
+def get_sound_table_json(path: Path) -> SoundTable_T:
+    """Load a sound table as JSON."""
+    sound_table: SoundTable_T = {}
+
+    # open the file and load all readings
+    with open(path, encoding="utf8") as file:
+        entries = json.loads(file.read())
+        for char, readings in entries.items():
+
+            # FIXME just using first reading for now, ignoring multiple
+            # NOTE final two entries in current table are source info; ignore
+            *reading, _src, _src2 = readings[0]
+            sound_table[char] = tuple(reading)
+
+    # log and return finished table
+    logging.info(f"sound table {path.resolve()} loaded")
+    return sound_table
