ab315ecab021056888a91497702f08872f54ffd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:32 +0000

    Add package rchitect/0.3.30/rchitect-0.3.30-pp37-pypy37_pp73-manylinux1_x86_64.whl

---
+++ packages/rchitect/rchitect-0.3.30-pp37-pypy37_pp73-manylinux1_x86_64.whl/rchitect/completion.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals
+
+from rchitect.interface import rcall, reval, rcopy, rstring, rint
+from six import text_type
+
+
+def assign_line_buffer(buf):
+    rcall(reval("utils:::.assignLinebuffer"), rstring(buf))
+    rcall(reval("utils:::.assignEnd"), rint(len(buf)))
+    token = rcopy(text_type, rcall(reval("utils:::.guessTokenFromLine")))
+    return token
+
+
+CompleteCode = """
+tryCatch(
+    {{
+        if ({settimelimit}) base::setTimeLimit({timeout})
+        utils:::.completeToken()
+        if ({settimelimit}) base::setTimeLimit()
+    }},
+    error = function(e) {{
+        if ({settimelimit}) base::setTimeLimit()
+        assign("comps", NULL, envir = utils:::.CompletionEnv)
+    }}
+)
+"""
+
+
+def complete_token(timeout=0):
+    try:
+        reval(
+            CompleteCode.format(
+                settimelimit="TRUE" if timeout > 0 else "FALSE",
+                timeout=str(timeout)),
+            envir=rcall("new.env")
+        )
+    except Exception:
+        if timeout:
+            rcall(("base", "setTimeLimit"))
+            rcall(("base", "assign"), "comps", None, env=reval("utils:::.CompletionEnv"))
+
+
+def retrieve_completions():
+    completions = rcopy(list, rcall(reval("utils:::.retrieveCompletions")))
+    if not completions:
+        return []
+    else:
+        return completions
