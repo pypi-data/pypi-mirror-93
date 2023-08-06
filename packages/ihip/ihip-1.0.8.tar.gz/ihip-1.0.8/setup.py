545e67ecfa3c3ab674d71e7bc1135465a1915a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:30 +0000

    Add package ihip/1.0.8/ihip-1.0.8.tar.gz

---
+++ packages/ihip/ihip-1.0.8.tar.gz/ihip-1.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='ihip',
+      version='1.0.8',
+      description='Jupyter integration Package for generating models',
+      url='https://infygithub.ad.infosys.com/veena-devipalamuri/JupyterIntegration.git',
+      author='Infosys',
+      author_email='veena.devipalamuri@infosys.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
