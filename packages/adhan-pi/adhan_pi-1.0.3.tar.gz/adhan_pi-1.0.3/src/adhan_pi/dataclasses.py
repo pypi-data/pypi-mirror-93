3a62bb76454993091321fb3f68c0112b8eaf2f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:57 +0000

    Add package adhan-pi/1.0.3/adhan_pi-1.0.3.tar.gz

---
+++ packages/adhan-pi/adhan_pi-1.0.3.tar.gz/adhan_pi-1.0.3/src/adhan_pi/dataclasses.py
@@ -0,0 +1,60 @@
+import datetime as dt
+from collections.abc import Iterable, Iterator
+from typing import NamedTuple, Union
+
+import attr
+
+
+def extract_time(t: Union[dt.datetime, str]) -> dt.time:
+    if isinstance(t, str):
+        t = dt.datetime.strptime(t[:5], "%H:%M")
+    return t.time()
+
+
+@attr.s
+class Prayer(object):
+    name = attr.ib(type=str)
+    time = attr.ib(type=dt.time, converter=extract_time)
+
+
+class Coordinates(NamedTuple):
+    latitude: float
+    longitude: float
+
+
+@attr.s
+class PrayerTimes(Iterable):
+    date = attr.ib(type=dt.date)
+    fajr = attr.ib(type=Prayer)
+    dhuhr = attr.ib(type=Prayer)
+    asr = attr.ib(type=Prayer)
+    maghrib = attr.ib(type=Prayer)
+    isha = attr.ib(type=Prayer)
+
+    def __iter__(self):
+        return PrayerIterator(self)
+
+
+class PrayerIterator(Iterator):
+    def __init__(self, pt: PrayerTimes):
+        self.index = 0
+        self.pt = pt
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Prayer:
+        if self.index == 0:
+            prayer = self.pt.fajr
+        elif self.index == 1:
+            prayer = self.pt.dhuhr
+        elif self.index == 2:
+            prayer = self.pt.asr
+        elif self.index == 3:
+            prayer = self.pt.maghrib
+        elif self.index == 4:
+            prayer = self.pt.isha
+        else:
+            raise StopIteration
+        self.index += 1
+        return prayer
---
+++ packages/adhan-pi/adhan_pi-1.0.3.tar.gz/adhan_pi-1.0.3/src/adhan_pi/exceptions.py
@@ -0,0 +1,28 @@
+from typing import Dict, Optional
+
+import requests
+
+
+class PrayerAPIError(Exception):
+    def __init__(
+        self,
+        response: Optional[requests.Response] = None,
+        data: Optional[Dict] = None,
+    ):
+        if response:
+            super().__init__(
+                "Could not find prayertimes status={status}".format(
+                    status=response.status_code
+                )
+            )
+        elif data:
+            super().__init__(
+                "Could not find prayertimes data={data}".format(data=data)
+            )
+        else:
+            super().__init__()
+
+
+class LocationNotFoundError(Exception):
+    def __init__(self, query: str):
+        super().__init__("Could not find location for {}".format(query))
