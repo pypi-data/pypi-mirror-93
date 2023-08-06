3499fea3d4c0626029425c6030961baf5807004b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:13 +0000

    Add package every-script-is-a-vk-bot/1.0/every_script_is_a_vk_bot-1.0.tar.gz

---
+++ packages/every-script-is-a-vk-bot/every_script_is_a_vk_bot-1.0.tar.gz/every_script_is_a_vk_bot-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='every_script_is_a_vk_bot',
+    version='1.0',
+    packages=['every_script_is_vk_a_bot'],
+    url='https://github.com/megahomyak/every_script_is_a_vk_bot',
+    license='OSI Approved :: GNU General Public License v3 (GPLv3)',
+    author='megahomyak',
+    author_email='g.megahomyak@gmail.com',
+    description='This library can turn your code into the VK bot.',
+    download_url=(
+        "https://github.com/megahomyak/every_script_is_a_vk_bot/archive/"
+        "v1.0.tar.gz"
+    ),
+    dependencies=["saya"]
+)
