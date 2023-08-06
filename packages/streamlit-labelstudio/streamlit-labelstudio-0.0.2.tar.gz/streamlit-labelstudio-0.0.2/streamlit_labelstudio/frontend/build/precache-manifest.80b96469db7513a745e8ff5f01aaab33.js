430f2799c78410275e2d513b5ea6a16eb8341936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package streamlit-labelstudio/0.0.2/streamlit-labelstudio-0.0.2.tar.gz

---
+++ packages/streamlit-labelstudio/streamlit-labelstudio-0.0.2.tar.gz/streamlit-labelstudio-0.0.2/streamlit_labelstudio/frontend/build/precache-manifest.80b96469db7513a745e8ff5f01aaab33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae3c0cf87825a14bb7cbc621d1efa375",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94d22e432682f9865708",
+    "url": "./static/css/2.b789f26a.chunk.css"
+  },
+  {
+    "revision": "94d22e432682f9865708",
+    "url": "./static/js/2.d173a6f3.chunk.js"
+  },
+  {
+    "revision": "4ac32ede635c1c8e678e25f80a8b260c",
+    "url": "./static/js/2.d173a6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf56bb5e68e570636cc2",
+    "url": "./static/js/main.26a52596.chunk.js"
+  },
+  {
+    "revision": "2a64b617e6e08c8b68c5",
+    "url": "./static/js/runtime-main.7e58624a.js"
+  }
+]);
