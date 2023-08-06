feba9d08086655a98cf03f54aa6f520bf321cad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package nonebot-plugin-itreflection/0.0.1/nonebot_plugin_itreflection-0.0.1.tar.gz

---
+++ packages/nonebot-plugin-itreflection/nonebot_plugin_itreflection-0.0.1.tar.gz/nonebot_plugin_itreflection-0.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='nonebot_plugin_itreflection',
+    version='0.0.1',
+    author='maxesisn',
+    author_email='me@maxesisn.online',
+    url='https://github.com/maxesisn/nonebot_plugin_itreflection',
+    description=u'适用于nonebot2的交互式反射插件',
+    packages=['nonebot_plugin_itreflection'],
+    classifiers=[
+        "Framework :: AsyncIO",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
