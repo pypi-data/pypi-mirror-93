ab990bf60bf15e688b60a82ce66499a8a64a262c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:42 +0000

    Add package buycoins-python/0.3.1/buycoins_python-0.3.1-py3-none-any.whl

---
+++ packages/buycoins-python/buycoins_python-0.3.1-py3-none-any.whl/buycoins/exceptions/exceptions.py
@@ -0,0 +1,83 @@
+class ClientError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+            self.status_code = args[1]
+        else:
+            self.message = None
+            self.status_code = 401
+
+    @property
+    def response(self):
+        return {
+            "status_code": self.status_code,
+            "message": self.message
+        }
+
+
+class QueryError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+            self.status_code = args[1]
+        else:
+            self.message = None
+            self.status_code = 404
+
+    @property
+    def response(self):
+        return {
+            "status_code": self.status_code,
+            "message": self.message
+        }
+
+
+class P2PError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+            self.status_code = args[1]
+        else:
+            self.message = None
+            self.status_code = 404
+
+    @property
+    def response(self):
+        return {
+            "status_code": self.status_code,
+            "message": self.message
+        }
+
+
+class AccountError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+            self.status_code = args[1]
+        else:
+            self.message = None
+            self.status_code = 404
+
+    @property
+    def response(self):
+        return {
+            "status_code": self.status_code,
+            "message": self.message
+        }
+
+
+class WalletError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+            self.status_code = args[1]
+        else:
+            self.message = None
+            self.status_code = 404
+
+    @property
+    def response(self):
+        return {
+            "status_code": self.status_code,
+            "message": self.message
+        }
