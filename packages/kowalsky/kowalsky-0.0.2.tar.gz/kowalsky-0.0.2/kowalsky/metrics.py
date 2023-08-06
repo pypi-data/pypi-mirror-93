6b76cafb26401fcbe6cebf95353f5578b9b22893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:53 +0000

    Add package kowalsky/0.0.2/kowalsky-0.0.2.tar.gz

---
+++ packages/kowalsky/kowalsky-0.0.2.tar.gz/kowalsky-0.0.2/kowalsky/metrics.py
@@ -0,0 +1,10 @@
+from sklearn.metrics import mean_squared_log_error
+from sklearn.metrics import mean_squared_error
+
+
+def rmsle(actual, pred):
+    return mean_squared_log_error(actual, pred) ** 0.5
+
+
+def rmse(actual, pred):
+    return mean_squared_error(actual, pred) ** 0.5
