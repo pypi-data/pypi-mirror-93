55a0262885c62c7b1b7fb75969ab519983a844e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package deepsparse/0.1.0/deepsparse-0.1.0-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/deepsparse/deepsparse-0.1.0-cp38-cp38-manylinux2014_x86_64.whl/deepsparse/version.py
@@ -0,0 +1,9 @@
+# neuralmagic: no copyright
+# flake8: noqa
+__all__ = ["version", "optimized", "release", "revision", "splash", "build_date"]
+version = '0.1.0'
+optimized = 1
+release = 1
+revision = 'bbe00506'
+splash = 'DeepSparse Engine, Copyright 2021-present / Neuralmagic, Inc. version: 0.1.0 (bbe00506) (release) (optimized)'
+build_date = '20210204'
