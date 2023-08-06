7c87750958382afeb11e53bf71a423c131d0d251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:51 +0000

    Add package matching-network/0.0.6/matching_network-0.0.6-py3-none-any.whl

---
+++ packages/matching-network/matching_network-0.0.6-py3-none-any.whl/matching_network/__init__.py
@@ -0,0 +1,14 @@
+# @date        : Fri Mar 27 10:11:01 CET 2020
+# @author(s)   : Francesco Urbani <https://urbanij.github.io>
+# @file        : 
+# @descritpion : 
+#                
+# 
+
+"""Matching network module"""
+
+__title__ = 'matching_network'
+__version__ = '0.0.6'
+__author__ = u"Francesco Urbani"
+
+from .main import L_section_matching
