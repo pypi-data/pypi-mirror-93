9e1acdded7780eefc16af32cea36b0dd8e793574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:41 +0000

    Add package draft-sport/0.0.43/draft_sport-0.0.43-py3-none-any.whl

---
+++ packages/draft-sport/draft_sport-0.0.43-py3-none-any.whl/draft_sport/fantasy/competition/constants.py
@@ -0,0 +1,19 @@
+"""
+Draft Sport Python
+Competition Constants Module
+author: hugh@blinkybeach.com
+"""
+from draft_sport.fantasy.competition.competition import Competition
+
+
+class CompetitionConstants:
+
+    DRAFT_RUGBY_2020 = Competition(
+        public_id='sru_2020_2',
+        name='Draft Rugby 2020'
+    )
+
+    DRAFT_RUGBY_2020_PRE_COVID = Competition(
+        public_id='sru_2020_1',
+        name='Draft Rugby 2020 (Pre-Covid)'
+    )
