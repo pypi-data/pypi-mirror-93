49587c8b92074bea81195b307b0d7eac73eb576f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:38 +0000

    Add package se3-transformer-pytorch/0.0.2/se3-transformer-pytorch-0.0.2.tar.gz

---
+++ packages/se3-transformer-pytorch/se3-transformer-pytorch-0.0.2.tar.gz/se3-transformer-pytorch-0.0.2/se3_transformer_pytorch/irr_repr.py
@@ -0,0 +1,68 @@
+import torch
+from torch import sin, cos
+from math import pi
+from functools import wraps
+import numpy as np
+
+from lie_learn.representations.SO3.wigner_d import wigner_D_matrix
+
+from se3_transformer_pytorch.utils import exists, default, cast_torch_tensor
+from se3_transformer_pytorch.spherical_harmonics import get_spherical_harmonics, clear_spherical_harmonics_cache
+
+def irr_repr(order, alpha, beta, gamma, dtype = None):
+    """
+    irreducible representation of SO3
+    - compatible with compose and spherical_harmonics
+    """
+    dtype = default(dtype, torch.get_default_dtype())
+    alpha, beta, gamma = map(np.array, (alpha, beta, gamma))
+    return torch.tensor(wigner_D_matrix(order, alpha, beta, gamma), dtype = dtype)
+
+@cast_torch_tensor
+def rot_z(gamma):
+    '''
+    Rotation around Z axis
+    '''
+    return torch.tensor([
+        [cos(gamma), -sin(gamma), 0],
+        [sin(gamma), cos(gamma), 0],
+        [0, 0, 1]
+    ], dtype=gamma.dtype)
+
+@cast_torch_tensor
+def rot_y(beta):
+    '''
+    Rotation around Y axis
+    '''
+    return torch.tensor([
+        [cos(beta), 0, sin(beta)],
+        [0, 1, 0],
+        [-sin(beta), 0, cos(beta)]
+    ], dtype=beta.dtype)
+
+@cast_torch_tensor
+def x_to_alpha_beta(x):
+    '''
+    Convert point (x, y, z) on the sphere into (alpha, beta)
+    '''
+    x = x / torch.norm(x)
+    beta = torch.acos(x[2])
+    alpha = torch.atan2(x[1], x[0])
+    return (alpha, beta)
+
+def rot(alpha, beta, gamma):
+    '''
+    ZYZ Euler angles rotation
+    '''
+    return rot_z(alpha) @ rot_y(beta) @ rot_z(gamma)
+
+def compose(a1, b1, c1, a2, b2, c2):
+    """
+    (a, b, c) = (a1, b1, c1) composed with (a2, b2, c2)
+    """
+    comp = rot(a1, b1, c1) @ rot(a2, b2, c2)
+    xyz = comp @ torch.tensor([0, 0, 1.])
+    a, b = x_to_alpha_beta(xyz)
+    rotz = rot(0, -b, -a) @ comp
+    c = torch.atan2(rotz[1, 0], rotz[0, 0])
+    return a, b, c
