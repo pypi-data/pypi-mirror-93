ea8c8e94e95970966e86d82c8860feefdc2d73e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:10 +0000

    Add package cricket-tops/1.0.3/cricket-tops-1.0.3.tar.gz

---
+++ packages/cricket-tops/cricket-tops-1.0.3.tar.gz/cricket-tops-1.0.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  3 22:40:18 2021
+
+@author: Arghya Tarafdar
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="cricket-tops",
+    version="1.0.3",
+    description="A Python package to view the top ODI, Tests, T20 ranked players (men and women) as well as Top Tests, ODI and T20 ranked teams.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Arghya Tarafdar",
+    author_email="arghya.tarafdar86@yahoo.co.in",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["cricket_tops"],
+    include_package_data=True,
+    install_requires=["pandas"],
+    
+)
