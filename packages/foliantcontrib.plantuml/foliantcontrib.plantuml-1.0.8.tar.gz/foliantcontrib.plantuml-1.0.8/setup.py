c75a9ca75ac7a0af94216cee10290b66d4951d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:39 +0000

    Add package foliantcontrib.plantuml/1.0.8/foliantcontrib.plantuml-1.0.8.tar.gz

---
+++ packages/foliantcontrib.plantuml/foliantcontrib.plantuml-1.0.8.tar.gz/foliantcontrib.plantuml-1.0.8/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'PlantUML diagrams preprocessor for Foliant.'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.plantuml',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='1.0.8',
+    author='Artemy Lomov',
+    author_email='artemy@lomov.ru',
+    url='https://github.com/foliant-docs/foliantcontrib.plantuml',
+    packages=['foliant.preprocessors'],
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.8'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
