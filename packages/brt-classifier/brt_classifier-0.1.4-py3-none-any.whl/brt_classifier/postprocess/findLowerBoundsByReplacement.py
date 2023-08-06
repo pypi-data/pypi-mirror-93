41cc1c0af7146f67800e5734a86037da3456bb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:09 +0000

    Add package brt-classifier/0.1.4/brt_classifier-0.1.4-py3-none-any.whl

---
+++ packages/brt-classifier/brt_classifier-0.1.4-py3-none-any.whl/brt_classifier/postprocess/findLowerBoundsByReplacement.py
@@ -0,0 +1,24 @@
+from ..util import getCanonical
+
+def findLowerBoundsByReplacement(problem, idx, data):
+  constrs = problem["constraint"]
+  lowerBound = problem["lower-bound"]
+
+  if lowerBound == "":
+    newConstr1 = set([getCanonical(x.replace("3", "1")) for x in constrs])
+    newConstr2 = set([getCanonical(x.replace("2", "1")) for x in constrs])
+    newConstr3 = set([getCanonical(x.replace("3", "2")) for x in constrs])
+    newConstrs = [newConstr1, newConstr2, newConstr3]
+
+    prevProblems = data[:idx]
+    for prevProblem in prevProblems:
+      prevConstrs = set(prevProblem["constraint"])
+      prevLowerBound = prevProblem["lower-bound"]
+
+      if prevLowerBound != "" and prevLowerBound != "(1)" and prevConstrs in newConstrs:
+        data[idx]["lower-bound"] = prevLowerBound
+        print(constrs, prevLowerBound)
+        return 1
+  
+  return 0
+
