d4429fa98addbf3747a0aa0f84e85ce8466242f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:47 +0000

    Add package pyrogis/0.1.3/pyrogis-0.1.3-cp37-cp37m-win_amd64.whl

---
+++ packages/pyrogis/pyrogis-0.1.3-cp37-cp37m-win_amd64.whl/pyrogis/ingredients/seasonings/seasoning.py
@@ -0,0 +1,87 @@
+"""
+seasoning base ingredient
+"""
+
+import numpy as np
+
+from pyrogis.ingredients.ingredient import Ingredient
+
+
+class Seasoning(Ingredient):
+    """
+    Seasonings can perform their computation before the cook stage.
+
+    Seasonings are masks that use their season function to generate
+    a selection of pixels to be operated on by a cook operation.
+
+    They can be put into a mix to define a mask for a default
+    target.pixels.shape sized "all passing" array.
+    The can also be added to an ingredient through the season method.
+
+    Return from cook should be binary (black_pixel, or white_pixel)
+    """
+
+    def prep(
+            self,
+            target: Ingredient = None,
+            include_pixel: np.ndarray = None,
+            exclude_pixel: np.ndarray = None,
+            **kwargs
+    ):
+        """
+        :param target: If set, target will be the pixels that are cooked
+        :param include_pixel: The color to use for included pixels
+        Think of them like little flakes of seasoning
+        :param exclude_pixel: The color to use for excluded pixels
+        """
+        self.target = target
+
+        if include_pixel is None:
+            include_pixel = self._white_pixel
+        self.include_pixel = np.asarray(include_pixel)
+
+        if exclude_pixel is None:
+            exclude_pixel = self._black_pixel
+        self.exclude_pixel = np.asarray(exclude_pixel)
+
+    def season(self, recipient: Ingredient):
+        """
+        Set the input ingredient's mask to the output of a cook.
+
+        If self.target is none,
+        recipient will be the pixels that are cooked as well.
+
+        :param recipient: ingredient which will have its mask set
+        """
+
+        recipient_pixels = recipient.pixels
+
+        recipient.mask = self.cook(recipient_pixels)
+
+        return recipient
+
+    def cook(self, pixels: np.ndarray):
+        """
+        Pixels that match the include pixel should be set as the include pixel,
+        otherwise set as the exclude pixel
+
+        This isn't very useful, this is mostly an abstract class.
+        """
+        # tries to use self.target and defaults to the passed in pixels
+        target_pixels = pixels
+
+        if self.target is not None:
+            target_pixels = self.target.pixels
+
+        # turn pixels into boolean array
+        # True replaces a pixel that matches self.include_pixel
+        boolean_array = np.all(
+            target_pixels == self.include_pixel, axis=2
+        )
+
+        # replace True with include_pixel and False with exclude_pixel
+        binary_pixels = np.where(
+            boolean_array, self.include_pixel, self.exclude_pixel
+        )
+
+        return binary_pixels
---
+++ packages/pyrogis/pyrogis-0.1.3-cp37-cp37m-win_amd64.whl/pyrogis/ingredients/sort.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from .ingredient import Ingredient
+from .rotate import Rotate
+from .seasonings import Seasoning, Threshold
+
+
+class Sort(Ingredient):
+    """
+    sort a pixel array
+
+    uses its mask to determine which groups of pixels to sort
+    (white pixels get sorted)
+
+    can use a seasoning to create that mask when cooking,
+    or have it preloaded using a season method
+    """
+
+    def prep(self, rotate: Rotate = None, seasoning: Seasoning = Threshold(),
+             delimiter: np.ndarray = np.array([255, 255, 255]), **kwargs):
+        """
+        cook sorts from bottom to top after rotation, then unrotates
+        extra kwargs get passed to the Rotate if one is not provided
+
+        :param rotate: define the direction to rotate on
+        :param seasoning: create a mask from a seasoning
+        :param delimiter: pixel used as the sort subgroup delimiter
+        """
+        self.delimiter = delimiter
+
+        if rotate is None:
+            rotate = Rotate(**kwargs)
+
+        self.rotate = rotate
+
+        # apply seasoning as mask if provided
+        self.seasoning = seasoning
+
+    def cook(self, pixels: np.ndarray):
+        """
+        sort within each sequence group of contiguous white pixels
+        in the mask (may be all white)
+        """
+        # rotate self.mask and pixels to correspond to self.angle
+        rotate = self.rotate
+
+        if self.mask is None:
+            self.seasoning.target = Ingredient(pixels)
+            self.seasoning.season(self)
+
+        rotated_mask = rotate.cook(self.mask)
+        rotated_pixels = rotate.cook(pixels)
+
+        # false indicates that the pixel should not be sorted
+        boolean_array = np.all(rotated_mask == self._white_pixel, axis=2)
+
+        sorted_pixels = rotated_pixels
+        # loop through one axis
+        for i in range(rotated_pixels.shape[0]):
+            # get that axis
+            axis = rotated_pixels[i]
+            # and the axis for the mask-truth
+            boolean_axis = boolean_array[i]
+            # get the indices for this row on the mask that are True
+            masked_indices_axis = np.nonzero(boolean_axis)[0]
+            # split up the axis into sub groups at indices where mask is black
+            sort_groups = np.split(axis, masked_indices_axis)
+
+            sorted_groups = []
+            # loop through the groups
+            for group in sort_groups:
+                # np.sort(group)
+                # if the subgroup to be sorted contains 0 or 1 pixels, ignore
+                if group.size > 3:
+                    # intensity as the sorting criterion
+                    intensities = np.average(group, axis=1)
+                    # get "sort order" indices of the intensities of this group
+                    indices = np.argsort(intensities)
+                    # sort the group by these indices
+                    group = group[indices]
+                sorted_groups.append(group)
+
+            # concatenate the row back together, sorted in the mask
+            sorted_pixels[i] = np.concatenate(sorted_groups)
+
+        # unrotate sorted_pixels to return to correct orientation
+        unrotate = Rotate.unrotate(rotate)
+        sorted_pixels = unrotate.cook(sorted_pixels)
+
+        return sorted_pixels
