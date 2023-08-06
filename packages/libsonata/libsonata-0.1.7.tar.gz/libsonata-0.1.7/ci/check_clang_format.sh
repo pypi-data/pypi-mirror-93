5927cabe4e2fb8fbdc80d161ec571ba36f94f92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:12 +0000

    Add package libsonata/0.1.7/libsonata-0.1.7.tar.gz

---
+++ packages/libsonata/libsonata-0.1.7.tar.gz/libsonata-0.1.7/ci/check_clang_format.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install the desired clang-format, check the lines
+# changed by this diff are formatted correctly
+
+set -euo pipefail
+
+VENV=build/venv-clang-format
+CLANG_FORMAT_VERSION=9.0.0
+
+if [[ ! -d $VENV ]]; then
+    python3 -mvenv "$VENV"
+    "$VENV/bin/pip" install clang-format=="$CLANG_FORMAT_VERSION"
+fi
+
+set +u  # ignore errors in virtualenv's activate
+source "$VENV/bin/activate"
+set -u
+
+changes=$(git-clang-format 'HEAD~1')
+if [[ $(echo "$changes" | grep -n1 'changed files' | grep -v 'extlib') ]]; then
+    echo "The following files require changes to pass the current clang-format"
+    echo "$changes" | grep -v 'extlib'
+    exit 1
+fi
