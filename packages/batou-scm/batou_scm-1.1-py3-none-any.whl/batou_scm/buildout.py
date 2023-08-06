f6119e6780fecf2c391aead4d07c9f2785f49a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:24 +0000

    Add package batou-scm/1.1/batou_scm-1.1-py3-none-any.whl

---
+++ packages/batou-scm/batou_scm-1.1-py3-none-any.whl/batou_scm/buildout.py
@@ -0,0 +1,100 @@
+from batou import SilentConfigurationError
+from batou import UpdateNeeded
+from batou.lib.buildout import Buildout
+from batou.lib.file import Directory
+from batou.lib.file import File
+import pkg_resources
+
+
+class Buildout(Buildout):
+
+    python = '3.7'
+    version = '2.13.3'
+    setuptools = '49.2.0'
+
+    eggs_directory = '~/.batou-shared-eggs'
+    versionpins = None
+
+    def configure(self):
+        try:
+            self.source = self.require_one('source', self.host)
+        except SilentConfigurationError:
+            have_dists = False
+            self.source = None
+        else:
+            have_dists = len(self.source.distributions)
+
+        if have_dists:
+            self.dist_names, distributions = list(zip(
+                *sorted(self.source.distributions.items())))
+            self.dist_paths = [clone.target for clone in distributions]
+        else:
+            self.dist_names = []
+            self.dist_paths = []
+
+        # A directory for eggs shared by buildouts within the deployment is
+        # created for the service user. Assuming that a user cannot have
+        # multiple non-sandboxed deployments and that development deployments
+        # are sandboxed, eggs are never shared across deployments or users.
+        self.eggs_directory = Directory(self.eggs_directory)
+        self += self.eggs_directory
+
+        self.overrides = File(
+            'buildout_overrides.cfg',
+            source=pkg_resources.resource_filename(
+                'batou_scm', 'resources/buildout_overrides.cfg'))
+        self.additional_config += (self.overrides,)
+
+        super(Buildout, self).configure()
+
+    @property
+    def versions(self):
+        result = {'setuptools': self.setuptools, 'zc.buildout': self.version}
+        result.update({name: '' for name in self.dist_names})
+        return sorted(result.items())
+
+    __update_needed = None
+
+    def verify(self):
+        # We need to re-run buildout if any of the sources has changed.
+        # This check incurs network access for each source checkout, so we
+        # want to short-cut repeated calls.
+        if self.source and self.__update_needed is None:
+            try:
+                self.source.assert_no_changes()
+            except UpdateNeeded:
+                self.__update_needed = True
+            else:
+                self.__update_needed = False
+            for clone in self.source.clones.values():
+                if clone.has_changes():
+                    self.__update_needed = True
+        if self.__update_needed:
+            raise UpdateNeeded()
+        super(Buildout, self).verify()
+
+
+class BuildoutWithVersionPins(Buildout):
+    """Buildout which expects a version attribute on the source component.
+
+    It has to be a directory containing a file named `versions.cfg`.
+    """
+
+    def configure(self):
+        source = self.require_one('source', self.host)
+        self.versionpins = source.versions
+
+        # XXX We should depend on all clones we are using in the buildout.cfg,
+        # not just the versionpins.
+        self.additional_config += (self.versionpins, )
+
+        super(BuildoutWithVersionPins, self).configure()
+
+    def verify(self):
+        super(BuildoutWithVersionPins, self).verify()
+        # XXX Only checking has_changes won't detect advanced SCM operations
+        # like updating to an older revision, but as an 80-20 solution it's
+        # quite good enough.
+        if (self.versionpins.has_changes() or
+                self.versionpins.has_outgoing_changesets()):
+            raise UpdateNeeded()
