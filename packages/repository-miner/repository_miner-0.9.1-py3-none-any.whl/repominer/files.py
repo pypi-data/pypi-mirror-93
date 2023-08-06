fdc3fc1a90101253eef6f134e4fbb3234fae7947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:12 +0000

    Add package repository-miner/0.9.1/repository_miner-0.9.1-py3-none-any.whl

---
+++ packages/repository-miner/repository_miner-0.9.1-py3-none-any.whl/repominer/files.py
@@ -0,0 +1,100 @@
+import json
+from dataclasses import dataclass
+
+
+class FixedFileEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, FixedFile):
+            return {
+                "filepath": o.filepath,
+                "fic": o.fic,
+                "bic": o.bic
+            }
+
+        return json.JSONEncoder.default(self, o)
+
+
+class FixedFileDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.to_object, *args, **kwargs)
+
+    def to_object(self, o):
+        if type(o) == dict:
+            return FixedFile(filepath=o["filepath"],
+                             fic=o["fic"],
+                             bic=o["bic"])
+
+
+@dataclass
+class FixedFile:
+    """ This class stores information about fixed files (i.e., files modified by fixing commits)
+
+    Attributes
+    ----------
+    filepath : str
+        The file of the path at the bug-fixing commit
+    fic : str
+        The bug-fixing commit sha
+    bic : str
+        The bug-introducing commit sha
+
+    """
+
+    filepath: str
+    fic: str
+    bic: str
+
+    def __eq__(self, other):
+        if isinstance(other, FixedFile):
+            return self.filepath == other.filepath
+
+        return False
+
+
+class FailureProneFileEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, FailureProneFile):
+            return {
+                "filepath": o.filepath,
+                "commit": o.commit,
+                "fixing_commit": o.fixing_commit
+            }
+
+        return json.JSONEncoder.default(self, o)
+
+
+class FailureProneFileDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.to_object, *args, **kwargs)
+
+    def to_object(self, o):
+        if type(o) == dict:
+            return FailureProneFile(filepath=o["filepath"],
+                                    commit=o["commit"],
+                                    fixing_commit=o["fixing_commit"])
+
+
+@dataclass
+class FailureProneFile:
+    """ This class stores information about failure prone files
+
+    Attributes
+    ----------
+    filepath : str
+        The filepath relative to the repository's root
+    commit : str
+        The commit sha
+    fixing_commit : str
+        The bug-fixing commit sha
+
+    """
+
+    filepath: str
+    commit: str
+    fixing_commit: str
+
+    def __eq__(self, other):
+        if isinstance(other, FailureProneFile):
+            return self.filepath == other.filepath and self.commit == other.commit
+
+        return False
---
+++ packages/repository-miner/repository_miner-0.9.1-py3-none-any.whl/tests/mining/test_rules.py
@@ -0,0 +1,110 @@
+import unittest
+
+from repominer.mining import rules
+
+
+class RulesTestCase(unittest.TestCase):
+
+    @staticmethod
+    def test_has_defect_pattern_true():
+        assert rules.has_defect_pattern('fix wrong package dependency for Debian/Ubuntu')
+
+    @staticmethod
+    def test_has_defect_pattern_false():
+        assert not rules.has_defect_pattern('refactored code')
+
+    @staticmethod
+    def test_has_conditional_pattern_true():
+        assert rules.has_conditional_pattern('fix wrong condition in foo.yml')
+
+    @staticmethod
+    def test_has_conditional_pattern_false():
+        assert not rules.has_conditional_pattern('refactored code')
+
+    @staticmethod
+    def test_has_storage_configuration_pattern_true():
+        assert rules.has_storage_configuration_pattern('Fix sql query')
+
+    @staticmethod
+    def test_has_storage_configuration_pattern_false():
+        assert not rules.has_storage_configuration_pattern('refactored code')
+
+    @staticmethod
+    def test_has_file_configuration_pattern_true():
+        assert rules.has_file_configuration_pattern('fix permission to file by adding explicit mode')
+
+    @staticmethod
+    def test_has_file_configuration_pattern_false():
+        assert not rules.has_file_configuration_pattern('refactored code')
+
+    @staticmethod
+    def test_has_network_configuration_pattern_true():
+        assert rules.has_network_configuration_pattern('fix ip address binding')
+
+    @staticmethod
+    def test_has_network_configuration_pattern_false():
+        assert not rules.has_network_configuration_pattern('refactored code')
+
+    @staticmethod
+    def test_has_user_configuration_pattern_true():
+        assert rules.has_user_configuration_pattern('removed hard-coded string password')
+
+    @staticmethod
+    def test_has_user_configuration_pattern_false():
+        assert not rules.has_user_configuration_pattern('refactored code')
+
+    @staticmethod
+    def test_has_cache_configuration_pattern_true():
+        assert rules.has_cache_configuration_pattern('Delete cache after task execution')
+
+    @staticmethod
+    def test_has_cache_configuration_pattern_false():
+        assert not rules.has_cache_configuration_pattern('refactored code')
+
+    @staticmethod
+    def test_has_dependency_pattern_true():
+        assert rules.has_dependency_pattern('fix wrong package dependency for Debian/Ubuntu')
+
+    @staticmethod
+    def test_has_dependency_pattern_false():
+        assert not rules.has_dependency_pattern('refactored code')
+
+    @staticmethod
+    def test_has_documentation_pattern_true():
+        assert rules.has_documentation_pattern('Updated readme and specifications')
+
+    @staticmethod
+    def test_has_documentation_pattern_false():
+        assert not rules.has_documentation_pattern('refactored code')
+
+    @staticmethod
+    def test_has_idempotency_pattern_true():
+        assert rules.has_idempotency_pattern('Fix task primary-swift-proxy idempotency.')
+
+    @staticmethod
+    def test_has_idempotency_pattern_false():
+        assert not rules.has_idempotency_pattern('refactored code')
+
+    @staticmethod
+    def test_has_security_pattern_true():
+        assert rules.has_security_pattern('Deletes keystone token after deploying keystone to minimize security risk')
+
+    @staticmethod
+    def test_has_security_pattern_false():
+        assert not rules.has_security_pattern('refactored code')
+
+    @staticmethod
+    def test_has_service_pattern_true():
+        assert rules.has_service_pattern('Fix Upstart service issues')
+
+    @staticmethod
+    def test_has_service_pattern_false():
+        assert not rules.has_service_pattern('refactored code')
+
+    @staticmethod
+    def test_has_syntax_pattern_true():
+        assert rules.has_syntax_pattern('Fix lint issue')
+
+    @staticmethod
+    def test_has_syntax_pattern_false():
+        assert not rules.has_syntax_pattern('refactored code')
