7f06ec82a039ce3c597c51d2f89109451e262ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:10 +0000

    Add package xontrib-powerline2/1.5.0/xontrib-powerline2-1.5.0.tar.gz

---
+++ packages/xontrib-powerline2/xontrib-powerline2-1.5.0.tar.gz/xontrib-powerline2-1.5.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='xontrib-powerline2',
+    version='1.5.0',
+    description='Powerline for Xonsh shell',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/vaaaaanquish/xontrib-powerline2',
+    author='vaaaaanquish',
+    author_email='6syun9@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.xsh']},
+    platforms='any',
+)
