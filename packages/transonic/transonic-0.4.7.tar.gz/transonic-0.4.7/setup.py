cbc5fddcc72a29d3879af09bfafeecfca8cd81ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:57 +0000

    Add package transonic/0.4.7/transonic-0.4.7.tar.gz

---
+++ packages/transonic/transonic-0.4.7.tar.gz/transonic-0.4.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from runpy import run_path as run_path
+from pathlib import Path
+
+here = Path(__file__).parent.absolute()
+
+d = run_path(str(here / "transonic/_version.py"))
+__version__ = d["__version__"]
+__about__ = d["__about__"]
+
+print(__about__)
+
+setup(
+    version=__version__,
+    packages=find_packages(exclude=["doc", "tmp", "data_tests"]),
+    entry_points={"console_scripts": ["transonic = transonic.run:run"]},
+)
