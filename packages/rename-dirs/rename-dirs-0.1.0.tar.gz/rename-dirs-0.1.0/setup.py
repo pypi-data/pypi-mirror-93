252e02de4edbb519701d2e8f2f99789faed5e82d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:06 +0000

    Add package rename-dirs/0.1.0/rename-dirs-0.1.0.tar.gz

---
+++ packages/rename-dirs/rename-dirs-0.1.0.tar.gz/rename-dirs-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rename-dirs",  # Replace with your own username
+    version="0.1.0",
+    author="Rachit Gupta",
+    author_email="guptarachit2004@gmail.com",
+    description="A python script to rename multiple directories to lowercase words with underscore separators.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rchtgpt/rename-dirs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
