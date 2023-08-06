10fb4f4ad6e97b51f91a7ff225810decdacc6e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:22 +0000

    Add package six-pack-trade-algo/0.0.44/six-pack-trade-algo-0.0.44.tar.gz

---
+++ packages/six-pack-trade-algo/six-pack-trade-algo-0.0.44.tar.gz/six-pack-trade-algo-0.0.44/six_pack_trade_algo/GME.py
@@ -0,0 +1,6 @@
+api_endpoint = tradeapi.REST(
+            key_id="PK43JFFQPUJX72ELLEB1",
+            secret_key="XYYaN3t5mtdv4paWapjacwA12epOp0wpxvNF399r",
+            base_url="https://paper-api.alpaca.markets"
+)
+
