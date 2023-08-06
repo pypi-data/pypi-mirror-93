d4c3cdbded4c948f57f07d23360c288b5a8b345c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:48 +0000

    Add package FreshServiceApi/0.0.3/FreshServiceApi-0.0.3.tar.gz

---
+++ packages/FreshServiceApi/FreshServiceApi-0.0.3.tar.gz/FreshServiceApi-0.0.3/README.md
@@ -0,0 +1,1 @@
+This moudle is for the FreshService API.
---
+++ packages/FreshServiceApi/FreshServiceApi-0.0.3.tar.gz/FreshServiceApi-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Information Technology',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='FreshServiceApi',
+    version='0.0.3',
+    description='Modules for the FreshService API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Stephen Fitzsimmons',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='FreshService',
+    packages=find_packages(),
+    install_requires=['requests']
+)
