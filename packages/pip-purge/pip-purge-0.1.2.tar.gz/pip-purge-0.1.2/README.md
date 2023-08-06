521760005f5f7aa31324431e02d8eca934958ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:45 +0000

    Add package pip-purge/0.1.2/pip-purge-0.1.2.tar.gz

---
+++ packages/pip-purge/pip-purge-0.1.2.tar.gz/pip-purge-0.1.2/README.md
@@ -0,0 +1,22 @@
+[![Build Status](https://github.com/ParthS007/pip-purge/workflows/CI/badge.svg)](https://github.com/ParthS007/pip-purge/actions)
+
+# pip-purge: uninstall everything in your virtualenv.
+
+I don't like destroying and recreating virtualenvs to get a clean `pip freeze`.
+
+## Usage
+
+    $ pip-purge
+    Found 12 packages, uninstalling...
+    Purged!
+
+That's basically it.
+
+## Installation
+
+    $ pip install pip-purge
+
+Make sure to only run in a virtualenv ;)
+
+✨🍰✨
+
---
+++ packages/pip-purge/pip-purge-0.1.2.tar.gz/pip-purge-0.1.2/setup.py
@@ -0,0 +1,31 @@
+"""
+pip-purge manages your requirements files.
+"""
+from setuptools import setup
+
+setup(
+    name="pip-purge",
+    version="0.1.2",
+    url="https://github.com/kennethreitz/pip-purge",
+    license="MIT",
+    author="Kenneth Reitz",
+    author_email="me@kennethreitz.org",
+    MAINTAINER="Parth Shandilya",
+    MAINTAINER_EMAIL="parth1989shandilya@gmail.com",
+    description=__doc__.strip("\n"),
+    scripts=["pip-purge"],
+    zip_safe=False,
+    platforms="any",
+    install_requires=["envoy"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Systems Administration",
+    ],
+)
