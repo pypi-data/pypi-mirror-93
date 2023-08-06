e2c17c7515a3fdf8a02861fce2ce09acae4f2fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:46 +0000

    Add package djangoldp-energiepartagee/0.0.6/djangoldp_energiepartagee-0.0.6.tar.gz

---
+++ packages/djangoldp-energiepartagee/djangoldp_energiepartagee-0.0.6.tar.gz/djangoldp_energiepartagee-0.0.6/djangoldp_energiepartagee/djangoldp_urls.py
@@ -0,0 +1,12 @@
+"""djangoldp uploader URL Configuration"""
+
+from django.conf import settings
+from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
+
+from djangoldp_energiepartagee import views
+from djangoldp_energiepartagee.views import ContributionsCallView
+
+urlpatterns = [
+    url(r'^contributions/call/$', csrf_exempt(ContributionsCallView.as_view()), name='contributions-call'),
+]
