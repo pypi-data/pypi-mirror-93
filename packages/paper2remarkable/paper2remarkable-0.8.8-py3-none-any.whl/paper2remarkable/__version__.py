9abac85e2057a07dfe2b7665f4d9ba94ff5ac18d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:56 +0000

    Add package paper2remarkable/0.8.8/paper2remarkable-0.8.8-py3-none-any.whl

---
+++ packages/paper2remarkable/paper2remarkable-0.8.8-py3-none-any.whl/paper2remarkable/__version__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+VERSION = (0, 8, 8)
+
+__version__ = ".".join(map(str, VERSION))
---
+++ packages/paper2remarkable/paper2remarkable-0.8.8-py3-none-any.whl/paper2remarkable/providers/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from .acm import ACM
+from .arxiv import Arxiv
+from .citeseerx import CiteSeerX
+from .cvf import CVF
+from .html import HTML
+from .jmlr import JMLR
+from .local import LocalFile
+from .nature import Nature
+from .nber import NBER
+from .neurips import NeurIPS
+from .openreview import OpenReview
+from .pdf_url import PdfUrl
+from .pmlr import PMLR
+from .pubmed import PubMed
+from .sagepub import SagePub
+from .science_direct import ScienceDirect
+from .semantic_scholar import SemanticScholar
+from .springer import Springer
+from .tandfonline import TandFOnline
+
+# NOTE: Order matters here, PdfUrl and HTML should be last
+providers = [
+    ACM,
+    Arxiv,
+    CiteSeerX,
+    CVF,
+    JMLR,
+    Nature,
+    NBER,
+    NeurIPS,
+    OpenReview,
+    PMLR,
+    PubMed,
+    SagePub,
+    ScienceDirect,
+    Springer,
+    SemanticScholar,
+    TandFOnline,
+    LocalFile,
+    PdfUrl,
+    HTML,
+]
