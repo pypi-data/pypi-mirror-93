5970d24295ead0d63b7db03086a3c7331361cb0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:19 +0000

    Add package img-processor/0.8.0/img_processor-0.8.0.tar.gz

---
+++ packages/img-processor/img_processor-0.8.0.tar.gz/img_processor-0.8.0/img_processor/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for img_processor."""
+
+__author__ = """Justin Keller"""
+__email__ = "kellerjustin@protonmail.com"
+__version__ = "__version__ = '0.8.0'"
+
+
+from .img_processor import ImageProcessor  # noqa
