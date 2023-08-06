eee4f084d40c932568ee1f18c471b890e2cb2f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:00 +0000

    Add package ROHSApy/0.2.3/ROHSApy-0.2.3.tar.gz

---
+++ packages/ROHSApy/ROHSApy-0.2.3.tar.gz/ROHSApy-0.2.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('Readme.md') as f:
+        readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+    
+setup(
+    name='ROHSApy',
+    version='0.2.3',
+    description='python interface package for ROHSA software',
+    long_description=readme,
+    classifiers=[
+        'Development status :: 1 - Alpha',
+        'License :: CC-By-SA2.0',
+        'Programming Language :: Python',
+        'Topic :: Data Analysis'
+    ],
+    author='Antoine Marchal',
+    author_email='amarchal@cita.utoronto.ca',
+    url='https://github.com/antoinemarchal/ROHSA',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+            'numpy',
+            'matplotlib',
+            'astropy'
+    ],
+    include_package_data=True
+)
