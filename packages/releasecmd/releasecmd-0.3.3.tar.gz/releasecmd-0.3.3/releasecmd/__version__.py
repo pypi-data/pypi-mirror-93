9474542f546388f2e5210c651a353280ab144825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package releasecmd/0.3.3/releasecmd-0.3.3.tar.gz

---
+++ packages/releasecmd/releasecmd-0.3.3.tar.gz/releasecmd-0.3.3/releasecmd/__version__.py
@@ -0,0 +1,6 @@
+__author__ = "Tsuyoshi Hombashi"
+__copyright__ = "Copyright 2018, {}".format(__author__)
+__license__ = "MIT License"
+__version__ = "0.3.3"
+__maintainer__ = __author__
+__email__ = "tsuyoshi.hombashi@gmail.com"
