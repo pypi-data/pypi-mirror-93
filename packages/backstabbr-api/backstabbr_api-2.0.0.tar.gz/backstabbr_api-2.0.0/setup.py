f490cd0384241b0da7bb8d6d07ba4c87d2f7c1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:56 +0000

    Add package backstabbr-api/2.0.0/backstabbr_api-2.0.0.tar.gz

---
+++ packages/backstabbr-api/backstabbr_api-2.0.0.tar.gz/backstabbr_api-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='backstabbr_api',
+		version='2.0.0',
+		description='Web-scraper API and Discord Bot for the online diplomacy program Backstabbr',
+		url='https://github.com/afkhurana/backstabbr_api',
+		author='Arjun Khurana',
+		author_email='afkhurana@gmail.com',
+		license='MIT',
+		packages=['backstabbr_api', 'backstabbr_bot'],
+		install_requires=["discord.py", "html5print", "requests"])
