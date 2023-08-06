f0dca8ae8066a0ee320263c87bfd29265d9eee5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:42 +0000

    Add package meraki-cli/1.0.2/meraki_cli-1.0.2-py3-none-any.whl

---
+++ packages/meraki-cli/meraki_cli-1.0.2-py3-none-any.whl/meraki_cli/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+
+'''
+Main import file used when 'import meraki-cli'
+'''
+
+
+from . import __main__
+from . import __version__
+
+
+name = "meraki_cli"
+__version__ = __version__.version
