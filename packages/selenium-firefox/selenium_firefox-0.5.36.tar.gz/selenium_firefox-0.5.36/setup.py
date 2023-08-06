594efa3a4a744f511f33313969d918abcadc02f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package selenium-firefox/0.5.36/selenium_firefox-0.5.36.tar.gz

---
+++ packages/selenium-firefox/selenium_firefox-0.5.36.tar.gz/selenium_firefox-0.5.36/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium_firefox",
+    version="0.5.36",
+    author="Kovács Kristóf-Attila",
+    description="selenium_firefox",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/selenium_firefox",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'geckodriver-autoinstaller',
+        'noraise',
+        'selenium',
+        'tldextract'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+)
