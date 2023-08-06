cc5f9da8e43fbf5d1a0dbb871148c070cfe303e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:38 +0000

    Add package clscreen/0.0.2/clscreen-0.0.2.tar.gz

---
+++ packages/clscreen/clscreen-0.0.2.tar.gz/clscreen-0.0.2/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='clscreen',
+    version='0.0.2',
+    author='Flyin Ryan',
+    author_email='haasrr@etsu.edu',
+    description='Just clear the screen for goodness sake.',
+    long_description='Just clear the screen for goodness sake. What more do I need to say?',
+    long_description_content_type='text/x-rst',
+    packages=['clscreen'],
+    classifiers=[
+        'Intended Audience :: Other Audience',
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    python_requires='>=3.0',
+)
