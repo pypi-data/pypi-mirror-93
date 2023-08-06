1628d8a9bdb21b1ca58e5e4a571d825811469a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:39 +0000

    Add package StyleTransferTensorFlow/0.0.13/StyleTransferTensorFlow-0.0.13.tar.gz

---
+++ packages/StyleTransferTensorFlow/StyleTransferTensorFlow-0.0.13.tar.gz/StyleTransferTensorFlow-0.0.13/StyleTransferTensorFlow/__init__.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+from pytube import YouTube
+import os
+import cv2
+from PIL import Image
+import shutil
+import glob
+import ffmpy
+
+from StyleTransferTensorFlow import extract
+from StyleTransferTensorFlow import style
+from StyleTransferTensorFlow import convert
