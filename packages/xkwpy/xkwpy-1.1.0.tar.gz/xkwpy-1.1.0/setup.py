5e208061dfae80be93afff88cbfd136ee8fbbdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:38 +0000

    Add package xkwpy/1.1.0/xkwpy-1.1.0.tar.gz

---
+++ packages/xkwpy/xkwpy-1.1.0.tar.gz/xkwpy-1.1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='xkwpy',
+    version='1.1.0',
+    description='you can use it to change your texts format',
+    author='royalxkw',
+    url='https://github.com',
+    author_email='xkwwangyi@163.com',
+)
