e493443aa5501578504f7ee2086e38a08e58716b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:32 +0000

    Add package clvm-rs/0.1.0/clvm_rs-0.1.0.tar.gz

---
+++ packages/clvm-rs/clvm_rs-0.1.0.tar.gz/src/py/api.rs
@@ -0,0 +1,154 @@
+use super::arc_allocator::{ArcAllocator, ArcSExp};
+use super::native_op_lookup::NativeOpLookup;
+use super::py_node::PyNode;
+use crate::node::Node;
+use crate::py::run_program::__pyo3_get_function_serialize_and_run_program;
+use crate::reduction::{EvalErr, Reduction};
+use crate::run_program::{run_program, OperatorHandler, PostEval, PreEval};
+use crate::serialize::{node_from_bytes, node_to_bytes};
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyDict, PyString};
+use pyo3::wrap_pyfunction;
+use pyo3::PyObject;
+
+fn note_result(obj: &PyObject, result: Option<&ArcSExp>) {
+    Python::with_gil(|py| {
+        if let Some(node) = result {
+            let node: PyNode = node.into();
+            let _r: PyResult<PyObject> = obj.call1(py, (node,));
+        }
+    });
+}
+
+fn post_eval_for_pyobject(obj: PyObject) -> Option<Box<PostEval<ArcAllocator>>> {
+    let py_post_eval: Option<Box<PostEval<ArcAllocator>>> =
+        if Python::with_gil(|py| obj.is_none(py)) {
+            None
+        } else {
+            Some(Box::new(move |result: Option<&ArcSExp>| {
+                note_result(&obj, result)
+            }))
+        };
+    py_post_eval
+}
+
+#[pyfunction]
+#[allow(clippy::too_many_arguments)]
+fn py_run_program(
+    py: Python,
+    program: &PyNode,
+    args: &PyNode,
+    quote_kw: u8,
+    apply_kw: u8,
+    max_cost: u32,
+    op_lookup: NativeOpLookup,
+    pre_eval: PyObject,
+) -> PyResult<(u32, PyNode)> {
+    let allocator = ArcAllocator::new();
+
+    let py_pre_eval_t: Option<PreEval<ArcAllocator>> = if pre_eval.is_none(py) {
+        None
+    } else {
+        Some(Box::new(move |program: &ArcSExp, args: &ArcSExp| {
+            Python::with_gil(|py| {
+                let program_clone: PyNode = program.into();
+                let args: PyNode = args.into();
+                let r: PyResult<PyObject> = pre_eval.call1(py, (program_clone, args));
+                match r {
+                    Ok(py_post_eval) => {
+                        let f = post_eval_for_pyobject(py_post_eval);
+                        Ok(f)
+                    }
+                    Err(ref err) => allocator.err(program, &err.to_string()),
+                }
+            })
+        }))
+    };
+
+    // BRAIN DAMAGE: we create a second `ArcAllocator` here
+    // This only works because this allocator type has the property that
+    // you can create a pair from nodes from different allocators.
+
+    let allocator: ArcAllocator = ArcAllocator::new();
+    let f: OperatorHandler<ArcAllocator> =
+        Box::new(move |allocator, op, args| op_lookup.operator_handler(allocator, op, args));
+
+    let r: Result<Reduction<ArcSExp>, EvalErr<ArcSExp>> = run_program(
+        &allocator,
+        &program.into(),
+        &args.into(),
+        quote_kw,
+        apply_kw,
+        max_cost,
+        &f,
+        py_pre_eval_t,
+    );
+    match r {
+        Ok(reduction) => Ok((reduction.0, reduction.1.into())),
+        Err(eval_err) => {
+            let node: PyNode = eval_err.0.into();
+            let s: String = eval_err.1;
+            let s1: &str = &s;
+            let msg: &PyString = PyString::new(py, s1);
+            let sexp_any: PyNode = node;
+            match raise_eval_error(py, &msg, &sexp_any) {
+                Err(x) => Err(x),
+                _ => panic!(),
+            }
+        }
+    }
+}
+
+#[pyfunction]
+fn raise_eval_error(py: Python, msg: &PyString, sexp: &PyNode) -> PyResult<PyObject> {
+    let local_sexp: PyNode = sexp.clone();
+    let sexp_any: PyObject = local_sexp.into_py(py);
+    let msg_any: PyObject = msg.into_py(py);
+
+    let s0: &PyString = PyString::new(py, "msg");
+    let s1: &PyString = PyString::new(py, "sexp");
+    let ctx: &PyDict = PyDict::new(py);
+    ctx.set_item(s0, msg_any)?;
+    ctx.set_item(s1, sexp_any)?;
+
+    let r = py.run(
+        "from clvm.EvalError import EvalError; raise EvalError(msg, sexp)",
+        None,
+        Some(ctx),
+    );
+    match r {
+        Err(x) => Err(x),
+        Ok(_) => Ok(ctx.into()),
+    }
+}
+
+#[pyfunction]
+fn serialize_from_bytes(blob: &[u8]) -> PyNode {
+    let allocator: ArcAllocator = ArcAllocator::new();
+    node_from_bytes(&allocator, blob).unwrap().into()
+}
+
+#[pyfunction]
+fn serialize_to_bytes(py: Python, sexp: &PyNode) -> PyObject {
+    let allocator: ArcAllocator = ArcAllocator::new();
+    let node_t: Node<ArcAllocator> = Node::new(&allocator, sexp.into());
+    let blob = node_to_bytes(&node_t).unwrap();
+    let pybytes = PyBytes::new(py, &blob);
+    pybytes.to_object(py)
+}
+
+/// This module is a python module implemented in Rust.
+#[pymodule]
+fn clvm_rs(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(py_run_program, m)?)?;
+    m.add_function(wrap_pyfunction!(serialize_and_run_program, m)?)?;
+    m.add_function(wrap_pyfunction!(serialize_from_bytes, m)?)?;
+    m.add_function(wrap_pyfunction!(serialize_to_bytes, m)?)?;
+
+    m.add_class::<PyNode>()?;
+    m.add_class::<NativeOpLookup>()?;
+
+    m.add_function(wrap_pyfunction!(raise_eval_error, m)?)?;
+
+    Ok(())
+}
---
+++ packages/clvm-rs/clvm_rs-0.1.0.tar.gz/src/py/py_node.rs
@@ -0,0 +1,167 @@
+use super::arc_allocator::{ArcAllocator, ArcSExp};
+use crate::allocator::{Allocator, SExp};
+use std::cell::RefCell;
+use std::sync::Arc;
+
+use pyo3::exceptions::PyValueError;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyTuple};
+
+#[pyclass(subclass, unsendable)]
+#[derive(Clone)]
+pub struct PyNode {
+    node: ArcSExp,
+    pyobj: RefCell<Option<PyObject>>,
+}
+
+fn extract_atom(_allocator: &ArcAllocator, obj: &PyAny) -> PyResult<PyNode> {
+    let py_bytes: &PyBytes = obj.extract()?;
+    let r: &[u8] = obj.extract()?;
+    let r1: Vec<u8> = r.to_owned();
+    let r2: Arc<Vec<u8>> = Arc::new(r1);
+    let inner_node: ArcSExp = ArcSExp::Atom(r2);
+    let py_node = PyNode {
+        node: inner_node,
+        pyobj: RefCell::new(Some(py_bytes.into())),
+    };
+    //println!("py_bytes is {:?}", py_node.bytes);
+    Ok(py_node)
+}
+
+fn extract_node<'a>(_allocator: &ArcAllocator, obj: &'a PyAny) -> PyResult<PyRef<'a, PyNode>> {
+    let ps: &PyCell<PyNode> = obj.downcast()?;
+    let node: PyRef<'a, PyNode> = ps.try_borrow()?;
+    Ok(node)
+}
+
+fn extract_tuple(allocator: &ArcAllocator, obj: &PyAny) -> PyResult<PyNode> {
+    let v: &PyTuple = obj.downcast()?;
+    if v.len() != 2 {
+        return Err(PyValueError::new_err("SExp tuples must be size 2"));
+    }
+    let i0: &PyAny = v.get_item(0);
+    let i1: &PyAny = v.get_item(1);
+    let left: PyRef<PyNode> = extract_node(&allocator, i0)?;
+    let right: PyRef<PyNode> = extract_node(&allocator, i1)?;
+    let left: &PyNode = &left;
+    let right: &PyNode = &right;
+    let left: ArcSExp = left.into();
+    let right: ArcSExp = right.into();
+    let node: ArcSExp = allocator.new_pair(&left, &right);
+    let py_node = PyNode {
+        node,
+        pyobj: RefCell::new(Some(obj.into())),
+    };
+
+    Ok(py_node)
+}
+
+impl From<&ArcSExp> for PyNode {
+    fn from(item: &ArcSExp) -> Self {
+        item.clone().into()
+    }
+}
+
+impl From<ArcSExp> for PyNode {
+    fn from(item: ArcSExp) -> Self {
+        PyNode {
+            node: item,
+            pyobj: RefCell::new(None),
+        }
+    }
+}
+
+#[pymethods]
+impl PyNode {
+    #[new]
+    pub fn py_new(obj: &PyAny) -> PyResult<Self> {
+        let allocator = ArcAllocator::new();
+        let node: PyNode = {
+            let n = extract_atom(&allocator, obj);
+            if let Ok(r) = n {
+                r
+            } else {
+                extract_tuple(&allocator, obj)?
+            }
+        };
+        Ok(node)
+    }
+
+    #[getter(pair)]
+    pub fn pair(&self, py: Python) -> PyResult<Option<PyObject>> {
+        match ArcAllocator::new().sexp(&self.node) {
+            SExp::Pair(p1, p2) => {
+                {
+                    let mut borrowed_pair = self.pyobj.borrow_mut();
+                    if borrowed_pair.is_none() {
+                        let r1 = PyCell::new(
+                            py,
+                            PyNode {
+                                node: p1,
+                                pyobj: RefCell::new(None),
+                            },
+                        )?;
+                        let r2 = PyCell::new(
+                            py,
+                            PyNode {
+                                node: p2,
+                                pyobj: RefCell::new(None),
+                            },
+                        )?;
+                        let v: &PyTuple = PyTuple::new(py, &[r1, r2]);
+                        let v: PyObject = v.into();
+                        *borrowed_pair = Some(v);
+                    }
+                };
+                Ok(self.pyobj.borrow().clone())
+            }
+            _ => Ok(None),
+        }
+    }
+
+    pub fn _pair(&self) -> Option<(PyNode, PyNode)> {
+        match ArcAllocator::new().sexp(&self.node) {
+            SExp::Pair(p1, p2) => Some((p1.into(), p2.into())),
+            _ => None,
+        }
+    }
+
+    #[getter(atom)]
+    pub fn atom(&self, py: Python) -> Option<PyObject> {
+        match ArcAllocator::new().sexp(&self.node) {
+            SExp::Atom(a) => {
+                {
+                    let mut borrowed_bytes = self.pyobj.borrow_mut();
+                    if borrowed_bytes.is_none() {
+                        let b: &PyBytes = PyBytes::new(py, a);
+                        let obj: PyObject = b.into();
+                        *borrowed_bytes = Some(obj);
+                    };
+                }
+                self.pyobj.borrow().clone()
+            }
+            _ => None,
+        }
+    }
+}
+
+impl PyNode {
+    pub fn new(_allocator: &Arc<ArcAllocator>, node: ArcSExp) -> Self {
+        PyNode {
+            node,
+            pyobj: RefCell::new(None),
+        }
+    }
+}
+
+impl From<&PyNode> for ArcSExp {
+    fn from(node: &PyNode) -> Self {
+        node.clone().into()
+    }
+}
+
+impl From<PyNode> for ArcSExp {
+    fn from(node: PyNode) -> Self {
+        node.node
+    }
+}
