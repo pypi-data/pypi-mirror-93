9ca1864f0ac850d548b5469c35c22bcc43a28906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:24 +0000

    Add package diffprivlib/0.4.1/diffprivlib-0.4.1.tar.gz

---
+++ packages/diffprivlib/diffprivlib-0.4.1.tar.gz/diffprivlib-0.4.1/setup.py
@@ -0,0 +1,65 @@
+import codecs
+import os
+
+import setuptools
+
+
+def read(file_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+
+    with codecs.open(os.path.join(here, file_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(file_path):
+    for line in read(file_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+
+    raise RuntimeError("Unable to find version string.")
+
+
+install_requires = ['numpy >= 1.17.0',
+                    'setuptools >= 39.0.1',
+                    'scikit-learn >= 0.22.0',
+                    'scipy >= 1.2.1',
+                    'joblib >= 0.11',
+                    ]
+
+docs_require = ['sphinx >= 1.4',
+                'sphinx_rtd_theme']
+
+setuptools.setup(name='diffprivlib',
+                 version=get_version("diffprivlib/__init__.py"),
+                 description='IBM Differential Privacy Library',
+                 long_description=read("README.md"),
+                 long_description_content_type='text/markdown',
+                 author='Naoise Holohan',
+                 author_email='naoise.holohan@ibm.com',
+                 url='https://github.com/IBM/differential-privacy-library',
+                 license='MIT',
+                 install_requires=install_requires,
+                 extras_require={
+                     'docs': docs_require
+                 },
+                 python_requires='>=3.6',
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved',
+                     'License :: OSI Approved :: MIT License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3 :: Only',
+                     'Programming Language :: Python :: 3.6',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Topic :: Software Development :: Libraries',
+                     'Topic :: Software Development :: Libraries :: Python Modules',
+                     'Topic :: Scientific/Engineering',
+                     'Topic :: Security',
+                 ],
+                 packages=setuptools.find_packages())
