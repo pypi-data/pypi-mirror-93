bf6c2b4a1f038fed292e317624c4499fd4fab9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:08 +0000

    Add package evidently/0.1.6.dev0/evidently-0.1.6.dev0.tar.gz

---
+++ packages/evidently/evidently-0.1.6.dev0.tar.gz/evidently-0.1.6.dev0/evidently/tabs/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from .drift_tab import DriftTab 
+from .num_target_drift_tab import NumTargetDriftTab
+from .cat_target_drift_tab import CatTargetDriftTab
+from .regression_performance_tab import RegressionPerformanceTab
+
