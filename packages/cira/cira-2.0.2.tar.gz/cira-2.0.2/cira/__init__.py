908683a6a28dc30bd4a83da0349e8a6cfbe69d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:12 +0000

    Add package cira/2.0.2/cira-2.0.2.tar.gz

---
+++ packages/cira/cira-2.0.2.tar.gz/cira-2.0.2/cira/__init__.py
@@ -0,0 +1,19 @@
+"""
+Cira
+
+A simpler libray for alpaca-trade-api from Alpaca Markets.
+"""
+
+import alpaca_trade_api as tradeapi
+
+from . import alpaca
+from . import config
+from . import util
+from . import logging
+from .exchange import Exchange
+from .stock import Stock
+from .portfolio import Portfolio
+
+__version__ = "2.0.2"
+__author__ = "Axel Gard"
+__credits__ = "alpaca.markets"
---
+++ packages/cira/cira-2.0.2.tar.gz/cira-2.0.2/cira/config.py
@@ -0,0 +1,6 @@
+# logging
+IS_LOGGING = False
+LOG_FILE = ""
+
+# debugging
+DEBUG = False
