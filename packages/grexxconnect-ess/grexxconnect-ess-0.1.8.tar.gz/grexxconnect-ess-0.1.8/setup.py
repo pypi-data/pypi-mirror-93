a78e19a00f39d7dabce9abfa441cbdd6b4e6d221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:58 +0000

    Add package grexxconnect-ess/0.1.8/grexxconnect-ess-0.1.8.tar.gz

---
+++ packages/grexxconnect-ess/grexxconnect-ess-0.1.8.tar.gz/grexxconnect-ess-0.1.8/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="grexxconnect-ess",
+    version="0.1.8",
+    description="Grexx External System Service Helper",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Grexx",
+    author_email="servicedesk@grexx.net",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=["websockets", "colorlog"],
+)
