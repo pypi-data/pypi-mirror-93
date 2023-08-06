8553393ef52495166d9c46765affac488090a7e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:46 +0000

    Add package numba-stats/0.1.1/numba-stats-0.1.1.tar.gz

---
+++ packages/numba-stats/numba-stats-0.1.1.tar.gz/numba-stats-0.1.1/numba_stats/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.1"
+
+from .stats import (  # noqa
+    norm_pdf,
+    norm_cdf,
+    norm_ppf,
+    poisson_pmf,
+    poisson_cdf,
+    expon_pdf,
+    expon_cdf,
+    expon_ppf,
+)
