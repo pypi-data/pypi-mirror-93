b9fb74dddf6b1de83197854b3713bcb58c7da7d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:31 +0000

    Add package hamamatsu/0.1.1/hamamatsu-0.1.1.tar.gz

---
+++ packages/hamamatsu/hamamatsu-0.1.1.tar.gz/hamamatsu-0.1.1/hamamatsu/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the hamamatsu project
+#
+# Copyright (c) 2021 Tiago Coutinho
+# Distributed under the GPLv3 license. See LICENSE for more info.
+
+__version__ = '0.1.1'
