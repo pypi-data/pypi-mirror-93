637607e439df3697117d6b07eb0d7dc000f66831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:16 +0000

    Add package truefalsepython/0.1.5/truefalsepython-0.1.5.tar.gz

---
+++ packages/truefalsepython/truefalsepython-0.1.5.tar.gz/truefalsepython-0.1.5/truefalsepython/__init__.py
@@ -0,0 +1,13 @@
+
+from .logical import TRUE, true, T, FALSE, F, false, NULL
+from .little import is_number, is_odd, is_even
+
+from .r_funcs import nrow, ncol, colMeans, rowMeans, colSums, rowSums, apply, lapply, sapply, sample, sample_int
+
+from .useful_funcs import fast_sample, randomTrue
+
+
+
+from .other import time_to_seconds
+
+
