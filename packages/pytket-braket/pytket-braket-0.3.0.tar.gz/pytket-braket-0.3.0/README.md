0672abc091a5728b12283166b98bb554b03834b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:52 +0000

    Add package pytket-braket/0.3.0/pytket-braket-0.3.0.tar.gz

---
+++ packages/pytket-braket/pytket-braket-0.3.0.tar.gz/pytket-braket-0.3.0/README.md
@@ -0,0 +1,21 @@
+# pytket-braket
+
+[Pytket](https://cqcl.github.io/pytket) is a Python module for interfacing
+with CQC tket, a set of quantum programming tools.
+
+`pytket-braket` is an extension to `pytket` that allows `pytket` circuits to be
+executed on a range of devices and simulators via Amazon's Braket service.
+
+## Getting started
+
+`pytket-braket` is available for Python 3.7 and 3.8, on Linux, MacOS and
+Windows. To install, run:
+
+```pip install pytket-braket```
+
+## LICENCE
+
+Copyright 2020-2021 Cambridge Quantum Computing
+
+You may not use this product except in compliance with the Licence. You may view
+the License [here](https://cqcl.github.io/pytket/build/html/licence.html).
---
+++ packages/pytket-braket/pytket-braket-0.3.0.tar.gz/pytket-braket-0.3.0/setup.py
@@ -0,0 +1,49 @@
+# Copyright 2020-2021 Cambridge Quantum Computing
+#
+# You may not use this file except in compliance with the Licence.
+# You may obtain a copy of the Licence in the LICENCE file accompanying
+# these documents or at:
+#
+#     https://cqcl.github.io/pytket/build/html/licence.html
+
+import setuptools  # type: ignore
+from setuptools import setup, find_namespace_packages
+import shutil
+import os
+
+metadata: dict = {}
+with open("_metadata.py") as fp:
+    exec(fp.read(), metadata)
+shutil.copy(
+    "_metadata.py",
+    os.path.join("pytket", "extensions", "backends", "braket", "_metadata.py"),
+)
+
+setup(
+    name=metadata["__extension_name__"],
+    version=metadata["__extension_version__"],
+    author="Alec Edgington",
+    author_email="alec.edgington@cambridgequantum.com",
+    python_requires=">=3.7",
+    url="https://github.com/CQCL/pytket",
+    description="Extension for pytket, providing access to Amazon Braket backends",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="CQC Software Licence",
+    packages=find_namespace_packages(include=["pytket.*"]),
+    include_package_data=True,
+    install_requires=["pytket ~= 0.7", "amazon-braket-sdk ~= 1.0"],
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: Other/Proprietary License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+    zip_safe=False,
+)
