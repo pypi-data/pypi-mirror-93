8439c542bff682ddce599c28a5e22f3389614d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package mf4parser/0.0.5/mf4parser-0.0.5.tar.gz

---
+++ packages/mf4parser/mf4parser-0.0.5.tar.gz/mf4parser-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md') as fh:
+	README = fh.read()
+
+
+setup(
+    name="mf4parser",
+    version="0.0.5",
+    description="A Python package to extract signals from MDF4 files.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sridhar-eswaran/mf4-parser.git",
+    author="Sridhar Eswaran",
+    author_email="mail2sridhare@gmail.com",
+    license="GNU GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Operating System :: OS Independent",
+    ],
+	py_modules=["mf4parser"],
+    package_dir={'':'src'},
+    include_package_data=True,
+    install_requires=["asammdf","pandas","numpy"],
+)
