ffc244ac098e6d78df1009c12aca369c5e3e4786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package Apppath/0.5.6/Apppath-0.5.6-py36-none-any.whl

---
+++ packages/Apppath/Apppath-0.5.6-py36-none-any.whl/samples/apppath_demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 05/04/2020
+           """
+
+from apppath import AppPath
+
+if __name__ == "__main__":
+
+    def main():
+        apppath = AppPath("AppPath")
+        print(apppath.user_config)
+        print(apppath.user_log)
+        print(apppath.user_data)
+        print(apppath.user_cache)
+
+    main()
