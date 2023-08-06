0ce444dcd33fca3645316d34f0feb74557752d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:51 +0000

    Add package aiotrading/0.0.4/aiotrading-0.0.4-py3-none-any.whl

---
+++ packages/aiotrading/aiotrading-0.0.4-py3-none-any.whl/aiotrading/trade.py
@@ -0,0 +1,16 @@
+
+class Trade:
+
+    def __init__(self, symbol, id, time, price, volume, buy):
+        self.symbol = symbol
+        self.id = id
+        self.time = time
+        self.price = price
+        self.volume = volume
+        self.buy = buy
+
+    def __str__(self):
+        return f'trade symbol:{self.symbol}, id:{self.id}, time:{self.time}, price:{self.price}, volume:{self.volume}'
+
+    def __repr__(self):
+        return self.__str__()
