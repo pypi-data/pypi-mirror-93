28d453d338de79540e46813247a788a0eb0d9bd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:23 +0000

    Add package pyaviso/0.9.2/pyaviso-0.9.2.tar.gz

---
+++ packages/pyaviso/pyaviso-0.9.2.tar.gz/pyaviso-0.9.2/setup.py
@@ -0,0 +1,57 @@
+# (C) Copyright 1996- ECMWF.
+# 
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation
+# nor does it submit to any jurisdiction.
+
+import io
+import re
+import pathlib
+from setuptools import setup, find_packages
+
+__version__ = re.search(
+    r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',  # It excludes inline comment too
+    io.open('pyaviso/version.py', encoding='utf_8_sig').read()
+).group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+INSTALL_REQUIRES = (pathlib.Path(__file__).parent / "requirements.txt").read_text().splitlines()
+
+setup(
+    name='pyaviso',
+    author="ECMWF",
+    author_email="software.support@ecmwf.int",
+    license="Apache 2.0",
+    url='https://github.com/ecmwf/aviso',
+    description="Time-critical notification system designed to trigger users' workflows across HPC and Cloud systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=__version__,
+    packages=find_packages(exclude=("tests", "aviso-server")),
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    entry_points={
+        'console_scripts': [
+            'aviso=pyaviso.cli_aviso:cli',
+            'aviso-config=pyaviso.cli_aviso_config:cli'
+        ]
+    }
+)
