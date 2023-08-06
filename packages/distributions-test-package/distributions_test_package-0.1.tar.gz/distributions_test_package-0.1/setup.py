7dc297caa7e983e406c0166b25011405a9ca803f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package distributions-test-package/0.1/distributions_test_package-0.1.tar.gz

---
+++ packages/distributions-test-package/distributions_test_package-0.1.tar.gz/distributions_test_package-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_test_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_test_package'],
+      author = 'Julien Jaber',
+      author_email = 'julien-jaber@hotmail.com',
+      zip_safe=False) # package can't be run directly from a zip file
