4b0e811548a933c87a0044fbcdc18c20938efb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:32 +0000

    Add package oso/0.10.1/oso-0.10.1-cp39-cp39-win_amd64.whl

---
+++ packages/oso/oso-0.10.1-cp39-cp39-win_amd64.whl/polar/__init__.py
@@ -0,0 +1,6 @@
+from .polar import Polar, polar_class
+from .query import Query, QueryResult
+from .variable import Variable
+from .predicate import Predicate
+from .partial import Partial
+from .expression import Expression, Pattern
