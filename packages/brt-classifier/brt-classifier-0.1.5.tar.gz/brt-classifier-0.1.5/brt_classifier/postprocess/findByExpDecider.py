21559093eb59e076e17b6a84be53c1e2a40b9868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:04 +0000

    Add package brt-classifier/0.1.5/brt-classifier-0.1.5.tar.gz

---
+++ packages/brt-classifier/brt-classifier-0.1.5.tar.gz/brt-classifier-0.1.5/brt_classifier/postprocess/findByExpDecider.py
@@ -0,0 +1,29 @@
+from rooted_tree_classifier import is_log_solvable, is_log_star_solvable
+from ..complexity import complexities
+
+def findByExpDecider(problem, idx, data):
+  constraints = set(problem["constraint"])
+  lowerBound = problem["lower-bound"]
+  upperBound = problem["upper-bound"]
+
+  if upperBound == "" or lowerBound == "":
+    decidedUpperBound = upperBound
+    decidedLowerBound = lowerBound
+
+    if is_log_star_solvable(constraints):
+      decidedUpperBound = complexities[min(complexities.index(decidedUpperBound) if decidedUpperBound != "" else 100, complexities.index("(log* n)"))]
+    else:
+      decidedLowerBound = complexities[max(complexities.index(decidedLowerBound) if decidedLowerBound != "" else -1, complexities.index("(loglog n)"))]
+
+    if is_log_solvable(constraints):
+      decidedUpperBound = complexities[min(complexities.index(decidedUpperBound) if decidedUpperBound != "" else 100, complexities.index("(log n)"))]
+    else:
+      decidedLowerBound = complexities[max(complexities.index(decidedLowerBound) if decidedLowerBound != "" else -1, complexities.index("(n)"))]
+
+    if (lowerBound != decidedLowerBound or upperBound != decidedUpperBound):
+      data[idx]["lower-bound"] = decidedLowerBound
+      data[idx]["upper-bound"] = decidedUpperBound
+      print(constraints, lowerBound, upperBound, decidedLowerBound, decidedUpperBound)
+      return 1
+
+  return 0
---
+++ packages/brt-classifier/brt-classifier-0.1.5.tar.gz/brt-classifier-0.1.5/brt_classifier/postprocess/findRootOnlyParents.py
@@ -0,0 +1,25 @@
+from ..util import flatMap
+
+def findRootOnlyParents(problem, idx, data):
+  constrs = problem["constraint"]
+  lowerBound = problem["lower-bound"]
+  upperBound = problem["upper-bound"]
+
+  if lowerBound == "" or upperBound == "":
+    parents = set([x[1] for x in constrs])
+    children = set(flatMap(lambda x: [x[0], x[2]], constrs))
+    if not parents.issubset(children):
+      prunedConstrs = [x for x in constrs if x[1] != "3"]
+      prevProblems = data[:idx]
+      for prevProblem in prevProblems:
+        prevConstrs = prevProblem["constraint"]
+        if prevConstrs == prunedConstrs:
+          print(constrs, prevProblem["lower-bound"], prevProblem["upper-bound"])
+          data[idx]["lower-bound"] = prevProblem["lower-bound"]
+          data[idx]["upper-bound"] = prevProblem["upper-bound"]
+          break
+
+      return 1
+
+  return 0
+
