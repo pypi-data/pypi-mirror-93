1df45bace440ddda53b3f8e489343cb52876b51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:13 +0000

    Add package nmdc-mongo-tools/0.0.6/nmdc-mongo-tools-0.0.6.tar.gz

---
+++ packages/nmdc-mongo-tools/nmdc-mongo-tools-0.0.6.tar.gz/nmdc-mongo-tools-0.0.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("requirements/main.in") as f:
+    install_requires = f.read().splitlines()
+
+with open("requirements/dev.in") as f:
+    dev_requires = f.read().splitlines()
+
+setup(
+    name="nmdc-mongo-tools",
+    url="https://github.com/polyneme/nmdc-mongo-tools",
+    packages=find_packages(),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    author="Donny Winston",
+    author_email="donny@polyneme.xyz",
+    description="Tools for validation and storage of JSON data using the NMDC Schema",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=install_requires,
+    extras_require={
+        "dev": dev_requires,
+    },
+    python_requires=">=3.6",
+)
