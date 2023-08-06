db697f67f5bf1a819da30cb67a7f028954db934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:46 +0000

    Add package dkist-processing-core/0.0.5rc1/dkist-processing-core-0.0.5rc1.tar.gz

---
+++ packages/dkist-processing-core/dkist-processing-core-0.0.5rc1.tar.gz/dkist-processing-core-0.0.5rc1/dkist_processing_core/tests/invalid_workflow_package/workflow.py
@@ -0,0 +1,16 @@
+"""
+Example invalid workflow
+"""
+from dkist_processing_core import Workflow
+from dkist_processing_core.tests.task_example import Task
+from dkist_processing_core.tests.task_example import Task2
+
+
+# |<--------------------|
+# |-->Task -> Task2 --> |
+
+
+example = Workflow(workflow_name="invalid_workflow")
+example.add_node(task=Task, upstreams=Task2)
+example.add_node(task=Task2, upstreams=Task)
+example_dag = example.load()
---
+++ packages/dkist-processing-core/dkist-processing-core-0.0.5rc1.tar.gz/dkist-processing-core-0.0.5rc1/dkist_processing_core/tests/valid_workflow_package/workflow.py
@@ -0,0 +1,20 @@
+"""
+Example valid workflow
+"""
+from dkist_processing_core import Workflow
+from dkist_processing_core.tests.task_example import Task
+from dkist_processing_core.tests.task_example import Task2
+from dkist_processing_core.tests.task_example import Task3
+from dkist_processing_core.tests.task_example import Task4
+
+#        |---> Task2 --->|
+# Task ->|               |----> Task4
+#        |---> Task3 --->|
+
+example = Workflow(workflow_name="valid_workflow")
+example.add_node(task=Task, upstreams=None)
+example.add_node(task=Task2, upstreams=Task)
+example.add_node(task=Task3, upstreams=Task)
+example.add_node(task=Task4, upstreams=[Task2, Task3])
+
+example_dag = example.load()
