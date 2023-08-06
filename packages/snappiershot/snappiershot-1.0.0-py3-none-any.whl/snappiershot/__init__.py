47788471662d80c7d0e59647d19ba0330aba6c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:29 +0000

    Add package snappiershot/1.0.0/snappiershot-1.0.0-py3-none-any.whl

---
+++ packages/snappiershot/snappiershot-1.0.0-py3-none-any.whl/snappiershot/__init__.py
@@ -0,0 +1,20 @@
+""" Package level imports and versioning. """
+
+# Python version compatibility.
+try:
+    # >= 3.8
+    import importlib.metadata as metadata
+except ImportError:
+    #  < 3.8
+    import importlib_metadata as metadata  # type: ignore
+
+from . import errors
+from .config import Config
+from .inspection import CallerInfo
+from .snapshot import Snapshot, SnapshotMetadata
+
+__version__ = ""
+try:
+    __version__ = metadata.version(__name__)  # type: ignore
+except FileNotFoundError:
+    pass
