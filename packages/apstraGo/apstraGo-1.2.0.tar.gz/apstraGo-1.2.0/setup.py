0a945db438c7c2e90355a0762d5d5186fc2c5f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package apstraGo/1.2.0/apstraGo-1.2.0.tar.gz

---
+++ packages/apstraGo/apstraGo-1.2.0.tar.gz/apstraGo-1.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='apstraGo',
+    version='1.2.0',
+    description='Python wrapper for Apstra OS',
+    author='Adam Jarvis',
+    url='https://github.com/iamjarvs/apstraGo',
+    install_requires=['requests', 'PyYAML', 'cerberus', 'rich'],
+    keywords='apstra juniper',
+    packages=['apstraGo'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+    package_data={
+        'apstraGo': ['data/*.py'],
+    },
+    entry_points={
+        'console_scripts': [
+            'apstraGo-auto = apstraGo.apstraGoAuto:main',
+            'apstraGo-yaml = apstraGo.apstraGoYaml:main'
+        ]      
+    }
+)
