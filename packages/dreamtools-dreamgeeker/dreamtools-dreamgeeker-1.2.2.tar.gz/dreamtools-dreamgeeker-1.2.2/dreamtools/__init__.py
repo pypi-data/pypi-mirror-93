6b5139cf110e021005e5a2b3569a4a0294915372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:58 +0000

    Add package dreamtools-dreamgeeker/1.2.2/dreamtools-dreamgeeker-1.2.2.tar.gz

---
+++ packages/dreamtools-dreamgeeker/dreamtools-dreamgeeker-1.2.2.tar.gz/dreamtools-dreamgeeker-1.2.2/dreamtools/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Liste de fonction standard reutilisable
+
+.. author: Dreamgeeker
+"""
+
+__author__ = "Ketsia LENTIN"
+__copyright__ = "Copyright 2020, Couleur West IT"
+__credits__ = "Ketsia LENTIN"
+__license__ = "MIT"
+__version__ = "1.2.2"
+__email__ = "contact@couleurwest-it"
+__status__ = "Production"
+__docformat__ = 'reStructuredText'
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
