8614233cd5e7648962452d194ef27ca3d1254d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:51 +0000

    Add package flask-rebar-auth0/0.2.0/flask_rebar_auth0-0.2.0-py3-none-any.whl

---
+++ packages/flask-rebar-auth0/flask_rebar_auth0-0.2.0-py3-none-any.whl/flask_rebar_auth0/__init__.py
@@ -0,0 +1,3 @@
+from .authenticator import Auth0Authenticator, Auth0AuthenticatorRBAC
+from .helpers import get_access_token_claims, get_authenticated_user
+from .swagger import register_authenticators
