10272f0f7ec4e5cc1233512b94debed338afa6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:28 +0000

    Add package serveRmore/0.1.0/serveRmore-0.1.0.tar.gz

---
+++ packages/serveRmore/serveRmore-0.1.0.tar.gz/serveRmore-0.1.0/setup.py
@@ -0,0 +1,35 @@
+#!python3
+# -*- coding: utf-8 -*-
+
+import VERSION
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='serveRmore',
+    version=VERSION.srm_VERSION,
+    description='Serve R more on Serverless.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Origent Data Sciences',
+    author_email='cloudhosting@origent.com',
+    scripts=['bin/srm', 'README.md'],
+    py_modules = ['ServeRmore', 'srvl_config', 'VERSION'],
+    install_requires=['paramiko', 'boto3', 'PyYAML', 'requests'],
+    python_requires='>=3',
+    url='https://github.com/Origent/ServeRmore.git',
+    license='Apache License 2.0',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
