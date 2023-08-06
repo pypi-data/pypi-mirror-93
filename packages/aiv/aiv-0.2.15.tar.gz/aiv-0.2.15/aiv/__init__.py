686ec30f95d765a2072986bb1a5ac7aad629921a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:08 +0000

    Add package aiv/0.2.15/aiv-0.2.15.tar.gz

---
+++ packages/aiv/aiv-0.2.15.tar.gz/aiv-0.2.15/aiv/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  2 16:20:32 2021
+
+@author: tardis
+"""
+from . import AIV
+
+# __all__ = ['annotate_identified_variants', 'annotate_mutations']
