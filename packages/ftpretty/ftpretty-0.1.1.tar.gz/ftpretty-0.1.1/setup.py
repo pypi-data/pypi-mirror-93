6f1dd8ef24a0ff5e2bdf7e650746c929481a8bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:12 +0000

    Add package ftpretty/0.1.1/ftpretty-0.1.1.tar.gz

---
+++ packages/ftpretty/ftpretty-0.1.1.tar.gz/ftpretty-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ftpretty',
+      version='0.1.1',
+      description='Pretty FTP wrapper',
+      author='Rob Harrigan',
+      author_email='harrigan.rob@gmail.com',
+      url='https://github.com/codebynumbers/ftpretty',
+      download_url='https://github.com/codebynumbers/ftpretty/tarball/0.1.1',
+      py_modules=['ftpretty'],
+     )
+
