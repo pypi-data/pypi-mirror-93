6946afd07ed04996820cc699b5171a545f2258f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:00 +0000

    Add package tracepooling/0.1/tracepooling-0.1.tar.gz

---
+++ packages/tracepooling/tracepooling-0.1.tar.gz/tracepooling-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["tensorflow>=1"]
+
+setup(
+    name="tracepooling",
+    version="0.1",
+    author="Irene Martin",
+    author_email="irene.martin@uv.es",
+    description="A package to implement trace pooling layer",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spatUV/tracepooling/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
