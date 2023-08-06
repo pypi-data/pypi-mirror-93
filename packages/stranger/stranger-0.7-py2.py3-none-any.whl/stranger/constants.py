a1e7b9cc0f7e9cbd908756e0e36776266db3abc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:16 +0000

    Add package stranger/0.7/stranger-0.7-py2.py3-none-any.whl

---
+++ packages/stranger/stranger-0.7-py2.py3-none-any.whl/stranger/constants.py
@@ -0,0 +1,17 @@
+RANK_SCORE = {
+        'normal' : 1,
+        'pre_mutation': 3,
+        'full_mutation': 5
+        }
+
+ANNOTATE_REPEAT_KEYS = [
+    'HGNCId',
+    'InheritanceMode',
+    'DisplayRU',
+    'SourceDisplay',
+    'Source',
+    'SourceId',
+    'SweGenMean',
+    'SweGenStd',
+    'Disease',
+]
