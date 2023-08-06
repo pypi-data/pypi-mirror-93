7d4d70f14b0ba26b650c100691211562c7630fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:50 +0000

    Add package figuregen/0.7.1/figuregen-0.7.1-py3-none-any.whl

---
+++ packages/figuregen/figuregen-0.7.1-py3-none-any.whl/figuregen/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['figuregen', 'implementation']
+
+# Import API functions
+from .figuregen import *
+from .element_data import *
+from .matplot_lineplot import MatplotLinePlot
+from .pgf_lineplot import PgfLinePlot
