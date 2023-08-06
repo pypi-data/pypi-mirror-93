7db7fd1565815729530c0fd2de2cac4f4151f952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package codechecklib/0.3.11/codechecklib-0.3.11.tar.gz

---
+++ packages/codechecklib/codechecklib-0.3.11.tar.gz/codechecklib-0.3.11/codechecklib/const.py
@@ -0,0 +1,88 @@
+import getpass
+from enum import Enum
+from types import SimpleNamespace
+from typing import List
+
+LANGUAGES = {
+    'cpp': 'C++',
+    'cs': 'C#',
+    'c': 'C',
+    'py': 'Python',
+    'rb': 'Ruby',
+    'js': 'JavaScript',
+    'rs': 'Rust'
+}
+
+COMPILE_COMMANDS = {
+    'cs': lambda filename: ['mcs', '-optimize', '-nologo', '-out:' + filename + '.o', filename],
+    'cpp': lambda filename: ['g++', '-x', 'c++', '-std=c++20', '-o', filename + '.o', filename],
+    'c': lambda filename: ['gcc', '-x', 'c++', '-o', filename + '.o', filename],
+    'py': lambda filename: [['pylint', '--disable=R,C', filename], ['cp', filename, filename + '.o']],
+    'py_nl': lambda filename: ['cp', filename, filename + '.o'],
+    'rb': lambda filename: ['cp', filename, filename + '.o'],
+    'js': lambda filename: ['cp', filename, filename + '.o'],
+    'rs': lambda filename: ['rustc', filename, '-o', filename + '.o'],
+}
+EXEC_COMMANDS = {
+    'cs': lambda filename: ['mono', filename],
+    'cpp': lambda filename: [filename],
+    'c': lambda filename: [filename],
+    'py': lambda filename: ['python3', filename],
+    'py_nl': lambda filename: ['python3', filename],
+    'rb': lambda filename: ['ruby', filename],
+    'js': lambda filename: ['node', filename],
+    'rs': lambda filename: [filename],
+}
+COMMON_AVAILABLE_BINARIES = ['env', 'bash', 'sh']
+AVAILABLE_BINARIES = {
+    'cs': ['mono', 'mcs'],
+    'cpp': ['g++', 'ld', 'as'],
+    'c': ['gcc', 'ld', 'as'],
+    'py': ['python', 'python3', 'pylint', 'cp'],
+    'py_nl': ['python', 'python3', 'cp'],
+    'rb': ['ruby', 'cp'],
+    'js': ['node', 'cp'],
+    'rs': ['rustc', 'cc', 'ld'],
+}
+
+MY_USER = getpass.getuser()
+
+
+class TestingException(Exception):
+    pass
+
+
+class CgroupSetupException(TestingException):
+    pass
+
+
+class ExecStatus(str, Enum):
+    CE = 'CE'
+    TL = 'TL'
+    ML = 'ML'
+    RE = 'RE'
+    OK = 'OK'
+    WA = 'WA'
+
+
+class ExecResult(SimpleNamespace):
+    status: ExecStatus
+    compilation_time: float
+    compiler_message: str
+    time: float
+    stdout: str
+    stderr: str
+
+
+class TestResult(SimpleNamespace):
+    success: bool
+    compilation_time: float
+    compiler_message: str
+    compilation_error: bool
+    results: List[ExecResult]
+    first_error_test: int
+
+
+__all__ = ['LANGUAGES', 'COMPILE_COMMANDS', 'EXEC_COMMANDS', 'COMMON_AVAILABLE_BINARIES',
+           'AVAILABLE_BINARIES', 'MY_USER', 'TestingException', 'CgroupSetupException',
+           'ExecStatus', 'ExecResult', 'TestResult']
---
+++ packages/codechecklib/codechecklib-0.3.11.tar.gz/codechecklib-0.3.11/codechecklib/example_code.py
@@ -0,0 +1,37 @@
+EXAMPLE_CODES = {
+    'cs': '''using System;
+
+namespace Example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}''',
+    'cpp': '''#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, world!" << endl;
+    return 0; 
+}''',
+    'c': '''#include <stdio.h>
+int main() {
+   printf("Hello, world!");
+   return 0;
+}''',
+    'py': 'print(\'Hello, world!\')',
+    'rb': 'puts \'Hello, world!\'',
+    'js': 'console.log(\'Hello, world!\')',
+    'rs': '''fn main() {
+    println!("Hello world!");
+}'''
+}
+EXAMPLE_CODES['py_nl'] = EXAMPLE_CODES['py']
+
+__all__ = ['EXAMPLE_CODES']
