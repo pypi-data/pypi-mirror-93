0edc745307db6f23652d1be2d035231383cb1885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:23 +0000

    Add package jzt/1.0.0.3/jzt-1.0.0.3.tar.gz

---
+++ packages/jzt/jzt-1.0.0.3.tar.gz/jzt-1.0.0.3/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+setup(
+  name = 'jzt',
+  packages = ['jzt'],
+  version = '1.0.0.3',
+  license='MIT',
+  description = 'JazeeTech Bot Manager',
+  author = 'Talha Ijaz',
+  author_email = 'talhabinijaz576@yahoo.com',
+  url = 'https://github.com/talhabinijaz576/jzt',
+  download_url = 'https://github.com/talhabinijaz576/jzt/archive/1.0.0.3.tar.gz',
+  keywords = ['jzt', 'jazee tech', 'jazee', 'commandcenter', 'bot manager', 'asset manager', 'assetmanager'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
