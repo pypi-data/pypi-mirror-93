265744b230d8b36ee24a2391c01909f6a58f640b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:24 +0000

    Add package tufts-dining/0.1.0/tufts_dining-0.1.0.tar.gz

---
+++ packages/tufts-dining/tufts_dining-0.1.0.tar.gz/tufts_dining-0.1.0/README.md
@@ -0,0 +1,4 @@
+# Tufts Dining
+### Ankur Dahal
+
+A Python package for retrieving dining menu data for Tufts University Dining. This package is under development.
---
+++ packages/tufts-dining/tufts_dining-0.1.0.tar.gz/tufts_dining-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tufts_dining",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A Python package for retrieving menus from Tufts Dining",
+    url="https://github.com/dahalankur/tufts_dining",
+    author="Ankur Dahal",
+    author_email="dahalankur123@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python"
+    ],
+    keywords="tufts-dining-package tufts-university dining-package",
+    install_requires=[
+        "beautifulsoup4==4.9.3",
+        "certifi==2020.12.5",
+        "chardet==4.0.0",
+        "idna==2.10",
+        "requests==2.25.1",
+        "soupsieve==2.1",
+        "urllib3==1.26.2"],
+    python_requires=">=3.6"
+)
