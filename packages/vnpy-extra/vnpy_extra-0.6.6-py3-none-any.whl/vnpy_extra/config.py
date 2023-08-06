74f9c13f9ca886ee3a837385b3c50f17f9de9903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package vnpy-extra/0.6.6/vnpy_extra-0.6.6-py3-none-any.whl

---
+++ packages/vnpy-extra/vnpy_extra-0.6.6-py3-none-any.whl/vnpy_extra/config.py
@@ -0,0 +1,47 @@
+"""
+@author  : MG
+@Time    : 2020/9/2 16:58
+@File    : config.py
+@contact : mmmaaaggg@163.com
+@desc    : 用于配置文件
+"""
+import logging
+from logging.config import dictConfig
+
+# log settings
+logging_config = dict(
+    version=1,
+    formatters={
+        'simple': {
+            'format': '%(asctime)s %(name)s|%(module)s.%(funcName)s:%(lineno)d %(levelname)s %(message)s'}
+    },
+    handlers={
+        'file_handler':
+            {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': 'logger.log',
+                'maxBytes': 1024 * 1024 * 50,
+                'backupCount': 5,
+                'level': logging.INFO,
+                # 'level': logging.WARNING,
+                'formatter': 'simple',
+                'encoding': 'utf8'
+            },
+        'console_handler':
+            {
+                'class': 'logging.StreamHandler',
+                'level': logging.INFO,
+                'formatter': 'simple'
+            }
+    },
+
+    root={
+        'handlers': ['console_handler', 'file_handler'],
+        'level': logging.INFO,
+    }
+)
+dictConfig(logging_config)
+logging.info("加载配置文件")
+
+if __name__ == "__main__":
+    pass
