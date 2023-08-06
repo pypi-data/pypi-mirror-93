617354bf6b8e61548cba9b205156dcd10efe02ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:13 +0000

    Add package pytest-markers-presence/0.12.0/pytest-markers-presence-0.12.0.tar.gz

---
+++ packages/pytest-markers-presence/pytest-markers-presence-0.12.0.tar.gz/pytest-markers-presence-0.12.0/CHANGES.rst
@@ -0,0 +1,143 @@
+Changelog
+---------
+
+0.12.0
+~~~~~~
+
+* Deleted `--bdd-title` option (transferred as feature into `Overhave framework`_)
+
+0.11.1
+~~~~~~
+
+* Updated deps after `pytest_bdd` updates and fixed import
+
+0.11.0
+~~~~~~
+
+* Removed options `--browse-url` and `--links-keyword` (transferred to Overhave framework)
+* Updated tests
+* Updated library dependencies
+
+0.10.0
+~~~~~~
+
+* Updated libraries versions
+
+0.9.2
+~~~~~
+
+* Updated directory supported patterns for dynamic pytest mark: excluded directories with names like ".*".
+
+0.9.1
+~~~~~
+
+* Hotfix for plugin
+
+0.9.0
+~~~~~
+
+* Added enabling of fail exitcode setting when all session tests were skipped
+
+0.8.0
+~~~~~
+
+* Added task tracker URL specification for ticket links compiling
+* Added specification of keyword for task tracker tickets collecting
+
+0.7.6
+~~~~~
+
+* Fix for dataclasses objects extraction
+
+0.7.5
+~~~~~
+
+* Added support for not traditional pytest items (for example, YamlItem)
+* Updated tests
+
+0.7.4
+~~~~~
+
+* Add option '--staging-warnings' to external enabling of warnings
+* Updated tests
+
+0.7.3
+~~~~~
+
+* No assertions rewriting for built-in types
+* Updated tests
+
+0.7.2
+~~~~~
+
+* Hotfix for assertions rewriting
+
+0.7.1
+~~~~~
+
+* Tagged again (pypi package broken)
+
+0.7.0
+~~~~~
+
+* Implemented Allure titles setting for BDD tests
+* Updated tests
+* Minor changes
+
+0.6.3
+~~~~~
+
+* Hotfix for Allure step compilation
+
+0.6.2
+~~~~~
+
+* Updated assertions rewriting: implemented smart Allure step compilation with attachments
+* Updated tests
+
+0.6.1
+~~~~~
+
+* Hotfix for assertions rewriting
+
+0.6.0
+~~~~~
+
+* Implemented assertions rewriting with '--assert-steps' option
+* Minor changes
+* Updated tests
+
+0.5.0
+~~~~~
+
+* Implemented new logic for '--staging' option
+* Implemented support for dynamical tests marking
+* Updated '--bdd-format' logic
+* Updated tests
+
+0.4.0
+~~~~~
+
+* Implemented detection of not classified functions usage
+* Updated documentation
+* Updated dev-requirements
+* Updated tests
+
+0.3.0
+~~~~~
+
+* Updated documentation
+* Updated dev-requirements
+* Minor fixes
+
+0.2.0
+~~~~~
+
+* Implemented ignore of session items which do not contain 'originalname'
+
+0.1.0
+~~~~~
+
+* First release
+
+.. _`Overhave`: https://github.com/TinkoffCreditSystems/overhave
---
+++ packages/pytest-markers-presence/pytest-markers-presence-0.12.0.tar.gz/pytest-markers-presence-0.12.0/setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import codecs
+import os
+import re
+
+from setuptools import Command, setup
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+def get_version():
+    changes_path = os.path.join(BASE_PATH, "CHANGES.rst")
+    regex = r"^#*\s*(?P<version>[0-9]+\.[0-9]+(\.[0-9]+)?)$"
+    with codecs.open(changes_path, encoding="utf-8") as changes_file:
+        for line in changes_file:
+            res = re.match(regex, line)
+            if res:
+                return res.group("version")
+    return "0.0.0"
+
+
+version = get_version()
+
+
+class VersionCommand(Command):
+    description = "print current library version"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print(version)
+
+
+setup(
+    name="pytest-markers-presence",
+    version=version,
+    author="Vladislav Mukhamatnurov",
+    author_email="livestreamepidemz@yandex.ru",
+    maintainer="Vladislav Mukhamatnurov",
+    maintainer_email="livestreamepidemz@yandex.ru",
+    license="MIT",
+    url="https://github.com/livestreamx/pytest-markers-presence",
+    description='A simple plugin to detect missed pytest tags and markers"',
+    long_description=read("README.rst"),
+    py_modules=["pytest_markers_presence"],
+    python_requires=">=3.7",
+    install_requires=[
+        "pytest>=6.0",
+        "allure-pytest>=2.8.19",
+        "pydantic>=1.2",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    cmdclass={"version": VersionCommand},
+    entry_points={"pytest11": ["markers-presence = pytest_markers_presence"]},
+)
