bd7c94793d88e1b2559dc63a410cadc5501745a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:06 +0000

    Add package guillotina-gcloudstorage/6.0.1/guillotina_gcloudstorage-6.0.1.tar.gz

---
+++ packages/guillotina-gcloudstorage/guillotina_gcloudstorage-6.0.1.tar.gz/guillotina_gcloudstorage-6.0.1/guillotina_gcloudstorage/interfaces.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from zope.interface import Interface
+
+from guillotina.interfaces import IFile
+from guillotina.interfaces import IFileField
+
+
+class IGCloudFileField(IFileField):
+    """Field marked as GCloudFileField"""
+
+
+# Configuration Utility
+
+
+class IGCloudBlobStore(Interface):
+    """Configuration utility."""
+
+
+class IGCloudFile(IFile):
+    """Marker for a GCloudFile"""
