761a54212c3de6d41da410f97ac6c9b6e3dd4216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:09 +0000

    Add package flake8-printf-formatting/1.1.2/flake8_printf_formatting-1.1.2-py2.py3-none-any.whl

---
+++ packages/flake8-printf-formatting/flake8_printf_formatting-1.1.2-py2.py3-none-any.whl/flake8_printf_formatting.py
@@ -0,0 +1,35 @@
+import ast
+import sys
+from typing import Any, Generator, List, Tuple, Type
+
+if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
+    import importlib_metadata
+else:  # pragma: no cover (PY38+)
+    import importlib.metadata as importlib_metadata
+
+MOD001 = "MOD001 do not use printf-style string formatting"
+
+
+class Visitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self.expressions: List[Tuple[int, int]] = []
+
+    def visit_BinOp(self, node: ast.BinOp) -> None:
+        if isinstance(node.op, ast.Mod) and isinstance(node.left, (ast.Str, ast.Bytes)):
+            self.expressions.append((node.lineno, node.col_offset))
+        self.generic_visit(node)
+
+
+class Plugin:
+    name = __name__
+    version = importlib_metadata.version(__name__)
+
+    def __init__(self, tree: ast.AST) -> None:
+        self._tree = tree
+
+    def run(self) -> Generator[Tuple[int, int, str, Type[Any]], None, None]:
+        visitor = Visitor()
+        visitor.visit(self._tree)
+
+        for line, col in visitor.expressions:
+            yield line, col, MOD001, type(self)
