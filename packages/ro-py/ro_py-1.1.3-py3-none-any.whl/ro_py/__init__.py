361681d1f7f3d4c9ae6d3dad7397e2c18cba60e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:40 +0000

    Add package ro-py/1.1.3/ro_py-1.1.3-py3-none-any.whl

---
+++ packages/ro-py/ro_py-1.1.3-py3-none-any.whl/ro_py/__init__.py
@@ -0,0 +1,20 @@
+"""
+
+<h1 align="center">
+    <img src="https://raw.githubusercontent.com/rbx-libdev/ro.py/main/resources/header.png" alt="ro.py" width="400" />
+    <br>
+</h1>
+<h4 align="center">ro.py is a powerful Python 3 wrapper for the Roblox Web API.</h4>
+<p align="center">
+  <a href="https://github.com/rbx-libdev/ro.py#information">Information</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#requirements">Requirements</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#disclaimer">Disclaimer</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#documentation">Documentation</a> |
+  <a href="https://github.com/rbx-libdev/ro.py/tree/main/examples">Examples</a> |
+  <a href="https://github.com/rbx-libdev/ro.py#credits">Credits</a> |
+  <a href="https://github.com/rbx-libdev/ro.py/blob/main/LICENSE">License</a>
+</p>
+
+"""
+
+from ro_py.client import Client
