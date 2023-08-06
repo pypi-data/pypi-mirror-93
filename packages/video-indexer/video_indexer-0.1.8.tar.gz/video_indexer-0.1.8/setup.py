f3749b246ec6c0295a8ec2a1f1c63e3d735f53a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:14 +0000

    Add package video-indexer/0.1.8/video_indexer-0.1.8.tar.gz

---
+++ packages/video-indexer/video_indexer-0.1.8.tar.gz/video_indexer-0.1.8/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from os import path
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='video_indexer',
+    version='0.1.8',
+    url='https://github.com/bklim5/python_video_indexer_lib',
+    description='Common function to query Microsoft Video Indexer',
+    author='BK Lim',
+    author_email='bklim5@hotmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
