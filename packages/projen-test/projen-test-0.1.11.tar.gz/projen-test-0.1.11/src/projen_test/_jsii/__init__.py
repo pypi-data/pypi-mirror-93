165c278cf846273e5aa366e42cfe026f06528407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:45 +0000

    Add package projen-test/0.1.11/projen-test-0.1.11.tar.gz

---
+++ packages/projen-test/projen-test-0.1.11.tar.gz/projen-test-0.1.11/src/projen_test/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@seeebiii/projen-test", "0.1.11", __name__[0:-6], "projen-test@0.1.11.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
