21ebd29d7041db21c46807ad4f166c87d49e3dfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:12 +0000

    Add package pascal-voc-tools/0.1.30.dev11/pascal_voc_tools-0.1.30.dev11.tar.gz

---
+++ packages/pascal-voc-tools/pascal_voc_tools-0.1.30.dev11.tar.gz/pascal_voc_tools-0.1.30.dev11/README.md
@@ -0,0 +1,16 @@
+# Pascal VOC Tools
+
+This package includes some tools about pascal voc format dataset like read xml, write xml, resize image.
+
+[![Documentation Status](https://readthedocs.org/projects/pascal-voc-tools/badge/?version=latest)](https://pascal-voc-tools.readthedocs.io/en/latest/?badge=latest)
+
+# Functions
+- annotations file laod, edit and save;
+- dataset resize;
+- dataset argument(function from [link](https://github.com/Paperspace/DataAugmentationForObjectDetection));
+- darknet anchors k-means;
+
+# Install
+```bash
+pip install pascal-voc-tools
+```
---
+++ packages/pascal-voc-tools/pascal_voc_tools-0.1.30.dev11.tar.gz/pascal_voc_tools-0.1.30.dev11/pascal_voc_tools/__init__.py
@@ -0,0 +1,25 @@
+"""
+Pascal Voc Tools
+
+This package provide some tools using for 
+pascal voc format dataset and some usrful
+functions.
+"""
+
+from .resize import DatasetResize
+from .datatools import DataSplit
+from .datatools import DarknetDataset
+from .image_annotation_split import SplitImageAnnotation
+from .annotation_tools import AnnotationTools
+from .anchors_kmeans import AnchorsKMeans
+from .darknet_config import DarknetConfig
+from ._xml_parser import XmlParser
+from ._xml_parser import PascalXml
+
+__all__ = [
+    'DatasetResize', 'DataSplit', 'DarknetDataset', 'SplitImageAnnotation',
+    'AnnotationTools', 'AnchorsKMeans', 'DarknetConfig', 'XmlParser',
+    'PascalXml'
+]
+
+name = 'pascal_voc_tools'
