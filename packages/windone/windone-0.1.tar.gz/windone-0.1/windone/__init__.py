0b0464ba7dcca3b93ce2e6e13e45ef2e48fb3a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:26 +0000

    Add package windone/0.1/windone-0.1.tar.gz

---
+++ packages/windone/windone-0.1.tar.gz/windone-0.1/windone/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: wind
+@contact: 367059791@qq.com
+@time: 2021/1/29 10:37
+@file: __init__.py.py
+@desc: 
+"""
+
+def demo():
+    print("simple sdk demo")
