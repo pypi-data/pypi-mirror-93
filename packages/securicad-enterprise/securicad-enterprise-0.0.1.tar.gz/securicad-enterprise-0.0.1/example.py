01e8125956caac7207ce1ca26092d1b7cd18dc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:41 +0000

    Add package securicad-enterprise/0.0.1/securicad-enterprise-0.0.1.tar.gz

---
+++ packages/securicad-enterprise/securicad-enterprise-0.0.1.tar.gz/securicad-enterprise-0.0.1/example.py
@@ -0,0 +1,67 @@
+import securicad.enterprise.aws_import_cli as aws
+from securicad import enterprise
+
+# Create a config with credentials for the AWS data fetcher
+config = {
+    "accounts": [
+        {
+            "access_key": "AWS ACCESS KEY",
+            "secret_key": "AWS SECRET KEY",
+            "regions": ["REGION"],  # e.g., us-east-1
+        },
+    ],
+}
+
+# Fetch AWS data
+aws_data = aws.import_cli(config=config)
+
+# securiCAD Enterprise credentials
+username = "username"
+password = "password"
+
+# (Optional) Organization of user
+# If you are using the system admin account set org = None
+org = "My organization"
+
+# (Optional) CA certificate of securiCAD Enterprise
+# If you don't want to verify the certificate set cacert = None
+cacert = "/path/to/cacert.pem"
+
+# securiCAD Enterprise URL
+url = "https://xx.xx.xx.xx"
+
+# Create an authenticated enterprise client
+client = enterprise.client(
+    url=url, username=username, password=password, org=org, cacert=cacert
+)
+
+# Get project id of project where the model will be added
+project_id = client.get_project(name="My project")
+
+# Generate securiCAD model from AWS data
+model = client.add_aws_model(project_id, name="my-model", cli_files=[aws_data])
+
+# securiCAD metadata with all assets and attacksteps
+metadata = client.get_metadata()
+
+high_value_assets = [
+    {
+        "metaconcept": "S3Bucket",
+        "attackstep": "ReadObject",
+        "consequence": 7,
+    }
+]
+
+# Set high value assets in securiCAD model
+model.set_high_value_assets(high_value_assets=high_value_assets)
+
+# Save changes to model in project
+client.save_model(project_id, model)
+
+# Start a new simulation in a new scenario
+sim_id, scenario_id = client.start_simulation(
+    project_id, model.id, name="My first simulation"
+)
+
+# Poll for results and return them when simulation is done
+results = client.get_results(project_id, scenario_id, sim_id)
---
+++ packages/securicad-enterprise/securicad-enterprise-0.0.1.tar.gz/securicad-enterprise-0.0.1/securicad/enterprise/__init__.py
@@ -0,0 +1,37 @@
+# Copyright 2020-2021 Foreseeti AB <https://foreseeti.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum
+
+from securicad.enterprise.client import Client
+
+__version__ = "0.0.1"
+__author__ = "Foreseeti AB"
+
+
+class Role(Enum):
+    USER = ["user"]
+    PROJECT_CREATOR = ["user", "project_creator"]
+    ADMIN = ["user", "project_creator", "admin"]
+    SYSADMIN = ["user", "project_creator", "admin", "system_admin"]
+
+
+class AccessLevel(Enum):
+    GUEST = 100
+    USER = 180
+    OWNER = 250
+
+
+def client(url, username, password, org=None, cacert=None):
+    return Client(url, username, password, org, cacert)
