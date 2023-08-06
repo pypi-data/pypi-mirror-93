084c1d0a41e9c5502dcd58552e79aee805fa6b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:56 +0000

    Add package spock/1.2.1/spock-1.2.1.tar.gz

---
+++ packages/spock/spock-1.2.1.tar.gz/spock-1.2.1/spock/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Stability of Planetary Orbital Configurations Klassifier"""
+
+from .featureclassifier import FeatureClassifier
+from .deepregressor import DeepRegressor
+from .nbodyregressor import NbodyRegressor
+from .version import __version__
+
+__all__ = ["FeatureClassifier", "DeepRegressor", "NbodyRegressor"]
