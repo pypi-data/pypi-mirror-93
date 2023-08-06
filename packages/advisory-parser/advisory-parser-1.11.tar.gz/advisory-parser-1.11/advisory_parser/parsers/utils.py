7692e16d022640df9458ed46532d1d3d4da97d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:58 +0000

    Add package advisory-parser/1.11/advisory-parser-1.11.tar.gz

---
+++ packages/advisory-parser/advisory-parser-1.11.tar.gz/advisory-parser-1.11/advisory_parser/parsers/utils.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2017 Red Hat, Inc.
+# Author: Martin Prpič,, Red Hat Product Security
+# License: LGPLv3+
+
+import re
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+from advisory_parser.exceptions import AdvisoryParserGetContentException
+
+CVE_REGEX = re.compile(r"CVE-\d{4}-\d{4,}")
+
+
+def get_request(url):
+    try:
+        res = urlopen(url)
+    except HTTPError as e:
+        error_msg = "Failed to GET with status code: {}".format(e.code)
+        raise AdvisoryParserGetContentException(error_msg)
+    except URLError as e:
+        error_msg = "Failed to establish connection: {}".format(e.reason)
+        raise AdvisoryParserGetContentException(error_msg)
+    except ValueError:
+        raise AdvisoryParserGetContentException("Invalid URL specified.")
+    else:
+        return res.read()
+
+
+def get_text_from_url(url):
+    html = get_request(url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Remove script and style tags and their contents
+    for script in soup(["script", "style"]):
+        script.decompose()
+
+    text = soup.get_text()
+
+    # Filter out blank lines and leading/trailing spaces
+    text = "\n".join(line.strip() for line in text.splitlines() if line)
+
+    return text
---
+++ packages/advisory-parser/advisory-parser-1.11.tar.gz/advisory-parser-1.11/setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2019 Red Hat, Inc.
+# Author: Martin Prpič, Red Hat Product Security
+# License: LGPLv3+
+
+from setuptools import setup
+
+
+with open("README.rst", "r") as f:
+    description = f.read()
+
+requires = ["beautifulsoup4>=4.0.0"]
+
+setup(
+    name="advisory-parser",
+    version="1.11",
+    description="Security flaw parser for upstream security advisories",
+    long_description=description,
+    url="https://github.com/mprpic/advisory-parser",
+    author="Martin Prpič, Red Hat Product Security",
+    author_email="mprpic@redhat.com",
+    license="LGPLv3+",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Security",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="security advisory parser scraper",
+    packages=["advisory_parser", "advisory_parser.parsers"],
+    install_requires=requires,
+)
