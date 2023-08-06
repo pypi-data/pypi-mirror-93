ba54437f024fdb97598137fe0735ba982f1de719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:03 +0000

    Add package cytolysis/0.0.3/cytolysis-0.0.3.tar.gz

---
+++ packages/cytolysis/cytolysis-0.0.3.tar.gz/cytolysis-0.0.3/setup_cytolysis.py
@@ -0,0 +1,33 @@
+from setuptools import setup, Extension, find_packages
+import re
+
+def find_version(fname):
+    with open(fname,'r') as file:
+        version_file=file.read()
+        version_match = re.search(r"__VERSION__ = ['\"]([^'\"]*)['\"]",version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+with open("README.md", "r") as handle:
+    cytolysis_description = handle.read()
+
+version=find_version("src/cytosim_analysis.py")
+
+setup(
+    name='cytolysis',
+    version=version,
+
+    author="Serge Dmitrieff",
+    description="An API to analyze cytosim simulations",
+    long_description=cytolysis_description,
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/SergeDmi/cytosim_analysis",
+    install_requires=[
+      'numpy',
+      'sio_tools',
+      'pandas'
+    ],
+    packages=['cytolysis'],
+    package_dir={'cytolysis': 'src'},
+ )
