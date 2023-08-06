570d150ce6fb0930c8925cea1324ea813cc44030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:22 +0000

    Add package ibm-wos-utils/2.0.4/ibm_wos_utils-2.0.4-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/ibm-wos-utils/ibm_wos_utils-2.0.4-cp36-cp36m-manylinux1_x86_64.whl/ibm_wos_utils/joblib/utils/upload_file_info.py
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------------------------------------------
+# IBM Confidential
+# OCO Source Materials
+# 5900-A3Q, 5737-H76
+# Copyright IBM Corp. 2020, 2021
+# The source code for this program is not published or other-wise divested of its trade
+# secrets, irrespective of what has been deposited with the U.S.Copyright Office.
+# ----------------------------------------------------------------------------------------------------
+
+class UploadFileInfo():
+    def __init__(self):
+        self.server_url = None
+        self.username = None
+        self.password = None
+        self.volumn_name = None
+        self.src_file_path = None
+        self.tgt_file_path = None
