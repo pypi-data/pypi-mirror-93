a597132191978dc3e59b139d257755ba2f73844d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:29 +0000

    Add package ssm-mass-uploader/0.1.1/ssm_mass_uploader-0.1.1.tar.gz

---
+++ packages/ssm-mass-uploader/ssm_mass_uploader-0.1.1.tar.gz/ssm_mass_uploader-0.1.1/ssmu/__main__.py
@@ -0,0 +1,33 @@
+from .upload_ssm import *
+from .encrypt_secrets import *
+import argparse
+
+parser = argparse.ArgumentParser()
+
+group = parser.add_mutually_exclusive_group()
+
+group.add_argument('-u', '--upload', action='store_true')
+group.add_argument('-e', '--encrypt', action='store_true')
+
+parser.add_argument('-keyid', '-k', type=str, help='KMS key id.')
+parser.add_argument('--profile', '--p', type=str, default='default',
+                    help='Name of profile in your aws credentails file. Default is "default"')
+parser.add_argument('--region', '--r', type=str, default='us-east-1',
+                    help='Region you want your ssm credentials uploaded to. Default is us-east-1')
+parser.add_argument('-file', '-f', type=str, default='secret.sandbox',
+                    help='File that contains encrypted secrets.')
+parser.add_argument('-stage', '-s', type=str, default='dev',
+                    help='Stage')
+parser.add_argument('--output', '--o', type=str, default='.',
+                    help='Stage')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    if args.upload:
+        upload(args.profile, args.region, args.keyid, args.file, args.stage)
+
+    if args.encrypt:
+        encrypt_data(args.profile, args.region, args.keyid,
+                     args.file, args.stage, args.output)
