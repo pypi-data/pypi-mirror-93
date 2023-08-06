6eacabbbe08dc03689c72a61840bda025e430e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:48 +0000

    Add package logging-logger/0.1/logging_logger-0.1.tar.gz

---
+++ packages/logging-logger/logging_logger-0.1.tar.gz/logging_logger-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'logging_logger',         
+  packages = ['logging_logger'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'loggiing in python made easy - receive the log file you like in just one line of code!',   
+  author = 'payal kutana',                   
+  author_email = 'kutanapayal@gmail.com',    
+  url = 'https://github.com/payalkutana/logging_logger',  
+  download_url = 'https://github.com/payalkutana/logging_logger/archive/logging_logger_01.tar.gz',   
+  keywords = ['logger','logging','logs','easy'],   
+  
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
