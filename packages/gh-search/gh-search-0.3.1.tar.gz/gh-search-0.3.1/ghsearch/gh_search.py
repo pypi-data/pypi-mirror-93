a28704074cdeff0530f8d27dfbfc2b2fd9689f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:44 +0000

    Add package gh-search/0.3.1/gh-search-0.3.1.tar.gz

---
+++ packages/gh-search/gh-search-0.3.1.tar.gz/gh-search-0.3.1/ghsearch/gh_search.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+from typing import Callable, Dict, List
+
+import click
+import github
+from github.ContentFile import ContentFile
+
+from ghsearch.terminal import ProgressPrinter
+
+
+class GHSearch:
+    def __init__(self, client: github.Github, filters: List[Callable], verbose: bool = False):
+        self.client = client
+        self.filters = filters
+        self.verbose = verbose
+
+    def get_filtered_results(self, query: List[str]) -> Dict[str, List[ContentFile]]:
+        results = self.client.search_code(query=" ".join(query))
+        repos = defaultdict(list)
+
+        with ProgressPrinter(overwrite=not self.verbose) as printer:
+            for result in results:
+                printer(f"Checking result for {result.repository.full_name}")
+                exclude_reason = self._should_exclude(result)
+                if not exclude_reason:
+                    repos[result.repository.full_name].append(result)
+                elif self.verbose:
+                    click.echo(f"Skipping result for {result.repository.full_name} via {exclude_reason}")
+
+        return repos
+
+    def _should_exclude(self, result):
+        for result_filter in self.filters:
+            if not result_filter(result):
+                return result_filter.__name__
+        return False
---
+++ packages/gh-search/gh-search-0.3.1.tar.gz/gh-search-0.3.1/ghsearch/main.py
@@ -0,0 +1,117 @@
+from typing import Callable, Dict, List
+from urllib import parse
+
+import click
+from github import BadCredentialsException
+from github.ContentFile import ContentFile
+
+from ghsearch.client import build_client
+from ghsearch.filters import build_content_filter, build_not_archived_filter, build_path_filter
+from ghsearch.gh_search import GHSearch
+
+
+def _sanitize_qualifiers_for_search_url(query: List[str]) -> List[str]:
+    return [q for q in query if not (q.startswith("repo:") or q.startswith("org:"))]
+
+
+def _print_results(query: List[str], results: Dict[str, List[ContentFile]]) -> None:
+    if len(results) == 0:
+        click.echo("No results!")
+        return
+    sorted_results = sorted(results.items(), key=lambda kv: len(kv[1]), reverse=True)
+
+    q_param = parse.quote(" ".join(_sanitize_qualifiers_for_search_url(query)))
+    click.echo("Results:")
+    for repo, repo_results in sorted_results:
+        repo_result = repo_results[0]
+        url = f"{repo_result.repository.html_url}/search?utf8=✓&q={q_param}"
+        click.echo(f" {len(repo_results)} - {repo}: {url}")
+
+        repo_results.sort(key=lambda x: x.path)
+        for result in repo_results:
+            click.echo(f"\t- {result.path}")
+
+
+def _build_filters(
+    path_filter: str = None, include_archived: bool = True, content_filter: str = None
+) -> List[Callable]:
+    filters = []
+    if path_filter:
+        filters.append(build_path_filter(path_filter))
+    if not include_archived:
+        filters.append(build_not_archived_filter())
+    if content_filter:
+        filters.append(build_content_filter(content_filter))
+    return filters
+
+
+def run(
+    query: List[str],
+    github_token: str,
+    github_api_url: str = None,
+    path_filter: str = "",
+    content_filter: str = "",
+    include_archived: bool = False,
+    verbose: bool = False,
+) -> None:
+    client = build_client(github_token, github_api_url)
+    try:
+        if verbose:
+            rate_limit = client.get_rate_limit()
+            click.echo(f"Starting with GH Rate limit: {rate_limit.search}")
+
+        filters = _build_filters(path_filter, include_archived, content_filter)
+        gh_search = GHSearch(client, filters, verbose)
+        results = gh_search.get_filtered_results(query)
+
+        _print_results(query, results)
+
+        if verbose:
+            rate_limit = client.get_rate_limit()
+            click.echo(f"Ending with GH Rate limit: {rate_limit.search}")
+    except BadCredentialsException as e:
+        click.echo(f"Bad Credentials: {e}\n\nrun gh-search --help", err=True)
+
+
+def _create_none_value_validator(message):
+    def _validator(ctx, param, value):
+        if value is None:
+            raise click.UsageError(message, ctx=ctx)
+        return value
+
+    return _validator
+
+
+def _ensure_search_term(ctx, param, value):
+    if any(":" not in word for word in value):
+        return value
+    raise click.UsageError("QUERY must contain at least one search term", ctx=ctx)
+
+
+@click.command(
+    help="QUERY must contain at least one search term, but may also contain search qualifiers"
+    " (https://docs.github.com/en/github/searching-for-information-on-github/searching-code)",
+    context_settings={"max_content_width": 120},
+)
+@click.argument("QUERY", nargs=-1, required=True, callback=_ensure_search_term)
+@click.option(
+    "--github-token",
+    envvar="GITHUB_TOKEN",
+    help="GitHub Auth Token. Will fall back on GITHUB_TOKEN envvar.",
+    callback=_create_none_value_validator("GitHub token must be set via --github-token option or GITHUB_TOKEN envvar."),
+)
+@click.option(
+    "--github-api-url",
+    envvar="GITHUB_API_URL",
+    help="Override default GitHub API URL. Can also specify via GITHUB_API_URL envvar.",
+)
+@click.option("-p", "--path-filter", help="Exclude results whose path (or part of path) does not match this.")
+@click.option("-c", "--content-filter", help="Exclude results whose content does not match this.")
+@click.option("-a", "--include-archived", help="Include results from archived repos.", default=False, is_flag=True)
+@click.option("-v", "--verbose", help="Verbose output.", default=False, is_flag=True)
+def cli(*args, **kwargs):
+    run(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    cli()
