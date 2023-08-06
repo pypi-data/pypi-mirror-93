0659bb9d2be0da0c9c0d964f9dc3aca5101342aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:47 +0000

    Add package audio-degrader/1.3.0/audio_degrader-1.3.0-py3-none-any.whl

---
+++ packages/audio-degrader/audio_degrader-1.3.0-py3-none-any.whl/audio_degrader/__init__.py
@@ -0,0 +1,36 @@
+from .AudioFile import AudioFile
+from .BaseDegradation import Degradation, DegradationUsageDocGenerator
+from .DegradationConvolution import DegradationConvolution
+from .DegradationDynamicRangeCompression import \
+        DegradationDynamicRangeCompression
+from .DegradationGain import DegradationGain
+from .DegradationMix import DegradationMix
+from .DegradationMp3 import DegradationMp3
+from .DegradationNormalization import DegradationNormalization
+from .DegradationPitchShifting import DegradationPitchShifting
+from .DegradationResample import DegradationResample
+from .DegradationSpeed import DegradationSpeed
+from .DegradationTimeStretching import DegradationTimeStretching
+from .DegradationTrim import DegradationTrim
+from .DegradationEqualization import DegradationEqualization
+from .ParametersParser import ParametersParser
+from .AllDegradations import ALL_DEGRADATIONS
+
+
+__all__ = ["AudioFile",
+           "Degradation",
+           "DegradationUsageDocGenerator",
+           "DegradationTrim",
+           "DegradationMp3",
+           "DegradationGain",
+           "DegradationNormalization",
+           "DegradationMix",
+           "DegradationConvolution",
+           "DegradationDynamicRangeCompression",
+           "DegradationSpeed",
+           "DegradationTimeStretching",
+           "DegradationPitchShifting",
+           "DegradationResample",
+           "DegradationEqualization",
+           "ALL_DEGRADATIONS",
+           "ParametersParser"]
