be4e1048928c43a0872bdc19bc163616d635566b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:42 +0000

    Add package django-billjobs/0.9.0/django_billjobs-0.9.0-py3-none-any.whl

---
+++ packages/django-billjobs/django_billjobs-0.9.0-py3-none-any.whl/billjobs/admin.py
@@ -0,0 +1,183 @@
+import csv
+from django import forms
+from django.http import HttpResponse
+from django.db.models import Q
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
+from django.forms.models import BaseInlineFormSet
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
+from django.urls import reverse
+from .models import Bill, BillLine, Service, UserProfile
+from .views import statistics
+
+
+class BilljobsAdminSite(admin.AdminSite):
+    index_template = 'billjobs/admin_index.html'
+
+    def get_urls(self):
+        urls = super(BilljobsAdminSite, self).get_urls()
+
+        my_urls = [
+            url(r'^statistics$', self.admin_view(statistics),
+                name='billjobs_statistics')
+        ]
+
+        return my_urls + urls
+
+
+class BillLineInlineForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BillLineInlineForm, self).__init__(*args, **kwargs)
+        if self.instance.id:
+            self.fields['service'].queryset = Service.objects.filter(
+                    Q(is_available=True) | Q(name=self.instance.service.name))
+            print(self.fields['service'].choices)
+        else:
+            self.fields['service'].queryset = Service.objects.filter(
+                    is_available=True)
+
+    class Meta:
+        model = BillLine
+        fields = ('service', 'quantity', 'total', 'note')
+
+
+class BillLineInline(admin.TabularInline):
+    model = BillLine
+    extra = 1
+    form = BillLineInlineForm
+
+
+class BillAdmin(admin.ModelAdmin):
+    readonly_fields = ('number', 'billing_date', 'amount')
+    exclude = ('issuer_address', 'billing_address')
+    inlines = [BillLineInline]
+    list_display = ('__str__', 'coworker_name_link', 'amount', 'billing_date',
+                    'isPaid', 'pdf_file_url')
+    list_editable = ('isPaid',)
+    list_filter = ('isPaid', )
+    search_fields = ('user__first_name', 'user__last_name', 'number', 'amount')
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        """Return the User field foreign key with overwrited properties
+
+        In form to create or update a bill (invoice) the User field display a
+        list of users based on username sorted by alphabetical asc. This
+        function use the current user session as default field value and return
+        a string based on user full name and username
+
+        Returns
+        ------
+        Field
+            The User django admin form field
+
+        """
+        field = super(BillAdmin, self).formfield_for_foreignkey(
+                                                db_field, request, **kwargs)
+        if db_field.name == 'user':
+            field.initial = request.user.id
+            field.label_from_instance = self.get_user_label
+        return field
+
+    def get_user_label(self, user):
+        name = user.get_full_name()
+        username = user.username
+        return (name and name != username and '%s (%s)' % (name, username)
+                or username)
+
+    def coworker_name_link(self, obj):
+        ''' Create a link to user admin edit view '''
+        return format_html(
+                '<a href="{}">{}</a>',
+                reverse('admin:auth_user_change', args=(obj.user.id,)),
+                obj.coworker_name())
+    coworker_name_link.short_description = _('Coworker name')
+
+    def pdf_file_url(self, obj):
+        return format_html(
+                '<a href="{}">{}.pdf</a>',
+                reverse('generate-pdf', args=(obj.id,)),
+                obj.number)
+
+    pdf_file_url.short_description = _('Download invoice')
+
+
+class RequiredInlineFormSet(BaseInlineFormSet):
+    """
+    Generates an inline formset that is required
+    """
+
+    def _construct_form(self, i, **kwargs):
+        """
+        Override the method to change the form attribute empty_permitted
+        """
+        form = super(RequiredInlineFormSet, self)._construct_form(i, **kwargs)
+        form.empty_permitted = False
+        return form
+
+
+class UserProfileAdmin(admin.StackedInline):
+    model = UserProfile
+    formset = RequiredInlineFormSet
+
+
+class UserForm(UserChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+        self.fields['first_name'].required = True
+        self.fields['last_name'].required = True
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileAdmin, )
+    fieldsets = (
+            (None, {
+                'fields': ('username', 'password')
+                }),
+            (_('Personal info'), {
+                'fields': ('first_name', 'last_name', 'email')
+                }),
+            (_('Permissions'), {
+                'classes': ('collapse',),
+                'fields': ('is_active', 'is_staff', 'is_superuser',
+                           'groups', 'user_permissions')
+                }),
+            (_('Important dates'), {
+                'classes': ('collapse',),
+                'fields': ('last_login', 'date_joined')
+                })
+            )
+    list_display = ('username', 'get_full_name', 'email')
+    actions = ['export_email']
+    form = UserForm
+
+    def export_email(self, request, queryset):
+        """ Export emails of selected account """
+        response = HttpResponse(content_type='text/csv')
+
+        writer = csv.writer(response)
+        for email in queryset.values_list('email'):
+            writer.writerow(email)
+
+        return response
+    export_email.short_description = _('Export email of selected users')
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    model = Service
+    list_display = ('__str__', 'price', 'is_available')
+    list_editable = ('is_available',)
+    list_filter = ('is_available',)
+
+
+admin_site = BilljobsAdminSite(name='myadmin')
+# User have to be unregistered
+# admin_site.unregister(User)
+admin_site.register(User, UserAdmin)
+admin_site.register(Bill, BillAdmin)
+admin_site.register(Service, ServiceAdmin)
---
+++ packages/django-billjobs/django_billjobs-0.9.0-py3-none-any.whl/billjobs/models.py
@@ -0,0 +1,163 @@
+from django.db import models
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db.models.signals import pre_save, post_save, post_delete
+from django.utils.translation import ugettext_lazy as _
+from .settings import BILLJOBS_BILL_ISSUER
+import datetime
+
+
+class Bill(models.Model):
+
+    user = models.ForeignKey(
+            User, verbose_name=_('Coworker'), on_delete=models.PROTECT)
+    number = models.CharField(
+            max_length=16,
+            unique=True,
+            blank=True,
+            verbose_name=_('Bill number'),
+            help_text=_('This value is set automatically.'))
+    isPaid = models.BooleanField(
+            default=False,
+            verbose_name=_('Bill is paid ?'),
+            help_text=_('Check this value when bill is paid'))
+    billing_date = models.DateField(
+            auto_now_add=True,
+            verbose_name=_('Date'),
+            help_text=_('This value is set automatically.'))
+    amount = models.FloatField(
+            blank=True,
+            default=0,
+            verbose_name=_('Bill total amount'),
+            help_text=_('The amount is computed automatically.'))
+    issuer_address = models.CharField(
+            max_length=1024,
+            blank=False,
+            default=BILLJOBS_BILL_ISSUER)
+    billing_address = models.CharField(max_length=1024, blank=True)
+
+    def __str__(self):
+        return self.number
+
+    def coworker_name(self):
+        return '%s %s' % (self.user.first_name, self.user.last_name)
+    coworker_name.short_description = _('Coworker name')
+
+    class Meta:
+        verbose_name = _('Bill')
+
+    def save(self, *args, **kwargs):
+        if not self.billing_address:
+            self.billing_address = self.user.userprofile.billing_address
+        super(Bill, self).save(*args, **kwargs)
+
+
+class Service(models.Model):
+
+    reference = models.CharField(max_length=5, verbose_name=_('Reference'))
+    name = models.CharField(max_length=128, verbose_name=_('Name'))
+    description = models.CharField(
+            max_length=256,
+            verbose_name=_('Description'),
+            help_text=_('Write service description limited to 256 characters'))
+    price = models.FloatField(verbose_name=_('Price'))
+    is_available = models.BooleanField(
+            verbose_name=_('Is available ?'),
+            default=True)
+
+    def __str__(self):
+        """ Return name as object representation """
+        return self.name
+
+    class Meta:
+        verbose_name = _('Service')
+
+
+class BillLine(models.Model):
+
+    bill = models.ForeignKey(Bill, models.PROTECT)
+    service = models.ForeignKey(Service, models.PROTECT)
+    quantity = models.SmallIntegerField(default=1, verbose_name=_('Quantity'))
+    total = models.FloatField(
+            blank=True,
+            help_text=_('This value is computed automatically'),
+            verbose_name=_('Total'))
+    note = models.CharField(
+            max_length=1024,
+            verbose_name=_('Note'),
+            blank=True,
+            help_text=_('Write a simple note which will be added in your bill')
+            )
+
+    class Meta:
+        verbose_name = _('Bill Line')
+        verbose_name_plural = _('Bill Lines')
+
+
+class UserProfile(models.Model):
+    """ extend User class """
+    user = models.OneToOneField(User, models.PROTECT)
+    billing_address = models.TextField(
+            max_length=1024,
+            verbose_name=_('Billing Address'))
+
+    class Meta:
+        verbose_name = _('User Profile')
+        verbose_name_plural = _('User Profiles')
+
+
+@receiver(pre_save, sender=BillLine)
+def compute_total(sender, instance, **kwargs):
+        """ set total of line automatically """
+        if not instance.total:
+            instance.total = instance.service.price * instance.quantity
+
+
+@receiver(pre_save, sender=Bill)
+def define_number(sender, instance, **kwargs):
+    """ set bill number incrementally """
+
+    # only when we create record for the first time
+    if not instance.number:
+        today = datetime.date.today()
+        # get last id in base, we assume it's the last record
+        try:
+            last_record = sender.objects.latest('id')
+            # get last bill number and increment it
+            # add 00 for padding if less than 999
+            last_num = '%03d' % (int(last_record.number[7:])+1)
+        # no Bill in db
+        except sender.DoesNotExist:
+            last_num = '001'
+
+        instance.number = 'F{}{}'.format(today.strftime('%Y%m'), last_num)
+
+
+@receiver(pre_save, sender=Bill)
+def bill_pre_save(sender, instance, **kwargs):
+    """ Always compute the total amount of one bill before save. """
+    set_bill_amount(sender, instance, **kwargs)
+
+
+# If you change a BillLine, Bill object is not save, so pre_save do not compute
+# the total amount.
+@receiver(post_save, sender=BillLine)
+@receiver(post_delete, sender=BillLine)
+def bill_billLine_post_save_and_delete(sender, instance, **kwargs):
+    """ Update Bill total amount when related billLines change
+        When admin modify or delete a BillLine, Bill instance has no change, so
+        the pre_save is not called and total amount is not computed.
+    """
+    set_bill_amount(sender, instance.bill, **kwargs)
+
+
+def set_bill_amount(sender, instance, **kwargs):
+    """ set total price of billing when saving """
+    # reset self.amount in case is already set
+    bill = instance
+    bill.amount = 0
+    for line in bill.billline_set.all():
+        bill.amount += line.total
+
+    if sender is not Bill:
+        bill.save()
