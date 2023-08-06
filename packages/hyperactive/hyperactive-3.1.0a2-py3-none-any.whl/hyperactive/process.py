12dfc2724743078fc035b769158a33420e0cd258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:08 +0000

    Add package hyperactive/3.1.0a2/hyperactive-3.1.0a2-py3-none-any.whl

---
+++ packages/hyperactive/hyperactive-3.1.0a2-py3-none-any.whl/hyperactive/process.py
@@ -0,0 +1,73 @@
+# Author: Simon Blanke
+# Email: simon.blanke@yahoo.com
+# License: MIT License
+
+
+def gfo2hyper(search_space, para):
+    values_dict = {}
+    for i, key in enumerate(search_space.keys()):
+        pos_ = int(para[key])
+        values_dict[key] = search_space[key][pos_]
+
+    return values_dict
+
+
+def _process_(
+    nth_process,
+    objective_function,
+    search_space,
+    optimizer,
+    n_iter,
+    memory,
+    memory_warm_start,
+    max_time,
+    max_score,
+    random_state,
+    verbosity,
+    **kwargs
+):
+    def gfo_wrapper_model():
+        # wrapper for GFOs
+        def _model(para):
+            para = gfo2hyper(search_space, para)
+            optimizer.para_dict = para
+            return objective_function(optimizer)
+
+        _model.__name__ = objective_function.__name__
+        return _model
+
+    if "progress_bar" in verbosity:
+        verbosity_gfo = ["progress_bar"]
+    else:
+        verbosity_gfo = []
+
+    optimizer.search(
+        objective_function=gfo_wrapper_model(),
+        n_iter=n_iter,
+        max_time=max_time,
+        max_score=max_score,
+        memory=memory,
+        memory_warm_start=memory_warm_start,
+        verbosity=verbosity_gfo,
+        random_state=random_state,
+        nth_process=nth_process,
+    )
+
+    optimizer.print_info(
+        verbosity,
+        objective_function,
+        optimizer.best_score,
+        optimizer.best_para,
+        optimizer.eval_time,
+        optimizer.iter_time,
+        n_iter,
+    )
+
+    return {
+        "nth_process": nth_process,
+        "best_para": optimizer.best_para,
+        "best_score": optimizer.best_score,
+        "positions": optimizer.positions,
+        "results": optimizer.results,
+        "memory_values_df": optimizer.memory_values_df,
+    }
---
+++ packages/hyperactive/hyperactive-3.1.0a2-py3-none-any.whl/tests/test_optimizers/test_best_results.py
@@ -0,0 +1,102 @@
+import pytest
+import numpy as np
+
+
+from hyperactive import Hyperactive
+from ._parametrize import optimizers
+
+
+def objective_function(opt):
+    score = -opt["x1"] * opt["x1"]
+    return score
+
+
+def objective_function_m5(opt):
+    score = -(opt["x1"] - 5) * (opt["x1"] - 5)
+    return score
+
+
+def objective_function_p5(opt):
+    score = -(opt["x1"] + 5) * (opt["x1"] + 5)
+    return score
+
+
+search_space_0 = {"x1": np.arange(-100, 101, 1)}
+search_space_1 = {"x1": np.arange(0, 101, 1)}
+search_space_2 = {"x1": np.arange(-100, 1, 1)}
+
+search_space_3 = {"x1": np.arange(-10, 11, 0.1)}
+search_space_4 = {"x1": np.arange(0, 11, 0.1)}
+search_space_5 = {"x1": np.arange(-10, 1, 0.1)}
+
+
+objective_para = (
+    "objective",
+    [
+        (objective_function),
+        (objective_function_m5),
+        (objective_function_p5),
+    ],
+)
+
+search_space_para = (
+    "search_space",
+    [
+        (search_space_0),
+        (search_space_1),
+        (search_space_2),
+        (search_space_3),
+        (search_space_4),
+        (search_space_5),
+    ],
+)
+
+
+@pytest.mark.parametrize(*objective_para)
+@pytest.mark.parametrize(*search_space_para)
+@pytest.mark.parametrize(*optimizers)
+def test_best_results_0(Optimizer, search_space, objective):
+    search_space = search_space
+    objective_function = objective
+
+    initialize = {"vertices": 2}
+
+    hyper = Hyperactive()
+    hyper.add_search(
+        objective_function,
+        search_space,
+        optimizer=Optimizer(),
+        n_iter=10,
+        memory=False,
+        initialize=initialize,
+    )
+    hyper.run()
+
+    assert hyper.best_score(objective_function) == objective_function(
+        hyper.best_para(objective_function)
+    )
+
+
+@pytest.mark.parametrize(*objective_para)
+@pytest.mark.parametrize(*search_space_para)
+@pytest.mark.parametrize(*optimizers)
+def test_best_results_1(Optimizer, search_space, objective):
+    search_space = search_space
+    objective_function = objective
+
+    initialize = {"vertices": 2}
+
+    hyper = Hyperactive()
+    hyper.add_search(
+        objective_function,
+        search_space,
+        optimizer=Optimizer(),
+        n_iter=10,
+        memory=False,
+        initialize=initialize,
+    )
+    hyper.run()
+
+    assert hyper.best_para(objective_function)["x1"] in list(
+        hyper.results(objective_function)["x1"]
+    )
