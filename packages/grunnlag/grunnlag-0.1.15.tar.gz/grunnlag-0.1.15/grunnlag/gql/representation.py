028f0d5085c3281d49dd575767e608cf647b2d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:40 +0000

    Add package grunnlag/0.1.15/grunnlag-0.1.15.tar.gz

---
+++ packages/grunnlag/grunnlag-0.1.15.tar.gz/grunnlag-0.1.15/grunnlag/gql/representation.py
@@ -0,0 +1,50 @@
+from bergen.query import DelayedGQL
+
+
+GET_REPRESENTATION = DelayedGQL("""
+query Representation($id: Int!){
+  representation(id: $id){
+    id
+    name
+    image
+    store
+    unique
+  
+  }
+}
+""")
+
+CREATE_REPRESENTATION = DelayedGQL("""
+mutation Representation($sample: ID!, $name: String!){
+  createRepresentation(sample: $sample, name: $name){
+    name
+    id
+    image
+    store
+    unique
+  
+  }
+}
+""")
+
+UPDATE_REPRESENTATION = DelayedGQL("""
+mutation Representation($id: ID!){
+  updateRepresentation(rep: $id){
+    name
+    id
+    image
+    store
+    unique
+  }
+}
+""")
+
+
+FILTER_REPRESENTATION = DelayedGQL("""
+query Representation($name: String) {
+  representations(name: $name) {
+    name
+    store
+  }
+}
+""")
