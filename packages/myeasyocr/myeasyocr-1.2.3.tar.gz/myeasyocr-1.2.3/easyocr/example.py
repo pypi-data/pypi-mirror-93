e2d3c34498921c2d6448505ad44aa2309f6b5303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:28 +0000

    Add package myeasyocr/1.2.3/myeasyocr-1.2.3.tar.gz

---
+++ packages/myeasyocr/myeasyocr-1.2.3.tar.gz/myeasyocr-1.2.3/easyocr/example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2021-02-03 10:35 
+# @Author : YXH
+# @Site :  
+# @File : example.py 
+# @Software: PyCharm
+# from easyocr import Reader
+#
+# if __name__ == '__main__':
+#     reader = Reader(['ch_sim','en']) # need to run only once to load model into memory
+#     result = reader.readtext('chinese.jpg')
+#     print(result)
