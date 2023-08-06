36d87bb2464ff1bc57c8f1364c5b647f48b32f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:16 +0000

    Add package sondehub/0.1.3/sondehub-0.1.3.tar.gz

---
+++ packages/sondehub/sondehub-0.1.3.tar.gz/sondehub-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sondehub']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['paho-mqtt>=1.5.1,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['sondehub = sondehub:__main__.main']}
+
+setup_kwargs = {
+    'name': 'sondehub',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Michaela',
+    'author_email': 'git@michaela.lgbt',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
