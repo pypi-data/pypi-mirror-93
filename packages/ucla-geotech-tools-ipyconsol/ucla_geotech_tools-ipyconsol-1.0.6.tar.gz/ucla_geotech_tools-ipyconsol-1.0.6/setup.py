a7f84f2004d1d2d057f6ddbdc7aa82ba480b2183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:40 +0000

    Add package ucla-geotech-tools-ipyconsol/1.0.6/ucla_geotech_tools-ipyconsol-1.0.6.tar.gz

---
+++ packages/ucla-geotech-tools-ipyconsol/ucla_geotech_tools-ipyconsol-1.0.6.tar.gz/ucla_geotech_tools-ipyconsol-1.0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import Extension, setup, find_packages
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+sourcefiles = ['ipyconsol.c']
+extensions = [Extension("ucla_geotech_tools.ipyconsol",sourcefiles)]
+
+setup(
+    name="ucla_geotech_tools-ipyconsol",
+    version="1.0.6",
+    author="Scott J. Brandenberg",
+    author_email="sjbrandenberg@ucla.edu",
+    description="Python implementation of iConsol program for computing nonlinear consolidation of soil.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    ext_modules=[Extension('ucla_geotech_tools.ipyconsol',['ipyconsol.c'])],
+    namespace_packages=['ucla_geotech_tools']
+)
