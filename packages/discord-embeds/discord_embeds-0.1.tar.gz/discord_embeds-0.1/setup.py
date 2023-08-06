a338f536f733094cd71e89a116d9b29124487050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:17 +0000

    Add package discord-embeds/0.1/discord_embeds-0.1.tar.gz

---
+++ packages/discord-embeds/discord_embeds-0.1.tar.gz/discord_embeds-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = "discord_embeds",
+	version = '0.1',
+	description = "Package for make eazy embeds",
+	packages = ['discord_embeds'],
+	author_email = "sashakilin12@gmail.com",
+	zip_safe = False)
