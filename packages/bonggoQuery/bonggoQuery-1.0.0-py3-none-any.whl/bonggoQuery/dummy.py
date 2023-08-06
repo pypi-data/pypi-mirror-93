ad79bd3bc62d211b9a092ad05e71636b62b200d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:31 +0000

    Add package bonggoQuery/1.0.0/bonggoQuery-1.0.0-py3-none-any.whl

---
+++ packages/bonggoQuery/bonggoQuery-1.0.0-py3-none-any.whl/bonggoQuery/dummy.py
@@ -0,0 +1,91 @@
+import wolframalpha as wf
+import speech_recognition as sr
+import pyttsx3
+
+
+
+
+app = wf.Client("3YEQYW-RA6A8LREGV")
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices)
+engine.setProperty('voice', voices[1].id)
+def speak(audio):
+           engine.say(audio)
+           engine.runAndWait()
+
+
+
+# NEW FEATURES WILL COME IN SOON
+
+
+def takeCommand():
+            r = sr.Recognizer()
+            with sr.Microphone() as m:
+              print("Listening...")
+              # r.pause_threshold=1
+              audio = r.listen(m)
+
+            try:
+              print("Recognizing...")
+              query = r.recognize_google(audio, language="eng-in")
+              print(f"User Said:{query}\n")
+        
+            except Exception as e:
+                # print(e)
+
+                print("Say that again Please...")
+                speak("Say that again Please...")
+
+            query1 = query.lower()
+        
+            return  query1
+
+class Query:
+     
+        class normal_query():
+          def printing(querys):
+            querys= querys.lower()
+            res = app.query(querys)
+            # speak(next(res.results).text)
+            try: 
+              return f"{(next(res.results).text)}"
+            except Exception:
+              print("There is an error!")
+
+          def speaking(querys):
+            querys= querys.lower()
+            res = app.query(querys)
+            # speak(next(res.results).text)
+            try: 
+              speak(next(res.results).text)
+              return f"{(next(res.results).text)}"
+            except Exception:
+
+              print("There is an error!")
+              speak("There is an error!")
+
+          
+
+        
+if __name__=='__main__':
+    data = takeCommand()
+    # data1 = data.lower()
+    print(Query.normal_query.speaking(data))
+
+        
+
+
+
+
+
+
+          
+
+        
+        
+  
+    
+
+ 
+    
