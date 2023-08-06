d1e72dddf6af0bff6cac35ce3c1366e0afb9f974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:08 +0000

    Add package tep/0.6.7/tep-0.6.7-py3-none-any.whl

---
+++ packages/tep/tep-0.6.7-py3-none-any.whl/tep/plugin.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+"""
+@Author  :  Don
+@Date    :  8/14/2020 9:16 AM
+@Desc    :  
+"""
+
+import os
+import shutil
+import tempfile
+
+import allure_commons
+from allure_commons.logger import AllureFileLogger
+from allure_pytest.listener import AllureListener
+from allure_pytest.plugin import cleanup_factory
+
+from tep.fixture import Project
+from tep.funcs import current_time
+
+allure_temp = tempfile.mkdtemp()
+
+
+class Plugin:
+    @staticmethod
+    def pytest_addoption(parser):
+        parser.addoption(
+            "--tep-reports",
+            action="store_const",
+            const=True,
+            help="Create tep allure HTML reports."
+        )
+
+    @staticmethod
+    def _tep_reports(config):
+        if config.getoption("--tep-reports") and not config.getoption("allure_report_dir"):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def pytest_configure(config):
+        if Plugin._tep_reports(config):
+            test_listener = AllureListener(config)
+            config.pluginmanager.register(test_listener)
+            allure_commons.plugin_manager.register(test_listener)
+            config.add_cleanup(cleanup_factory(test_listener))
+
+            clean = config.option.clean_alluredir
+            file_logger = AllureFileLogger(allure_temp, clean)
+            allure_commons.plugin_manager.register(file_logger)
+            config.add_cleanup(cleanup_factory(file_logger))
+
+    @staticmethod
+    def pytest_sessionfinish(session):
+        if Plugin._tep_reports(session.config):
+            reports_dir = os.path.join(Project.dir, "reports")
+            new_report = os.path.join(reports_dir, "report-" + current_time().replace(":", "-").replace(" ", "-"))
+            if os.path.exists(reports_dir):
+                his_reports = os.listdir(reports_dir)
+                if his_reports:
+                    latest_report_history = os.path.join(reports_dir, his_reports[-1], "history")
+                    shutil.copytree(latest_report_history, os.path.join(allure_temp, "history"))
+            os.system(f"allure generate {allure_temp} -o {new_report}  --clean")
+            shutil.rmtree(allure_temp)
