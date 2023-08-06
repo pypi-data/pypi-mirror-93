dd2c154f2cd5e8a2823984324e984ff42e4ea5d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:55 +0000

    Add package geotagging-alexr96/0.0.22/geotagging-alexr96-0.0.22.tar.gz

---
+++ packages/geotagging-alexr96/geotagging-alexr96-0.0.22.tar.gz/geotagging-alexr96-0.0.22/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geotagging-alexr96",
+    version="0.0.22",
+    author="Alex Roy",
+    author_email="Alex@devpipeline.com",
+    description="Geotagging database package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alex-96-devpipeline/geotagging_db",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["psycopg2-binary"],
+    package_data={
+    '': ['*.txt'],
+    }
+)
