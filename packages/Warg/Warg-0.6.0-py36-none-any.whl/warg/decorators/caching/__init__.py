f3970b3051d078b838d1c195286ffeedfa8f84eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package Warg/0.6.0/Warg-0.6.0-py36-none-any.whl

---
+++ packages/Warg/Warg-0.6.0-py36-none-any.whl/warg/decorators/caching/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Nielsen"
+__doc__ = r"""
+
+           Created on 30/03/2020
+           """
+
+import pathlib
+
+with open(pathlib.Path(__file__).parent / "README.md", "r") as this_init_file:
+    __doc__ += this_init_file.read()
+
+from .look_up_table import *
+from .property_caching import *
